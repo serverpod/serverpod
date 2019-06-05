9d2b04ba906096f1383a35232cce59f2f2f852d7
Viktor Lidholt <viktorlidholt@gmail.com>
2019-06-05 15:38:47 +0200

    Additional datetime support

--- packages/serverpod/lib/src/server/endpoint.dart
+++ packages/serverpod/lib/src/server/endpoint.dart
@@ -232,7 +232,7 @@ class _Parameter {
     type = parameterMirror.type.reflectedType;
     name = MirrorSystem.getName(parameterMirror.simpleName);
 
-    assert(type == Session || type == int || type == double || type == bool || type == String || serializationManager.constructors[type.toString()] != null, 'Unserializable type $type');
+    assert(type == Session || type == int || type == double || type == bool || type == String || type == DateTime || serializationManager.constructors[type.toString()] != null, 'Unserializable type $type');
   }
 
   String name;
