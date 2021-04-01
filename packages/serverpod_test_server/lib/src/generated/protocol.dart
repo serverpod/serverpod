9bd7cd685de945cba28c9cbe86639ab40dd7be8d
Viktor Lidholt <viktorlidholt@gmail.com>
2021-04-01 14:43:04 +0200

    Adds simple data table

--- packages/serverpod_test_server/lib/src/generated/protocol.dart
+++ packages/serverpod_test_server/lib/src/generated/protocol.dart
@@ -5,10 +5,12 @@ library protocol;
 
 import 'package:serverpod/serverpod.dart';
 
+import 'simple_data.dart';
 import 'user_info.dart';
 import 'types.dart';
 import 'company_info.dart';
 
+export 'simple_data.dart';
 export 'user_info.dart';
 export 'types.dart';
 export 'company_info.dart';
@@ -22,10 +24,12 @@ class Protocol extends SerializationManager {
   Map<String,String> get tableClassMapping => _tableClassMapping;
 
   Protocol() {
+    constructors['SimpleData'] = (Map<String, dynamic> serialization) => SimpleData.fromSerialization(serialization);
     constructors['UserInfo'] = (Map<String, dynamic> serialization) => UserInfo.fromSerialization(serialization);
     constructors['Types'] = (Map<String, dynamic> serialization) => Types.fromSerialization(serialization);
     constructors['CompanyInfo'] = (Map<String, dynamic> serialization) => CompanyInfo.fromSerialization(serialization);
 
+    tableClassMapping['simple_data'] = 'SimpleData';
     tableClassMapping['user_info'] = 'UserInfo';
     tableClassMapping['types'] = 'Types';
   }
---
+++ packages/serverpod_test_server/lib/src/generated/simple_data.dart
@@ -0,0 +1,62 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:serverpod/database.dart';
+// ignore: unused_import
+import 'protocol.dart';
+
+class SimpleData extends TableRow {
+  String get className => 'SimpleData';
+  String get tableName => 'simple_data';
+
+  int? id;
+  int? num;
+
+  SimpleData({
+    this.id,
+    this.num,
+});
+
+  SimpleData.fromSerialization(Map<String, dynamic> serialization) {
+    var _data = unwrapSerializationData(serialization);
+    id = _data['id'];
+    num = _data['num'];
+  }
+
+  Map<String, dynamic> serialize() {
+    return wrapSerializationData({
+      'id': id,
+      'num': num,
+    });
+  }
+  Map<String, dynamic> serializeForDatabase() {
+    return wrapSerializationData({
+      'id': id,
+      'num': num,
+    });
+  }
+
+  Map<String, dynamic> serializeAll() {
+    return wrapSerializationData({
+      'id': id,
+      'num': num,
+    });
+  }
+}
+
+class SimpleDataTable extends Table {
+  SimpleDataTable() : super(tableName: 'simple_data');
+
+  String tableName = 'simple_data';
+  final id = ColumnInt('id');
+  final num = ColumnInt('num');
+
+  List<Column> get columns => [
+    id,
+    num,
+  ];
+}
+
+SimpleDataTable tSimpleData = SimpleDataTable();
