2d04056ddbcf8eafa3a94e5a6bd20133ad9bf44b
Viktor Lidholt <viktorlidholt@gmail.com>
2021-05-22 14:05:32 +0200

    Updates version to 0.8.4

--- tools/serverpod_cli/bin/generated/version.dart
+++ tools/serverpod_cli/bin/generated/version.dart
@@ -1,5 +1,5 @@
 // This file is generated. Do not modify, instead edit the files in the
 // templates/pubspecs directory.
 
-const templateVersion = '0.8.3';
+const templateVersion = '0.8.4';
 const productionMode = false;
