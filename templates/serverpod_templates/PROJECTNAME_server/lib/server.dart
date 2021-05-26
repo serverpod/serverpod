2e0a1c7ca470c7a0974a8b3ec5939172053d1820
Viktor Lidholt <viktorlidholt@gmail.com>
2021-05-26 18:54:53 +0200

    Adds documentation to example server code.

--- templates/serverpod_templates/PROJECTNAME_server/lib/server.dart
+++ templates/serverpod_templates/PROJECTNAME_server/lib/server.dart
@@ -3,17 +3,21 @@ import 'package:serverpod/serverpod.dart';
 import 'src/generated/protocol.dart';
 import 'src/generated/endpoints.dart';
 
+// This is the starting point of your Serverpod server. In most cases, you will
+// only need to make additions to this file if you add future calls,  are
+// configuring Relic (Serverpod's web-server), or need custom setup work.
+
 void run(List<String> args) async {
-  // Create serverpod
+  // Initialize Serverpod and connect it with your generated code.
   final pod = Serverpod(
     args,
     Protocol(),
     Endpoints(),
   );
 
-  // Add any future calls
-  // pod.registerFutureCall(MyFutureCall(), 'myFutureCall');
+  // If you are using any future calls, they need to be registered here.
+  // pod.registerFutureCall(ExampleFutureCall(), 'exampleFutureCall');
 
-  // Start the server
+  // Start the server.
   await pod.start();
 }
