4ad58b8eb1be916137e23c63cd9f70968fe563fa
Viktor Lidholt <viktorlidholt@gmail.com>
2021-05-27 20:46:07 +0200

    Spelling in comment

--- templates/serverpod_templates/PROJECTNAME_flutter/lib/main.dart
+++ templates/serverpod_templates/PROJECTNAME_flutter/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:PROJECTNAME_client/PROJECTNAME_client.dart';
 
 // Sets up a singleton client object that can be used to talk to the server from
-// anywhere in our app. The client is generated from our server code.
+// anywhere in our app. The client is generated from your server code.
 // The client is set up to connect to a Serverpod running on a local server on
 // the default port. You will need to modify this to connect to staging or
 // production servers.
