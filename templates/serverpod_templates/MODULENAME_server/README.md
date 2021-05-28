a09537fb635b22bbeecc7882171d72747fb14c17
Viktor Lidholt <viktorlidholt@gmail.com>
2021-05-28 14:18:08 +0200

    Adds docs to module templates

--- templates/serverpod_templates/MODULENAME_server/README.md
+++ templates/serverpod_templates/MODULENAME_server/README.md
@@ -1,4 +1,1 @@
-A simple command-line application.
-
-Created from templates made available by Stagehand under a BSD-style
-[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
+A sample Serverpod module.
--- templates/serverpod_templates/MODULENAME_server/lib/src/endpoints/module_endpoint.dart
+++ templates/serverpod_templates/MODULENAME_server/lib/src/endpoints/module_endpoint.dart
@@ -1,3 +1,7 @@
+// Add your modules' endpoints to the `endpoints` directory. Run
+// `serverpod generate` to produce the modules server and client code. Refer to
+// the documentation on how to add endpoints to your server.
+
 import 'package:serverpod/serverpod.dart';
 import '../generated/protocol.dart';
 
