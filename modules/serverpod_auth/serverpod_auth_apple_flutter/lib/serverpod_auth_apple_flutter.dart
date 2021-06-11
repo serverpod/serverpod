206a7928ff9e1370c214133d2145f789b9cf845f
Viktor Lidholt <viktorlidholt@gmail.com>
2021-06-11 14:29:10 +0200

    Moves sign in button out of widget directory for auth flutter code

--- modules/serverpod_auth/serverpod_auth_apple_flutter/lib/serverpod_auth_apple_flutter.dart
+++ modules/serverpod_auth/serverpod_auth_apple_flutter/lib/serverpod_auth_apple_flutter.dart
@@ -1,3 +1,5 @@
 library serverpod_auth_apple_flutter;
 
-export 'src/widgets/signin_button.dart';
+export 'src/signin_button.dart';
+export 'src/auth.dart';
+export 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
