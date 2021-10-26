a31d1b90ee96f8931fa4a87b4ae4df6a5dc482d5
Viktor Lidholt <viktorlidholt@gmail.com>
2021-10-26 14:08:08 +0200

    Fixes file picker on image upload

--- modules/serverpod_auth/serverpod_auth_shared_flutter/lib/src/image_uploader.dart
+++ modules/serverpod_auth/serverpod_auth_shared_flutter/lib/src/image_uploader.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image/image.dart' as img;
 import 'package:image_picker/image_picker.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:image_cropper/image_cropper.dart';
 
 import 'session_manager.dart';
@@ -14,24 +15,32 @@ class ImageUploader {
     required SessionManager sessionManager,
     int imageSize = 256,
   }) async {
-
-    var toolbarColor = Theme.of(context).primaryColor;
-    var toolbarWidgetColor = Theme.of(context).buttonTheme.colorScheme?.onPrimary ?? Colors.white;
-
-    final picker = ImagePicker();
-    // Pick an image
-    var imageFile = await picker.pickImage(source: ImageSource.gallery);
-    if (imageFile == null)
-      return;
-
-    print('picked image: $imageFile');
-
     if (kIsWeb) {
-      var image = img.decodeImage(await imageFile.readAsBytes());
+      var result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['jpg', 'png'],
+      );
+      if (result == null)
+        return;
+      if (result.files.first.bytes == null)
+        return;
+
+      var image = img.decodeImage(result.files.first.bytes!);
       if (image == null)
         return;
 
       print('loaded image');
+      if (image.width > image.height) {
+        var h = imageSize;
+        var w = imageSize * image.width / image.height;
+        image = img.copyResize(image, width: w.floor(), height: h, interpolation: img.Interpolation.average);
+      }
+      else {
+        var w = imageSize;
+        var h = imageSize * image.height / image.width;
+        image = img.copyResize(image, width: w, height: h.floor(), interpolation: img.Interpolation.average);
+      }
+
       image = img.copyResizeCropSquare(image, imageSize);
 
       print('resized image');
@@ -45,6 +54,15 @@ class ImageUploader {
       await sessionManager.uploadUserImage(data);
     }
     else {
+      var toolbarColor = Theme.of(context).primaryColor;
+      var toolbarWidgetColor = Theme.of(context).buttonTheme.colorScheme?.onPrimary ?? Colors.white;
+
+      final picker = ImagePicker();
+      // Pick an image
+      var imageFile = await picker.pickImage(source: ImageSource.gallery);
+      if (imageFile == null)
+        return;
+
       // Crop the image
       var croppedImageFile = await ImageCropper.cropImage(
         sourcePath: imageFile.path,
