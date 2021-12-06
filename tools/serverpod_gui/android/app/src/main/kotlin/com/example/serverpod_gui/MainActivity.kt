6986a6ca0e5d78be23c76e07795d78e31a20d6b7
Viktor Lidholt <viktorlidholt@gmail.com>
2021-12-06 23:23:22 +0100

    Moves serverpod_gui to its own repo

--- tools/serverpod_gui/android/app/src/main/kotlin/com/example/serverpod_gui/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.serverpod_gui
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tools/serverpod_gui/lib/util/formatters.dart
+++
@@ -1,4 +0,0 @@
-import 'package:intl/intl.dart';
-
-final NumberFormat durationFormat = NumberFormat('###.00');
-final DateFormat dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
