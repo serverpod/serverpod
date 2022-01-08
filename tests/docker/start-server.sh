e025fc1f7608db7f15ebada2b8c222fd2c8b05f7
Viktor Lidholt <viktorlidholt@gmail.com>
2022-01-08 20:49:36 +0100

    Fixes tests (hopefully)

--- tests/docker/start-server.sh
+++ tests/docker/start-server.sh
@@ -8,13 +8,13 @@ echo ""
 
 # Reset the database
 echo "### Setting up database tables"
-env PGPASSWORD="rotkiv42" psql -h postgres -U postgres -d serverpod_test -f /app/tests/docker/reset_db.pgsql
+env PGPASSWORD="password" psql -h postgres -U postgres -d serverpod_test -f /app/tests/docker/reset_db.pgsql
 
 # Create tables
-env PGPASSWORD="rotkiv42" psql -h postgres -U postgres -d serverpod_test -f /app/packages/serverpod/generated/tables.pgsql
-env PGPASSWORD="rotkiv42" psql -h postgres -U postgres -d serverpod_test -f /app/tests/serverpod_test_server/generated/tables.pgsql
-env PGPASSWORD="rotkiv42" psql -h postgres -U postgres -d serverpod_test -f /app/tests/serverpod_test_module/serverpod_test_module_server/generated/tables.pgsql
-env PGPASSWORD="rotkiv42" psql -h postgres -U postgres -d serverpod_test -f /app/modules/serverpod_auth/serverpod_auth_server/generated/tables.pgsql
+env PGPASSWORD="password" psql -h postgres -U postgres -d serverpod_test -f /app/packages/serverpod/generated/tables.pgsql
+env PGPASSWORD="password" psql -h postgres -U postgres -d serverpod_test -f /app/tests/serverpod_test_server/generated/tables.pgsql
+env PGPASSWORD="password" psql -h postgres -U postgres -d serverpod_test -f /app/tests/serverpod_test_module/serverpod_test_module_server/generated/tables.pgsql
+env PGPASSWORD="password" psql -h postgres -U postgres -d serverpod_test -f /app/modules/serverpod_auth/serverpod_auth_server/generated/tables.pgsql
 
 echo ""
 
