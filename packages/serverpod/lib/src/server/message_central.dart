2e77955eb37d2db48d7a45f1c800d98c19c2cfce
Timm Preetz <timm@preetz.us>
2022-01-17 11:52:43 +0100

    Adds check to only subscribe session to channels if that's not already the case

--- packages/serverpod/lib/src/server/message_central.dart
+++ packages/serverpod/lib/src/server/message_central.dart
@@ -69,19 +69,23 @@ class MessageCentral {
       subscribedChannels = {};
       _sessionToChannelNamesLookup[session] = subscribedChannels;
     }
-    subscribedChannels.add(channelName);
 
-    var callbacks = _sessionToCallbacksLookup[session];
-    if (callbacks == null) {
-      callbacks = {};
-      _sessionToCallbacksLookup[session] = callbacks;
+    // Only suscribe if it's not already done
+    if (!subscribedChannels.contains(channelName)) {
+      subscribedChannels.add(channelName);
+
+      var callbacks = _sessionToCallbacksLookup[session];
+      if (callbacks == null) {
+        callbacks = {};
+        _sessionToCallbacksLookup[session] = callbacks;
+      }
+      callbacks.add(listener);
+
+      session.serverpod.redisController.subscribe(
+        channelName,
+        _receivedRedisMessage,
+      );
     }
-    callbacks.add(listener);
-
-    session.serverpod.redisController.subscribe(
-      channelName,
-      _receivedRedisMessage,
-    );
   }
 
   void _receivedRedisMessage(String channelName, String message) {
