7a932bffdb2ea8c8658ebfc9d2cee370bbaffdd5
Viktor Lidholt <viktorlidholt@gmail.com>
2022-05-12 18:35:16 +0200

    Adds dart analyze tests for templates and fixes lints

--- templates/pubspecs/packages/serverpod/lib/src/config/version.dart
+++ templates/pubspecs/packages/serverpod/lib/src/config/version.dart
@@ -1,4 +1,5 @@
 // This file is generated. Do not modify, instead edit the files in the
 // templates/pubspecs directory.
 
+/// Current version of serverpod.
 const serverpodVersion = 'VERSION';
--- templates/serverpod_templates/modulename_server/lib/src/endpoints/module_endpoint.dart
+++ templates/serverpod_templates/modulename_server/lib/src/endpoints/module_endpoint.dart
@@ -3,7 +3,7 @@
 // the documentation on how to add endpoints to your server.
 
 import 'package:serverpod/serverpod.dart';
-import '../generated/protocol.dart';
+// import '../generated/protocol.dart';
 
 class ModuleEndpoint extends Endpoint {
   Future<String> hello(Session session, String name) async {
