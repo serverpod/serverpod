a0646000c55e837a9a32718f1d57f85b43a02ca4
Viktor Lidholt <viktorlidholt@gmail.com>
2022-08-16 09:52:07 +0200

    Updates terraform scripts

--- templates/serverpod_templates/projectname_server/aws/terraform/balancers.tf
+++ templates/serverpod_templates/projectname_server/aws/terraform/balancers.tf
@@ -1,23 +1,11 @@
 # API load balancer setup
 
-resource "aws_lb" "api" {
-  name               = "${var.project_name}-api"
+resource "aws_lb" "serverpod" {
+  name               = "${var.project_name}-serverpod"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.api.id]
   subnets            = module.vpc.public_subnets
-}
-
-resource "aws_lb_listener" "api" {
-  load_balancer_arn = aws_lb.api.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.api.arn
-  }
 }
 
 resource "aws_security_group" "api" {
@@ -25,6 +13,13 @@ resource "aws_security_group" "api" {
   ingress {
     from_port   = 443
     to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -39,11 +34,39 @@ resource "aws_security_group" "api" {
   vpc_id = module.vpc.vpc_id
 }
 
+resource "aws_lb_listener" "api" {
+  load_balancer_arn = aws_lb.serverpod.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+}
+
 resource "aws_lb_target_group" "api" {
   name     = "${var.project_name}-api"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb_listener_rule" "api" {
+  listener_arn = aws_lb_listener.api.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.api.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.subdomain_api}.${var.top_domain}"]
+    }
+  }
 }
 
 resource "aws_autoscaling_attachment" "api" {
@@ -56,49 +79,10 @@ resource "aws_route53_record" "api" {
   name    = "${var.subdomain_api}.${var.top_domain}"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_lb.api.dns_name}"]
+  records = ["${aws_lb.serverpod.dns_name}"]
 }
 
-# Insights load balancer setup
-
-resource "aws_lb" "insights" {
-  name               = "${var.project_name}-insights"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.api.id]
-  subnets            = module.vpc.public_subnets
-}
-
-resource "aws_lb_listener" "insights" {
-  load_balancer_arn = aws_lb.insights.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.insights.arn
-  }
-}
-
-resource "aws_security_group" "insights" {
-  name = "${var.project_name}-insights"
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  vpc_id = module.vpc.vpc_id
-}
+# Insights load balancer rules
 
 resource "aws_lb_target_group" "insights" {
   name     = "${var.project_name}-insights"
@@ -112,54 +96,31 @@ resource "aws_autoscaling_attachment" "insights" {
   lb_target_group_arn    = aws_lb_target_group.insights.arn
 }
 
+resource "aws_lb_listener_rule" "insights" {
+  listener_arn = aws_lb_listener.api.arn
+  priority     = 99
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.insights.arn
+  }
+
+  condition {
+    host_header {
+      values = ["${var.subdomain_insights}.${var.top_domain}"]
+    }
+  }
+}
+
 resource "aws_route53_record" "insights" {
   zone_id = var.hosted_zone_id
   name    = "${var.subdomain_insights}.${var.top_domain}"
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_lb.insights.dns_name}"]
+  records = ["${aws_lb.serverpod.dns_name}"]
 }
 
-# Web server load balancer setup
-
-resource "aws_lb" "web" {
-  name               = "${var.project_name}-web"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.api.id]
-  subnets            = module.vpc.public_subnets
-}
-
-resource "aws_lb_listener" "web" {
-  load_balancer_arn = aws_lb.web.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web.arn
-  }
-}
-
-resource "aws_security_group" "web" {
-  name = "${var.project_name}-web"
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  vpc_id = module.vpc.vpc_id
-}
+# Web server load balancer rules
 
 resource "aws_lb_target_group" "web" {
   name     = "${var.project_name}-web"
@@ -173,10 +134,15 @@ resource "aws_autoscaling_attachment" "web" {
   lb_target_group_arn    = aws_lb_target_group.web.arn
 }
 
-resource "aws_route53_record" "web" {
-  zone_id = var.hosted_zone_id
-  name    = "${var.subdomain_web}.${var.top_domain}"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["${aws_lb.web.dns_name}"]
+resource "aws_lb_listener" "web" {
+  load_balancer_arn = aws_lb.serverpod.arn
+  port              = "80"
+  protocol          = "HTTP"
+  # certificate_arn   = var.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
 }
+
