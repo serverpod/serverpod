3967227defdd0d7c01b8d9ca1db399a9638a8b72
Viktor Lidholt <viktorlidholt@gmail.com>
2022-09-28 15:22:12 +0200

    fix: Updates project template to use latest version of flutter_lints.

--- templates/serverpod_templates/projectname_flutter/lib/main.dart
+++ templates/serverpod_templates/projectname_flutter/lib/main.dart
@@ -33,10 +33,10 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  MyHomePageState createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
   // These fields hold the last result or error message that we've received from
   // the server or null if no result exists yet.
   String? _resultMessage;
