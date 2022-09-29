3744b68b8561fef15147316d1375aa3fe6f3670e
Viktor Lidholt <viktorlidholt@gmail.com>
2022-09-29 10:18:32 +0200

    fix: FlutterConnectivityMonitor now correctly processes disconnects that happen one second after the monitor is launched.

--- packages/serverpod_flutter/lib/src/flutter_connectivity_monitor.dart
+++ packages/serverpod_flutter/lib/src/flutter_connectivity_monitor.dart
@@ -4,17 +4,27 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:serverpod_client/serverpod_client.dart';
 
+/// Concrete implementation of [ConnectivityMonitor] for use with Flutter.
 class FlutterConnectivityMonitor extends ConnectivityMonitor {
   late StreamSubscription<ConnectivityResult> _connectivitySubscription;
   bool _receivedFirstEvent = false;
 
+  /// Creates a new connectivity monitor.
   FlutterConnectivityMonitor() {
     WidgetsFlutterBinding.ensureInitialized();
+    const warmupDuration = Duration(seconds: 1);
+    var connectionTime = DateTime.now();
+
+    // Start listening to connection status changes.
     _connectivitySubscription =
         Connectivity().onConnectivityChanged.listen((event) {
       if (!_receivedFirstEvent) {
+        // Skip the first event if it happens immediately on launch as it may
+        // not be correct on some platforms.
         _receivedFirstEvent = true;
-        if (event == ConnectivityResult.none) {
+        var durationSinceStart = DateTime.now().difference(connectionTime);
+        if (event == ConnectivityResult.none &&
+            durationSinceStart < warmupDuration) {
           return;
         }
       }
