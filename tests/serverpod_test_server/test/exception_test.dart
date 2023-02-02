b7a6ef6fccd73c93c33b9dc4dc639082f2c93a7c
Dmytro Virych <dmytro.virych@space-whale.io>
2023-02-02 14:26:20 +0200

    Updated tests

--- tests/serverpod_test_server/test/exception_test.dart
+++ tests/serverpod_test_server/test/exception_test.dart
@@ -13,20 +13,43 @@ void main() {
 
   group('Exception tests', () {
     const successUserId = 1;
-    const failureUserId = -1;
     const unknownErrorUserId = 0;
+    const failureUserId = -1;
     test('Working without exception', () async {
       expect(await client.exceptionApproach.checkUserExist(successUserId),
           'Success');
     });
     test('Working with ServerpodException exception', () async {
-      expect(await client.exceptionApproach.checkUserExist(unknownErrorUserId),
-          throwsA(const TypeMatcher<test_client.ServerpodException>()));
+      dynamic exception;
+      try {
+        await client.exceptionApproach.checkUserExist(unknownErrorUserId);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception is test_client.ServerpodException, true);
     });
 
     test('Working with UserNotFoundException exception', () async {
-      expect(await client.exceptionApproach.checkUserExist(failureUserId),
-          throwsA(const TypeMatcher<test_client.UserNotFound>()));
+      dynamic exception;
+      try {
+        await client.exceptionApproach.checkUserExist(failureUserId);
+      } catch (e) {
+        exception = e;
+      }
+
+      expect(exception is test_client.UserNotFound, true);
+    });
+
+    test('Catch specific type', () async {
+      String message = '';
+      try {
+        await client.exceptionApproach.checkUserExist(failureUserId);
+      } on test_client.UserNotFound catch (e) {
+        message = e.message;
+      }
+
+      expect(message, 'User with id: $failureUserId not found');
     });
   });
 }
