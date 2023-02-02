59c4015c2c1325b450f3d570abd3b7f6e5e65015
Dmytro Virych <dmytro.virych@space-whale.io>
2023-02-02 10:36:07 +0200

    Added new ServerpodException #486

--- packages/serverpod/lib/serverpod.dart
+++ packages/serverpod/lib/serverpod.dart
@@ -1,19 +1,17 @@
 library serverpod;
 
-// Server
-export 'package:serverpod/server.dart';
-
-// Web server
-export 'package:serverpod/relic.dart';
-
 // Database
 export 'package:serverpod/database.dart';
-
-// Serialization and logging
-export 'package:serverpod_serialization/serverpod_serialization.dart';
-export 'package:serverpod/src/util/http_request_extension.dart';
-export 'package:serverpod/src/generated/log_level.dart';
-
+// Web server
+export 'package:serverpod/relic.dart';
+// Server
+export 'package:serverpod/server.dart';
 // Cloud storage
 export 'package:serverpod/src/cloud_storage/cloud_storage.dart';
 export 'package:serverpod/src/cloud_storage/database_cloud_storage.dart';
+export 'package:serverpod/src/generated/log_level.dart';
+// Exceptions
+export 'package:serverpod/src/util/exceptions.dart';
+export 'package:serverpod/src/util/http_request_extension.dart';
+// Serialization and logging
+export 'package:serverpod_serialization/serverpod_serialization.dart';
---
+++ packages/serverpod/lib/src/util/exceptions.dart
@@ -0,0 +1,9 @@
+/// This is `ServerpodException` that can be used to pass Domain exceptions from the Server to the Client
+///
+/// You can `throw ServerpodException()`
+///
+/// Based on issue [#486](https://github.com/serverpod/serverpod/issues/486)
+class ServerpodException implements Exception {
+  /// Const constructor to pass empty exception with `statusCode 500`
+  const ServerpodException();
+}
