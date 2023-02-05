08aacded8d79f412e673d4c85954391d5eeeb8c6
Dmytro Virych <dmytro.virych@space-whale.io>
2023-02-05 17:31:11 +0200

    Updated promt message; Removed unnecessary library #611

--- tools/serverpod_cli/bin/util/promt_to_update.dart
+++ tools/serverpod_cli/bin/util/promt_to_update.dart
@@ -2,21 +2,20 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart';
-import 'package:logger/logger.dart';
 import 'package:yaml/yaml.dart';
+
+const promtMessage = '''
+┌───────────────────────────────────────────────────────────────────────────────────────┐
+│ A new version of Serverpod is available!                                              │
+│                                                                                       │
+│ To update to the latest version, run "dart pub global activate serverpod_cli".        │
+│ Also, do not forget to update packages in your server, client, and flutter projects.  │
+└───────────────────────────────────────────────────────────────────────────────────────┘
+''';
 
 /// Check current Serverpod CLI version and promt user to update if needed
 Future<void> promtToUpdateIfNeeded() async {
   try {
-    Logger logger = Logger(
-      filter: _WithoutFiltering(),
-      printer: PrettyPrinter(
-        methodCount: 0,
-        colors: false,
-        printEmojis: true,
-        lineLength: 80,
-      ),
-    );
     File file = File('pubspec.yaml');
     String str = file.readAsStringSync();
     YamlDocument doc = loadYamlDocument(str);
@@ -25,14 +24,13 @@ Future<void> promtToUpdateIfNeeded() async {
     String? packageName = contents.nodes['name']?.value.toString();
     String? currentVersion = contents.nodes['version']?.value.toString();
     if (currentVersion == null || packageName == null) return;
-    Package? package = await _PackageService.getLatestPackage(packageName);
+    _Package? package = await _PackageService.getLatestPackage(packageName);
     if (package == null) return;
     bool isUpToDate = package.version == currentVersion;
 
     if (!isUpToDate) {
       print('');
-      logger.i(
-          '\nA new version ${package.version} of Serverpod CLI is available.\nhttps://pub.dev/packages/$packageName\n');
+      print(promtMessage);
       print('');
     }
   } catch (e) {
@@ -40,18 +38,13 @@ Future<void> promtToUpdateIfNeeded() async {
   }
 }
 
-class _WithoutFiltering extends LogFilter {
-  @override
-  bool shouldLog(LogEvent event) => true;
-}
-
 abstract class _PackageService {
-  static Future<Package?> getLatestPackage(String name) async {
+  static Future<_Package?> getLatestPackage(String name) async {
     try {
       Response respose =
           await get(Uri.parse('https://pub.dev/api/packages/$name'));
       Map<String, dynamic> map = jsonDecode(respose.body);
-      return Package(name: map['name'], version: map['latest']['version']);
+      return _Package(name: map['name'], version: map['latest']['version']);
     } catch (e) {
       print(e);
       return null;
@@ -59,10 +52,10 @@ abstract class _PackageService {
   }
 }
 
-class Package {
+class _Package {
   final String name;
   final String version;
-  Package({
+  _Package({
     required this.name,
     required this.version,
   });
