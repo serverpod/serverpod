99c4627f553b410c79326d1f6c365a4abdc17aee
Maximilian Fischer <github@maaeps.de>
2023-02-10 17:41:28 +0100

    feat: Export everything from uuid

--- packages/serverpod_serialization/lib/serverpod_serialization.dart
+++ packages/serverpod_serialization/lib/serverpod_serialization.dart
@@ -1,6 +1,6 @@
 library serverpod_serialization;
 
-export 'package:uuid/uuid.dart' show UuidValue;
+export 'package:uuid/uuid.dart';
 
 export 'src/bytedata_base64_ext.dart';
 export 'src/exceptions.dart';
