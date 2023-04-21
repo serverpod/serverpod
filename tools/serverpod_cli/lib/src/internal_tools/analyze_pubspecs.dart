cfe3a44d2edea8e92f0c451709c6046464f64f90
Viktor Lidholt <viktorlidholt@gmail.com>
2023-04-21 14:34:38 +0200

    chore: Keeps versions consistent across packages. (#832)

---
+++ tools/serverpod_cli/lib/src/internal_tools/analyze_pubspecs.dart
@@ -0,0 +1,126 @@
+import 'dart:io';
+
+import 'package:pubspec_parse/pubspec_parse.dart';
+
+/// The internal tool for analyzing the pubspec.yaml files in the Serverpod
+/// repo.
+void performAnalyzePubspecs() {
+  // Verify that we are in the serverpod directory
+  var dirPackages = Directory('packages');
+  var dirTemplates = Directory('templates/pubspecs');
+  var dirRoot = Directory('.');
+
+  if (!dirPackages.existsSync() ||
+      !dirTemplates.existsSync() ||
+      !dirRoot.existsSync()) {
+    print('Must be run from the serverpod repository root');
+    exit(1);
+  }
+
+  var pubspecs = _getPubspecs();
+  var dependencies = <String, List<_ServerpodDependency>>{};
+
+  for (var pubspec in pubspecs) {
+    for (var depName in pubspec.dependencies.keys) {
+      var dep = pubspec.dependencies[depName]!;
+
+      if (dep is HostedDependency) {
+        var depList = dependencies[depName] ?? [];
+        depList.add(
+          _ServerpodDependency(
+            serverpodPackage: pubspec.name,
+            dependencyName: depName,
+            version: dep.version.toString(),
+          ),
+        );
+        dependencies[depName] = depList;
+      }
+    }
+  }
+
+  // Find missmatched dependencies
+  var missmatchedDeps = <String>{};
+  for (var depName in dependencies.keys) {
+    var deps = dependencies[depName]!;
+    String? version;
+    for (var dep in deps) {
+      if (version != null && version != dep.version) {
+        missmatchedDeps.add(depName);
+      }
+      version = dep.version;
+    }
+  }
+
+  if (missmatchedDeps.isNotEmpty) {
+    print('Found missmatched dependencies:');
+    for (var depName in missmatchedDeps) {
+      print(depName);
+      var deps = dependencies[depName]!;
+      for (var dep in deps) {
+        print('  ${dep.version} ${dep.serverpodPackage}');
+      }
+    }
+
+    exit(1);
+  }
+
+  print('Dependencies match.');
+}
+
+List<Pubspec> _getPubspecs() {
+  try {
+    var pubspecFiles = _findPubspecsFiles(['/serverpod/templates/pubspecs/']);
+
+    var pubspecs = <Pubspec>[];
+    for (var file in pubspecFiles) {
+      var yaml = file.readAsStringSync();
+      var pubspec = Pubspec.parse(yaml);
+      pubspecs.add(pubspec);
+    }
+    return pubspecs;
+  } catch (e) {
+    print(
+      'Failed to load PUBLISHABLE_PACKAGES or the pubspec files. Are you'
+      'running this command from the serverpod repository root?',
+    );
+    print(e);
+    exit(1);
+  }
+}
+
+List<File> _findPubspecsFiles(List<String> ignorePaths) {
+  var dir = Directory.current;
+  var pubspecFiles = <File>[];
+  for (var file in dir.listSync(recursive: true)) {
+    bool ignore = false;
+    for (var ignorePath in ignorePaths) {
+      if (file.path.contains(ignorePath)) {
+        ignore = true;
+      }
+    }
+
+    if (ignore) continue;
+
+    if (file is File && file.path.endsWith('pubspec.yaml')) {
+      pubspecFiles.add(file);
+    }
+  }
+
+  for (var file in pubspecFiles) {
+    print('path: ${file.path}');
+  }
+
+  return pubspecFiles;
+}
+
+class _ServerpodDependency {
+  final String serverpodPackage;
+  final String dependencyName;
+  final String version;
+
+  _ServerpodDependency({
+    required this.serverpodPackage,
+    required this.dependencyName,
+    required this.version,
+  });
+}
