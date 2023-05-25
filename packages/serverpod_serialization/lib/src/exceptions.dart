5f837f82d1d01f5bb362bf454d49b9ce4208f37c
Isak <isak.duner@gmail.com>
2023-05-25 14:21:58 +0200

    fix: Serializable exeptions now working with modules. (#935)

--- packages/serverpod_serialization/lib/src/exceptions.dart
+++ packages/serverpod_serialization/lib/src/exceptions.dart
@@ -1,11 +1,13 @@
 import 'package:serverpod_serialization/serverpod_serialization.dart';
 
-/// This is `SerializableException` that can be used to pass Domain exceptions from the Server to the Client
+/// This is `SerializableException` that can be used to pass Domain exceptions
+/// from the Server to the Client
 ///
 /// You can `throw SerializableException()`
 ///
 /// Based on issue [#486](https://github.com/serverpod/serverpod/issues/486)
-class SerializableException extends SerializableEntity implements Exception {
+abstract class SerializableException extends SerializableEntity
+    implements Exception {
   /// Const constructor to pass empty exception with `statusCode 500`
   SerializableException();
 
