cd47f4589969a19a0b845e36d98b4491857aa24a
Luke Hutchison <luke.hutch@gmail.com>
2023-06-02 16:09:33 -0600

    Add admin endpoint to block/unblock user

--- modules/serverpod_auth/serverpod_auth_server/lib/src/endpoints/admin_endpoint.dart
+++ modules/serverpod_auth/serverpod_auth_server/lib/src/endpoints/admin_endpoint.dart
@@ -10,7 +10,16 @@ class AdminEndpoint extends Endpoint {
   Set<Scope> get requiredScopes => {Scope.admin};
 
   /// Finds a user by its id.
-  Future<UserInfo?> getUserInfo(Session session, int userId) async {
-    return Users.findUserByUserId(session, userId);
-  }
+  Future<UserInfo?> getUserInfo(Session session, int userId) async =>
+      Users.findUserByUserId(session, userId);
+
+  /// Marks a user as blocked so that they can't log in, and invalidates their
+  /// auth key so that they can't keep calling endpoints through their current
+  /// session.
+  Future<void> blockUser(Session session, int userId) async =>
+      Users.blockUser(session, userId);
+
+  /// Unblocks a user so that they can log in again.
+  Future<void> unblockUser(Session session, int userId) async =>
+      Users.unblockUser(session, userId);
 }
