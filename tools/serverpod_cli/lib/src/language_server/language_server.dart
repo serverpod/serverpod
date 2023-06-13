f62f7f7b1855ead28d9613293f3ed41983f40c62
Isakdl <isak.duner@gmail.com>
2023-06-13 14:05:18 +0200

    feat: add language-server command to cli

---
+++ tools/serverpod_cli/lib/src/language_server/language_server.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'package:json_rpc_2/json_rpc_2.dart';
+import 'package:serverpod_cli/src/language_server/wireformat.dart';
+
+Future<void> startLanguageServer() async {
+  var peer = Peer(lspChannel(stdin, stdout));
+  peer
+    ..registerMethod('initialize', (params) async {
+      return ({
+        'capabilities': {
+          'textDocumentSync': 1,
+        }
+      });
+    })
+    ..registerMethod('textDocument/didChange', (Parameters params) async {
+      // todo implement
+    });
+
+  await peer.listen();
+}
