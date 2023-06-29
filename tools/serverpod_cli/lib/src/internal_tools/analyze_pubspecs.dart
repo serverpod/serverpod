bf187659129a2d12c173af8c757f7d1a954fef3a
Alexander Sandor <alexander@serverpod.dev>
2023-06-29 08:42:57 +0200

    refactor: Ensure same abstraction layer is kept in method

--- tools/serverpod_cli/lib/src/internal_tools/analyze_pubspecs.dart
+++ tools/serverpod_cli/lib/src/internal_tools/analyze_pubspecs.dart
@@ -7,6 +7,60 @@ import 'package:serverpod_cli/src/internal_tools/pubspec_helpers.dart';
 /// The internal tool for analyzing the pubspec.yaml files in the Serverpod
 /// repo.
 Future<void> performAnalyzePubspecs(bool checkLatestVersion) async {
+  if (!_isServerpodRootDirectory()) {
+    print('Must be run from the serverpod repository root');
+    exit(1);
+  }
+
+  var pubspecFiles = findPubspecsFiles(Directory.current,
+      ignorePaths: ['/serverpod/templates/pubspecs/']);
+
+  var dependencies = _getDependencies(pubspecFiles);
+
+  var missmatchedDeps = _findMissmatchedDependencies(dependencies);
+
+  if (missmatchedDeps.isNotEmpty) {
+    _printMissmatchedDependencies(missmatchedDeps, dependencies);
+    exit(1);
+  }
+
+  print('Dependencies match.');
+
+  if (checkLatestVersion) {
+    await _checkLatestVersion(dependencies);
+  }
+}
+
+Future<void> _checkLatestVersion(
+    Map<String, List<_ServerpodDependency>> dependencies) async {
+  print('Checking latest pub versions.');
+  try {
+    var pub = PubClient();
+    for (var depName in dependencies.keys) {
+      var deps = dependencies[depName]!;
+      var depVersion = deps.first.version;
+      var latestPubVersion = _latestStableVersion(
+        await pub.packageVersions(depName),
+      );
+
+      if (depVersion != '^$latestPubVersion') {
+        print(depName);
+        print('  local: $depVersion');
+        print('  pub:   ^$latestPubVersion');
+        print('  found in:');
+        for (var dep in deps) {
+          print('   - ${dep.serverpodPackage}');
+        }
+      }
+    }
+    pub.close();
+  } catch (e) {
+    print('Version check failed.');
+    print(e);
+  }
+}
+
+bool _isServerpodRootDirectory() {
   // Verify that we are in the serverpod directory
   var dirPackages = Directory('packages');
   var dirTemplates = Directory('templates/pubspecs');
@@ -15,17 +69,46 @@ Future<void> performAnalyzePubspecs(bool checkLatestVersion) async {
   if (!dirPackages.existsSync() ||
       !dirTemplates.existsSync() ||
       !dirRoot.existsSync()) {
-    print('Must be run from the serverpod repository root');
-    exit(1);
+    return false;
   }
 
-  var pubspecFiles = findPubspecsFiles(Directory.current,
-      ignorePaths: ['/serverpod/templates/pubspecs/']);
-  var dependencies = <String, List<_ServerpodDependency>>{};
+  return true;
+}
 
+void _printMissmatchedDependencies(Set<String> missmatchedDeps,
+    Map<String, List<_ServerpodDependency>> dependencies) {
+  print('Found missmatched dependencies:');
+  for (var depName in missmatchedDeps) {
+    print(depName);
+    var deps = dependencies[depName]!;
+    for (var dep in deps) {
+      print('  ${dep.version} ${dep.serverpodPackage}');
+    }
+  }
+}
+
+Set<String> _findMissmatchedDependencies(
+    Map<String, List<_ServerpodDependency>> dependencies) {
+  var missmatchedDeps = <String>{};
+  for (var depName in dependencies.keys) {
+    var deps = dependencies[depName]!;
+    String? version;
+    for (var dep in deps) {
+      if (version != null && version != dep.version) {
+        missmatchedDeps.add(depName);
+      }
+      version = dep.version;
+    }
+  }
+  return missmatchedDeps;
+}
+
+Map<String, List<_ServerpodDependency>> _getDependencies(
+    List<File> pubspecFiles) {
+  var dependencies = <String, List<_ServerpodDependency>>{};
   for (var pubspecFile in pubspecFiles) {
     var parseErrorMessage =
-        'Failed to load PUBLISHABLE_PACKAGES or the pubspec files. Are you'
+        'Failed to load PUBLISHABLE_PACKAGES or the pubspec files. Are you '
         'running this command from the serverpod repository root?';
     var pubspec = parsePubspec(pubspecFile, parseErrorMessage);
 
@@ -64,62 +147,7 @@ Future<void> performAnalyzePubspecs(bool checkLatestVersion) async {
     }
   }
 
-  // Find missmatched dependencies
-  var missmatchedDeps = <String>{};
-  for (var depName in dependencies.keys) {
-    var deps = dependencies[depName]!;
-    String? version;
-    for (var dep in deps) {
-      if (version != null && version != dep.version) {
-        missmatchedDeps.add(depName);
-      }
-      version = dep.version;
-    }
-  }
-
-  if (missmatchedDeps.isNotEmpty) {
-    print('Found missmatched dependencies:');
-    for (var depName in missmatchedDeps) {
-      print(depName);
-      var deps = dependencies[depName]!;
-      for (var dep in deps) {
-        print('  ${dep.version} ${dep.serverpodPackage}');
-      }
-    }
-
-    exit(1);
-  }
-
-  print('Dependencies match.');
-
-  // Check versions.
-  if (checkLatestVersion) {
-    print('Checking latest pub versions.');
-    try {
-      var pub = PubClient();
-      for (var depName in dependencies.keys) {
-        var deps = dependencies[depName]!;
-        var depVersion = deps.first.version;
-        var latestPubVersion = _latestStableVersion(
-          await pub.packageVersions(depName),
-        );
-
-        if (depVersion != '^$latestPubVersion') {
-          print(depName);
-          print('  local: $depVersion');
-          print('  pub:   ^$latestPubVersion');
-          print('  found in:');
-          for (var dep in deps) {
-            print('   - ${dep.serverpodPackage}');
-          }
-        }
-      }
-      pub.close();
-    } catch (e) {
-      print('Version check failed.');
-      print(e);
-    }
-  }
+  return dependencies;
 }
 
 String _latestStableVersion(List<String> packageVersions) {
