b84f3f3365f2ba26280d423760c27db1dad72f1d
Isakdl <isak.duner@gmail.com>
2023-06-30 15:44:16 +0200

    refactor: restructure tests and delete duplicated tests

--- tools/serverpod_cli/test/analyzer/entities/entity_analyzer/field_validation_test.dart
+++ tools/serverpod_cli/test/analyzer/entities/entity_analyzer/field_validation_test.dart
@@ -4,51 +4,154 @@ import 'package:serverpod_cli/src/generator/code_generation_collector.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test(
-      'Given a class without the fields key, then collect an error that the fields key is required',
-      () {
-    var collector = CodeGenerationCollector();
-    var analyzer = SerializableEntityAnalyzer(
-      yaml: '''
+  group('Test invalid top level fields key values', () {
+    test(
+        'Given a class without the fields key, then collect an error that the fields key is required',
+        () {
+      var collector = CodeGenerationCollector();
+      var analyzer = SerializableEntityAnalyzer(
+        yaml: '''
 class: Example
 ''',
-      sourceFileName: 'lib/src/protocol/example.yaml',
-      outFileName: 'example.yaml',
-      subDirectoryParts: ['lib', 'src', 'protocol'],
-      collector: collector,
-    );
+        sourceFileName: 'lib/src/protocol/example.yaml',
+        outFileName: 'example.yaml',
+        subDirectoryParts: ['lib', 'src', 'protocol'],
+        collector: collector,
+      );
 
-    analyzer.analyze();
+      analyzer.analyze();
 
-    expect(collector.errors.length, greaterThan(0));
+      expect(collector.errors.length, greaterThan(0));
 
-    var error = collector.errors.first;
+      var error = collector.errors.first;
 
-    expect(error.message, 'No "fields" property is defined.');
-  });
+      expect(error.message, 'No "fields" property is defined.');
+    });
 
-  test(
-      'Given an exception without the fields key, then collect an error that the fields key is required',
-      () {
-    var collector = CodeGenerationCollector();
-    var analyzer = SerializableEntityAnalyzer(
-      yaml: '''
+    test(
+        'Given an exception without the fields key, then collect an error that the fields key is required',
+        () {
+      var collector = CodeGenerationCollector();
+      var analyzer = SerializableEntityAnalyzer(
+        yaml: '''
 exception: Example
 ''',
-      sourceFileName: 'lib/src/protocol/example.yaml',
-      outFileName: 'example.yaml',
-      subDirectoryParts: ['lib', 'src', 'protocol'],
-      collector: collector,
-    );
+        sourceFileName: 'lib/src/protocol/example.yaml',
+        outFileName: 'example.yaml',
+        subDirectoryParts: ['lib', 'src', 'protocol'],
+        collector: collector,
+      );
 
-    analyzer.analyze();
+      analyzer.analyze();
 
-    expect(collector.errors.length, greaterThan(0));
+      expect(collector.errors.length, greaterThan(0));
 
-    var error = collector.errors.first;
+      var error = collector.errors.first;
 
-    expect(error.message, 'No "fields" property is defined.');
+      expect(error.message, 'No "fields" property is defined.');
+    });
+
+    test(
+        'Given a class with the fields key defined but without any field, then collect an error that at least one field has to be added.',
+        () {
+      var collector = CodeGenerationCollector();
+      var analyzer = SerializableEntityAnalyzer(
+        yaml: '''
+class: Example
+fields:
+''',
+        sourceFileName: 'lib/src/protocol/example.yaml',
+        outFileName: 'example.yaml',
+        subDirectoryParts: ['lib', 'src', 'protocol'],
+        collector: collector,
+      );
+
+      analyzer.analyze();
+
+      expect(collector.errors.length, greaterThan(0));
+
+      var error = collector.errors.first;
+
+      expect(
+          error.message, 'The "fields" property must have at least one value.');
+    });
+
+    test(
+        'Given an exception with the fields key defined but without any field, then collect an error that at least one field has to be added.',
+        () {
+      var collector = CodeGenerationCollector();
+      var analyzer = SerializableEntityAnalyzer(
+        yaml: '''
+exception: Example
+fields:
+''',
+        sourceFileName: 'lib/src/protocol/example.yaml',
+        outFileName: 'example.yaml',
+        subDirectoryParts: ['lib', 'src', 'protocol'],
+        collector: collector,
+      );
+
+      analyzer.analyze();
+
+      expect(collector.errors.length, greaterThan(0));
+
+      var error = collector.errors.first;
+
+      expect(
+          error.message, 'The "fields" property must have at least one value.');
+    });
+
+    test(
+        'Given an class with the fields key defined as a primitive datatype instead of a Map, then collect an error that at least one field has to be added.',
+        () {
+      var collector = CodeGenerationCollector();
+      var analyzer = SerializableEntityAnalyzer(
+        yaml: '''
+class: Example
+fields: int
+''',
+        sourceFileName: 'lib/src/protocol/example.yaml',
+        outFileName: 'example.yaml',
+        subDirectoryParts: ['lib', 'src', 'protocol'],
+        collector: collector,
+      );
+
+      analyzer.analyze();
+
+      expect(collector.errors.length, greaterThan(0));
+
+      var error = collector.errors.first;
+
+      expect(
+          error.message, 'The "fields" property must have at least one value.');
+    });
+
+    test(
+        'Given an enum with the fields key defined, then collect an error that fields are not allowed.',
+        () {
+      var collector = CodeGenerationCollector();
+      var analyzer = SerializableEntityAnalyzer(
+        yaml: '''
+enum: Example
+fields:
+''',
+        sourceFileName: 'lib/src/protocol/example.yaml',
+        outFileName: 'example.yaml',
+        subDirectoryParts: ['lib', 'src', 'protocol'],
+        collector: collector,
+      );
+
+      analyzer.analyze();
+
+      expect(collector.errors.length, greaterThan(0));
+
+      var error = collector.errors.first;
+
+      expect(error.message,
+          'The "fields" property is not allowed for enum type. Valid keys are {enum, serverOnly, values}.');
+    });
   });
+
   group('Testing key of fields.', () {
     test(
         'Given a class with a field key that is not a string, then collect an error that field keys have to be of the type string.',
