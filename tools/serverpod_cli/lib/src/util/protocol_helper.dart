8d2b89dd52624405f9d99bd641612a5b5d677f0a
Isakdl <isak.duner@gmail.com>
2023-07-05 16:21:31 +0200

    fix: add protocol helper methods

--- tools/serverpod_cli/lib/src/util/protocol_helper.dart
+++ tools/serverpod_cli/lib/src/util/protocol_helper.dart
@@ -6,13 +6,15 @@ import 'package:path/path.dart';
 class ProtocolSource {
   String yaml;
   Uri uri;
+  List<String> protocolRootPathParts;
 
-  ProtocolSource(this.yaml, this.uri);
+  ProtocolSource(this.yaml, this.uri, this.protocolRootPathParts);
 }
 
 class ProtocolHelper {
   static Future<List<ProtocolSource>> loadProjectYamlProtocolsFromDisk(
-      GeneratorConfig config) async {
+    GeneratorConfig config,
+  ) async {
     var sourceDir = Directory(joinAll(config.protocolSourcePathParts));
     var sourceFileList = await sourceDir.list(recursive: true).toList();
     sourceFileList.sort((a, b) => a.path.compareTo(b.path));
@@ -25,9 +27,20 @@ class ProtocolHelper {
     for (var entity in files) {
       var yaml = await entity.readAsString();
 
-      sources.add(ProtocolSource(yaml, entity.uri));
+      sources.add(ProtocolSource(
+        yaml,
+        entity.uri,
+        extractPathFromProtocolRoot(config, entity.uri),
+      ));
     }
 
     return sources;
+  }
+
+  static List<String> extractPathFromProtocolRoot(
+      GeneratorConfig config, Uri fileUri) {
+    var sourceDir = Directory(joinAll(config.protocolSourcePathParts));
+    var sourceDirPartsLength = split(sourceDir.path).length;
+    return split(dirname(fileUri.path)).skip(sourceDirPartsLength).toList();
   }
 }
