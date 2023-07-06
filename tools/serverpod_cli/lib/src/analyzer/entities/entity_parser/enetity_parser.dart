ae05a87599905ff042069f418e672dc62152caf5
Isakdl <isak.duner@gmail.com>
2023-07-06 15:55:32 +0200

    refactor: extract serialze class and enum into parser class

---
+++ tools/serverpod_cli/lib/src/analyzer/entities/entity_parser/enetity_parser.dart
@@ -0,0 +1,313 @@
+import 'package:serverpod_cli/src/util/extensions.dart';
+import 'package:serverpod_cli/src/util/protocol_helper.dart';
+import 'package:serverpod_cli/src/util/yaml_docs.dart';
+import 'package:source_span/source_span.dart';
+import 'package:yaml/yaml.dart';
+
+
+import '../../../generator/types.dart';
+import '../converter/converter.dart';
+import '../definitions.dart';
+import '../validation/keywords.dart';
+import '../validation/validate_node.dart';
+
+class EntityParser {
+  static SerializableEntityDefinition? serializeClassFile(
+    String documentType,
+    ProtocolSource protocolSource,
+    String outFileName,
+    YamlMap documentContents,
+    YamlDocumentationExtractor docsExtractor,
+    ValidateNode fieldStructure,
+  ) {
+    YamlNode? classNode = documentContents.nodes[documentType];
+
+    if (classNode == null) {
+      throw ArgumentError(
+        'No $documentType node found, only valid to call this function if '
+        ' the documentType exists as a top level key in the document.',
+      );
+    }
+
+    var classDocumentation = docsExtractor.getDocumentation(
+      documentContents.key(documentType)!.span.start,
+    );
+
+    var className = classNode.value;
+    if (className is! String) return null;
+
+    var tableName = _parseTableName(documentContents);
+    var serverOnly = _parseServerOnly(documentContents);
+    var fields = _parseClassFields(
+      documentContents,
+      docsExtractor,
+      tableName != null,
+      fieldStructure,
+    );
+    var indexes = _parseIndexes(documentContents, fields);
+
+    return ClassDefinition(
+      yamlProtocol: protocolSource.yaml,
+      className: className,
+      sourceFileName: protocolSource.yamlSourceUri.path,
+      tableName: tableName,
+      fileName: outFileName,
+      fields: fields,
+      indexes: indexes,
+      subDirParts: protocolSource.protocolRootPathParts,
+      documentation: classDocumentation,
+      isException: documentType == Keyword.exceptionType,
+      serverOnly: serverOnly,
+    );
+  }
+
+  static SerializableEntityDefinition? serializeEnumFile(
+    String documentType,
+    ProtocolSource protocolSource,
+    String outFileName,
+    YamlMap documentContents,
+    YamlDocumentationExtractor docsExtractor,
+  ) {
+    var className = documentContents[Keyword.enumType];
+    if (className is! String) return null;
+
+    var enumDocumentation = docsExtractor.getDocumentation(
+      documentContents.key(Keyword.enumType)!.span.start,
+    );
+
+    var serverOnly = _parseServerOnly(documentContents);
+    var values = _parseEnumValues(documentContents, docsExtractor);
+
+    return EnumDefinition(
+      yamlProtocol: protocolSource.yaml,
+      fileName: outFileName,
+      sourceFileName: protocolSource.yamlSourceUri.path,
+      className: className,
+      values: values,
+      documentation: enumDocumentation,
+      subDirParts: protocolSource.protocolRootPathParts,
+      serverOnly: serverOnly,
+    );
+  }
+
+  static bool _parseServerOnly(YamlMap documentContents) {
+    var serverOnly = documentContents.nodes[Keyword.serverOnly]?.value;
+    if (serverOnly is! bool) return false;
+
+    return serverOnly;
+  }
+
+  static String? _parseTableName(YamlMap documentContents) {
+    var tableName = documentContents.nodes[Keyword.table]?.value;
+    if (tableName is! String) return null;
+
+    return tableName;
+  }
+
+  static List<SerializableEntityFieldDefinition> _parseClassFields(
+    YamlMap documentContents,
+    YamlDocumentationExtractor docsExtractor,
+    bool hasTable,
+    ValidateNode fieldStructure,
+  ) {
+    var fieldsNode = documentContents.nodes[Keyword.fields];
+    if (fieldsNode is! YamlMap) return [];
+
+    var parsedFields = fieldsNode.nodes.entries.map((fieldNode) {
+      return _parseEntityFieldDefinition(
+        fieldNode,
+        fieldStructure,
+        docsExtractor,
+      );
+    });
+
+    var fields = parsedFields
+        .where((field) => field != null)
+        .cast<SerializableEntityFieldDefinition>()
+        .toList();
+
+    if (hasTable) {
+      fields = [
+        SerializableEntityFieldDefinition(
+          name: 'id',
+          type: TypeDefinition.int.asNullable,
+          scope: SerializableEntityFieldScope.all,
+          documentation: [
+            '/// The database id, set if the object has been inserted into the',
+            '/// database or if it has been fetched from the database. Otherwise,',
+            '/// the id will be null.',
+          ],
+        ),
+        ...fields,
+      ];
+    }
+
+    return fields;
+  }
+
+  static SerializableEntityFieldDefinition? _parseEntityFieldDefinition(
+    MapEntry<dynamic, YamlNode> fieldNode,
+    ValidateNode fieldStructure,
+    YamlDocumentationExtractor docsExtractor,
+  ) {
+    var key = fieldNode.key;
+    if (key is! YamlScalar) return null;
+
+    var nodeValue = fieldNode.value;
+    var value = nodeValue.value;
+    if (value is String) {
+      value = convertStringifiedNestedNodesToYamlMap(
+        value,
+        nodeValue,
+        fieldStructure,
+      );
+    }
+    if (value is! YamlMap) return null;
+
+    var fieldName = key.value;
+    if (fieldName is! String) return null;
+
+    var typeNode = value.nodes[Keyword.type];
+    var typeValue = typeNode?.value;
+    if (typeNode is! YamlScalar) return null;
+    if (typeValue is! String) return null;
+
+    var fieldDocumentation = docsExtractor.getDocumentation(key.span.start);
+    var typeResult = parseAndAnalyzeType(
+      typeValue.replaceAll(' ', ''),
+      sourceSpan: typeNode.span,
+    );
+    var scope = _parseClassFieldscope(value);
+    var parentTable = _parseParentTable(value);
+    var isEnum = _parseIsEnumField(value);
+
+    return SerializableEntityFieldDefinition(
+      name: fieldName,
+      scope: scope,
+      type: typeResult.type..isEnum = isEnum,
+      parentTable: parentTable,
+      documentation: fieldDocumentation,
+    );
+  }
+
+  static SerializableEntityFieldScope _parseClassFieldscope(YamlMap documentContents) {
+    var database = documentContents.containsKey(Keyword.database);
+    var api = documentContents.containsKey(Keyword.api);
+
+    if (database) return SerializableEntityFieldScope.database;
+    if (api) return SerializableEntityFieldScope.api;
+
+    return SerializableEntityFieldScope.all;
+  }
+
+  static String? _parseParentTable(YamlMap documentContents) {
+    var parent = documentContents.nodes[Keyword.parent]?.value;
+    if (parent is! String) return null;
+
+    return parent;
+  }
+
+  static bool _parseIsEnumField(YamlMap documentContents) {
+    return documentContents.containsKey(Keyword.enumType);
+  }
+
+  static List<SerializableEntityIndexDefinition>? _parseIndexes(
+    YamlMap documentContents,
+    List<SerializableEntityFieldDefinition> fields,
+  ) {
+    var indexesNode = documentContents.nodes[Keyword.indexes];
+    if (indexesNode is! YamlMap) return null;
+
+    var indexes = indexesNode.nodes.entries.map((node) {
+      var keyScalar = node.key;
+      var nodeDocument = node.value;
+      if (keyScalar is! YamlScalar) return null;
+      if (nodeDocument is! YamlMap) return null;
+
+      var indexName = keyScalar.value;
+      if (indexName is! String) return null;
+
+      var indexFields = _parseIndexFields(nodeDocument, fields);
+      var type = _parseIndexType(nodeDocument);
+      var unique = _parseUniqueKey(nodeDocument);
+
+      return SerializableEntityIndexDefinition(
+        name: indexName,
+        type: type,
+        unique: unique,
+        fields: indexFields,
+      );
+    });
+
+    return indexes
+        .where((index) => index != null)
+        .cast<SerializableEntityIndexDefinition>()
+        .toList();
+  }
+
+  static List<String> _parseIndexFields(
+    YamlMap documentContents,
+    List<SerializableEntityFieldDefinition> fields,
+  ) {
+    var fieldsNode = documentContents.nodes[Keyword.fields];
+    if (fieldsNode is! YamlNode) return [];
+
+    var stringifiedFields = fieldsNode.value;
+    if (stringifiedFields is! String) return [];
+
+    var indexFields = stringifiedFields
+        .split(',')
+        .map(
+          (String str) => str.trim(),
+        )
+        .toList();
+
+    return indexFields;
+  }
+
+  static String _parseIndexType(YamlMap documentContents) {
+    var typeNode = documentContents.nodes[Keyword.type];
+    var type = typeNode?.value;
+
+    if (type == null || type is! String) {
+      return 'btree';
+    }
+
+    return type;
+  }
+
+  static bool _parseUniqueKey(YamlMap documentContents) {
+    var node = documentContents.nodes[Keyword.unique];
+    var nodeValue = node?.value;
+    return nodeValue is bool ? nodeValue : false;
+  }
+
+  static List<ProtocolEnumValueDefinition> _parseEnumValues(
+    YamlMap documentContents,
+    YamlDocumentationExtractor docsExtractor,
+  ) {
+    var valuesNode = documentContents.nodes[Keyword.values];
+    if (valuesNode is! YamlList) return [];
+
+    var values = valuesNode.nodes.map((node) {
+      var value = node.value;
+      if (value is! String) return null;
+
+      var start = node.span.start;
+      // 2 is the length of '- ' in '- enumValue'
+      var valueDocumentation = docsExtractor.getDocumentation(
+        SourceLocation(start.offset - 2,
+            column: start.column - 2,
+            line: start.line,
+            sourceUrl: start.sourceUrl),
+      );
+
+      return ProtocolEnumValueDefinition(value, valueDocumentation);
+    });
+
+    return values
+        .where((value) => value != null)
+        .cast<ProtocolEnumValueDefinition>()
+        .toList();
+  }
+}
