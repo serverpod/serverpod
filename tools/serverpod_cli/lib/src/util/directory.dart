ea651dea53b37e4195c7bd0157416a43295e8e0c
Isakdl <isak.duner@gmail.com>
2023-07-07 10:37:50 +0200

    revert: none nullable

--- tools/serverpod_cli/lib/src/util/directory.dart
+++ tools/serverpod_cli/lib/src/util/directory.dart
@@ -18,9 +18,7 @@ bool isServerpodRootDirectory([Directory? directory]) {
   return true;
 }
 
-bool isServerDirectory(Directory? directory) {
-  if (directory == null) return false;
-
+bool isServerDirectory(Directory directory) {
   var pubspec = File(p.join(directory.path, 'pubspec.yaml'));
   var generator = File(p.join(directory.path, 'config', 'generator.yaml'));
 
