0d664506a03dc438cbbf3a07d0b65895b981b5b4
Isakdl <isak.duner@gmail.com>
2023-07-12 14:30:20 +0200

    style: typo

--- tools/serverpod_cli/lib/src/analyzer/entities/validation/protocol_validator.dart
+++ tools/serverpod_cli/lib/src/analyzer/entities/validation/protocol_validator.dart
@@ -42,7 +42,7 @@ List<SourceSpanException> validateTopLevelEntityType(
   return errors;
 }
 
-/// Recursivly validates a yaml document against a set of [ValidateNode]s.
+/// Recursively validates a yaml document against a set of [ValidateNode]s.
 /// The [documentType] represents the parent key of the [documentContents],
 /// in the initial processing this is expected to be the top level entity type
 /// we are checking. E.g. 'class', 'enum', 'exception', etc.
