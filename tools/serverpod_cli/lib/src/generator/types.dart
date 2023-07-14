f6b10c8a72ec44cdba70de5f4657931f1baf5716
Luke Hutchison <luke.hutch@gmail.com>
2023-07-13 18:14:48 -0600

    Fix omission

--- tools/serverpod_cli/lib/src/generator/types.dart
+++ tools/serverpod_cli/lib/src/generator/types.dart
@@ -201,7 +201,11 @@ class TypeDefinition {
   String get columnType {
     // TODO: add all supported types here
     if (className == 'int') return 'ColumnInt';
-    if (isEnum) return 'ColumnEnum';
+    if (isEnum) {
+      return GeneratorConfig.instance!.serializeEnumValuesAsStrings
+          ? 'ColumnEnumSerializedAsString'
+          : 'ColumnEnumSerializedAsInteger';
+    }
     if (className == 'double') return 'ColumnDouble';
     if (className == 'bool') return 'ColumnBool';
     if (className == 'String') return 'ColumnString';
@@ -372,7 +376,7 @@ class TypeDefinition {
 
 /// Analyze the type at the start of [input].
 /// [input] must not contain spaces.
-/// Returns a [_TypeResult] containing the type,
+/// Returns a [TypeParseResult] containing the type,
 /// as well as the position of the last parsed character.
 /// So when calling with "List<List<String?>?>,database",
 /// the position will point at the ','.
