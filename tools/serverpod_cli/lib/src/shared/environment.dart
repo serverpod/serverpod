6be55c712ba11703a0c37e35a89448b5f1370c62
Alexander Sandor <alexander@serverpod.dev>
2023-07-14 11:05:40 +0200

    refactor: convert environment.dart to new logger interface

--- tools/serverpod_cli/lib/src/shared/environment.dart
+++ tools/serverpod_cli/lib/src/shared/environment.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:serverpod_cli/src/logger/logger.dart';
+
 import '../generated/version.dart';
 
 String serverpodHome = '';
@@ -8,7 +10,7 @@ bool loadEnvironmentVars() {
   if (!productionMode) {
     var home = Platform.environment['SERVERPOD_HOME'];
     if (home == null || home == '' || !Directory(home).existsSync()) {
-      print(
+      log.error(
         'The SERVERPOD_HOME environmental variable is required in development mode,',
       );
       return false;
