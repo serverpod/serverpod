8ba7563646cb6644a0dfbba7c71482bb0ce20d5e
Alexander Sandor <alexander@serverpod.dev>
2023-07-17 17:43:49 +0200

    feature: introduce void logger

---
+++ tools/serverpod_cli/lib/src/logger/loggers/void_logger.dart
@@ -0,0 +1,31 @@
+import 'package:serverpod_cli/src/logger/logger.dart';
+import 'package:source_span/source_span.dart';
+
+/// Logger that logs no output.
+///
+/// Intended to be used for testing to silence any printed output.
+class VoidLogger extends Logger {
+  VoidLogger() : super(LogLevel.debug);
+
+  @override
+  void debug(String message, {LogStyle style = const LogStyle()}) {}
+
+  @override
+  void info(String message, {LogStyle style = const LogStyle()}) {}
+
+  @override
+  void warning(String message, {LogStyle style = const LogStyle()}) {}
+
+  @override
+  void error(String message,
+      {StackTrace? stackTrace, LogStyle style = const LogStyle()}) {}
+
+  @override
+  void sourceSpanException(SourceSpanException sourceSpan,
+      {bool newParagraph = false}) {}
+
+  @override
+  Future<void> flush() {
+    return Future(() => {});
+  }
+}
