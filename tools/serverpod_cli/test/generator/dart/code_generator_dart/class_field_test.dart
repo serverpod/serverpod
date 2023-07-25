517172f0cde131a87cb0a6a2b6434d997af3d664
Isakdl <isak.duner@gmail.com>
2023-07-25 16:49:28 +0200

    test: Add field validation tests for code generator

---
+++ tools/serverpod_cli/test/generator/dart/code_generator_dart/class_field_test.dart
@@ -0,0 +1,69 @@
+import 'package:serverpod_cli/src/generator/dart/code_generator_dart.dart';
+import 'package:test/test.dart';
+
+import '../../../_test_util/builders/class_definition_builder.dart';
+import '../../../_test_util/builders/generator_config_builder.dart';
+
+const projectName = 'example_project';
+final config = GeneratorConfigBuilder().withName(projectName).build();
+const generator = DartCodeGenerator();
+
+void main() {
+  group('Given a class with a none nullable field', () {
+    var entities = [
+      ClassDefinitionBuilder()
+          .withClassName('Example')
+          .withFileName('example')
+          .withSimpleField('title', 'String')
+          .build()
+    ];
+
+    var codeMap = generator.generateSerializableEntitiesCode(
+      entities: entities,
+      config: config,
+    );
+
+    test('then a server-side class is generated with that class variable.', () {
+      var expectedFileName = 'lib/src/generated/example.dart';
+
+      expect(codeMap[expectedFileName], contains('String title;'));
+    });
+
+    test('then a client-side class is generated with that class variable.', () {
+      var expectedFileName =
+          '../example_project_client/lib/src/protocol/example.dart';
+
+      print(codeMap.keys);
+
+      expect(codeMap[expectedFileName], contains('String title;'));
+    });
+  });
+
+  group('Given a class with a nullable field', () {
+    var entities = [
+      ClassDefinitionBuilder()
+          .withClassName('Example')
+          .withFileName('example')
+          .withSimpleField('title', 'String', nullable: true)
+          .build()
+    ];
+
+    var codeMap = generator.generateSerializableEntitiesCode(
+      entities: entities,
+      config: config,
+    );
+
+    test('then a server-side class is generated with that class variable.', () {
+      var expectedFileName = 'lib/src/generated/example.dart';
+
+      expect(codeMap[expectedFileName], contains('String? title;'));
+    });
+
+    test('then a client-side class is generated with that class variable.', () {
+      var expectedFileName =
+          '../example_project_client/lib/src/protocol/example.dart';
+
+      expect(codeMap[expectedFileName], contains('String? title;'));
+    });
+  });
+}
