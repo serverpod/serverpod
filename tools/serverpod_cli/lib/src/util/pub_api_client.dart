42efc5620fb3a2fa4012b6c660555955b2624fe0
Alexander Sandor <alexander@serverpod.dev>
2023-07-27 10:34:48 +0200

    fix: introduce pub_api_client

---
+++ tools/serverpod_cli/lib/src/util/pub_api_client.dart
@@ -0,0 +1,58 @@
+import 'package:http/http.dart' as http;
+import 'package:pub_api_client/pub_api_client.dart';
+import 'package:pub_semver/pub_semver.dart';
+import 'package:serverpod_cli/src/logger/logger.dart';
+
+class PubApiClient {
+  final PubClient _pubClient;
+  final Duration _requestTimeout;
+
+  PubApiClient(
+      {http.Client? httpClient, requestTimeout = const Duration(seconds: 2)})
+      : _pubClient = PubClient(client: httpClient),
+        _requestTimeout = requestTimeout;
+
+  Future<Version?> tryFetchLatestStableVersion(String packageName) async {
+    String? latestStableVersion;
+    try {
+      var packageVersions = await _pubClient
+          .packageVersions(packageName)
+          .timeout(_requestTimeout);
+      latestStableVersion = _tryGetLatestStableVersion(packageVersions);
+      _pubClient.close();
+    } catch (e) {
+      log.error('Failed to fetch latest version for $packageName.');
+      _logPubClientException(e);
+      return null;
+    }
+
+    if (latestStableVersion == null) return null;
+
+    try {
+      return Version.parse(latestStableVersion);
+    } catch (e) {
+      log.error('Failed to parse version for $packageName');
+      log.error(e.toString());
+      return null;
+    }
+  }
+
+  String? _tryGetLatestStableVersion(List<String> packageVersions) {
+    for (var version in packageVersions) {
+      if (!version.contains('-') && !version.contains('+')) {
+        return version;
+      }
+    }
+
+    return null;
+  }
+
+  /// Required because https://github.com/leoafarias/pub_api_client/issues/35
+  void _logPubClientException(Object exception) {
+    try {
+      log.error(exception.toString());
+    } catch (_) {
+      log.error(exception.runtimeType.toString());
+    }
+  }
+}
