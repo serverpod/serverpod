61db5244dc26d7faf47f0a1732d24e3171de8181
Isak <isak.duner@gmail.com>
2023-08-02 10:19:11 +0200

    fix: Update tools/serverpod_cli/lib/src/analyzer/entities/validation/restrictions.dart
    Co-authored-by: Alexander Sandor <137198655+SandPod@users.noreply.github.com>

--- tools/serverpod_cli/lib/src/analyzer/entities/validation/restrictions.dart
+++ tools/serverpod_cli/lib/src/analyzer/entities/validation/restrictions.dart
@@ -245,7 +245,7 @@ class Restrictions {
     if (definition is! ClassDefinition) return errors;
 
     var field = definition.findField(parentNodeName);
-    if (field == null) return [];
+    if (field == null) return errors;
     var type = field.type.className;
 
     if (AnalyzeChecker.isIdType(type) &&
