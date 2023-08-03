60ec405e9a17516e893fce89faff809a01f95389
Luke Hutchison <luke.hutch@gmail.com>
2023-08-03 00:46:58 -0600

    Make HTTP timeout configurable

--- packages/serverpod_client/lib/src/serverpod_client_io.dart
+++ packages/serverpod_client/lib/src/serverpod_client_io.dart
@@ -15,23 +15,26 @@ import 'serverpod_client_shared_private.dart';
 /// (for Flutter native apps).
 abstract class ServerpodClient extends ServerpodClientShared {
   late HttpClient _httpClient;
-  final Duration timeout;
   bool _initialized = false;
+  late Duration _timeout;
 
   /// Creates a new ServerpodClient.
   ServerpodClient(
     super.host,
     super.serializationManager, {
     dynamic context,
+
     super.authenticationKeyManager,
     super.logFailedCalls,
     Duration? timeout,
-  }) : timeout = timeout ?? const Duration(seconds: 20) {
+  }) {
     assert(context == null || context is SecurityContext);
+    _timeout = timeout ?? const Duration(seconds: 20);
 
     // Setup client
     _httpClient = HttpClient(context: context);
-    _httpClient.connectionTimeout = this.timeout;
+    _httpClient.connectionTimeout = _timeout;
+
     // TODO: Generate working certificates
     _httpClient.badCertificateCallback =
         ((X509Certificate cert, String host, int port) {
@@ -73,7 +76,11 @@ abstract class ServerpodClient extends ServerpodClientShared {
 
       var response = await request
           .close() // done instead of close() ?
+<<<<<<< HEAD
           .timeout(timeout);
+=======
+          .timeout(_timeout);
+>>>>>>> c863eb72 (Make HTTP timeout configurable)
       var data = await _readResponse(response);
 
       if (response.statusCode != HttpStatus.ok) {
