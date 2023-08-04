eae4aed1a4afc6e8da7e4fa37b592de113365441
Alexander Sandor <alexander@serverpod.dev>
2023-08-04 10:51:18 +0200

    test: convert old tests to use compilation unit analysis

--- tools/serverpod_cli/test/generator/dart/client_code_generator/class_field_test.dart
+++ tools/serverpod_cli/test/generator/dart/client_code_generator/class_field_test.dart
@@ -1,4 +1,6 @@
+import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:serverpod_cli/src/generator/dart/client_code_generator.dart';
+import 'package:serverpod_cli/src/test_util/compilation_unit_helpers.dart';
 import 'package:test/test.dart';
 import 'package:path/path.dart' as path;
 
@@ -10,6 +12,14 @@ final config = GeneratorConfigBuilder().withName(projectName).build();
 const generator = DartClientCodeGenerator();
 
 void main() {
+  var expectedFileName = path.join(
+    '..',
+    'example_project_client',
+    'lib',
+    'src',
+    'protocol',
+    'example.dart',
+  );
   group('Given a class with a none nullable field', () {
     var entities = [
       ClassDefinitionBuilder()
@@ -24,21 +34,28 @@ void main() {
       config: config,
     );
 
-    test('then a client-side class is generated with that class variable.', () {
-      var expectedFileName = path.join(
-        '..',
-        'example_project_client',
-        'lib',
-        'src',
-        'protocol',
-        'example.dart',
-      );
-
-      expect(codeMap[expectedFileName], contains('String title;'));
-    });
+    var compilationUnit = parseString(content: codeMap[expectedFileName]!).unit;
+    var maybeClassNamedExample = CompilationUnitHelpers.tryFindClassDeclaration(
+        compilationUnit,
+        name: 'Example');
+    test(
+      'then a class is generated with that class variable.',
+      () {
+        var exampleClass = maybeClassNamedExample!;
+        expect(
+          CompilationUnitHelpers.hasFieldDeclaration(exampleClass,
+              name: 'title', type: 'String'),
+          isTrue,
+          reason: 'Missing declaration for title field',
+        );
+      },
+      skip: maybeClassNamedExample == null
+          ? 'Could not run test because Example class was not found'
+          : false,
+    );
   });
 
-  group('Given a class with a nullable field', () {
+  group('Given a class with a nullable field when generating code', () {
     var entities = [
       ClassDefinitionBuilder()
           .withClassName('Example')
@@ -52,17 +69,24 @@ void main() {
       config: config,
     );
 
-    test('then a client-side class is generated with that class variable.', () {
-      var expectedFileName = path.join(
-        '..',
-        'example_project_client',
-        'lib',
-        'src',
-        'protocol',
-        'example.dart',
-      );
-
-      expect(codeMap[expectedFileName], contains('String? title;'));
-    });
+    var compilationUnit = parseString(content: codeMap[expectedFileName]!).unit;
+    var maybeClassNamedExample = CompilationUnitHelpers.tryFindClassDeclaration(
+        compilationUnit,
+        name: 'Example');
+    test(
+      'then a class is generated with that class variable.',
+      () {
+        var exampleClass = maybeClassNamedExample!;
+        expect(
+          CompilationUnitHelpers.hasFieldDeclaration(exampleClass,
+              name: 'title', type: 'String?'),
+          isTrue,
+          reason: 'Missing declaration for title field',
+        );
+      },
+      skip: maybeClassNamedExample == null
+          ? 'Could not run test because Example class was not found'
+          : false,
+    );
   });
 }
--- tools/serverpod_cli/test/generator/dart/server_code_generator/class_field_test.dart
+++ tools/serverpod_cli/test/generator/dart/server_code_generator/class_field_test.dart
@@ -1,4 +1,6 @@
+import 'package:analyzer/dart/analysis/utilities.dart';
 import 'package:serverpod_cli/src/generator/dart/server_code_generator.dart';
+import 'package:serverpod_cli/src/test_util/compilation_unit_helpers.dart';
 import 'package:test/test.dart';
 import 'package:path/path.dart' as path;
 
@@ -10,6 +12,7 @@ final config = GeneratorConfigBuilder().withName(projectName).build();
 const generator = DartServerCodeGenerator();
 
 void main() {
+  var expectedFileName = path.join('lib', 'src', 'generated', 'example.dart');
   group('Given a class with a none nullable field', () {
     var entities = [
       ClassDefinitionBuilder()
@@ -24,12 +27,25 @@ void main() {
       config: config,
     );
 
-    test('then a server-side class is generated with that class variable.', () {
-      var expectedFileName =
-          path.join('lib', 'src', 'generated', 'example.dart');
-
-      expect(codeMap[expectedFileName], contains('String title;'));
-    });
+    var compilationUnit = parseString(content: codeMap[expectedFileName]!).unit;
+    var maybeClassNamedExample = CompilationUnitHelpers.tryFindClassDeclaration(
+        compilationUnit,
+        name: 'Example');
+    test(
+      'then a class is generated with that class variable.',
+      () {
+        var exampleClass = maybeClassNamedExample!;
+        expect(
+          CompilationUnitHelpers.hasFieldDeclaration(exampleClass,
+              name: 'title', type: 'String'),
+          isTrue,
+          reason: 'Missing declaration for title field',
+        );
+      },
+      skip: maybeClassNamedExample == null
+          ? 'Could not run test because Example class was not found'
+          : false,
+    );
   });
 
   group('Given a class with a nullable field', () {
@@ -46,11 +62,24 @@ void main() {
       config: config,
     );
 
-    test('then a server-side class is generated with that class variable.', () {
-      var expectedFileName =
-          path.join('lib', 'src', 'generated', 'example.dart');
-
-      expect(codeMap[expectedFileName], contains('String? title;'));
-    });
+    var compilationUnit = parseString(content: codeMap[expectedFileName]!).unit;
+    var maybeClassNamedExample = CompilationUnitHelpers.tryFindClassDeclaration(
+        compilationUnit,
+        name: 'Example');
+    test(
+      'then a class is generated with that class variable.',
+      () {
+        var exampleClass = maybeClassNamedExample!;
+        expect(
+          CompilationUnitHelpers.hasFieldDeclaration(exampleClass,
+              name: 'title', type: 'String?'),
+          isTrue,
+          reason: 'Missing declaration for title field',
+        );
+      },
+      skip: maybeClassNamedExample == null
+          ? 'Could not run test because Example class was not found'
+          : false,
+    );
   });
 }
