3c7c360f7d967ac829d03317129fcc9efee9bbb4
Isakdl <isak.duner@gmail.com>
2023-09-05 10:06:14 +0200

    refactor: test to stateful analyzer.

--- tools/serverpod_cli/test/analyzer/entities/entity_analyzer/documentation_test.dart
+++ tools/serverpod_cli/test/analyzer/entities/entity_analyzer/documentation_test.dart
@@ -1,199 +1,227 @@
 import 'package:serverpod_cli/src/analyzer/entities/definitions.dart';
-import 'package:serverpod_cli/src/analyzer/entities/entity_analyzer.dart';
-import 'package:serverpod_cli/src/util/protocol_helper.dart';
+import 'package:serverpod_cli/src/analyzer/entities/stateful_analyzer.dart';
+import 'package:serverpod_cli/src/test_util/builders/protocol_source_builder.dart';
 import 'package:test/test.dart';
 
 void main() {
   test(
-      'Given a class with a class documentation comment then an entity with the class documentation set is generated.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-### This is a comment.
-class: Example
-fields:
-  name: String
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
+    'Given a class with a class documentation comment then an entity with the class documentation set is generated.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+          ### This is a comment.
+          class: Example
+          fields:
+            name: String
+          ''',
+        ).build(),
+      ];
 
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as ClassDefinition?;
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as ClassDefinition;
 
-    expect(entities?.documentation, ['/// This is a comment.']);
-  });
-
-  test(
-      'Given a class with a multiline class documentation comment then an entity with the class documentation set is generated.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-### This is a...
-### multiline comment.
-class: Example
-fields:
-  name: String
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
-
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as ClassDefinition?;
-
-    expect(entities?.documentation,
-        ['/// This is a...', '/// multiline comment.']);
-  });
+      expect(definition.documentation, ['/// This is a comment.']);
+    },
+  );
 
   test(
-      'Given a class with a normal class comment, then the entity that is generated has no documentation set.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-# This is a normal comment.
-class: Example
-fields:
-  name: String
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
+    'Given a class with a multiline class documentation comment then an entity with the class documentation set is generated.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+          ### This is a...
+          ### multiline comment.
+          class: Example
+          fields:
+            name: String
+          ''',
+        ).build(),
+      ];
 
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as ClassDefinition?;
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as ClassDefinition;
 
-    expect(entities?.documentation, null);
-  });
-
-  test(
-      'Given a class with a field documentation comment then the entity that is generated has the documentation set for that specific field.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-class: Example
-fields:
-  ### This is a field comment.
-  name: String
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
-
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as ClassDefinition?;
-
-    expect(
-        entities?.fields.first.documentation, ['/// This is a field comment.']);
-  });
+      expect(
+        definition.documentation,
+        ['/// This is a...', '/// multiline comment.'],
+      );
+    },
+  );
 
   test(
-      'Given a class with a multiline field documentation comment then the entity that is generated has the documentation set for that specific field.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-class: Example
-fields:
-  ### This is a multiline
-  ### field comment.
-  name: String
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
+    'Given a class with a normal class comment, then the entity that is generated has no documentation set.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+          # This is a normal comment.
+          class: Example
+          fields:
+            name: String
+          ''',
+        ).build(),
+      ];
 
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as ClassDefinition?;
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as ClassDefinition;
 
-    expect(entities?.fields.first.documentation,
-        ['/// This is a multiline', '/// field comment.']);
-  });
-
-  test(
-      'Given a class with multiple fields but only one has a documentation comment then the entity that is generated has the documentation set for that specific field.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-class: Example
-fields:
-  name: String
-  ### This is a multiline
-  ### field comment.
-  age: int
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
-
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as ClassDefinition?;
-
-    expect(entities?.fields.first.documentation, null);
-    expect(entities?.fields.last.documentation,
-        ['/// This is a multiline', '/// field comment.']);
-  });
+      expect(definition.documentation, null);
+    },
+  );
 
   test(
-      'Given a class with a field with a normal comment, then the entity that is generated has no documentation set.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-class: Example
-fields:
-  # This is a normal comment.
-  name: String
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
+    'Given a class with a field documentation comment then the entity that is generated has the documentation set for that specific field.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+        class: Example
+        fields:
+          ### This is a field comment.
+          name: String
+        ''',
+        ).build(),
+      ];
 
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as ClassDefinition?;
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as ClassDefinition;
 
-    expect(entities?.fields.first.documentation, null);
-  });
-
-  test(
-      'Given an enum with a multiline class documentation comment then an entity with the class documentation set is generated.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-### This is a...
-### multiline comment.
-enum: Example
-values:
-  - first
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
-
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as EnumDefinition?;
-
-    expect(entities?.documentation,
-        ['/// This is a...', '/// multiline comment.']);
-  });
+      expect(
+        definition.fields.first.documentation,
+        ['/// This is a field comment.'],
+      );
+    },
+  );
 
   test(
-      'Given an enum with a multiline value documentation comment then the entity that is generated has the documentation set for that specific field.',
-      () {
-    var protocol = ProtocolSource(
-      '''
-enum: Example
-values:
-  ### This is a multiline
-  ### field comment.
-  - first
-''',
-      Uri(path: 'lib/src/protocol/example.yaml'),
-      ['lib', 'src', 'protocol'],
-    );
+    'Given a class with a multiline field documentation comment then the entity that is generated has the documentation set for that specific field.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+        class: Example
+        fields:
+          ### This is a multiline
+          ### field comment.
+          name: String
+        ''',
+        ).build(),
+      ];
 
-    var entities = SerializableEntityAnalyzer.extractEntityDefinition(protocol)
-        as EnumDefinition?;
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as ClassDefinition;
 
-    expect(entities?.values.first.documentation,
-        ['/// This is a multiline', '/// field comment.']);
-  });
+      expect(definition.fields.first.documentation,
+          ['/// This is a multiline', '/// field comment.']);
+    },
+  );
+
+  test(
+    'Given a class with multiple fields but only one has a documentation comment then the entity that is generated has the documentation set for that specific field.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+          class: Example
+          fields:
+            name: String
+            ### This is a multiline
+            ### field comment.
+            age: int
+          ''',
+        ).build(),
+      ];
+
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as ClassDefinition;
+
+      expect(definition.fields.first.documentation, null);
+      expect(
+        definition.fields.last.documentation,
+        ['/// This is a multiline', '/// field comment.'],
+      );
+    },
+  );
+
+  test(
+    'Given a class with a field with a normal comment, then the entity that is generated has no documentation set.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+          class: Example
+          fields:
+            # This is a normal comment.
+            name: String
+          ''',
+        ).build(),
+      ];
+
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as ClassDefinition;
+
+      expect(definition.fields.first.documentation, null);
+    },
+  );
+
+  test(
+    'Given an enum with a multiline class documentation comment then an entity with the class documentation set is generated.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+          ### This is a...
+          ### multiline comment.
+          enum: Example
+          values:
+            - first
+          ''',
+        ).build(),
+      ];
+
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as EnumDefinition;
+
+      expect(
+        definition.documentation,
+        ['/// This is a...', '/// multiline comment.'],
+      );
+    },
+  );
+
+  test(
+    'Given an enum with a multiline value documentation comment then the entity that is generated has the documentation set for that specific field.',
+    () {
+      var protocols = [
+        ProtocolSourceBuilder().withYaml(
+          '''
+          enum: Example
+          values:
+            ### This is a multiline
+            ### field comment.
+            - first
+          ''',
+        ).build(),
+      ];
+
+      StatefulAnalyzer analyzer = StatefulAnalyzer(protocols);
+      var definitions = analyzer.validateAll();
+      var definition = definitions.first as EnumDefinition;
+
+      expect(
+        definition.values.first.documentation,
+        ['/// This is a multiline', '/// field comment.'],
+      );
+    },
+  );
 }
