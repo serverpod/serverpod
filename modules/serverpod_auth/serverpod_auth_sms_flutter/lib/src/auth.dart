8f2eba7e1bf3c9c000f0f972e69729da71361d95
Faizan Pasha <faizanpasha265@gmail.com>
2023-09-13 15:58:04 +0530

    SMS Auth logic for the package

---
+++ modules/serverpod_auth/serverpod_auth_sms_flutter/lib/src/auth.dart
@@ -0,0 +1,51 @@
+import 'package:serverpod_auth_client/module.dart';
+import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
+
+/// Controller for SMS authentication.
+class SMSAuthController {
+  /// A reference to the [AuthModule] as retrieved from the client object.
+  final Caller caller;
+
+  /// Creates a new [SMSAuthController].
+  SMSAuthController(this.caller);
+
+  /// Attempts to sign in a user with SMS authentication. If successful, a
+  /// [UserInfo] is returned. If the user doesn't exist, a new user is created
+  /// and returned. If the attempt fails, null is returned.
+  Future<SmsOtpResponse> signInWithSMS({
+    required String phoneNumber,
+  }) async {
+    return await caller.sms.startAuthentication(phoneNumber);
+  }
+
+  /// Attempts to sign in a user with SMS authentication. If successful, a
+  /// [UserInfo] is returned. If the user doesn't exist, a new user is created
+  /// and returned. If the attempt fails, null is returned.
+  Future<AuthenticationResponse> verifySMS({
+    required String phoneNumber,
+    required String otp,
+    required String storedHash,
+  }) async {
+    var serverResponse = await caller.sms.verifyAuthentication(
+      phoneNumber,
+      otp,
+      storedHash,
+    );
+    if (!serverResponse.success ||
+        serverResponse.userInfo == null ||
+        serverResponse.keyId == null ||
+        serverResponse.key == null) {
+      return serverResponse;
+    }
+
+    // Authentication was successful, store the key.
+    var sessionManager = await SessionManager.instance;
+
+    sessionManager.registerSignedInUser(
+      serverResponse.userInfo!,
+      serverResponse.keyId!,
+      serverResponse.key!,
+    );
+    return serverResponse;
+  }
+}
