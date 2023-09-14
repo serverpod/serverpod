d0fd87181e964de34c594804c704d3aaa84856a9
Isakdl <isak.duner@gmail.com>
2023-09-14 10:02:22 +0200

    test: Add e2e tests validating error paths on attaching object relations.

--- tests/serverpod_test_server/test/entity_relations/relational_updates_test.dart
+++ tests/serverpod_test_server/test/entity_relations/relational_updates_test.dart
@@ -25,33 +25,15 @@ Future<void> _createTestDatabase(Client client) async {
 void main() {
   var client = Client(serverUrl);
 
-  group('Given a citizen ', () {
+  group('Given an address', () {
     late List<Citizen> citizens;
-    late List<Company> companies;
 
     setUp(() async {
       await _createTestDatabase(client);
       citizens = await client.relation.citizenFindAll();
-      companies = await client.relation.companyFindAll();
     });
 
-    //tearDown(() async => await client.relation.deleteAll());
-
-    group('group name', () {
-      test('test name', () async {
-        var citizen = citizens.first;
-        var company = companies.last;
-
-        await client.relation.citizenAttachCompany(citizen, company);
-
-        var alice = await client.relation.citizenFindByIdWithIncludes(
-          citizen.id!,
-        );
-
-        expect(alice?.companyId, company.id);
-      });
-    });
-
+    tearDown(() async => await client.relation.deleteAll());
     test(
         'when attaching an address from the foreign key side the object holding the foreign key is updated in the database',
         () async {
@@ -60,20 +42,6 @@ void main() {
       address.id = await client.relation.addressInsert(address);
 
       await client.relation.addressAttachCitizen(address, alice);
-
-      var updatedAddress = await client.relation.addressFindById(address.id!);
-
-      expect(updatedAddress?.inhabitantId, alice.id);
-    });
-
-    test(
-        'when attaching an address from the none foreign key side the object holding the foreign key is updated in the database',
-        () async {
-      var alice = citizens.first;
-      var address = Address(street: 'Street');
-      address.id = await client.relation.addressInsert(address);
-
-      await client.relation.citizenAttachAddress(alice, address);
 
       var updatedAddress = await client.relation.addressFindById(address.id!);
 
@@ -94,6 +62,94 @@ void main() {
     });
 
     test(
+        'when attaching an address with modified values only the foreign key field is modified.',
+        () async {
+      var alice = citizens.first;
+      var address = Address(street: 'Street');
+      address.id = await client.relation.addressInsert(address);
+
+      var copy = address.copyWith(street: 'New street');
+
+      await client.relation.addressAttachCitizen(copy, alice);
+
+      var updatedAddress = await client.relation.addressFindById(address.id!);
+
+      expect(updatedAddress?.street, 'Street');
+    });
+
+    test(
+        'when trying to attach a citizen to an address that is not stored in the database then an exception is thrown',
+        () async {
+      var alice = citizens.first;
+      var address = Address(street: 'Street');
+
+      try {
+        await client.relation.addressAttachCitizen(address, alice);
+        fail('Expected an exception to be thrown');
+      } catch (e) {
+        // TODO: check real exception when we support testing on the server
+        expect(e.runtimeType, ServerpodClientException);
+      }
+    });
+
+    test(
+        'when trying to attach a citizen that is not stored in the db to an address then an exception is thrown',
+        () async {
+      var carol = Citizen(name: 'Carol', companyId: 0);
+      var address = Address(street: 'Street');
+      address.id = await client.relation.addressInsert(address);
+
+      try {
+        await client.relation.addressAttachCitizen(address, carol);
+        fail('Expected an exception to be thrown');
+      } catch (e) {
+        // TODO: check real exception when we support testing on the server
+        expect(e.runtimeType, ServerpodClientException);
+      }
+    });
+
+    test(
+        'when trying to detach a citizen from an address that is not stored in the database then an exception is thrown',
+        () async {
+      var address = Address(street: 'Street');
+
+      try {
+        await client.relation.addressDetachCitizen(address);
+        fail('Expected an exception to be thrown');
+      } catch (e) {
+        // TODO: check real exception when we support testing on the server
+        expect(e.runtimeType, ServerpodClientException);
+      }
+    });
+  });
+
+  group('Given a citizen ', () {
+    late List<Citizen> citizens;
+    late List<Company> companies;
+
+    setUp(() async {
+      await _createTestDatabase(client);
+      citizens = await client.relation.citizenFindAll();
+      companies = await client.relation.companyFindAll();
+    });
+
+    tearDown(() async => await client.relation.deleteAll());
+
+    test(
+        'when attaching an address from the none foreign key side the object holding the foreign key is updated in the database',
+        () async {
+      var alice = citizens.first;
+      var address = Address(street: 'Street');
+      address.id = await client.relation.addressInsert(address);
+
+      await client.relation.citizenAttachAddress(alice, address);
+
+      var updatedAddress = await client.relation.addressFindById(address.id!);
+
+      expect(updatedAddress?.inhabitantId, alice.id);
+    });
+
+    test(
         'when detaching an address from the none foreign key side the object holding the foreign key has the foreign key set to null.',
         () async {
       var bob = citizens.last;
@@ -108,6 +164,83 @@ void main() {
       var updatedAddress = await client.relation.addressFindById(address.id!);
 
       expect(updatedAddress?.inhabitantId, null);
+    });
+
+    test(
+        'when attaching to an object that already have an entry then the new value is set in the database',
+        () async {
+      var citizen = citizens.first;
+      var company = companies.last;
+
+      await client.relation.citizenAttachCompany(citizen, company);
+
+      var alice = await client.relation.citizenFindByIdWithIncludes(
+        citizen.id!,
+      );
+
+      expect(alice?.companyId, company.id);
+    });
+
+    test(
+        'when trying to attach a citizen to an address that is not stored in the database then an exception is thrown',
+        () async {
+      var alice = citizens.first;
+      var address = Address(street: 'Street');
+
+      try {
+        await client.relation.citizenAttachAddress(alice, address);
+        fail('Expected an exception to be thrown');
+      } catch (e) {
+        // TODO: check real exception when we support testing on the server
+        expect(e.runtimeType, ServerpodClientException);
+      }
+    });
+
+    test(
+        'when trying to attach a citizen that is not stored in the db to an address then an exception is thrown',
+        () async {
+      var carol = Citizen(name: 'Carol', companyId: 0);
+      var address = Address(street: 'Street');
+      address.id = await client.relation.addressInsert(address);
+
+      try {
+        await client.relation.citizenAttachAddress(carol, address);
+        fail('Expected an exception to be thrown');
+      } catch (e) {
+        // TODO: check real exception when we support testing on the server
+        expect(e.runtimeType, ServerpodClientException);
+      }
+    });
+
+    test(
+        'when trying to detach an address from a citizen that is not stored in the database then an exception is thrown',
+        () async {
+      var address = Address(street: 'Street');
+      address.id = await client.relation.addressInsert(address);
+
+      var carol = Citizen(name: 'Carol', companyId: 0, address: address);
+
+      try {
+        await client.relation.citizenDetachAddress(carol);
+        fail('Expected an exception to be thrown');
+      } catch (e) {
+        // TODO: check real exception when we support testing on the server
+        expect(e.runtimeType, ServerpodClientException);
+      }
+    });
+
+    test(
+        'when trying to detach an address from a citizen that has no address in the passed object then an exception is thrown',
+        () async {
+      var alice = citizens.first;
+
+      try {
+        await client.relation.citizenDetachAddress(alice);
+        fail('Expected an exception to be thrown');
+      } catch (e) {
+        // TODO: check real exception when we support testing on the server
+        expect(e.runtimeType, ServerpodClientException);
+      }
     });
   });
 }
