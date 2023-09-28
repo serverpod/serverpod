8b67419572d2d0128998ce3355d48f613539cf93
Alexander Sandor <alexander@serverpod.dev>
2023-09-28 14:17:10 +0200

    test: add many_to_many test endpoint

---
+++ tests/serverpod_test_server/lib/src/endpoints/many_to_many.dart
@@ -0,0 +1,41 @@
+import 'package:serverpod/server.dart';
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_test_server/src/generated/protocol.dart';
+
+class ManyToManyEndpoint extends Endpoint {
+  Future<List<Posts>> findAllNonBlockedPosts(
+    Session session, {
+    required Author reader,
+  }) async {
+    return await Posts.find(
+      session,
+      where: (t) => t.author.blockedBy((b) => b.blockeeId.notEquals(reader.id)),
+    );
+  }
+
+  Future<int?> postInsert(Session session, Posts post) async {
+    await Posts.insert(session, post);
+    return post.id;
+  }
+
+  Future<int?> authorInsert(Session session, Author author) async {
+    await Author.insert(session, author);
+    return author.id;
+  }
+
+  Future<int?> blockedInsert(Session session, Blocked blocked) async {
+    await Blocked.insert(session, blocked);
+    return blocked.id;
+  }
+
+  Future<int> deleteAll(Session session) async {
+    var postDeletions =
+        await Posts.delete(session, where: (_) => Constant.bool(true));
+    var authorDeletions =
+        await Author.delete(session, where: (_) => Constant.bool(true));
+    var blockedDeletions =
+        await Blocked.delete(session, where: (_) => Constant.bool(true));
+
+    return postDeletions + authorDeletions + blockedDeletions;
+  }
+}
