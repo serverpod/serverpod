d2f5adeadc9735701c49636ce4180d45c964e307
Alexander Sandor <alexander@serverpod.dev>
2023-10-03 15:48:09 +0200

    fix: store each step separate for a table relation.

--- packages/serverpod/lib/src/database/table_relation.dart
+++ packages/serverpod/lib/src/database/table_relation.dart
@@ -5,41 +5,97 @@ import 'package:serverpod/src/database/columns.dart';
 /// This is typically only used internally by the serverpod framework.
 @internal
 class TableRelation {
-  /// Name of table.
-  final String tableName;
-
-  /// Foreign table relation column.
-  final Column foreignTableColumn;
-
-  /// Relation column.
-  final String column;
-
-  /// Query prefix for the relation.
-  final String queryPrefix;
-
-  /// Retrieves the name of the table with the query prefix applied.
-  String get tableNameWithQueryPrefix => '$queryPrefix$tableName';
-
-  /// Creates a new [TableRelation] from the perspective of the foreign table.
-  factory TableRelation.foreign({
-    required String foreignTableName,
-    required Column column,
-    required String foreignColumnName,
-    required String relationQueryPrefix,
-  }) {
-    return TableRelation(
-      tableName: foreignTableName,
-      foreignTableColumn: column,
-      column: '$relationQueryPrefix$foreignTableName."$foreignColumnName"',
-      queryPrefix: relationQueryPrefix,
-    );
-  }
+  /// Records the relationship between multiple tables.
+  /// Order is important, as it determines the order of joins in the query.
+  final List<TableRelationEntry> _tableRelationEntries;
 
   /// Creates a new [TableRelation].
-  const TableRelation({
-    required this.tableName,
-    required this.foreignTableColumn,
-    required this.column,
-    required this.queryPrefix,
+  /// Throws [ArgumentError] if [tableRelationEntries] is empty.
+  TableRelation(this._tableRelationEntries) {
+    if (_tableRelationEntries.isEmpty) {
+      throw ArgumentError('TableRelation must have at least one entry.');
+    }
+  }
+
+  /// Builds all table relations required to join the tables.
+  List<TableRelation> get getRelations {
+    List<TableRelation> relations = [];
+    for (var i = 0; i < _tableRelationEntries.length; i++) {
+      relations.add(TableRelation(_tableRelationEntries.sublist(0, i + 1)));
+    }
+
+    return relations;
+  }
+
+  /// Name of the table to be joined.
+  String get lastForeignTableName {
+    return _tableRelationEntries.last.foreignField.table.tableName;
+  }
+
+  /// Name of the last field to be joined.
+  String get lastJoiningField {
+    return '${_fromRelationQueryAlias()}."${_tableRelationEntries.last.field.columnName}"';
+  }
+
+  /// Name of the last foreign field to be joined.
+  String get lastJoiningForeignField {
+    return '${_buildRelationQueryAlias()}."${_tableRelationEntries.last.foreignField.columnName}"';
+  }
+
+  /// Creates a new [TableRelation] from [this] and [relation].
+  TableRelation copyAndAppend(TableRelationEntry relation) {
+    return TableRelation([..._tableRelationEntries, relation]);
+  }
+
+  /// Retrieves the name of the table with the query prefix applied.
+  String get relationQueryAlias => _buildRelationQueryAlias();
+
+  /// Builds the relation query alias including [TableRelationEntries]
+  /// up until [end] index.
+  ///
+  /// If [end] is larger than the number of relations, all relations are used.
+  String _buildRelationQueryAlias([int? end]) {
+    if (end != null && end > _tableRelationEntries.length) {
+      end = _tableRelationEntries.length;
+    }
+
+    var prefix = '';
+    if (_tableRelationEntries.isEmpty) {
+      return prefix;
+    }
+
+    prefix = _tableRelationEntries.first.field.table.tableName;
+
+    for (var relation in _tableRelationEntries.sublist(0, end)) {
+      prefix +=
+          '_${relation.relationFieldName}_${relation.foreignField.table.tableName}';
+    }
+
+    return prefix;
+  }
+
+  String _fromRelationQueryAlias() {
+    return _buildRelationQueryAlias(_tableRelationEntries.length - 1);
+  }
+}
+
+/// Entry for recording a relation between two tables.
+/// This is typically only used internally by the serverpod framework.
+@internal
+class TableRelationEntry {
+  /// Name of relation field.
+  final String relationFieldName;
+
+  /// Column field to join on.
+  final Column field;
+
+  /// Foreign Column field to join to.
+  final Column foreignField;
+
+  /// Creates a new [TableRelationEntry].
+  TableRelationEntry({
+    required this.relationFieldName,
+    required this.field,
+    required this.foreignField,
   });
 }
---
+++ packages/serverpod/test/database/table_relation_test.dart
@@ -0,0 +1,157 @@
+import 'package:serverpod/src/database/columns.dart';
+import 'package:serverpod/src/database/expressions.dart';
+import 'package:serverpod/src/database/table_relation.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Given empty list when trying to construct TableRelation then throws',
+      () {
+    expect(
+        () => TableRelation([]),
+        throwsA(isA<ArgumentError>().having(
+          (e) => e.toString(),
+          'message',
+          equals(
+              'Invalid argument(s): TableRelation must have at least one entry.'),
+        )));
+  });
+
+  group('Given table relation with relation definition', () {
+    var table = Table(tableName: 'company');
+    var foreignTable = Table(tableName: 'citizen');
+    var tableRelationEntries = [
+      TableRelationEntry(
+        relationFieldName: 'ceo',
+        field: ColumnInt('ceoId', table),
+        foreignField: ColumnInt('id', foreignTable),
+      ),
+    ];
+    TableRelation tableRelation = TableRelation(tableRelationEntries);
+
+    test(
+        'when buildRelationQueryAlias is called then query prefix is built correctly.',
+        () {
+      expect(
+        tableRelation.relationQueryAlias,
+        'company_ceo_citizen',
+      );
+    });
+
+    group('when getRelations is called', () {
+      test('then returns list with one element.', () {
+        expect(tableRelation.getRelations.length, 1);
+      });
+
+      test(
+          'then last element builds same buildRelationQueryAlias as outer element.',
+          () {
+        expect(tableRelation.getRelations.last.relationQueryAlias,
+            tableRelation.relationQueryAlias);
+      });
+    });
+
+    test('when lastForeignTableName is called then last table is returned.',
+        () {
+      expect(tableRelation.lastForeignTableName, 'citizen');
+    });
+
+    test(
+        'when lastJoiningField is called then last joining field name is returned.',
+        () {
+      expect(tableRelation.lastJoiningField, 'company."ceoId"');
+    });
+
+    test('when lastJoiningForeignField is called then last foreign field name.',
+        () {
+      expect(tableRelation.lastJoiningForeignField, 'company_ceo_citizen."id"');
+    });
+
+    group('when using copyAndAppend to create new table relation ', () {
+      var table = Table(tableName: 'citizen');
+      var foreignTable = Table(tableName: 'restaurant');
+      var newTableRelation = tableRelation.copyAndAppend(
+        TableRelationEntry(
+          relationFieldName: 'favoriteRestaurant',
+          field: ColumnInt('favoriteRestaurantId', table),
+          foreignField: ColumnInt('id', foreignTable),
+        ),
+      );
+      test('then new table relation still starts with base table name.', () {
+        expect(
+          newTableRelation.relationQueryAlias,
+          startsWith('company'),
+        );
+      });
+
+      test('then new table relation builds query prefix correctly.', () {
+        expect(
+          newTableRelation.relationQueryAlias,
+          'company_ceo_citizen_favoriteRestaurant_restaurant',
+        );
+      });
+    });
+  });
+
+  group('Given table relation with multiple relation definitions', () {
+    var companyTable = Table(tableName: 'company');
+    var citizenTable = Table(tableName: 'citizen');
+    var restaurantTable = Table(tableName: 'restaurant');
+    var tableRelationEntries = [
+      TableRelationEntry(
+        relationFieldName: 'ceo',
+        field: ColumnInt('ceoId', companyTable),
+        foreignField: ColumnInt('id', citizenTable),
+      ),
+      TableRelationEntry(
+        relationFieldName: 'favoriteRestaurant',
+        field: ColumnInt('favoriteRestaurantId', citizenTable),
+        foreignField: ColumnInt('id', restaurantTable),
+      ),
+    ];
+    TableRelation tableRelation = TableRelation(tableRelationEntries);
+
+    test(
+        'when buildRelationQueryAlias is called then query prefix is built correctly.',
+        () {
+      expect(
+        tableRelation.relationQueryAlias,
+        'company_ceo_citizen_favoriteRestaurant_restaurant',
+      );
+    });
+
+    group('when getRelations is called', () {
+      test(
+          'then returns list with one entry for each table relation definition.',
+          () {
+        expect(
+          tableRelation.getRelations,
+          hasLength(tableRelationEntries.length),
+        );
+      });
+
+      test(
+          'then last element builds same buildRelationQueryAlias as outer element.',
+          () {
+        expect(tableRelation.getRelations.last.relationQueryAlias,
+            tableRelation.relationQueryAlias);
+      });
+    });
+    test('when lastForeignTableName is called then last table is returned.',
+        () {
+      expect(tableRelation.lastForeignTableName, 'restaurant');
+    });
+
+    test(
+        'when lastJoiningField is called then last joining field name is returned.',
+        () {
+      expect(tableRelation.lastJoiningField,
+          'company_ceo_citizen."favoriteRestaurantId"');
+    });
+
+    test('when lastJoiningForeignField is called then last foreign field name.',
+        () {
+      expect(tableRelation.lastJoiningForeignField,
+          'company_ceo_citizen_favoriteRestaurant_restaurant."id"');
+    });
+  });
+}
