a1afbc61a1019533372f5cf8c547d4d514c82b30
Alexander Sandor <alexander@serverpod.dev>
2023-10-05 17:38:11 +0200

    fix: Introduce the many relation class and expose it.

--- packages/serverpod/lib/database.dart
+++ packages/serverpod/lib/database.dart
@@ -1,7 +1,8 @@
 library database;
 
+export 'src/database/columns.dart';
 export 'src/database/database_connection.dart';
 export 'src/database/database_pool_manager.dart';
 export 'src/database/expressions.dart';
-export 'src/database/columns.dart';
+export 'src/database/many_relation.dart';
 export 'src/database/table.dart';
