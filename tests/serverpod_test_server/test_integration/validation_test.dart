f65034acff4fae08b6c21125218e54b530d98640
Alexander Sandor <alexander@serverpod.dev>
2023-10-11 15:21:24 +0200

    test: remove prints

--- tests/serverpod_test_server/test_integration/validation_test.dart
+++ tests/serverpod_test_server/test_integration/validation_test.dart
@@ -3,18 +3,13 @@ import 'package:serverpod_test_server/test_util/test_serverpod.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  print('Starting server');
   var session = await IntegrationTestServer().session();
 
   test('passing db call test', () async {
-    print('Db call');
     var customers = await Customer.db.find(
       session,
     );
 
-    print(customers);
-
-    print('Should exit');
     expect(customers, []);
   });
 }
