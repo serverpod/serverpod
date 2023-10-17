a5961452b8ddd2faa4113a70028a6f292d55914f
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2023-10-17 12:29:23 +0630

    test: added test for headers response

--- tests/serverpod_test_server/test_e2e/headers_test.dart
+++ tests/serverpod_test_server/test_e2e/headers_test.dart
@@ -6,24 +6,55 @@ import 'package:test/test.dart';
 import 'config.dart';
 
 void main() {
-  test('Calling endpoint should return server default headers', () async {
-    var result = await http.post(Uri.parse('${serverUrl}simple/hello'),
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: jsonEncode({'name': 'mike'}));
+  group('Calling the endpoint should return the expected default headers', () {
+    var uri = Uri.parse('${serverUrl}simple/hello');
 
-    expect(
-      result.headers['access-control-allow-origin'],
-      equals("*"),
-    );
-    expect(
-      result.headers['access-control-allow-headers'],
-      equals("Content-Type"),
-    );
-    expect(
-      result.headers['access-control-allow-methods'],
-      equals("POST"),
-    );
+    test('When http method is POST', () async {
+      var result = await http.post(uri,
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: jsonEncode({'name': 'mike'}));
+
+      expect(
+        result.headers['access-control-allow-origin'],
+        equals("http://localhost:8082"),
+      );
+      expect(
+          result.headers.containsKey('access-control-allow-headers'), isFalse);
+      expect(
+        result.headers['access-control-allow-methods'],
+        equals("POST"),
+      );
+    });
+
+    test('When http method is OPTIONS', () async {
+      final client = http.Client();
+      final method = 'OPTIONS';
+
+      final request = http.Request(method, uri);
+      request.headers.addAll({
+        'content-type': 'application/json',
+        'X-Forwarded-For': '192.168.43.1'
+      });
+      final streamedResponse = await client.send(request);
+      final response = await http.Response.fromStream(streamedResponse);
+      print(response.statusCode);
+
+      expect(
+        response.headers['access-control-allow-origin'],
+        equals("http://localhost:8082"),
+      );
+      expect(
+        response.headers['content-length'],
+        equals('0'),
+      );
+      expect(
+          response.headers.containsKey('access-control-allow-headers'), isTrue);
+      expect(
+        response.headers['access-control-allow-methods'],
+        equals("POST"),
+      );
+    });
   });
 }
