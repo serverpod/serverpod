27c5321ca518fabdef58c214f8d3a8bfadeeb726
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2023-10-22 21:52:34 +0630

    test: update tests.

--- tools/serverpod_cli/test/generator/open_api/utils_test.dart
+++ tools/serverpod_cli/test/generator/open_api/utils_test.dart
@@ -16,40 +16,72 @@ void main() {
       expect('/api/v1', getExtraPath(subDirs));
     });
   });
-  group('Given a set of TypeDefinitions', () {
-    test(
-        'When converting TypeDefinition to OpenAPISchemaType then all are converted as excepted.',
+  group('Convert TypeDefinition to OpenAPISchemaType.', () {
+    test('Given a String when converting it then get OpenAPISchemaType.string.',
         () {
       expect(
         stringType.toOpenAPISchemaType,
         OpenAPISchemaType.string,
       );
+    });
+    test('Given an int when converting it then get OpenAPISchemaType.integer.',
+        () {
       expect(
         intType.toOpenAPISchemaType,
         OpenAPISchemaType.integer,
       );
+    });
+    test('Given a bool when converting it then get OpenAPISchemaType.boolean.',
+        () {
       expect(
         boolType.toOpenAPISchemaType,
         OpenAPISchemaType.boolean,
       );
-      expect(
-        boolType.toOpenAPISchemaType,
-        OpenAPISchemaType.boolean,
-      );
+    });
+    test('Given a double when converting it then get OpenAPISchemaType.number.',
+        () {
       expect(
         doubleType.toOpenAPISchemaType,
         OpenAPISchemaType.number,
       );
+    });
+    test('Given a BigInt when converting it then get OpenAPISchemaType.number.',
+        () {
+      expect(
+        bigIntType.toOpenAPISchemaType,
+        OpenAPISchemaType.number,
+      );
+    });
+
+    test('Given a List when converting it then get OpenAPISchemaType.array.',
+        () {
       expect(
         listBuilder.build().toOpenAPISchemaType,
         OpenAPISchemaType.array,
       );
+    });
+
+    test('Given a Map when converting it then get OpenAPISchemaType.object.',
+        () {
       expect(
         mapBuilder.build().toOpenAPISchemaType,
         OpenAPISchemaType.object,
       );
+    });
+
+    test(
+        'Given an Example class when converting it then get OpenAPISchemaType.serializableObjects.',
+        () {
       expect(
         exampleType.toOpenAPISchemaType,
+        OpenAPISchemaType.serializableObjects,
+      );
+    });
+    test(
+        'Given a dynamic type when converting it then get OpenAPISchemaType.serializableObjects.',
+        () {
+      expect(
+        dynamicType.toOpenAPISchemaType,
         OpenAPISchemaType.serializableObjects,
       );
     });
