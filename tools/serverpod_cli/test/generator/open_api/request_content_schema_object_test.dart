a7c06896bce2c0ffa1be9576e6503adba458e163
YeLinAung <40236753+b14cknc0d3@users.noreply.github.com>
2023-10-22 21:16:26 +0630

    test: refactor tests.

--- tools/serverpod_cli/test/generator/open_api/request_content_schema_object_test.dart
+++ tools/serverpod_cli/test/generator/open_api/request_content_schema_object_test.dart
@@ -5,137 +5,141 @@ import 'package:test/test.dart';
 import 'test_data_factory.dart';
 
 void main() {
-  test(
-      'Given (int id and string name) when converting OpenAPIRequestContentSchema to json then the type is set to object and properties contains id(integer) and name(string).',
-      () {
-    OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
-      params: [
-        ParameterDefinition(name: 'id', type: intType, required: true),
-        ParameterDefinition(name: 'name', type: stringType, required: true),
-      ],
-    );
+  group('Serializes OpenAPIRequestContentSchema.', () {
+    test(
+        'Given the parameters (int id and string name) when converting to json then the type is set to object and properties contains id (integer) and name (string).',
+        () {
+      OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
+        params: [
+          ParameterDefinition(name: 'id', type: intType, required: true),
+          ParameterDefinition(name: 'name', type: stringType, required: true),
+        ],
+      );
 
-    expect(
-      object.toJson(),
-      {
-        'type': 'object',
-        'properties': {
-          'id': {
-            'type': 'integer',
-          },
-          'name': {
-            'type': 'string',
+      expect(
+        object.toJson(),
+        {
+          'type': 'object',
+          'properties': {
+            'id': {
+              'type': 'integer',
+            },
+            'name': {
+              'type': 'string',
+            }
           }
-        }
-      },
-    );
-  });
-  test(
-      'Given (Example example) when converting OpenAPIRequestContentSchema to json then the type is set to object and properties contains example which references Example.',
-      () {
-    OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
-      params: [
-        ParameterDefinition(name: 'example', type: exampleType, required: true),
-      ],
-    );
+        },
+      );
+    });
+    test(
+        'Given the parameters (Example example) when converting to json then the type is set to object and properties contains example which references Example.',
+        () {
+      OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
+        params: [
+          ParameterDefinition(
+              name: 'example', type: exampleType, required: true),
+        ],
+      );
 
-    expect(
-      object.toJson(),
-      {
-        'type': 'object',
-        'properties': {
-          'example': {
-            '\$ref': '#/components/schemas/Example',
-          }
-        }
-      },
-    );
-  });
-  test(
-      'Given (int id,Example example) when converting OpenAPIRequestContentSchema to json then the type is set to object and properties contains  id(integer) and example which references Example.',
-      () {
-    OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
-      params: [
-        ParameterDefinition(name: 'id', type: intType, required: true),
-        ParameterDefinition(name: 'example', type: exampleType, required: true),
-      ],
-    );
-
-    expect(
-      object.toJson(),
-      {
-        'type': 'object',
-        'properties': {
-          'id': {
-            'type': 'integer',
-          },
-          'example': {
-            '\$ref': '#/components/schemas/Example',
-          }
-        }
-      },
-    );
-  });
-
-  test(
-      'Given (int id,List<Example> examples) when converting OpenAPIRequestContentSchema to json then the type is set to object and properties contains  id (integer) and examples (array) which references Example',
-      () {
-    OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
-      params: [
-        ParameterDefinition(name: 'id', type: intType, required: true),
-        ParameterDefinition(
-            name: 'examples',
-            type: listBuilder.withGenerics([exampleType]).build(),
-            required: true),
-      ],
-    );
-
-    expect(
-      object.toJson(),
-      {
-        'type': 'object',
-        'properties': {
-          'id': {
-            'type': 'integer',
-          },
-          'examples': {
-            'type': 'array',
-            'items': {
+      expect(
+        object.toJson(),
+        {
+          'type': 'object',
+          'properties': {
+            'example': {
               '\$ref': '#/components/schemas/Example',
             }
           }
-        }
-      },
-    );
-  });
+        },
+      );
+    });
+    test(
+        'Given the parameters (int id,Example example) when converting to json then the type is set to object and properties contains  id(integer) and example which references Example.',
+        () {
+      OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
+        params: [
+          ParameterDefinition(name: 'id', type: intType, required: true),
+          ParameterDefinition(
+              name: 'example', type: exampleType, required: true),
+        ],
+      );
 
-  test(
-      'Given  (int id,Map<String,dynamic> map) when converting OpenAPIRequestContentSchema to json then the type is set to object and properties contains  id (integer) and map with additionalProperties which references AnyValue.',
-      () {
-    OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
-      params: [
-        ParameterDefinition(name: 'id', type: intType, required: true),
-        ParameterDefinition(
-            name: 'map',
-            type: mapBuilder.withGenerics([stringType, dynamicType]).build(),
-            required: true),
-      ],
-    );
-
-    expect(
-      object.toJson(),
-      {
-        'type': 'object',
-        'properties': {
-          'id': {
-            'type': 'integer',
-          },
-          'map': {
-            'type': 'object',
-            'additionalProperties': {'\$ref': '#/components/schemas/AnyValue'}
+      expect(
+        object.toJson(),
+        {
+          'type': 'object',
+          'properties': {
+            'id': {
+              'type': 'integer',
+            },
+            'example': {
+              '\$ref': '#/components/schemas/Example',
+            }
           }
-        }
-      },
-    );
+        },
+      );
+    });
+
+    test(
+        'Given the parameters (int id,List<Example> examples) when converting to json then the type is set to object and properties contains  id (integer) and examples (array) which references Example',
+        () {
+      OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
+        params: [
+          ParameterDefinition(name: 'id', type: intType, required: true),
+          ParameterDefinition(
+              name: 'examples',
+              type: listBuilder.withGenerics([exampleType]).build(),
+              required: true),
+        ],
+      );
+
+      expect(
+        object.toJson(),
+        {
+          'type': 'object',
+          'properties': {
+            'id': {
+              'type': 'integer',
+            },
+            'examples': {
+              'type': 'array',
+              'items': {
+                '\$ref': '#/components/schemas/Example',
+              }
+            }
+          }
+        },
+      );
+    });
+
+    test(
+        'Given the parameters (int id,Map<String,dynamic> map) when converting to json then the type is set to object and properties contains id (integer) and map with additionalProperties which references AnyValue.',
+        () {
+      OpenAPIRequestContentSchema object = OpenAPIRequestContentSchema(
+        params: [
+          ParameterDefinition(name: 'id', type: intType, required: true),
+          ParameterDefinition(
+              name: 'map',
+              type: mapBuilder.withGenerics([stringType, dynamicType]).build(),
+              required: true),
+        ],
+      );
+
+      expect(
+        object.toJson(),
+        {
+          'type': 'object',
+          'properties': {
+            'id': {
+              'type': 'integer',
+            },
+            'map': {
+              'type': 'object',
+              'additionalProperties': {'\$ref': '#/components/schemas/AnyValue'}
+            }
+          }
+        },
+      );
+    });
   });
 }
