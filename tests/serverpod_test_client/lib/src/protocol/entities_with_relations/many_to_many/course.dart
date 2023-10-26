4d998467825e70ce256cf6ca07851113ec7f86e9
Alexander Sandor <alexander@serverpod.dev>
2023-10-26 09:12:12 +0200

    generate: generate project.

---
+++ tests/serverpod_test_client/lib/src/protocol/entities_with_relations/many_to_many/course.dart
@@ -0,0 +1,88 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: implementation_imports
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod_client/serverpod_client.dart' as _i1;
+import '../../protocol.dart' as _i2;
+
+abstract class Course extends _i1.SerializableEntity {
+  Course._({
+    this.id,
+    required this.name,
+    this.enrollments,
+  });
+
+  factory Course({
+    int? id,
+    required String name,
+    List<_i2.Enrollment>? enrollments,
+  }) = _CourseImpl;
+
+  factory Course.fromJson(
+    Map<String, dynamic> jsonSerialization,
+    _i1.SerializationManager serializationManager,
+  ) {
+    return Course(
+      id: serializationManager.deserialize<int?>(jsonSerialization['id']),
+      name: serializationManager.deserialize<String>(jsonSerialization['name']),
+      enrollments: serializationManager
+          .deserialize<List<_i2.Enrollment>?>(jsonSerialization['enrollments']),
+    );
+  }
+
+  /// The database id, set if the object has been inserted into the
+  /// database or if it has been fetched from the database. Otherwise,
+  /// the id will be null.
+  int? id;
+
+  String name;
+
+  List<_i2.Enrollment>? enrollments;
+
+  Course copyWith({
+    int? id,
+    String? name,
+    List<_i2.Enrollment>? enrollments,
+  });
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'enrollments': enrollments,
+    };
+  }
+}
+
+class _Undefined {}
+
+class _CourseImpl extends Course {
+  _CourseImpl({
+    int? id,
+    required String name,
+    List<_i2.Enrollment>? enrollments,
+  }) : super._(
+          id: id,
+          name: name,
+          enrollments: enrollments,
+        );
+
+  @override
+  Course copyWith({
+    Object? id = _Undefined,
+    String? name,
+    Object? enrollments = _Undefined,
+  }) {
+    return Course(
+      id: id is int? ? id : this.id,
+      name: name ?? this.name,
+      enrollments: enrollments is List<_i2.Enrollment>?
+          ? enrollments
+          : this.enrollments?.clone(),
+    );
+  }
+}
