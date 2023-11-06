d5bec3aed7187f6ff92f31154e6b576165274b99
Alexander Sandor <alexander@serverpod.dev>
2023-11-06 18:22:11 +0100

    refactor: remove dead code.

--- tools/serverpod_cli/lib/src/generator/generator_database_definition.dart
+++ tools/serverpod_cli/lib/src/generator/generator_database_definition.dart
@@ -2,60 +2,18 @@ import 'dart:io';
 
 import 'package:serverpod_cli/analyzer.dart';
 import 'package:serverpod_cli/src/analyzer/entities/stateful_analyzer.dart';
-import 'package:serverpod_shared/serverpod_shared.dart';
 import 'package:serverpod_cli/src/database/create_definition.dart';
-import 'package:serverpod_cli/src/util/locate_modules.dart';
 import 'package:serverpod_cli/src/util/protocol_helper.dart';
 import 'package:serverpod_service_client/serverpod_service_client.dart';
 
 Future<DatabaseDefinition> generateDatabaseDefinition({
   required Directory directory,
   required int priority,
-  bool full = false,
 }) {
-  if (full) {
-    return _generateFullDatabaseDefinition(directory: directory);
-  } else {
-    return _generateSinglePackageDatabaseDefinition(
-      directory: directory,
-      priority: priority,
-    );
-  }
-}
-
-Future<DatabaseDefinition> _generateFullDatabaseDefinition({
-  required Directory directory,
-}) async {
-  // Find paths to all modules
-  var tableDefinitions = <TableDefinition>[];
-
-  var paths = await locateAllModulePaths(directory: directory);
-  for (var path in paths) {
-    var config = await GeneratorConfig.load(path.toFilePath());
-
-    if (config == null) {
-      continue;
-    }
-
-    var protocols =
-        await ProtocolHelper.loadProjectYamlProtocolsFromDisk(config);
-    var moduleDefinitions = StatefulAnalyzer(protocols).validateAll();
-
-    var moduleDatabaseDefinition = createDatabaseDefinitionFromEntities(
-      moduleDefinitions,
-    );
-    for (var table in moduleDatabaseDefinition.tables) {
-      table.module = config.name;
-    }
-    tableDefinitions.addAll(moduleDatabaseDefinition.tables);
-  }
-
-  var databaseDefinition = DatabaseDefinition(
-    tables: tableDefinitions,
-    migrationApiVersion: DatabaseConstants.migrationApiVersion,
+  return _generateSinglePackageDatabaseDefinition(
+    directory: directory,
+    priority: priority,
   );
-
-  return databaseDefinition;
 }
 
 Future<DatabaseDefinition> _generateSinglePackageDatabaseDefinition({
