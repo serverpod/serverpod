11ea3efb9c2bdaea36d347b3bc7d5724e4f148f1
Alexander Sandor <alexander@serverpod.dev>
2023-11-09 13:16:25 +0100

    fix: read latest migration from migration registry in serverpod server.

--- packages/serverpod/lib/src/database/migrations/migrations.dart
+++ packages/serverpod/lib/src/database/migrations/migrations.dart
@@ -1,28 +1,29 @@
 import 'dart:io';
 
 import 'package:path/path.dart' as path;
+import 'package:serverpod/protocol.dart';
+import 'package:serverpod_shared/serverpod_shared.dart';
 
 /// Shared methods for migration versioning.
 class MigrationVersions {
   /// Provides a list of all available migration versions of a module.
+  static const _fileNameRegistryJson = 'migration_registry.json';
+
+  /// Returns all migration versions available for the module.
   static List<String> listVersions({
     Directory? directory,
     required String module,
   }) {
     directory ??= defaultMigrationsDirectory;
     try {
-      var versionDir = Directory(path.join(directory.path, module));
+      var migrationRegistry = load(
+        migrationsDirectory: directory,
+        module: module,
+      );
 
-      var versions = <String>[];
-      var entities = versionDir.listSync();
-      for (var entity in entities) {
-        if (entity is Directory) {
-          versions.add(path.basename(entity.path));
-        }
-      }
-      versions.sort();
-      return versions;
-    } catch (e) {
+      return migrationRegistry?.migrations ?? [];
+    } on MigrationRegistryLoadException catch (_) {
+      // TODO: add error handling
       return [];
     }
   }
@@ -50,4 +51,36 @@ class MigrationVersions {
   /// Gets the default migrations directory.
   static Directory get defaultMigrationsDirectory =>
       Directory(path.join(Directory.current.path, 'migrations'));
+
+  /// Loads the registry from the migrations directory.
+  /// Returns null registry if no migration registry is found.
+  ///
+  /// Throws [MigrationRegistryLoadException] if the registry file is corrupt.
+  static DatabaseMigrationRegistry? load({
+    required Directory migrationsDirectory,
+    required String module,
+  }) {
+    var registryFile = File(path.join(
+      migrationsDirectory.path,
+      module,
+      _fileNameRegistryJson,
+    ));
+
+    if (!registryFile.existsSync()) {
+      return null;
+    }
+
+    var registryData = registryFile.readAsStringSync();
+
+    try {
+      var registry =
+          Protocol().decodeWithType(registryData) as DatabaseMigrationRegistry;
+      return registry;
+    } catch (e) {
+      throw MigrationRegistryLoadException(
+        exception: e.toString(),
+        directoryPath: migrationsDirectory.path,
+      );
+    }
+  }
 }
