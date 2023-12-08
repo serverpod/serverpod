8635d06142554261d6c1b89fec6edb2b8060d4d2
Isakdl <isak.duner@gmail.com>
2023-12-08 13:54:20 +0100

    docs: Add documentation comment to docker volume name var.

--- tools/serverpod_cli/lib/src/util/constants.dart
+++ tools/serverpod_cli/lib/src/util/constants.dart
@@ -1,4 +1,7 @@
 class SetupConstants {
+  /// The name of the docker volume used to store the database.
+  /// This name is a combination of the folder name the compose file is in
+  /// and the volume name defined in the compose file.
   static String dockerVolumeName(String projectName) =>
       '${projectName}_server_${projectName}_data';
 }
