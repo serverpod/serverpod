5ac8a90f85a63431a14d48987761732b2975d83d
Isakdl <isak.duner@gmail.com>
2024-01-03 14:32:51 +0100

    fix: remove debug log.

--- tools/serverpod_cli/lib/src/generator/serverpod_code_generator.dart
+++ tools/serverpod_cli/lib/src/generator/serverpod_code_generator.dart
@@ -143,7 +143,6 @@ Future<void> _removeOldFilesInPath(
     if (!keepPaths.contains(file.path)) {
       log.debug('Remove: $file');
       await file.delete();
-      log.debug('  - removed.');
     }
   }
 }
