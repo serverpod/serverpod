72df23a062cbc257de47f33d656b4d5a7f6b4704
Alexander Sandor <137198655+SandPod@users.noreply.github.com>
2024-01-08 10:33:32 +0100

    fix: Set release second release candidate version. (#1721)

--- tools/serverpod_cli/lib/src/generated/version.dart
+++ tools/serverpod_cli/lib/src/generated/version.dart
@@ -1,5 +1,5 @@
 // This file is generated. Do not modify, instead edit the files in the
 // templates/pubspecs directory.
 
-const templateVersion = '1.2.0-rc.1';
+const templateVersion = '1.2.0-rc.2';
 const productionMode = false;
