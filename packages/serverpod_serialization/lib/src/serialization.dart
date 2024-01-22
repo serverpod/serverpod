3a74d98156d57b725383a9a605bb69293928eae1
Isakdl <isak.duner@gmail.com>
2024-01-22 17:11:52 +0100

    refactor: Change serialization logic to work on extensions instead.

--- packages/serverpod_serialization/lib/src/serialization.dart
+++ packages/serverpod_serialization/lib/src/serialization.dart
@@ -152,29 +152,7 @@ abstract class SerializationManager {
   /// indentation.
   static String encode(Object? object, {bool formatted = false}) {
     // This is the only time [jsonEncode] should be used in the project.
-    return JsonEncoder.withIndent(
-      formatted ? '  ' : null,
-      (nonEncodable) {
-        //TODO: all the "dart native" types should be listed here
-        if (nonEncodable is DateTime) {
-          return nonEncodable.toUtc().toIso8601String();
-        } else if (nonEncodable is ByteData) {
-          return nonEncodable.base64encodedString();
-        } else if (nonEncodable is Duration) {
-          return nonEncodable.inMilliseconds;
-        } else if (nonEncodable is UuidValue) {
-          return nonEncodable.uuid;
-        } else if (nonEncodable is Map && nonEncodable.keyType != String) {
-          return nonEncodable.entries
-              .map((e) => {'k': e.key, 'v': e.value})
-              .toList();
-        } else {
-          return (nonEncodable as dynamic)?.toJson();
-        }
-      },
-    ).convert(
-      object,
-    );
+    return JsonEncoder.withIndent(formatted ? '  ' : null).convert(object);
   }
 
   /// Encode the provided [object] to a json-formatted [String], include class
@@ -186,6 +164,71 @@ abstract class SerializationManager {
   }
 }
 
-extension<K, V> on Map<K, V> {
-  Type get keyType => K;
+/// Expose toJson on DateTime
+extension DateTimeToJson on DateTime {
+  /// Returns a serialized version of the [DateTime] in UTC.
+  String toJson() => toUtc().toIso8601String();
+}
+
+/// Expose toJson on Duration
+extension DurationToJson on Duration {
+  /// Returns a serialized version of the [Duration] in milliseconds.
+  int toJson() => inMilliseconds;
+}
+
+/// Expose toJson on UuidValue
+extension UuidValueToJson on UuidValue {
+  /// Returns a serialized version of the [UuidValue] as a [String].
+  String toJson() => uuid;
+}
+
+/// Expose toJson on ByteData
+extension ByteDataToJson on ByteData {
+  /// Returns a serialized version of the [ByteData] as a base64 encoded
+  /// [String].
+  String toJson() => base64encodedString();
+}
+
+/// Expose toJson on Map
+extension MapToJson<K, V> on Map<K, V> {
+  Type get _keyType => K;
+
+  /// Returns a serialized version of the [Map] with keys and values serialized.
+  dynamic toJson({
+    dynamic Function(K)? keyToJson,
+    dynamic Function(V)? valueToJson,
+  }) {
+    if (_keyType == String && keyToJson == null && valueToJson == null) {
+      return this;
+    }
+
+    // This implementation is here to support the old decoder behavior
+    // this should not be needed if the decoder is updated to not look for a nested
+    // map with 'k' and 'v' keys. If that is done the return type can be changed
+    // to Map<dynamic, dynamic>.
+    if (_keyType != String) {
+      return entries.map((e) {
+        var serializedKey = keyToJson != null ? keyToJson(e.key) : e.key;
+        var serializedValue =
+            valueToJson != null ? valueToJson(e.value) : e.value;
+        return {'k': serializedKey, 'v': serializedValue};
+      }).toList();
+    }
+
+    return map((key, value) {
+      var serializedKey = keyToJson != null ? keyToJson(key) : key;
+      var serializedValue = valueToJson != null ? valueToJson(value) : value;
+      return MapEntry(serializedKey, serializedValue);
+    });
+  }
+}
+
+/// Expose toJson on List
+extension ListToJson<T> on List<T> {
+  /// Returns a serialized version of the [List] with values serialized.
+  List<dynamic> toJson({dynamic Function(T)? valueToJson}) {
+    if (valueToJson == null) return this;
+
+    return map<dynamic>(valueToJson).toList();
+  }
 }
