a39bfe136bd5fed2f7a6d6a5627f53478ae4270d
Viktor Lidholt <viktorlidholt@gmail.com>
2024-01-22 10:23:18 -0600

    chore: Bumbs version to 1.2.1, updates README and CHANGELOG. (#1798)

--- tools/serverpod_cli/lib/src/generated/version.dart
+++ tools/serverpod_cli/lib/src/generated/version.dart
@@ -1,5 +1,5 @@
 // This file is generated. Do not modify, instead edit the files in the
 // templates/pubspecs directory.
 
-const templateVersion = '1.2.1-rc.3';
+const templateVersion = '1.2.1';
 const productionMode = false;
