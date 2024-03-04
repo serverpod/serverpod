442c90b388fb56f0903ce86b6165daa5e05e2b0f
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2024-03-04 07:52:58 +0100

    Upgrade command added to serverpod_cli

---
+++ tools/serverpod_cli/lib/src/commands/upgrade.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:serverpod_cli/src/logger/logger.dart';
+import 'package:serverpod_cli/src/runner/serverpod_command.dart';
+
+import '../generated/version.dart';
+
+class UpgradeCommand extends ServerpodCommand {
+  @override
+  final name = 'upgrade';
+
+  @override
+  final description = 'Upgrade Serverpod to the latest version.';
+
+  @override
+  void run() async {
+    var success = await log.progress('Running `dart pub global activate serverpod_cli`...', () async {
+      var process = await Process.run('dart', ['pub global activate serverpod_cli']);
+      log.error('Something was wrong: ${process.stderr}');
+      return !process.exitCode.isNegative;
+    });
+
+    if (success) {
+      log.info('Serverpod is already up to date: $templateVersion version.');
+    }
+  }
+}
