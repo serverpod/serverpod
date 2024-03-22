60da820553c1a1450067e0217147646735bc0961
Alexander Sandor <137198655+SandPod@users.noreply.github.com>
2024-03-22 15:22:23 +0100

    fix: Add maxMigratedEntries option to the migration helper method. (Targeting 1.2) (#2035)

--- modules/serverpod_auth/serverpod_auth_server/test/password_hash/legacy_password_hash_test.dart
+++ modules/serverpod_auth/serverpod_auth_server/test/password_hash/legacy_password_hash_test.dart
@@ -3,66 +3,66 @@ import 'package:test/test.dart';
 
 void main() {
   group('Given password', () {
-    test('when generating hash then hash matches regression value', () {
+    test('when generating hash then hash matches regression value', () async {
       // This regression hash value was derived from the old serverpod hash
       // generation algorithm.
       var expectedHash =
           '0713234b3cb6a6f98f6978f17a55a54578c580698dc1d56371502be6abb457eb';
 
       var actualHash =
-          PasswordHash.legacyHash('hunter2', 'serverpod password salt');
+          await PasswordHash.legacyHash('hunter2', 'serverpod password salt');
 
       expect(actualHash, expectedHash);
     });
 
     test('when validating with correct password then validator returns true',
-        () {
+        () async {
       var salt = 'serverpod password salt';
       var password = 'hunter2';
 
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(password, salt),
+        await PasswordHash.legacyHash(password, salt),
         legacySalt: salt,
       );
 
-      expect(passwordHash.validate(password), isTrue);
+      expect(await passwordHash.validate(password), isTrue);
     });
 
     test('when validating with incorrect password then validator returns false',
-        () {
+        () async {
       var salt = 'serverpod password salt';
 
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash('hunter2', salt),
+        await PasswordHash.legacyHash('hunter2', salt),
         legacySalt: salt,
       );
 
-      expect(passwordHash.validate('chaser1'), isFalse);
+      expect(await passwordHash.validate('chaser1'), isFalse);
     });
 
     test('when validating with different salts then validator returns false',
-        () {
+        () async {
       var password = 'hunter2';
 
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(password, 'first salt'),
+        await PasswordHash.legacyHash(password, 'first salt'),
         legacySalt: 'second salt',
       );
 
-      expect(passwordHash.validate(password), isFalse);
+      expect(await passwordHash.validate(password), isFalse);
     });
   });
 
   group('Given password and extra salt email', () {
     test(
         'when generating hash with email provided then result matches regression value',
-        () {
+        () async {
       // This regression hash value was derived from the old serverpod hash
       // generation algorithm.
       var expectedHash =
           '4005303e6e1effd8bc7293bc3e69fe3a480a88249453aef7f58fdcf264419147';
 
-      var actualHash = PasswordHash.legacyHash(
+      var actualHash = await PasswordHash.legacyHash(
         'hunter2',
         'serverpod password salt',
         email: 'test@serverpod.dev',
@@ -74,10 +74,10 @@ void main() {
 
   group('Given password hash', () {
     test('when checking if hash should be updated then no update is needed.',
-        () {
+        () async {
       var salt = 'saltySalt';
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(
+        await PasswordHash.legacyHash(
           'hunter2',
           salt,
         ),
@@ -87,10 +87,11 @@ void main() {
       expect(passwordHash.shouldUpdateHash(), isTrue);
     });
 
-    test('when checking if hash is legacy hash then method returns true.', () {
+    test('when checking if hash is legacy hash then method returns true.',
+        () async {
       var salt = 'saltySalt';
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(
+        await PasswordHash.legacyHash(
           'hunter2',
           salt,
         ),
@@ -100,13 +101,14 @@ void main() {
       expect(passwordHash.isLegacyHash(), isTrue);
     });
 
-    test('when matching with correct password then it evaluates to true', () {
+    test('when matching with correct password then it evaluates to true',
+        () async {
       var salt = 'serverpod password salt';
       var password = 'hunter2';
       var email = 'test@serverpod.dev';
 
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(
+        await PasswordHash.legacyHash(
           password,
           salt,
           email: email,
@@ -115,16 +117,16 @@ void main() {
         legacyEmail: email,
       );
 
-      expect(passwordHash.validate(password), isTrue);
+      expect(await passwordHash.validate(password), isTrue);
     });
 
     test('when matching with incorrect password then it evaluates to false',
-        () {
+        () async {
       var salt = 'serverpod password salt';
       var email = 'test@serverpod.dev';
 
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(
+        await PasswordHash.legacyHash(
           'hunter2',
           salt,
           email: email,
@@ -133,15 +135,16 @@ void main() {
         legacyEmail: email,
       );
 
-      expect(passwordHash.validate('chaser1'), isFalse);
+      expect(await passwordHash.validate('chaser1'), isFalse);
     });
 
-    test('when matching with incorrect salt then it evaluates to false', () {
+    test('when matching with incorrect salt then it evaluates to false',
+        () async {
       var password = 'hunter2';
       var email = 'test@serverpod.dev';
 
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(
+        await PasswordHash.legacyHash(
           password,
           'first salt',
           email: email,
@@ -150,15 +153,16 @@ void main() {
         legacyEmail: email,
       );
 
-      expect(passwordHash.validate(password), isFalse);
+      expect(await passwordHash.validate(password), isFalse);
     });
 
-    test('when matching with incorrect email then it evaluates to false', () {
+    test('when matching with incorrect email then it evaluates to false',
+        () async {
       var salt = 'serverpod password salt';
       var password = 'hunter2';
 
       var passwordHash = PasswordHash(
-        PasswordHash.legacyHash(
+        await PasswordHash.legacyHash(
           password,
           salt,
           email: 'first@serverpod.dev',
@@ -167,7 +171,7 @@ void main() {
         legacyEmail: 'second@serverpod.dev',
       );
 
-      expect(passwordHash.validate(password), isFalse);
+      expect(await passwordHash.validate(password), isFalse);
     });
   });
 }
