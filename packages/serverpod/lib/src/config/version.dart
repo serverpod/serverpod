421b3390f4a103cda2e041431b6ec16593722ce8
Viktor Lidholt <viktorlidholt@gmail.com>
2024-03-27 13:39:26 +0100

    chore: Bumps version to 1.2.6 and updates CHANGELOG.

--- packages/serverpod/lib/src/config/version.dart
+++ packages/serverpod/lib/src/config/version.dart
@@ -2,4 +2,4 @@
 // templates/pubspecs directory.
 
 /// Current version of serverpod.
-const serverpodVersion = '1.2.5';
+const serverpodVersion = '1.2.6';
--- tools/serverpod_cli/lib/src/generated/version.dart
+++ tools/serverpod_cli/lib/src/generated/version.dart
@@ -1,5 +1,5 @@
 // This file is generated. Do not modify, instead edit the files in the
 // templates/pubspecs directory.
 
-const templateVersion = '1.2.5';
+const templateVersion = '1.2.6';
 const productionMode = false;
