e57b7053529c4097ef5f05d3ae12bdd4575cb630
Alexander Sandor <137198655+SandPod@users.noreply.github.com>
2024-04-15 12:58:34 +0200

    fix: Prevent crash when missing web or template directory. (#2138)

---
+++ packages/serverpod/test/relic/templates_test.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'package:serverpod/serverpod.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test(
+      'Given missing templates folder when loading templates then no templates are loaded',
+      () async {
+    var uniqueUuid = const Uuid().v4();
+    var nonExistingDirectory =
+        Directory(path.joinAll([uniqueUuid, 'non-existing-directory']));
+
+    await templates.loadAll(nonExistingDirectory);
+
+    expect(templates, isEmpty);
+  });
+}
