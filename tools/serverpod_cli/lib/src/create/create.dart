3d51bdf79804264c9d0155ed40290df10caf5bb4
Abdelaziz Mahdy <zezohassam@gmail.com>
2024-05-06 00:15:56 +0300

    adding client true after serverpod create

--- tools/serverpod_cli/lib/src/create/create.dart
+++ tools/serverpod_cli/lib/src/create/create.dart
@@ -123,6 +123,10 @@ Future<bool> performCreate(
         await log.progress('Getting Flutter app package dependencies.', () {
       return CommandLineTools.flutterCreate(serverpodDirs.flutterDir);
     });
+    // success &=
+    //     await log.progress('Updating entitlements.', () {
+    //   return CommandLineTools.addNetworkToEntitlements(serverpodDirs.flutterDir);
+    // });
   } else if (template == ServerpodTemplateType.module) {
     success &= await log.progress(
         'Writing project files.',
