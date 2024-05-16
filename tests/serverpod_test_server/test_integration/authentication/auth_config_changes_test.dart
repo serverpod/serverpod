878e45d40d65e485ca0b107022b995acb343c310
Ben Auer <136239531+BenAuerDev@users.noreply.github.com>
2024-05-16 15:18:59 +0200

    make more readable tests

--- tests/serverpod_test_server/test_integration/authentication/auth_config_changes_test.dart
+++ tests/serverpod_test_server/test_integration/authentication/auth_config_changes_test.dart
@@ -1,74 +1,42 @@
 import 'package:serverpod_auth_server/module.dart';
-import 'package:serverpod_test_server/test_util/test_serverpod.dart';
 import 'package:test/test.dart';
 
 void main() async {
-  var userName = 'test';
-  var email = 'test@serverpod.dev';
-  var password = 'password';
-  var session = await IntegrationTestServer().session();
-
-  var codeLengthsToTest = [-16, -8, -4, -1, 0, 1, 4, 16];
-
   test(
-      'Not setting validationCodeLength will generate a code with default length 8',
-      () async {
-    String? generatedValidationCode;
-    AuthConfig.set(
-      AuthConfig(
-        sendValidationEmail: (session, email, validationCode) async {
-          generatedValidationCode = validationCode;
-          return true;
-        },
-      ),
-    );
-
-    var createAccountRequest =
-        await Emails.createAccountRequest(session, userName, email, password);
-
-    expect(
-      createAccountRequest,
-      isTrue,
-      reason: 'Generated validation code is 8 characters long, which is valid',
-    );
-
-    assert(generatedValidationCode?.length == 8);
+      'Given no validation code length when constructing AuthConfig then validation code has the default length of 8.',
+      () {
+    final auth = AuthConfig();
+    expect(auth.validationCodeLength, 8);
   });
 
-  for (var length in codeLengthsToTest) {
-    final willPass = length >= 1;
-    test(
-        willPass
-            ? 'Setting validationCodeLength to $length will generate a code with the given length'
-            : 'Setting validationCodeLength to $length will return null',
-        () async {
-      String? generatedValidationCode;
-      AuthConfig.set(
-        AuthConfig(
-          validationCodeLength: length,
-          sendValidationEmail: (session, email, validationCode) async {
-            generatedValidationCode = validationCode;
-            return true;
-          },
-        ),
-      );
+  test(
+      'Given a positive integer as validation code length when constructing AuthConfig then validation code has the specified length.',
+      () {
+    final auth = AuthConfig(validationCodeLength: 4);
+    expect(auth.validationCodeLength, 4);
+  });
 
-      var createAccountRequest =
-          await Emails.createAccountRequest(session, userName, email, password);
+  test(
+      'Given 0 as validation code length when trying to construct AuthConfig then throws.',
+      () {
+    expect(
+        () => AuthConfig(validationCodeLength: 0),
+        throwsA(isA<Exception>().having(
+          (e) => e.toString(),
+          'message',
+          contains('Invalid validation code length'),
+        )));
+  });
 
-      expect(
-        createAccountRequest,
-        willPass ? isTrue : isFalse,
-        reason: willPass
-            ? 'Generated validation code is $length characters long, which is valid'
-            : 'Generated validation code is null, which is invalid',
-      );
-
-      if (willPass) {
-        assert(generatedValidationCode?.length == length);
-      } else {
-        assert(generatedValidationCode == null);
-      }
-    });
-  }
+  test(
+      'Given a negative integer as validation code length when trying to construct AuthConfig then throws.',
+      () {
+    expect(
+        () => AuthConfig(validationCodeLength: -4),
+        throwsA(isA<Exception>().having(
+          (e) => e.toString(),
+          'message',
+          contains('Invalid validation code length'),
+        )));
+  });
 }
