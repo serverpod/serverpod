b409723195d11508eda16453c629a3f9d1a5f021
Isakdl <isak.duner@gmail.com>
2024-05-16 17:09:48 +0200

    fix: Regnerate commands.

--- modules/serverpod_auth/serverpod_auth_server/lib/src/generated/protocol.dart
+++ modules/serverpod_auth/serverpod_auth_server/lib/src/generated/protocol.dart
@@ -731,7 +731,7 @@ class Protocol extends _i1.SerializationManagerServer {
     }
     try {
       return _i2.Protocol().deserialize<T>(data, t);
-    } catch (_) {}
+    } on _i1.DeserializationTypeNotFoundException catch (_) {}
     return super.deserialize<T>(data, t);
   }
 
