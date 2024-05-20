94ea382c155f6c9d052965b894838f57e9f19b1e
Isakdl <isak.duner@gmail.com>
2024-05-20 12:27:29 +0200

    chore: Create new migration for auth module.

--- modules/serverpod_auth/serverpod_auth_server/migrations/20240520102713718/migration.sql
+++ modules/serverpod_auth/serverpod_auth_server/migrations/20240520102713718/migration.sql
@@ -1,13 +1,17 @@
 BEGIN;
 
+--
+-- ACTION ALTER TABLE
+--
+ALTER TABLE "serverpod_user_info" ALTER COLUMN "userName" DROP NOT NULL;
 
 --
 -- MIGRATION VERSION FOR serverpod_auth
 --
 INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
-    VALUES ('serverpod_auth', '20240516152014635', now())
+    VALUES ('serverpod_auth', '20240520102713718', now())
     ON CONFLICT ("module")
-    DO UPDATE SET "version" = '20240516152014635', "timestamp" = now();
+    DO UPDATE SET "version" = '20240520102713718', "timestamp" = now();
 
 --
 -- MIGRATION VERSION FOR serverpod
