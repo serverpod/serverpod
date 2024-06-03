4260cb3dbc17add9fc2649bf319b3cc789a4841c
Isakdl <isak.duner@gmail.com>
2024-06-03 13:47:53 +0200

    ci: Test docker setup modification to run on several platforms.

--- tests/docker/tests_single_server/run-tests.sh
+++ tests/docker/tests_single_server/run-tests.sh
@@ -3,14 +3,14 @@
 # Makes script exit on first non-zero error code
 set -e
 
+param=${1:-vm}
+
 # Wait for the server to be up (timeout after 60 seconds)
 echo "### Wait for test server"
 /app/tests/docker/tests_single_server/wait-for-it.sh serverpod_test_server:8080 -t 60 -- echo "### Server main is UP"
 /app/tests/docker/tests_single_server/wait-for-it.sh serverpod_test_server:8081 -t 60 -- echo "### Server insights is UP"
 echo ""
 
-# We are all set to start the server
-echo "### Running integration tests"
+echo "### Running e2e tests"
 cd tests/serverpod_test_server
-dart pub get
-dart test test_e2e --concurrency=1
+dart test test_e2e --concurrency=1 -p $param
