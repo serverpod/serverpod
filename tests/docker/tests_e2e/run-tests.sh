333c604f8e9ce9dd740711641bfc720a7791a7e5
Isakdl <isak.duner@gmail.com>
2024-06-03 14:53:22 +0200

    chore: Rename pathing to new e2e directory.

--- tests/docker/tests_e2e/run-tests.sh
+++ tests/docker/tests_e2e/run-tests.sh
@@ -7,8 +7,8 @@ param=${1:-vm}
 
 # Wait for the server to be up (timeout after 60 seconds)
 echo "### Wait for test server"
-/app/tests/docker/tests_single_server/wait-for-it.sh serverpod_test_server:8080 -t 60 -- echo "### Server main is UP"
-/app/tests/docker/tests_single_server/wait-for-it.sh serverpod_test_server:8081 -t 60 -- echo "### Server insights is UP"
+/app/tests/docker/tests_e2e/wait-for-it.sh serverpod_test_server:8080 -t 60 -- echo "### Server main is UP"
+/app/tests/docker/tests_e2e/wait-for-it.sh serverpod_test_server:8081 -t 60 -- echo "### Server insights is UP"
 echo ""
 
 echo "### Running e2e tests"
