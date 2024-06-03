093445e7b8fd7221c486eabde26d2ec8e8a13bac
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-06-03 16:06:38 +0200

    fix

--- modules/serverpod_auth/serverpod_auth_server/lib/src/firebase/firebase_auth_manager.dart
+++ modules/serverpod_auth/serverpod_auth_server/lib/src/firebase/firebase_auth_manager.dart
@@ -74,15 +74,18 @@ class FirebaseAuthManager {
   ) async {
     // Get tokens valid after time for the corresponding user.
     var user = await _accountApi.getUserByUiid(decodedIdToken.claims.subject);
+
     // If no tokens valid after time available, token is not revoked.
     if (user.validSince == null) return decodedIdToken;
 
     // Get the ID token authentication time.
     var authTimeUtc = decodedIdToken.claims.authTime!;
+
     // Get user tokens valid after time.
     var validSinceUtc = DateTime.fromMillisecondsSinceEpoch(
       num.parse(user.validSince!).toInt(),
     );
+
     // Check if authentication time is older than valid since time.
     if (authTimeUtc.isBefore(validSinceUtc)) {
       throw FirebaseJWTException('The Firebase ID token has been revoked.');
