9f36901aff7a856465d5b23ca347845e8b4539a0
Isakdl <isak.duner@gmail.com>
2024-06-11 12:27:12 +0200

    fix: Use default chars for verification code.

--- modules/serverpod_auth/serverpod_auth_server/lib/src/business/email_auth.dart
+++ modules/serverpod_auth/serverpod_auth_server/lib/src/business/email_auth.dart
@@ -582,7 +582,6 @@ class Emails {
   static String _generateVerificationCode() {
     return Random().nextString(
       length: AuthConfig.current.validationCodeLength,
-      chars: '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
     );
   }
 
