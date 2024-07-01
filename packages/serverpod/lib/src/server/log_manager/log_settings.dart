e065ca39c7ced369ff8c2804800033f2a5fc4bf6
Isakdl <isak.duner@gmail.com>
2024-07-01 09:44:42 +0200

    docs: Add documentation comment for log settings manager.

--- packages/serverpod/lib/src/server/log_manager/log_settings.dart
+++ packages/serverpod/lib/src/server/log_manager/log_settings.dart
@@ -1,12 +1,14 @@
 import 'package:serverpod/protocol.dart';
 import 'package:serverpod/serverpod.dart';
 
+/// Retrieve the log settings for a session.
 class LogSettingsManager {
   final RuntimeSettings _runtimeSettings;
 
   final Map<String, LogSettings> _endpointOverrides = {};
   final Map<String, LogSettings> _methodOverrides = {};
 
+  /// Creates a new [LogSettingsManager].
   LogSettingsManager(
     this._runtimeSettings,
   ) {
