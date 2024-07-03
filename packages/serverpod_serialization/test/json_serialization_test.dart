13acdb6f8fc9b7ef361d260929b74aa557d229f5
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-07-03 12:52:25 +0200

    test: Adding tests for List and Map object types when calling 'encodeForProtocol' method

--- packages/serverpod_serialization/test/json_serialization_test.dart
+++ packages/serverpod_serialization/test/json_serialization_test.dart
@@ -1,50 +1,6 @@
 import 'package:serverpod_serialization/serverpod_serialization.dart';
 import 'package:test/test.dart';
 
-void main() {
-  test('Given json data when encoded to string then output is stringified json',
-      () {
-    var jsonData = {'a': 1, 'b': 2};
-
-    var stringifiedJson = SerializationManager.encode(jsonData);
-    expect(stringifiedJson, '{"a":1,"b":2}');
-  });
-
-  test(
-      'Given json data when encoded to formatted string then output is formatted stringified json',
-      () {
-    var jsonData = {'a': 1, 'b': 2};
-
-    var stringifiedJson = SerializationManager.encode(
-      jsonData,
-      formatted: true,
-    );
-    expect(stringifiedJson, '''
-{
-  "a": 1,
-  "b": 2
-}''');
-  });
-
-  test('respect ProtocolSerialization even in List/Map', () {
-    var user = _User(name: 'John', password: '123');
-    var userList = [user];
-    var userMap = {'user': user, 'users': userList};
-
-    var stringifiedJson = SerializationManager.encode(user);
-    expect(true, stringifiedJson.contains('password'));
-
-    stringifiedJson = SerializationManager.encodeForProtocol(user);
-    expect(false, stringifiedJson.contains('password'));
-
-    stringifiedJson = SerializationManager.encode(userMap);
-    expect(true, stringifiedJson.contains('password'));
-
-    stringifiedJson = SerializationManager.encodeForProtocol(userMap);
-    expect(false, stringifiedJson.contains('password'));
-  });
-}
-
 class _User implements SerializableModel, ProtocolSerialization {
   final String name;
   final String password;
@@ -68,4 +24,154 @@ class _User implements SerializableModel, ProtocolSerialization {
       'name': name,
     };
   }
+}
+
+void main() {
+  test(
+    'Given simple JSON data, when encoded using encode method, then output is stringified JSON',
+    () {
+      var jsonData = {'a': 1, 'b': 2};
+      var stringifiedJson = SerializationManager.encode(jsonData);
+      expect(stringifiedJson, '{"a":1,"b":2}');
+    },
+  );
+
+  test(
+    'Given simple JSON data, when encoded using encode method with formatting, then output is formatted stringified JSON',
+    () {
+      var jsonData = {'a': 1, 'b': 2};
+      var stringifiedJson = SerializationManager.encode(
+        jsonData,
+        formatted: true,
+      );
+      expect(
+        stringifiedJson,
+        '''
+{
+  "a": 1,
+  "b": 2
+}''',
+      );
+    },
+  );
+
+  group('Given a user object,', () {
+    late _User user;
+
+    setUp(() {
+      user = _User(name: 'John', password: '123');
+    });
+
+    test(
+      'when encoded using encodeForProtocol method, then password is excluded from the output',
+      () {
+        var stringifiedJson = SerializationManager.encodeForProtocol(user);
+        expect(stringifiedJson.contains('password'), isFalse);
+      },
+    );
+
+    test(
+      'when a list of user objects is encoded using encodeForProtocol method, then passwords are excluded from the output',
+      () {
+        var userList = [user];
+        var stringifiedJson = SerializationManager.encodeForProtocol(userList);
+        expect(stringifiedJson.contains('password'), isFalse);
+      },
+    );
+
+    test(
+      'when a map containing user objects is encoded using encodeForProtocol method, then passwords are excluded from the output',
+      () {
+        var userMap = {
+          'user': user,
+          'users': [user]
+        };
+        var stringifiedJson = SerializationManager.encodeForProtocol(userMap);
+        expect(stringifiedJson.contains('password'), isFalse);
+      },
+    );
+  });
+
+  group('Given a list of primitives,', () {
+    var primitiveList = [1, 2.5, 'hello', true];
+
+    test(
+      'when encoded using encodeForProtocol method, then output matches the input list',
+      () {
+        var stringifiedJson =
+            SerializationManager.encodeForProtocol(primitiveList);
+        expect(stringifiedJson, '[1,2.5,"hello",true]');
+      },
+    );
+  });
+
+  group('Given a map of primitives,', () {
+    var primitiveMap = {'a': 1, 'b': 2.5, 'c': 'hello', 'd': true};
+
+    test(
+      'when encoded using encodeForProtocol method, then output matches the input map',
+      () {
+        var stringifiedJson =
+            SerializationManager.encodeForProtocol(primitiveMap);
+        expect(stringifiedJson, '{"a":1,"b":2.5,"c":"hello","d":true}');
+      },
+    );
+  });
+
+  group('Given a list of complex objects,', () {
+    late _User user;
+
+    setUp(() {
+      user = _User(name: 'John', password: '123');
+    });
+
+    test(
+      'when encoded using encodeForProtocol method, then passwords are excluded from the output',
+      () {
+        var list = [user, user];
+        var stringifiedJson = SerializationManager.encodeForProtocol(list);
+        expect(stringifiedJson.contains('password'), isFalse);
+      },
+    );
+  });
+
+  group('Given a map of complex objects,', () {
+    late _User user;
+
+    setUp(() {
+      user = _User(name: 'John', password: '123');
+    });
+
+    test(
+      'when encoded using encodeForProtocol method, then passwords are excluded from the output',
+      () {
+        var map = {'user1': user, 'user2': user};
+        var stringifiedJson = SerializationManager.encodeForProtocol(map);
+        expect(stringifiedJson.contains('password'), isFalse);
+      },
+    );
+  });
+
+  group('Given a map with complex nested structures,', () {
+    late _User user;
+
+    setUp(() {
+      user = _User(name: 'John', password: '123');
+    });
+
+    test(
+      'when encoded using encodeForProtocol method, then passwords are excluded from the output',
+      () {
+        var map = {
+          'list': [user],
+          'nestedMap': {
+            'innerUser': user,
+            'innerList': [user]
+          },
+        };
+        var stringifiedJson = SerializationManager.encodeForProtocol(map);
+        expect(stringifiedJson.contains('password'), isFalse);
+      },
+    );
+  });
 }
