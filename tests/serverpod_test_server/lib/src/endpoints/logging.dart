05eef4693d7ec406b143f024f95a94936fc7d1ac
Isakdl <isak.duner@gmail.com>
2024-07-10 17:50:01 +0200

    test: Add logging endpoints.

--- tests/serverpod_test_server/lib/src/endpoints/logging.dart
+++ tests/serverpod_test_server/lib/src/endpoints/logging.dart
@@ -2,8 +2,23 @@ import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_test_server/src/generated/protocol.dart';
 
 class LoggingEndpoint extends Endpoint {
+  Future<void> slowMethod(Session session, int delayMillis) async {
+    await Future.delayed(Duration(milliseconds: delayMillis));
+  }
+
+  Future<void> failingMethod(Session session) async {
+    throw Exception('This is an exception');
+  }
+
   Future<void> emptyMethod(Session session) async {
     // do nothing
+  }
+
+  Future<void> log(
+      Session session, String message, List<LogLevel> logLevels) async {
+    for (var logLevel in logLevels) {
+      session.log(message, level: logLevel);
+    }
   }
 
   Future<void> logInfo(Session session, String message) async {
