cf8b24339b6668f99ec33b52335d760db49f1b64
Klaudio Kuçaj <78809795+klkucaj@users.noreply.github.com>
2024-07-11 13:24:18 +0200

    feat: Implement default values for DateTime field type in Models. (#2383)

---
+++ tests/serverpod_test_server/test_integration/database_operations/default/datetime_default_test.dart
@@ -0,0 +1,152 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_test_server/src/generated/protocol.dart';
+import 'package:serverpod_test_server/test_util/test_serverpod.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  var session = await IntegrationTestServer().session();
+
+  group('Given a class with "default" fields,', () {
+    tearDownAll(() async => DateTimeDefault.db.deleteWhere(
+          session,
+          where: (_) => Constant.bool(true),
+        ));
+
+    test(
+      'when creating a record in the database, then the "default=now" field should be in UTC',
+      () async {
+        var object = DateTimeDefault();
+        var databaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(databaseObject.dateTimeDefaultNow.isUtc, isTrue);
+      },
+    );
+
+    test(
+      'when creating a record in the database, then the "default" field with UTC string should be in UTC',
+      () async {
+        var object = DateTimeDefault();
+        var databaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(databaseObject.dateTimeDefaultStr.isUtc, isTrue);
+      },
+    );
+
+    test(
+      'when creating a record in the database, then the nullable "default" field with UTC string should be in UTC',
+      () async {
+        var object = DateTimeDefault();
+        var databaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(databaseObject.dateTimeDefaultStrNull?.isUtc, isTrue);
+      },
+    );
+
+    test(
+      'when creating a record in the database, then the "default=now" field value should match the current time',
+      () async {
+        var object = DateTimeDefault();
+        var databaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(
+          databaseObject.dateTimeDefaultNow
+              .difference(DateTime.now())
+              .inSeconds,
+          0,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database, then the "default" field value should match the default',
+      () async {
+        var object = DateTimeDefault();
+        var databaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(
+          databaseObject.dateTimeDefaultStr,
+          DateTime.parse('2024-05-24T22:00:00.000Z'),
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database, then the nullable "default" field value should match the default',
+      () async {
+        var object = DateTimeDefault();
+        var databaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(
+          databaseObject.dateTimeDefaultStrNull,
+          DateTime.parse('2024-05-24T22:00:00.000Z'),
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database with a specific value, then the "dateTimeDefaultNow" field value should match the provided value',
+      () async {
+        var date = DateTime.parse('2024-05-01T22:00:00.000Z');
+        var specificObject = DateTimeDefault(
+          dateTimeDefaultNow: date,
+        );
+        var specificDatabaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(
+          specificDatabaseObject.dateTimeDefaultNow,
+          date,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database with a specific value, then the "dateTimeDefaultStr" field value should match the provided value',
+      () async {
+        var date = DateTime.parse('2024-05-01T22:00:00.000Z');
+        var specificObject = DateTimeDefault(
+          dateTimeDefaultStr: date,
+        );
+        var specificDatabaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(
+          specificDatabaseObject.dateTimeDefaultStr,
+          date,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database with a specific value, then the "dateTimeDefaultStrNull" field value should match the provided value',
+      () async {
+        var date = DateTime.parse('2024-05-01T22:00:00.000Z');
+        var specificObject = DateTimeDefault(
+          dateTimeDefaultStrNull: date,
+        );
+        var specificDatabaseObject = await DateTimeDefault.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(
+          specificDatabaseObject.dateTimeDefaultStrNull,
+          date,
+        );
+      },
+    );
+  });
+}
---
+++ tools/serverpod_cli/test/generator/dart/server_code_generator/defaults/datetime_default_server_test.dart
@@ -0,0 +1,185 @@
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer/dart/ast/ast.dart';
+import 'package:serverpod_cli/src/generator/dart/server_code_generator.dart';
+import 'package:serverpod_cli/src/test_util/builders/serializable_entity_field_definition_builder.dart';
+import 'package:serverpod_cli/src/test_util/compilation_unit_helpers.dart';
+import 'package:test/test.dart';
+import 'package:path/path.dart' as path;
+import 'package:serverpod_cli/src/test_util/builders/class_definition_builder.dart';
+import 'package:serverpod_cli/src/test_util/builders/generator_config_builder.dart';
+
+const projectName = 'example_project';
+final config = GeneratorConfigBuilder().withName(projectName).build();
+const generator = DartServerCodeGenerator();
+
+void main() {
+  group(
+      'Given a class named DateTimeDefault with DateTime fields having defaultModelValue when generating code',
+      () {
+    ClassDeclaration? baseClass;
+    ConstructorDeclaration? privateConstructor;
+
+    setUpAll(() {
+      var testClassName = 'DateTimeDefault';
+      var testClassFileName = 'datetime_default';
+      var expectedFilePath =
+          path.join('lib', 'src', 'generated', '$testClassFileName.dart');
+
+      var fields = [
+        FieldDefinitionBuilder()
+            .withName('dateTimeDefaultNow')
+            .withTypeDefinition('DateTime', false)
+            .withDefaults(defaultModelValue: 'now')
+            .build(),
+        FieldDefinitionBuilder()
+            .withName('dateTimeDefaultStr')
+            .withTypeDefinition('DateTime', false)
+            .withDefaults(defaultModelValue: '2024-05-24T22:00:00.000Z')
+            .build(),
+        FieldDefinitionBuilder()
+            .withName('dateTimeDefaultStrNull')
+            .withTypeDefinition('DateTime', true)
+            .withDefaults(defaultModelValue: '2024-05-24T22:00:00.000Z')
+            .build(),
+      ];
+
+      var models = [
+        ClassDefinitionBuilder()
+            .withClassName(testClassName)
+            .withFileName(testClassFileName)
+            .withFields(fields)
+            .build()
+      ];
+
+      var codeMap = generator.generateSerializableModelsCode(
+        models: models,
+        config: config,
+      );
+
+      var compilationUnit =
+          parseString(content: codeMap[expectedFilePath]!).unit;
+
+      baseClass = CompilationUnitHelpers.tryFindClassDeclaration(
+        compilationUnit,
+        name: testClassName,
+      );
+
+      privateConstructor = CompilationUnitHelpers.tryFindConstructorDeclaration(
+        baseClass!,
+        name: '_',
+      );
+    });
+
+    group('then the DateTimeDefault has a private constructor', () {
+      test('defined', () {
+        expect(privateConstructor, isNotNull);
+      });
+
+      test(
+        'with the class vars as params',
+        () {
+          expect(
+            privateConstructor?.parameters.toSource(),
+            '({DateTime? dateTimeDefaultNow, DateTime? dateTimeDefaultStr, DateTime? dateTimeDefaultStrNull})',
+          );
+        },
+      );
+
+      test(
+        'with dateTimeDefaultNow default value set correctly',
+        () {
+          var initializer = privateConstructor?.initializers
+              .firstWhere((e) => e.toSource().contains('dateTimeDefaultNow'));
+          expect(initializer?.toSource(),
+              'dateTimeDefaultNow = dateTimeDefaultNow ?? DateTime.now()');
+        },
+      );
+
+      test(
+        'with dateTimeDefaultStr default value set correctly',
+        () {
+          var initializer = privateConstructor?.initializers
+              .firstWhere((e) => e.toSource().contains('dateTimeDefaultStr'));
+          expect(initializer?.toSource(),
+              'dateTimeDefaultStr = dateTimeDefaultStr ?? DateTime.parse(\'2024-05-24T22:00:00.000Z\')');
+        },
+      );
+
+      test(
+        'with dateTimeDefaultStrNull default value set correctly',
+        () {
+          var initializer = privateConstructor?.initializers.firstWhere(
+              (e) => e.toSource().contains('dateTimeDefaultStrNull'));
+          expect(initializer?.toSource(),
+              'dateTimeDefaultStrNull = dateTimeDefaultStrNull ?? DateTime.parse(\'2024-05-24T22:00:00.000Z\')');
+        },
+      );
+    });
+  });
+
+  group(
+      'Given a class named DateTimeDefaultPersist with DateTime fields having defaultPersistValue when generating code',
+      () {
+    ClassDeclaration? baseClass;
+    ConstructorDeclaration? privateConstructor;
+
+    setUpAll(() {
+      var testClassName = 'DateTimeDefaultPersist';
+      var testClassFileName = 'datetime_default_persist';
+      var expectedFilePath =
+          path.join('lib', 'src', 'generated', '$testClassFileName.dart');
+
+      var fields = [
+        FieldDefinitionBuilder()
+            .withName('dateTimeDefaultPersistNow')
+            .withTypeDefinition('DateTime', true)
+            .withDefaults(defaultPersistValue: 'now')
+            .build(),
+        FieldDefinitionBuilder()
+            .withName('dateTimeDefaultPersistStr')
+            .withTypeDefinition('DateTime', true)
+            .withDefaults(defaultPersistValue: '2024-05-24T22:00:00.000Z')
+            .build(),
+      ];
+
+      var models = [
+        ClassDefinitionBuilder()
+            .withClassName(testClassName)
+            .withFileName(testClassFileName)
+            .withFields(fields)
+            .build()
+      ];
+
+      var codeMap = generator.generateSerializableModelsCode(
+        models: models,
+        config: config,
+      );
+
+      var compilationUnit =
+          parseString(content: codeMap[expectedFilePath]!).unit;
+      baseClass = CompilationUnitHelpers.tryFindClassDeclaration(
+        compilationUnit,
+        name: testClassName,
+      );
+
+      privateConstructor = CompilationUnitHelpers.tryFindConstructorDeclaration(
+        baseClass!,
+        name: '_',
+      );
+    });
+
+    group('then the DateTimeDefaultPersist has a private constructor', () {
+      test('defined', () {
+        expect(privateConstructor, isNotNull);
+      });
+
+      test(
+        'with the class vars as params',
+        () {
+          expect(privateConstructor?.parameters.toSource(),
+              '({this.dateTimeDefaultPersistNow, this.dateTimeDefaultPersistStr})');
+        },
+      );
+    });
+  });
+}
