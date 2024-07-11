d15a6d6f7df0c138d3b74a665ec4204a8b8efb3f
Luke Hutchison <luke.hutch@gmail.com>
2024-07-11 13:09:15 -0600

    Add missing generator line

--- tools/serverpod_cli/lib/src/generator/dart/library_generators/library_generator.dart
+++ tools/serverpod_cli/lib/src/generator/dart/library_generators/library_generator.dart
@@ -891,6 +891,7 @@ extension on DatabaseDefinition {
                 ]),
                 'type': literalString(index.type),
                 'isUnique': literalBool(index.isUnique),
+                'isNotNull': literalBool(index.isNotNull),
                 'isPrimary': literalBool(index.isPrimary),
               }),
           ]),
