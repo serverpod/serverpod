6e6cf3bac924a4b20aecddb1ed1c28d32f21d90d
Viktor Lidholt <viktorlidholt@gmail.com>
2024-07-12 10:36:00 +0200

    chore: Bumps version to 2.0.2 and updates CHANGELOG.

--- tools/serverpod_cli/lib/src/generated/version.dart
+++ tools/serverpod_cli/lib/src/generated/version.dart
@@ -1,5 +1,5 @@
 // This file is generated. Do not modify, instead edit the files in the
 // templates/pubspecs directory.
 
-const templateVersion = '2.0.1';
+const templateVersion = '2.0.2';
 const productionMode = false;
