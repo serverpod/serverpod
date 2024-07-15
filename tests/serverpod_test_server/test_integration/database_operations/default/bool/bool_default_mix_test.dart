b72b3f7c6cd12e0da1f9de8ba83ea63d0c0b33d9
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-07-15 13:40:44 +0200

    test: Implementing integration tests for bool default values

---
+++ tests/serverpod_test_server/test_integration/database_operations/default/bool/bool_default_mix_test.dart
@@ -0,0 +1,100 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_test_server/src/generated/protocol.dart';
+import 'package:serverpod_test_server/test_util/test_serverpod.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  var session = await IntegrationTestServer().session();
+
+  group('Given a class with mixed default fields,', () {
+    tearDownAll(() async => BoolDefaultMix.db.deleteWhere(
+          session,
+          where: (_) => Constant.bool(true),
+        ));
+
+    test(
+      'when creating a record in the database with an unsafe query, then the "boolDefaultAndDefaultModel" field value should match the provided value',
+      () async {
+        await session.db.unsafeQuery(
+          '''
+        INSERT INTO "${BoolDefaultMix.t.tableName}" ("boolDefaultAndDefaultModel", "boolDefaultAndDefaultPersist", "boolDefaultModelAndDefaultPersist")
+        VALUES (DEFAULT, DEFAULT, DEFAULT);
+        ''',
+        );
+        var databaseObject = await BoolDefaultMix.db.findFirstRow(session);
+        expect(databaseObject?.boolDefaultAndDefaultModel, isTrue);
+      },
+    );
+
+    test(
+      'when creating a record in the database with an unsafe query, then the "boolDefaultAndDefaultPersist" field value should match the defaultPersist value',
+      () async {
+        await session.db.unsafeQuery(
+          '''
+        INSERT INTO "${BoolDefaultMix.t.tableName}" ("boolDefaultAndDefaultModel", "boolDefaultAndDefaultPersist", "boolDefaultModelAndDefaultPersist")
+        VALUES (DEFAULT, DEFAULT, DEFAULT);
+        ''',
+        );
+        var databaseObject = await BoolDefaultMix.db.findFirstRow(session);
+        expect(databaseObject?.boolDefaultAndDefaultPersist, isFalse);
+      },
+    );
+
+    test(
+      'when creating a record in the database with an unsafe query, then the "boolDefaultModelAndDefaultPersist" field value should match the defaultPersist value',
+      () async {
+        await session.db.unsafeQuery(
+          '''
+        INSERT INTO "${BoolDefaultMix.t.tableName}" ("boolDefaultAndDefaultModel", "boolDefaultAndDefaultPersist", "boolDefaultModelAndDefaultPersist")
+        VALUES (DEFAULT, DEFAULT, DEFAULT);
+        ''',
+        );
+        var databaseObject = await BoolDefaultMix.db.findFirstRow(session);
+        expect(databaseObject?.boolDefaultModelAndDefaultPersist, isFalse);
+      },
+    );
+
+    test(
+      'when creating a record in the database with specific values, then the "boolDefaultAndDefaultModel" field value should match the provided value',
+      () async {
+        var specificObject = BoolDefaultMix(
+          boolDefaultAndDefaultModel: false,
+        );
+        var specificDatabaseObject = await BoolDefaultMix.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(specificDatabaseObject.boolDefaultAndDefaultModel, isFalse);
+      },
+    );
+
+    test(
+      'when creating a record in the database with specific values, then the "boolDefaultAndDefaultPersist" field value should match the provided value',
+      () async {
+        var specificObject = BoolDefaultMix(
+          boolDefaultAndDefaultPersist: true,
+        );
+        var specificDatabaseObject = await BoolDefaultMix.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(specificDatabaseObject.boolDefaultAndDefaultPersist, isTrue);
+      },
+    );
+
+    test(
+      'when creating a record in the database with specific values, then the "boolDefaultModelAndDefaultPersist" field value should match the provided value',
+      () async {
+        var specificObject = BoolDefaultMix(
+          boolDefaultModelAndDefaultPersist: false,
+        );
+        var specificDatabaseObject = await BoolDefaultMix.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(
+            specificDatabaseObject.boolDefaultModelAndDefaultPersist, isFalse);
+      },
+    );
+  });
+}
---
+++ tests/serverpod_test_server/test_integration/database_operations/default/bool/bool_default_test.dart
@@ -0,0 +1,111 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_test_server/src/generated/protocol.dart';
+import 'package:serverpod_test_server/test_util/test_serverpod.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  var session = await IntegrationTestServer().session();
+
+  group('Given a class with "default" fields,', () {
+    tearDownAll(() async => BoolDefault.db.deleteWhere(
+          session,
+          where: (_) => Constant.bool(true),
+        ));
+
+    test(
+      'when creating a record in the database, then the "default=true" field value should be true',
+      () async {
+        var object = BoolDefault();
+        var databaseObject = await BoolDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(
+          databaseObject.boolDefaultTrue,
+          isTrue,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database, then the "default=false" field value should be false',
+      () async {
+        var object = BoolDefault();
+        var databaseObject = await BoolDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(
+          databaseObject.boolDefaultFalse,
+          isFalse,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database, then the nullable "default=false" field value should be false',
+      () async {
+        var object = BoolDefault();
+        var databaseObject = await BoolDefault.db.insertRow(
+          session,
+          object,
+        );
+        expect(
+          databaseObject.boolDefaultNullFalse,
+          isFalse,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database with a specific value, then the "boolDefaultTrue" field value should match the provided value',
+      () async {
+        var specificObject = BoolDefault(
+          boolDefaultTrue: false,
+        );
+        var specificDatabaseObject = await BoolDefault.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(
+          specificDatabaseObject.boolDefaultTrue,
+          isFalse,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database with a specific value, then the "boolDefaultFalse" field value should match the provided value',
+      () async {
+        var specificObject = BoolDefault(
+          boolDefaultFalse: true,
+        );
+        var specificDatabaseObject = await BoolDefault.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(
+          specificDatabaseObject.boolDefaultFalse,
+          isTrue,
+        );
+      },
+    );
+
+    test(
+      'when creating a record in the database with a specific value, then the "boolDefaultNullFalse" field value should match the provided value',
+      () async {
+        var specificObject = BoolDefault(
+          boolDefaultNullFalse: true,
+        );
+        var specificDatabaseObject = await BoolDefault.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(
+          specificDatabaseObject.boolDefaultNullFalse,
+          isTrue,
+        );
+      },
+    );
+  });
+}
