69f4ef4381f755304027d89469446af0ebd9b9f0
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-07-16 13:52:03 +0200

    test: Implementing integration tests for int fields with default values

---
+++ tests/serverpod_test_server/test_integration/database_operations/default/integer/int_default_mix_test.dart
@@ -0,0 +1,99 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_test_server/src/generated/protocol.dart';
+import 'package:serverpod_test_server/test_util/test_serverpod.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  var session = await IntegrationTestServer().session();
+
+  group('Given a class with mixed default fields,', () {
+    tearDownAll(() async => IntDefaultMix.db.deleteWhere(
+          session,
+          where: (_) => Constant.bool(true),
+        ));
+
+    test(
+      'when creating a record in the database with an unsafe query, then the "intDefaultAndDefaultModel" field value should match the provided value',
+      () async {
+        await session.db.unsafeQuery(
+          '''
+        INSERT INTO "${IntDefaultMix.t.tableName}" ("intDefaultAndDefaultModel", "intDefaultAndDefaultPersist", "intDefaultModelAndDefaultPersist")
+        VALUES (DEFAULT, DEFAULT, DEFAULT);
+        ''',
+        );
+        var databaseObject = await IntDefaultMix.db.findFirstRow(session);
+        expect(databaseObject?.intDefaultAndDefaultModel, 10);
+      },
+    );
+
+    test(
+      'when creating a record in the database with an unsafe query, then the "intDefaultAndDefaultPersist" field value should match the defaultPersist value',
+      () async {
+        await session.db.unsafeQuery(
+          '''
+        INSERT INTO "${IntDefaultMix.t.tableName}" ("intDefaultAndDefaultModel", "intDefaultAndDefaultPersist", "intDefaultModelAndDefaultPersist")
+        VALUES (DEFAULT, DEFAULT, DEFAULT);
+        ''',
+        );
+        var databaseObject = await IntDefaultMix.db.findFirstRow(session);
+        expect(databaseObject?.intDefaultAndDefaultPersist, 20);
+      },
+    );
+
+    test(
+      'when creating a record in the database with an unsafe query, then the "intDefaultModelAndDefaultPersist" field value should match the defaultPersist value',
+      () async {
+        await session.db.unsafeQuery(
+          '''
+        INSERT INTO "${IntDefaultMix.t.tableName}" ("intDefaultAndDefaultModel", "intDefaultAndDefaultPersist", "intDefaultModelAndDefaultPersist")
+        VALUES (DEFAULT, DEFAULT, DEFAULT);
+        ''',
+        );
+        var databaseObject = await IntDefaultMix.db.findFirstRow(session);
+        expect(databaseObject?.intDefaultModelAndDefaultPersist, 20);
+      },
+    );
+
+    test(
+      'when creating a record in the database with specific values, then the "intDefaultAndDefaultModel" field value should match the provided value',
+      () async {
+        var specificObject = IntDefaultMix(
+          intDefaultAndDefaultModel: 30,
+        );
+        var specificDatabaseObject = await IntDefaultMix.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(specificDatabaseObject.intDefaultAndDefaultModel, 30);
+      },
+    );
+
+    test(
+      'when creating a record in the database with specific values, then the "intDefaultAndDefaultPersist" field value should match the provided value',
+      () async {
+        var specificObject = IntDefaultMix(
+          intDefaultAndDefaultPersist: 40,
+        );
+        var specificDatabaseObject = await IntDefaultMix.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(specificDatabaseObject.intDefaultAndDefaultPersist, 40);
+      },
+    );
+
+    test(
+      'when creating a record in the database with specific values, then the "intDefaultModelAndDefaultPersist" field value should match the provided value',
+      () async {
+        var specificObject = IntDefaultMix(
+          intDefaultModelAndDefaultPersist: 50,
+        );
+        var specificDatabaseObject = await IntDefaultMix.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(specificDatabaseObject.intDefaultModelAndDefaultPersist, 50);
+      },
+    );
+  });
+}
---
+++ tests/serverpod_test_server/test_integration/database_operations/default/integer/int_default_persist_test.dart
@@ -0,0 +1,55 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_test_server/src/generated/protocol.dart';
+import 'package:serverpod_test_server/test_util/test_serverpod.dart';
+import 'package:test/test.dart';
+
+void main() async {
+  var session = await IntegrationTestServer().session();
+
+  group('Given a class with "defaultPersist" fields,', () {
+    tearDownAll(() async => IntDefaultPersist.db.deleteWhere(
+          session,
+          where: (_) => Constant.bool(true),
+        ));
+
+    test(
+      'when creating a record in the database, then the "defaultPersist=10" field should be null',
+      () async {
+        var object = IntDefaultPersist();
+        var databaseObject = await IntDefaultPersist.db.insertRow(
+          session,
+          object,
+        );
+        expect(databaseObject.intDefaultPersist, isNull);
+      },
+    );
+
+    test(
+      'when creating a record in the database with an unsafe query, then the "defaultPersist=10" field should be 10',
+      () async {
+        await session.db.unsafeQuery(
+          '''
+        INSERT INTO ${IntDefaultPersist.t.tableName}
+        VALUES (DEFAULT);
+        ''',
+        );
+        var databaseObject = await IntDefaultPersist.db.findFirstRow(session);
+        expect(databaseObject?.intDefaultPersist, equals(10));
+      },
+    );
+
+    test(
+      'when creating a record in the database with a specific value, then the "intDefaultPersist" field value should match the provided value',
+      () async {
+        var specificObject = IntDefaultPersist(
+          intDefaultPersist: 20,
+        );
+        var specificDatabaseObject = await IntDefaultPersist.db.insertRow(
+          session,
+          specificObject,
+        );
+        expect(specificDatabaseObject.intDefaultPersist, equals(20));
+      },
+    );
+  });
+}
