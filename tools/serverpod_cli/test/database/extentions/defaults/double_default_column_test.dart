a79a67280af84d6b194103b95979230820457466
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-07-17 13:57:35 +0200

    test: Implementing column definitions tests for default values on double precision column types

---
+++ tools/serverpod_cli/test/database/extentions/defaults/double_default_column_test.dart
@@ -0,0 +1,119 @@
+import 'package:serverpod_cli/src/database/extensions.dart';
+import 'package:serverpod_service_client/serverpod_service_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Given Double column definition', () {
+    group('with 10.5 as default value', () {
+      ColumnDefinition defaultColumn = ColumnDefinition(
+        name: 'doubleDefault',
+        columnType: ColumnType.doublePrecision,
+        isNullable: false,
+        columnDefault: '10.5',
+        dartType: 'double',
+      );
+
+      test(
+          'when converting to PostgreSQL SQL code, then it should have the default value',
+          () {
+        expect(
+          defaultColumn.toPgSqlFragment(),
+          '"doubleDefault" double precision NOT NULL DEFAULT 10.5',
+        );
+      });
+    });
+
+    group('with 20.5 as default value', () {
+      ColumnDefinition defaultColumn = ColumnDefinition(
+        name: 'doubleDefault',
+        columnType: ColumnType.doublePrecision,
+        isNullable: false,
+        columnDefault: '20.5',
+        dartType: 'double',
+      );
+
+      test(
+          'when converting to PostgreSQL SQL code, then it should have the default value',
+          () {
+        expect(
+          defaultColumn.toPgSqlFragment(),
+          '"doubleDefault" double precision NOT NULL DEFAULT 20.5',
+        );
+      });
+    });
+
+    group('with no default value', () {
+      ColumnDefinition defaultColumn = ColumnDefinition(
+        name: 'doubleDefault',
+        columnType: ColumnType.doublePrecision,
+        isNullable: false,
+        dartType: 'double',
+      );
+
+      test(
+          'when converting to PostgreSQL SQL code, then it should not have the default value',
+          () {
+        expect(
+          defaultColumn.toPgSqlFragment(),
+          '"doubleDefault" double precision NOT NULL',
+        );
+      });
+    });
+
+    group('with nullable column and no default value', () {
+      ColumnDefinition defaultColumn = ColumnDefinition(
+        name: 'doubleDefault',
+        columnType: ColumnType.doublePrecision,
+        isNullable: true,
+        dartType: 'double',
+      );
+
+      test(
+          'when converting to PostgreSQL SQL code, then it should be nullable with no default value',
+          () {
+        expect(
+          defaultColumn.toPgSqlFragment(),
+          '"doubleDefault" double precision',
+        );
+      });
+    });
+
+    group('with nullable column and 10.5 as default value', () {
+      ColumnDefinition defaultColumn = ColumnDefinition(
+        name: 'doubleDefault',
+        columnType: ColumnType.doublePrecision,
+        isNullable: true,
+        columnDefault: '10.5',
+        dartType: 'double',
+      );
+
+      test(
+          'when converting to PostgreSQL SQL code, then it should be nullable with the default value',
+          () {
+        expect(
+          defaultColumn.toPgSqlFragment(),
+          '"doubleDefault" double precision DEFAULT 10.5',
+        );
+      });
+    });
+
+    group('with nullable column and 20.5 as default value', () {
+      ColumnDefinition defaultColumn = ColumnDefinition(
+        name: 'doubleDefault',
+        columnType: ColumnType.doublePrecision,
+        isNullable: true,
+        columnDefault: '20.5',
+        dartType: 'double',
+      );
+
+      test(
+          'when converting to PostgreSQL SQL code, then it should be nullable with the default value',
+          () {
+        expect(
+          defaultColumn.toPgSqlFragment(),
+          '"doubleDefault" double precision DEFAULT 20.5',
+        );
+      });
+    });
+  });
+}
