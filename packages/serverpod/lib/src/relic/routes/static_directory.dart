a8bc231688e9e4110ea455d9719e06ffa786f9b6
Luke Hutchison <luke.hutch@gmail.com>
2024-07-25 18:41:12 -0600

    Allow for rewrite rule for root path

--- packages/serverpod/lib/src/relic/routes/static_directory.dart
+++ packages/serverpod/lib/src/relic/routes/static_directory.dart
@@ -27,14 +27,25 @@ class RouteStaticDirectory extends Route {
   /// The path to the directory to serve static files from.
   final String? basePath;
 
+  /// The path to serve as the root path ('/'), e.g. '/index.html'.
+  final String? serveAsRootPath;
+
   /// Creates a static directory with the [serverDirectory] as its root.
-  RouteStaticDirectory({required this.serverDirectory, this.basePath});
+  RouteStaticDirectory({
+    required this.serverDirectory,
+    this.basePath,
+    this.serveAsRootPath,
+  });
 
   @override
   Future<bool> handleCall(Session session, HttpRequest request) async {
     session as MethodCallSession;
 
     var path = Uri.decodeFull(session.uri.path);
+
+    if (serveAsRootPath != null && path == '/') {
+      path = serveAsRootPath!;
+    }
 
     try {
       // Remove version control string
