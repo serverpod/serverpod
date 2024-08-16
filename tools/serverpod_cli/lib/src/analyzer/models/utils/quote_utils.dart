caf7b79bf3f863640d05d720691b712e9f06aec7
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-08-16 16:14:28 +0200

    docs: Adding information about code functionality on the quote_utils.dart file

--- tools/serverpod_cli/lib/src/analyzer/models/utils/quote_utils.dart
+++ tools/serverpod_cli/lib/src/analyzer/models/utils/quote_utils.dart
@@ -1,17 +1,39 @@
 /// Regular expression pattern that matches a string entirely enclosed
 /// in double quotes, allowing for escaped characters inside.
+///
+/// Example Matches:
+/// - `"Hello, world!"`
+/// - `"This is a \"quoted\" string"`
+/// - `"Another example with \\backslashes\\"`
 const fullDoubleQuotedStringPattern = r'^"(\\.|[^"\\])*"$';
 
 /// Regular expression pattern that matches a string entirely enclosed
 /// in single quotes, allowing for escaped characters inside.
+///
+/// Example Matches:
+/// - `'Hello, world!'`
+/// - `'This is a \'quoted\' string'`
+/// - `'Another example with \\backslashes\\'`
 const fullSingleQuotedStringPattern = r"^'(\\.|[^'\\])*'$";
 
 /// Regular expression pattern that captures the content of a double-quoted
 /// string, accounting for escaped characters, for replacement purposes.
+///
+/// Example Captures:
+/// - `"Hello, world!"` -> `Hello, world!`
+/// - `"This is a \"quoted\" string"` -> `This is a \"quoted\" string`
+/// - `"Another example with \\backslashes\\"` -> `Another example with \\backslashes\\`
 const doubleQuotedStringCapturePattern = r'"([^"\\]*(?:\\.[^"\\]*)*)"';
 
 /// Checks if the given value is a string that is entirely enclosed
 /// in double quotes, using the fullDoubleQuotedStringPattern.
+///
+/// Example:
+/// - Input: `"Hello, world!"`
+/// - Returns: `true`
+///
+/// - Input: `Hello, world!`
+/// - Returns: `false`
 bool isValidDoubleQuote(dynamic value) {
   if (value is! String) return false;
   return RegExp(fullDoubleQuotedStringPattern).hasMatch(value);
@@ -19,6 +41,13 @@ bool isValidDoubleQuote(dynamic value) {
 
 /// Checks if the given value is a string that is entirely enclosed
 /// in single quotes, using the fullSingleQuotedStringPattern.
+///
+/// Example:
+/// - Input: `'Hello, world!'`
+/// - Returns: `true`
+///
+/// - Input: `Hello, world!`
+/// - Returns: `false`
 bool isValidSingleQuote(dynamic value) {
   if (value is! String) return false;
   return RegExp(fullSingleQuotedStringPattern).hasMatch(value);
@@ -26,6 +55,13 @@ bool isValidSingleQuote(dynamic value) {
 
 /// Converts a double-quoted string to a single-quoted string, ensuring that
 /// any existing single quotes inside the string are properly escaped.
+///
+/// Example:
+/// - Input: `"Hello, world!"`
+/// - Output: `'Hello, world!'`
+///
+/// - Input: `"It's a \"beautiful\" day"`
+/// - Output: `'It\'s a "beautiful" day'`
 String convertToSingleQuotedString(String value) {
   return value.replaceAllMapped(
     RegExp(doubleQuotedStringCapturePattern),
