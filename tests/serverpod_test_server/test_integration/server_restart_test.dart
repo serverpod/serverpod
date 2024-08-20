d29f186dd59688818cdf8b6a1fa0241d63edf9ba
Alexander Sandor <alexander@serverpod.dev>
2024-08-20 13:48:59 +0200

    test: add failing test for scenario.

---
+++ tests/serverpod_test_server/test_integration/server_restart_test.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_test_server/test_util/mock_stdout.dart';
+import 'package:serverpod_test_server/test_util/test_serverpod.dart';
+import 'package:test/test.dart';
+
+void main() {
+  late Serverpod serverpod;
+
+  setUp(() {
+    serverpod = IntegrationTestServer.create();
+  });
+
+  tearDown(() async {
+    await serverpod.shutdown(exitProcess: false);
+  });
+
+  test(
+      'Given running Serverpod server when it is shutdown and started then it can be successfully started again.',
+      () async {
+    await serverpod.start();
+
+    await serverpod.shutdown(exitProcess: false);
+    var start = serverpod.start();
+
+    await expectLater(start, completes);
+  });
+
+  test(
+      'Given running Serverpod server when it is shutdown and started then database request can be made.',
+      () async {
+    await serverpod.start();
+
+    await serverpod.shutdown(exitProcess: false);
+    await serverpod.start();
+
+    var session = await serverpod.createSession();
+    await expectLater(session.db.testConnection(), completion(true));
+  });
+
+  test(
+      'Given running Serverpod server when it is shutdown and started then no error is written to stderr.',
+      () async {
+    var record = MockStdout();
+    await IOOverrides.runZoned(() async {
+      await serverpod.start();
+
+      await serverpod.shutdown(exitProcess: false);
+      var start = serverpod.start();
+
+      await expectLater(start, completes);
+    }, stderr: () => record);
+
+    expect(record.output, isEmpty);
+  });
+}
