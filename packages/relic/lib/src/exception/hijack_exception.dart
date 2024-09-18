58a612b1eba117a5cd3787c5aba525860630eebc
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-09-18 15:17:36 +0200

    chore: move Copyright notices from source files to LICENSE file

--- packages/relic/lib/src/exception/hijack_exception.dart
+++ packages/relic/lib/src/exception/hijack_exception.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2014, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 /// An exception used to indicate that a request has been hijacked.
 ///
 /// This shouldn't be captured by any code other than the relic server adapter that
