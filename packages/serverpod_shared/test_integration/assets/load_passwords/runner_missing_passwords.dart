c232560e5b31c09f740ed38b74d5db65876df1af
Isakdl <isak.duner@gmail.com>
2024-10-02 16:35:59 +0200

    refactor(review): Add random file name for missing passwords file.

--- packages/serverpod_shared/test_integration/assets/load_passwords/runner_missing_passwords.dart
+++ packages/serverpod_shared/test_integration/assets/load_passwords/runner_missing_passwords.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 import 'package:serverpod_shared/serverpod_shared.dart';
+import 'package:uuid/uuid.dart';
 
 void main() {
+  var missingFileName = const Uuid().v4();
+
   var passwords = PasswordManager(runMode: 'development').loadPasswords(
-    './config/missing_passwords.yaml',
+    './config/$missingFileName.yaml',
   );
 
   stdout.write(passwords.toString());
