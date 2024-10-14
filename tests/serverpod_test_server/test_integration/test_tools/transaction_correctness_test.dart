f19ff7e75e38f9d3ce71fa5aeeeaf8c34a69ed66
Hampus Lavin <hampus.lavin@gmail.com>
2024-10-14 15:39:36 +0200

    test(review): add demo test

--- tests/serverpod_test_server/test_integration/test_tools/transaction_correctness_test.dart
+++ tests/serverpod_test_server/test_integration/test_tools/transaction_correctness_test.dart
@@ -12,8 +12,7 @@ void main() {
 
       test(
           'when database exception occurs '
-          'then should close transaction after test suite and should not fail `dart test` by leaking exceptions',
-          () async {
+          'then should not fail `dart test` by leaking exceptions', () async {
         var future = session.db.transaction((tx) async {
           var data = UniqueData(number: 1, email: 'test@test.com');
           await UniqueData.db.insertRow(session, data);
@@ -32,4 +31,54 @@ void main() {
       });
     },
   );
+
+  group('Demontrate transaction difference between prod and test tools', () {
+    withServerpod(
+      'Given transaction call in test with database rollbacks enabled (default)',
+      (sessionBuilder, endpoints) {
+        var session = sessionBuilder.build();
+        test(
+            'when database exception occurs '
+            'then transaction WILL NOT throw exception if it was caught in the transaction',
+            () async {
+          var future = session.db.transaction((tx) async {
+            var data = UniqueData(number: 1, email: 'test@test.com');
+            try {
+              await UniqueData.db.insertRow(session, data);
+              await UniqueData.db.insertRow(session, data);
+            } catch (_) {}
+          });
+
+          // ATTENTION: This does not throw in test tools when rollbacks are enabled,
+          // but does throw in production environment where rollbacks are disabled!
+          await expectLater(future, completes);
+        });
+      },
+    );
+
+    withServerpod(
+      'Given transaction call in test with database rollbacks disabled',
+      (sessionBuilder, endpoints) {
+        var session = sessionBuilder.build();
+
+        test(
+            'when database exception occurs '
+            'then transaction WILL throw exception even if it was caught in the transaction',
+            () async {
+          var future = session.db.transaction((tx) async {
+            var data = UniqueData(number: 1, email: 'test@test.com');
+            try {
+              await UniqueData.db.insertRow(session, data, transaction: tx);
+              await UniqueData.db.insertRow(session, data, transaction: tx);
+            } catch (_) {}
+          });
+
+          // ATTENTION: This does not throw in test tools when rollbacks are enabled,
+          // but does throw in production environment where rollbacks are disabled!
+          await expectLater(future, throwsA(isA<Exception>()));
+        });
+      },
+      rollbackDatabase: RollbackDatabase.disabled,
+    );
+  });
 }
