d8be6ff005dbe2e28f97e3f36f075dff1761a384
Hampus Lavin <hampus.lavin@gmail.com>
2024-10-28 09:47:07 +0100

    refactor(review): add line break

--- templates/serverpod_templates/modulename_server/test/integration/module_endpoint_test.dart
+++ templates/serverpod_templates/modulename_server/test/integration/module_endpoint_test.dart
@@ -5,7 +5,8 @@ import 'test_tools/serverpod_test_tools.dart';
 
 void main() {
   // This is an example test that uses the `withServerpod` test helper.
-  // `withServerpod` enables you to call your endpoints directly from the test like regular functions.
+  // `withServerpod` enables you to call your endpoints directly from the test
+  // like regular functions.
   // Note that after adding or modifying an endpoint, you will need to run
   // `serverpod generate` to update the test tools code.
   // Refer to the docs for more information on how to use the test helper.
