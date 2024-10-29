4ee137449e9b2c860d138ea3f3e0e84201478977
Alexander Sandor <alexander@serverpod.dev>
2024-10-29 11:30:31 +0100

    test: add quick validation test.

--- tests/serverpod_test_server/test_integration/database_operations/transactions/transaction_rollback_test.dart
+++ tests/serverpod_test_server/test_integration/database_operations/transactions/transaction_rollback_test.dart
@@ -1,4 +1,4 @@
-import 'package:serverpod/database.dart';
+import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_test_server/src/generated/protocol.dart';
 import 'package:serverpod_test_server/test_util/test_serverpod.dart';
 import 'package:test/test.dart';
@@ -41,5 +41,98 @@ void main() async {
     expect(fetchedData, hasLength(2));
     expect(fetchedData.elementAtOrNull(0)?.number, data.number);
     expect(fetchedData.elementAtOrNull(1)?.number, data3.number);
+  });
+
+  tearDown(() async {
+    await UniqueData.db.deleteWhere(session, where: (t) => Constant.bool(true));
+  });
+
+  test(
+      'Given serializable transaction settings when modifying the same value concurrently then one transaction is rolled back.',
+      () async {
+    var initialValue = 'initial';
+    var data = await UniqueData.db
+        .insertRow(session, UniqueData(number: 1, email: initialValue));
+
+    var settings = TransactionSettings(
+      isolationLevel: IsolationLevel.serializable,
+    );
+
+    var update = (int id, Session session, String email) async {
+      try {
+        await session.db.transaction((t) async {
+          var localData = await UniqueData.db.findById(
+            session,
+            id,
+            transaction: t,
+          );
+          await Future.delayed(Duration(milliseconds: 100));
+          await UniqueData.db.updateRow(
+            session,
+            localData!.copyWith(email: email),
+            transaction: t,
+          );
+        }, settings: settings);
+      } catch (e) {
+        return false;
+      }
+
+      return true;
+    };
+
+    var result = await Future.wait([
+      update(data.id!, session, 'bob'),
+      update(data.id!, session, 'alex'),
+      update(data.id!, session, 'jim'),
+      update(data.id!, session, 'hank'),
+    ]);
+
+    var success = result.where((e) => e).length;
+    expect(success, 1);
+    var dataAfter = await UniqueData.db.findById(session, data.id!);
+    expect(dataAfter?.email, isNot(initialValue));
+  });
+
+  test(
+      'Given serializable transaction settings when value is modified outside concurrently then transaction is rolled back.',
+      () async {
+    var initialValue = 'initial';
+    var data = await UniqueData.db
+        .insertRow(session, UniqueData(number: 1, email: initialValue));
+
+    var settings = TransactionSettings(
+      isolationLevel: IsolationLevel.serializable,
+    );
+
+    var update = (int id, Session session, String email) async {
+      try {
+        await session.db.transaction((t) async {
+          var localData = await UniqueData.db.findById(
+            session,
+            id,
+            transaction: t,
+          );
+          await Future.delayed(Duration(milliseconds: 100));
+          await UniqueData.db.updateRow(
+            session,
+            localData!.copyWith(email: email),
+            transaction: t,
+          );
+        }, settings: settings);
+      } catch (e) {
+        return false;
+      }
+
+      return true;
+    };
+    var success = update(data.id!, session, 'bob');
+
+    await UniqueData.db.updateRow(
+      session,
+      data.copyWith(email: 'jim'),
+    );
+    expect(await success, isFalse);
+    var dataAfter = await UniqueData.db.findById(session, data.id!);
+    expect(dataAfter?.email, 'jim');
   });
 }
