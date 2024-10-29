98fb7cba85f1df958dffb0f4345fc4de93cd1437
Hampus Lavin <hampus.lavin@gmail.com>
2024-10-29 09:06:14 +0100

    refactor(review): fix typo

--- tests/serverpod_test_server/test_integration/test_tools/start_timeout_test/test_that_will_timeout_after_default.dart
+++ tests/serverpod_test_server/test_integration/test_tools/start_timeout_test/test_that_will_timeout_after_default.dart
@@ -6,7 +6,7 @@ import '../serverpod_test_tools.dart';
 void main() {
   withServerpod(
     'Given that runMode is set to staging when calling withServerpod',
-    // The staging config has intentionally been sedt to not find the database and provoke timeout.
+    // The staging config has intentionally been set to not find the database and provoke timeout.
     runMode: ServerpodRunMode.staging,
     (sessionBuilder, endpoints) {
       test('then dummy test should not be run and exit code should be non-zero',
