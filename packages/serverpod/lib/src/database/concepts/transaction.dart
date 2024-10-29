ab52f730faa62fc62cdba487a54b8584dd80c78b
Alexander Sandor <alexander@serverpod.dev>
2024-10-29 11:03:41 +0100

    feat: add transaction settings to database interface with isolation support.

--- packages/serverpod/lib/src/database/concepts/transaction.dart
+++ packages/serverpod/lib/src/database/concepts/transaction.dart
@@ -8,3 +8,34 @@ abstract interface class Transaction {
   /// exception depending on driver.
   Future<void> cancel();
 }
+
+/// Isolation levels for transactions.
+enum IsolationLevel {
+  /// A statement can only see rows committed before it began.
+  readCommitted,
+
+  /// Transaction may see uncommitted changes made by other transactions.
+  /// Not supported in PostgreSQL and is treated as readCommitted.
+  readUncommitted,
+
+  /// A transaction can only see rows committed before the first query or
+  /// data-modification statement was executed.
+  repeatableRead,
+
+  /// A transaction can only see rows committed before the first query or
+  /// data-modification statement was executed.
+  ///
+  /// If reads and writes among concurrent serializable transactions occur blablabla
+  serializable,
+}
+
+/// Settings for a transaction.
+class TransactionSettings {
+  /// The isolation level of the transaction.
+  final IsolationLevel isolationLevel;
+
+  /// Creates a new transaction settings object.
+  const TransactionSettings({
+    this.isolationLevel = IsolationLevel.readCommitted,
+  });
+}
