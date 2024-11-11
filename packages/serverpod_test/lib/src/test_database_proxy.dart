8297803720cecb2397e6adacda034b7fa3b67293
Alexander Sandor <alexander@serverpod.dev>
2024-11-11 14:37:35 +0100

    fix: migrate test framework to new savepoint system

--- packages/serverpod_test/lib/src/test_database_proxy.dart
+++ packages/serverpod_test/lib/src/test_database_proxy.dart
@@ -184,7 +184,7 @@ class TestDatabaseProxy implements Database {
     }
 
     try {
-      await _transactionManager.addSavePoint(lock: true);
+      await _transactionManager.addSavepoint(lock: true);
     } on ConcurrentTransactionsException {
       throw InvalidConfigurationException(
         'Concurrent calls to transaction are not supported when database rollbacks are enabled. '
@@ -194,10 +194,10 @@ class TestDatabaseProxy implements Database {
 
     try {
       var result = await transactionFunction(localTransaction);
-      await _transactionManager.releasePreviousSavePoint(unlock: true);
+      await _transactionManager.releasePreviousSavepoint(unlock: true);
       return result;
     } catch (e) {
-      await _transactionManager.rollbackToPreviousSavePoint(unlock: true);
+      await _transactionManager.rollbackToPreviousSavepoint(unlock: true);
       rethrow;
     }
   }
@@ -330,7 +330,7 @@ class TestDatabaseProxy implements Database {
 
     return _databaseOperationLock.synchronized(() async {
       try {
-        await _transactionManager.addSavePoint(
+        await _transactionManager.addSavepoint(
           lock: true,
           isPartOfTransaction: isPartOfUserTransaction,
         );
@@ -345,10 +345,10 @@ class TestDatabaseProxy implements Database {
 
       try {
         var result = await databaseOperation();
-        await _transactionManager.releasePreviousSavePoint(unlock: true);
+        await _transactionManager.releasePreviousSavepoint(unlock: true);
         return result;
       } on DatabaseException catch (_) {
-        await _transactionManager.rollbackToPreviousSavePoint(unlock: true);
+        await _transactionManager.rollbackToPreviousSavepoint(unlock: true);
         rethrow;
       }
     });
