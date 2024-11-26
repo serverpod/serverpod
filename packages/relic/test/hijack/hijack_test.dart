edd32f1e5d0f265c8a68307ab3fa1c0f490f003a
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-11-26 13:38:47 +0100

    refactor: Http request 'hijack' improvements

---
+++ packages/relic/test/hijack/hijack_test.dart
@@ -0,0 +1,89 @@
+import 'dart:async';
+
+import 'package:relic/relic.dart';
+import 'package:relic/src/method/method.dart';
+import 'package:stream_channel/stream_channel.dart';
+import 'package:test/test.dart';
+
+import '../util/test_util.dart';
+
+void main() {
+  const requestData = [1, 2, 3];
+  const responseData = [4, 5, 6];
+
+  test(
+    'Given a non-hijackable request when hijack is called then it throws a StateError',
+    () {
+      var request = Request(Method.get, localhostUri);
+      expect(() => request.hijack((_) {}), throwsStateError);
+    },
+  );
+
+  test(
+    'Given a hijackable request when hijack is called then it throws a HijackException and calls onHijack',
+    () {
+      var request =
+          Request(Method.get, localhostUri, onHijack: expectAsync1((callback) {
+        var streamController = StreamController<List<int>>();
+        streamController.add(requestData);
+        streamController.close();
+
+        var sinkController = StreamController<List<int>>();
+        expect(sinkController.stream.first, completion(equals(responseData)));
+
+        callback(StreamChannel(streamController.stream, sinkController));
+      }));
+
+      expect(
+        () => request.hijack(expectAsync1((channel) {
+          expect(channel.stream.first, completion(equals(requestData)));
+          channel.sink.add(responseData);
+          channel.sink.close();
+        })),
+        throwsHijackException,
+      );
+    },
+  );
+
+  test(
+    'Given a hijackable request when hijack is called twice then it throws a StateError',
+    () {
+      var request = Request(Method.get, localhostUri,
+          onHijack: expectAsync1((_) {}, count: 1));
+      expect(() => request.hijack((_) {}), throwsHijackException);
+      expect(() => request.hijack((_) {}), throwsStateError);
+    },
+  );
+
+  test(
+    'Given a hijackable request, when hijack is not called, then onHijack is not triggered',
+    () {
+      var request = Request(Method.get, localhostUri, onHijack: expectAsync1((_) {}, count: 0));
+      expect(() => request.hijack((_) {}), throwsStateError);
+    },
+  );
+
+  test(
+    'Given a hijackable request when hijack is called with empty data then it handles gracefully',
+    () {
+      var request =
+          Request(Method.get, localhostUri, onHijack: expectAsync1((callback) {
+        var streamController = StreamController<List<int>>();
+        streamController.close();
+
+        var sinkController = StreamController<List<int>>();
+        callback(StreamChannel(streamController.stream, sinkController));
+      }));
+
+      expect(
+        () => request.hijack(
+          expectAsync1((channel) {
+            expect(channel.stream.isEmpty, completion(isTrue));
+            channel.sink.close();
+          }),
+        ),
+        throwsHijackException,
+      );
+    },
+  );
+}
