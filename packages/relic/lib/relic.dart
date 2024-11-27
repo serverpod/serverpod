6b0e0b4224b1de03c36dff2f0c99cc9c3a3202ae
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2024-11-27 15:33:46 +0100

    refactor: Relic export order improvement

--- packages/relic/lib/relic.dart
+++ packages/relic/lib/relic.dart
@@ -1,19 +1,33 @@
 library serverpod_relic;
 
+/// Body related exports
 export 'src/body/body.dart' show Body;
-export 'src/handler/cascade.dart' show Cascade;
-export 'src/handler/handler.dart' show Handler;
-export 'src/headers/headers.dart' show Headers, CustomHeaders;
 export 'src/body/types/body_type.dart' show BodyType;
 export 'src/body/types/mime_type.dart' show MimeType;
-export 'src/hijack/exception/hijack_exception.dart' show HijackException;
-export 'src/middleware/middleware.dart' show Middleware, createMiddleware;
-export 'src/middleware/logger.dart' show logRequests;
-export 'src/middleware/middleware_extensions.dart' show MiddlewareExtensions;
+
+/// Handler related exports
+export 'src/handler/cascade.dart' show Cascade;
+export 'src/handler/handler.dart' show Handler;
 export 'src/handler/pipeline.dart' show Pipeline;
+
+// Headers related exports
+export 'src/headers/headers.dart' show Headers, CustomHeaders;
+
+/// Hijack related exports
+export 'src/hijack/exception/hijack_exception.dart' show HijackException;
+
+/// Message related exports
 export 'src/message/request.dart' show Request;
 export 'src/message/response.dart' show Response;
-export 'src/relic_server_serve.dart' show serve;
-export 'src/relic_server.dart' show RelicServer;
 
+/// Middleware related exports
+export 'src/middleware/middleware_logger.dart' show logRequests;
+export 'src/middleware/middleware.dart' show Middleware, createMiddleware;
+export 'src/middleware/middleware_extensions.dart' show MiddlewareExtensions;
+
+/// Relic server related exports
+export 'src/relic_server.dart' show RelicServer;
+export 'src/relic_server_serve.dart' show serve;
+
+/// Static handler export
 export 'src/static/static_handler.dart';
