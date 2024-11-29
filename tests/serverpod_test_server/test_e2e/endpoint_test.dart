1f800ac8e0cabb3cac17caedcff1f0106b45e9d0
Alexander Sandor <alexander@serverpod.dev>
2024-11-29 10:39:33 +0100

    test: add test for module endpoint and model defined outside of designated folders.

--- tests/serverpod_test_server/test_e2e/endpoint_test.dart
+++ tests/serverpod_test_server/test_e2e/endpoint_test.dart
@@ -1,4 +1,5 @@
 import 'package:serverpod_test_client/serverpod_test_client.dart';
+import 'package:serverpod_test_module_client/serverpod_test_module_client.dart';
 import 'package:serverpod_test_server/test_util/config.dart';
 import 'package:serverpod_test_server/test_util/test_key_manager.dart';
 import 'package:test/test.dart';
@@ -18,6 +19,15 @@ void main() {
   });
 
   group(
+      'Given a module endpoint that is defined outside of the endpoint directory',
+      () {
+    test('when calling the endpoint then call is successful', () async {
+      var response = client.modules.module.myModuleFeature.myFeatureMethod();
+      await expectLater(response, completion('Hello, world!'));
+    });
+  });
+
+  group(
       'Given an endpoint that returns a model defined outside of the models directory',
       () {
     test('when calling the endpoint then call is successful', () async {
@@ -26,6 +36,18 @@ void main() {
           response,
           completion(isA<MyFeatureModel>()
               .having((model) => model.name, 'name', 'Alex')));
+    });
+  });
+
+  group(
+      'Given a module endpoint that returns a model defined outside of the models directory',
+      () {
+    test('when calling the endpoint then call is successful', () async {
+      var response = client.modules.module.myModuleFeature.myFeatureModel();
+      await expectLater(
+          response,
+          completion(isA<MyModuleFeatureModel>()
+              .having((model) => model.name, 'name', 'Isak')));
     });
   });
 }
