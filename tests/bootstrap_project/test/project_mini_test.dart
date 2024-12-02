f03febc9c91291e428c522875389a8478ea12826
Alexander Sandor <alexander@serverpod.dev>
2024-12-02 17:07:06 +0100

    refactor: Clarify expects for project mini tests.

--- tests/bootstrap_project/test/project_mini_test.dart
+++ tests/bootstrap_project/test/project_mini_test.dart
@@ -134,10 +134,27 @@ void main() async {
     });
   });
 
-  group('Given a clean state', () {
+  group('Given a mini project', () {
     final (:projectName, :commandRoot) = createRandomProjectName(tempPath);
     final serverDir = createServerFolderPath(projectName);
     late Process createProcess;
+
+    setUpAll(() async {
+      createProcess = await startProcess(
+        'serverpod',
+        ['create', '--template', 'mini', projectName, '-v', '--no-analytics'],
+        workingDirectory: tempPath,
+        environment: {
+          'SERVERPOD_HOME': rootPath,
+        },
+      );
+
+      var createProjectExitCode = await createProcess.exitCode;
+      assert(
+        createProjectExitCode == 0,
+        'Failed to create the serverpod mini project.',
+      );
+    });
 
     tearDown(() async {
       await runProcess(
@@ -149,24 +166,9 @@ void main() async {
     });
 
     test(
-        'when creating a new project with the mini template and upgrading it to a full project then the project is created successfully and can be booted in maintenance mode with the apply-migrations flag.',
+        'when upgrading the project to a full project '
+        'then the project is created successfully and can be booted in maintenance mode with the apply-migrations flag.',
         () async {
-      createProcess = await startProcess(
-        'serverpod',
-        ['create', '--template', 'mini', projectName, '-v', '--no-analytics'],
-        workingDirectory: tempPath,
-        environment: {
-          'SERVERPOD_HOME': rootPath,
-        },
-      );
-
-      var createProjectExitCode = await createProcess.exitCode;
-      expect(
-        createProjectExitCode,
-        0,
-        reason: 'Failed to create the serverpod project.',
-      );
-
       var upgradeProcess = await startProcess(
         'serverpod',
         ['create', '--template', 'server', '.', '-v', '--no-analytics'],
@@ -212,17 +214,11 @@ void main() async {
             : null);
   });
 
-  group('Given a clean state', () {
+  group('Given a mini project', () {
     final (:projectName, :commandRoot) = createRandomProjectName(tempPath);
     final serverDir = createServerFolderPath(projectName);
     late Process createProcess;
-    tearDown(() async {
-      createProcess.kill();
-    });
-
-    test(
-        'when creating a new project with the mini template and upgrading it to a full project then the project is created successfully and can be booted in maintenance mode with the apply-migrations flag.',
-        () async {
+    setUpAll(() async {
       createProcess = await startProcess(
         'serverpod',
         ['create', '--template', 'mini', projectName, '-v', '--no-analytics'],
@@ -233,76 +229,100 @@ void main() async {
       );
 
       var createProjectExitCode = await createProcess.exitCode;
-      expect(
-        createProjectExitCode,
-        0,
-        reason: 'Failed to create the serverpod project.',
-      );
+      assert(createProjectExitCode == 0,
+          'Failed to create the serverpod mini project.');
+    });
 
-      var upgradeProcess = await startProcess(
-        'serverpod',
-        ['create', '--template', 'server', '.', '-v', '--no-analytics'],
-        workingDirectory: path.join(tempPath, serverDir),
-        environment: {
-          'SERVERPOD_HOME': rootPath,
-        },
-      );
+    tearDown(() async {
+      createProcess.kill();
+    });
 
-      var upgradeProjectExitCode = await upgradeProcess.exitCode;
-      expect(
-        upgradeProjectExitCode,
-        0,
-        reason: 'Failed to create the serverpod project.',
-      );
+    group(
+        'when creating a new project with the mini template and upgrading it to a full project',
+        () {
+      late Process upgradeProcess;
+      setUpAll(() async {
+        upgradeProcess = await startProcess(
+          'serverpod',
+          ['create', '--template', 'server', '.', '-v', '--no-analytics'],
+          workingDirectory: path.join(tempPath, serverDir),
+          environment: {
+            'SERVERPOD_HOME': rootPath,
+          },
+        );
 
-      var configDir =
-          Directory(path.join(tempPath, serverDir, 'config')).existsSync();
-      expect(
-        configDir,
-        isTrue,
-        reason: 'Config directory should exist but it was not found.',
-      );
+        await upgradeProcess.exitCode;
+      });
 
-      var deployDir =
-          Directory(path.join(tempPath, serverDir, 'deploy')).existsSync();
-      expect(
-        deployDir,
-        isTrue,
-        reason: 'Deploy directory should exist but it was not found.',
-      );
+      test('then the upgrade command completes successfully', () async {
+        var upgradeProjectExitCode = await upgradeProcess.exitCode;
+        expect(
+          upgradeProjectExitCode,
+          0,
+          reason: 'Failed to create the serverpod project.',
+        );
+      });
 
-      var webDir =
-          Directory(path.join(tempPath, serverDir, 'web')).existsSync();
-      expect(
-        webDir,
-        isTrue,
-        reason: 'Web directory should exist but it was not found.',
-      );
+      test('then the project contains a config directory', () {
+        var configDir =
+            Directory(path.join(tempPath, serverDir, 'config')).existsSync();
+        expect(
+          configDir,
+          isTrue,
+          reason: 'Config directory should exist but it was not found.',
+        );
+      });
 
-      var dockerFile =
-          File(path.join(tempPath, serverDir, 'Dockerfile')).existsSync();
-      expect(
-        dockerFile,
-        isTrue,
-        reason: 'Dockerfile should exist but it was not found.',
-      );
+      test('then the project contains a deploy directory', () {
+        var deployDir =
+            Directory(path.join(tempPath, serverDir, 'deploy')).existsSync();
+        expect(
+          deployDir,
+          isTrue,
+          reason: 'Deploy directory should exist but it was not found.',
+        );
+      });
 
-      var dockerComposeFile =
-          File(path.join(tempPath, serverDir, 'docker-compose.yaml'))
-              .existsSync();
-      expect(
-        dockerComposeFile,
-        isTrue,
-        reason: 'docker-compose.yml should exist but it was not found.',
-      );
+      test('then the project contains a web directory', () {
+        var webDir =
+            Directory(path.join(tempPath, serverDir, 'web')).existsSync();
+        expect(
+          webDir,
+          isTrue,
+          reason: 'Web directory should exist but it was not found.',
+        );
+      });
 
-      var gcloudIgnoreFile =
-          File(path.join(tempPath, serverDir, '.gcloudignore')).existsSync();
-      expect(
-        gcloudIgnoreFile,
-        isTrue,
-        reason: '.gcloudignore should exist but it was not found.',
-      );
+      test('then the project contains a dockerfile', () {
+        var dockerFile =
+            File(path.join(tempPath, serverDir, 'Dockerfile')).existsSync();
+        expect(
+          dockerFile,
+          isTrue,
+          reason: 'Dockerfile should exist but it was not found.',
+        );
+      });
+
+      test('then the project contains a docker-compose.yaml file', () {
+        var dockerComposeFile =
+            File(path.join(tempPath, serverDir, 'docker-compose.yaml'))
+                .existsSync();
+        expect(
+          dockerComposeFile,
+          isTrue,
+          reason: 'docker-compose.yml should exist but it was not found.',
+        );
+      });
+
+      test('then the project contains a .gcloudignore file', () {
+        var gcloudIgnoreFile =
+            File(path.join(tempPath, serverDir, '.gcloudignore')).existsSync();
+        expect(
+          gcloudIgnoreFile,
+          isTrue,
+          reason: '.gcloudignore should exist but it was not found.',
+        );
+      });
     });
   });
 
