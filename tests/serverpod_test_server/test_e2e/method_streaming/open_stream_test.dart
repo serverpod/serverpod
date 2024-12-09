e87d07062f402e163f3d778b2fb937a04af33b52
Alexander Sandor <alexander@serverpod.dev>
2024-12-09 13:47:53 +0100

    test(review): Add test for when streams are paused an resumed.

--- tests/serverpod_test_server/test_e2e/method_streaming/open_stream_test.dart
+++ tests/serverpod_test_server/test_e2e/method_streaming/open_stream_test.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:serverpod_test_client/serverpod_test_client.dart';
 import 'package:serverpod_test_server/test_util/config.dart';
 import 'package:test/test.dart';
@@ -13,6 +15,39 @@ void main() {
     for (var i = 0; i < 10; i++) {
       var stream = client.methodStreaming.intStreamFromValue(numMessages);
       streamCompleteFutures.add(stream.last);
+    }
+
+    for (var future in streamCompleteFutures) {
+      await expectLater(future, completes);
+    }
+  });
+
+  test(
+      'Given multiple method streaming connections to the same endpoint '
+      'when streams are listened, paused and resumed '
+      'then all streams complete successfully', () async {
+    var client = Client(serverUrl);
+    var numMessages = 1000;
+    List<Future> streamCompleteFutures = [];
+    for (var i = 0; i < 10; i++) {
+      var streamIsComplete = Completer();
+      streamCompleteFutures.add(streamIsComplete.future);
+
+      var stream = client.methodStreaming.intStreamFromValue(numMessages);
+      var beenPaused = false;
+      late StreamSubscription<int> subscription;
+      subscription = stream.listen(
+        (data) async {
+          if (!beenPaused) {
+            subscription.pause();
+            beenPaused = true;
+            await Future.delayed(Duration(milliseconds: 100), () {
+              subscription.resume();
+            });
+          }
+        },
+        onDone: () => streamIsComplete.complete(),
+      );
     }
 
     for (var future in streamCompleteFutures) {
