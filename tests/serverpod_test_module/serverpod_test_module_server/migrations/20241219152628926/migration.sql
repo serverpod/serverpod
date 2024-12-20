7f7e0c275512faee7714f15b1cae6976312e32a4
Alexander Sandor <137198655+SandPod@users.noreply.github.com>
2024-12-20 10:47:42 +0100

    chore: Update project migrations. (#3073)

---
+++ tests/serverpod_test_module/serverpod_test_module_server/migrations/20241219152628926/migration.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+--
+-- ACTION DROP TABLE
+--
+DROP TABLE "serverpod_auth_key" CASCADE;
+
+
+--
+-- MIGRATION VERSION FOR serverpod_test_module
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_test_module', '20241219152628926', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20241219152628926', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20240516151843329', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20240516151843329', "timestamp" = now();
+
+
+COMMIT;
