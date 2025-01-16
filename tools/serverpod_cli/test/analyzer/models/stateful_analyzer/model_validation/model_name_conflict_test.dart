405fcd54c14b7c661e0c5186d9019765565c3069
Klaudio Kuçaj <kkucaj.contact@gmail.com>
2025-01-16 13:18:45 +0100

    test: Adding module + project test cases and description improvements

--- tools/serverpod_cli/test/analyzer/models/stateful_analyzer/model_validation/model_name_conflict_test.dart
+++ tools/serverpod_cli/test/analyzer/models/stateful_analyzer/model_validation/model_name_conflict_test.dart
@@ -3,15 +3,128 @@ import 'package:serverpod_cli/src/analyzer/models/stateful_analyzer.dart';
 import 'package:serverpod_cli/src/generator/code_generation_collector.dart';
 import 'package:serverpod_cli/src/test_util/builders/generator_config_builder.dart';
 import 'package:serverpod_cli/src/test_util/builders/model_source_builder.dart';
+import 'package:serverpod_cli/src/util/model_helper.dart';
 import 'package:test/test.dart';
 
 void main() {
   var config = GeneratorConfigBuilder().build();
 
   test(
-      'Given multiple enums with the same name in different modules '
-      'when an enum is referenced '
-      'then the enum is resolved to the correct module', () {
+      'Given an enum with the same name defined in a module and then '
+      'the project (order matters) '
+      'when that enum is referenced '
+      'then the type is resolved to the correct module', () {
+    var commonEnumName = 'CommonEnum';
+    var firstModuleAlias = 'module1';
+    var modelSources = [
+      ModelSourceBuilder()
+          .withYaml(
+            '''
+        enum: $commonEnumName
+        values:
+          - value1
+          - value2
+        ''',
+          )
+          .withFileName('common_enum.yaml')
+          .withModuleAlias(firstModuleAlias)
+          .build(),
+      ModelSourceBuilder()
+          .withYaml(
+            '''
+        enum: $commonEnumName
+        values:
+          - value1
+          - value2
+        ''',
+          )
+          .withFileName('common_enum.yaml')
+          .withModuleAlias(defaultModuleAlias)
+          .build(),
+      ModelSourceBuilder()
+          .withYaml(
+            '''
+        class: Example
+        fields:
+          enumField: $commonEnumName
+        ''',
+          )
+          .withModuleAlias(defaultModuleAlias)
+          .build(),
+    ];
+
+    var collector = CodeGenerationCollector();
+    var models =
+        StatefulAnalyzer(config, modelSources, onErrorsCollector(collector))
+            .validateAll();
+
+    var model = models.last as ClassDefinition;
+    var enumField = model.fields.first;
+
+    expect(enumField.type.enumDefinition, isA<EnumDefinition>());
+
+    expect(enumField.type.enumDefinition?.moduleAlias, defaultModuleAlias);
+  });
+
+  test(
+      'Given a class with the same name defined in a module and then '
+      'the project (order matters) '
+      'when that class is referenced in a relation '
+      'then the relation is resolved to the correct module', () {
+    var commonClassName = 'CommonClass';
+    var firstModuleAlias = 'module1';
+    var modelSources = [
+      ModelSourceBuilder()
+          .withYaml(
+            '''
+        class: $commonClassName
+        table: common_class_$firstModuleAlias
+        fields:
+          name: String
+        ''',
+          )
+          .withFileName('common_class.yaml')
+          .withModuleAlias(firstModuleAlias)
+          .build(),
+      ModelSourceBuilder()
+          .withYaml(
+            '''
+        class: $commonClassName
+        table: common_class_$defaultModuleAlias
+        fields:
+          name: String
+        ''',
+          )
+          .withFileName('common_class.yaml')
+          .withModuleAlias(defaultModuleAlias)
+          .build(),
+      ModelSourceBuilder()
+          .withYaml(
+            '''
+        class: Example
+        table: example
+        fields:
+          objectRelation: $commonClassName?, relation
+        ''',
+          )
+          .withModuleAlias(defaultModuleAlias)
+          .build(),
+    ];
+
+    var collector = CodeGenerationCollector();
+    var models =
+        StatefulAnalyzer(config, modelSources, onErrorsCollector(collector))
+            .validateAll();
+
+    var model = models.last as ClassDefinition;
+    var fieldType = model.fields.last.relation as ObjectRelationDefinition;
+    expect(fieldType.parentTable, 'common_class_$defaultModuleAlias');
+  });
+
+  test(
+      'Given an enum with the same name defined in different modules (order matters)'
+      'when that enum is referenced '
+      'then the type is resolved to the correct module', () {
     var commonEnumName = 'CommonEnum';
     var firstModuleAlias = 'module1';
     var secondModuleAlias = 'module2';
@@ -63,8 +176,8 @@ void main() {
   });
 
   test(
-      'Given multiple classes with the same name in different modules '
-      'when a class has a relation to another class '
+      'Given a class with the same name defined in different modules (order matters) '
+      'when that class is referenced in a relation '
       'then the relation is resolved to the correct module', () {
     var commonClassName = 'CommonClass';
     var firstModuleAlias = 'module1';
