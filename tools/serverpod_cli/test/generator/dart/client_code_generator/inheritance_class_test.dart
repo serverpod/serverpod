2f567d12d5bf23ac27558afc38b0437a999b79e9
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-01-16 14:43:12 -0300

    Separate the test for the client generator

--- tools/serverpod_cli/test/generator/dart/client_code_generator/inheritance_class_test.dart
+++ tools/serverpod_cli/test/generator/dart/client_code_generator/inheritance_class_test.dart
@@ -35,6 +35,205 @@ void main() {
   );
 
   group(
+      'Given a child-class named $childClassName with one primitive var extending a parent-class named $parentClassName with one primitive var when generating code',
+      () {
+    var models = [
+      ClassDefinitionBuilder()
+          .withClassName(parentClassName)
+          .withFileName(parentClassFileName)
+          .withSimpleField('name', 'String')
+          .withChildClasses(
+        [
+          ClassDefinitionBuilder()
+              .withClassName(childClassName)
+              .withFileName(childClassFileName)
+              .withSimpleField('age', 'int')
+              .build(),
+        ],
+      ).build(),
+      ClassDefinitionBuilder()
+          .withClassName(childClassName)
+          .withFileName(childClassFileName)
+          .withSimpleField('age', 'int', nullable: true)
+          .withExtendsClass(
+            ClassDefinitionBuilder()
+                .withClassName(parentClassName)
+                .withFileName(parentClassFileName)
+                .withSimpleField('name', 'String')
+                .build(),
+          )
+          .build(),
+    ];
+
+    var codeMap = generator.generateSerializableModelsCode(
+      models: models,
+      config: config,
+    );
+
+    var parentCompilationUnit =
+        parseString(content: codeMap[parentExpectedFilePath]!).unit;
+    var childCompilationUnit =
+        parseString(content: codeMap[childExpectedFilePath]!).unit;
+
+    group('Then the $parentClassName', () {
+      var parentClass = CompilationUnitHelpers.tryFindClassDeclaration(
+        parentCompilationUnit,
+        name: parentClassName,
+      );
+
+      group('has a public constructor', () {
+        var publicConstructor =
+            CompilationUnitHelpers.tryFindConstructorDeclaration(
+          parentClass!,
+          name: null,
+        );
+
+        test('defined', () {
+          expect(publicConstructor, isNotNull);
+        });
+
+        test('without the factory keyword', () {
+          expect(publicConstructor?.factoryKeyword, isNull);
+        });
+      });
+    });
+
+    group('Then the $childClassName', () {
+      var childClass = CompilationUnitHelpers.tryFindClassDeclaration(
+        childCompilationUnit,
+        name: childClassName,
+      );
+
+      group('has a private constructor', () {
+        var privateConstructor =
+            CompilationUnitHelpers.tryFindConstructorDeclaration(
+          childClass!,
+          name: '_',
+        );
+
+        test('defined', () {
+          expect(privateConstructor, isNotNull);
+        });
+
+        test('with both classes vars as params', () {
+          expect(
+            privateConstructor?.parameters.toSource(),
+            '({required super.name, this.age})',
+          );
+        }, skip: privateConstructor == null);
+      });
+
+      group('has a factory constructor', () {
+        var factoryConstructor =
+            CompilationUnitHelpers.tryFindConstructorDeclaration(
+          childClass!,
+          name: null,
+        );
+
+        test('defined', () {
+          expect(factoryConstructor, isNotNull);
+        });
+
+        test('with the factory keyword', () {
+          expect(
+            factoryConstructor?.factoryKeyword,
+            isNotNull,
+            reason: 'No factory keyword found on $childClassName',
+          );
+        }, skip: factoryConstructor == null);
+
+        test('passed to private implementing class', () {
+          expect(
+            factoryConstructor?.redirectedConstructor?.toSource(),
+            '_${childClassName}Impl',
+          );
+        }, skip: factoryConstructor == null);
+
+        test('with the class vars as params', () {
+          expect(
+            factoryConstructor?.parameters.toSource(),
+            '({required String name, int? age})',
+          );
+        }, skip: factoryConstructor == null);
+      });
+
+      group('has a copyWith method', () {
+        var copyWithMethod = CompilationUnitHelpers.tryFindMethodDeclaration(
+          childClass!,
+          name: 'copyWith',
+        );
+
+        test('declared', () {
+          expect(
+            copyWithMethod,
+            isNotNull,
+            reason: 'No copyWith method found on $childClassName',
+          );
+        });
+
+        test('with the return type of $childClassName.', () {
+          expect(copyWithMethod?.returnType?.toSource(), childClassName);
+        }, skip: copyWithMethod == null);
+
+        test('with the named params set where all variables are nullable.', () {
+          expect(
+            copyWithMethod?.parameters?.toSource(),
+            '({String? name, int? age})',
+          );
+        }, skip: copyWithMethod == null);
+
+        test('with no code body.', () {
+          var sourceCode = copyWithMethod?.body.toSource();
+          expect(sourceCode, ';');
+        }, skip: copyWithMethod == null);
+      }, skip: childClass == null);
+    });
+    var copyWithClass = CompilationUnitHelpers.tryFindClassDeclaration(
+      childCompilationUnit,
+      name: '_${childClassName}Impl',
+    );
+    test('then a class named _${childClassName}Impl is generated.', () {
+      expect(
+        copyWithClass,
+        isNotNull,
+        reason: 'Missing definition for class named _${childClassName}Impl',
+      );
+    });
+
+    group('then the class named _${childClassName}Impl', () {
+      group('has a constructor', () {
+        var defaultConstructor =
+            CompilationUnitHelpers.tryFindConstructorDeclaration(
+          copyWithClass!,
+          name: null,
+        );
+
+        test('without a name.', () {
+          expect(
+            defaultConstructor,
+            isNotNull,
+            reason: 'Missing declaration for base private constructor.',
+          );
+        });
+
+        test('with the params set to the same as the parent class.', () {
+          expect(
+            defaultConstructor?.parameters.toSource(),
+            '({required String name, int? age})',
+          );
+        }, skip: defaultConstructor == null);
+
+        test('with super call to named private constructor', () {
+          expect(
+            defaultConstructor?.initializers.first.toSource(),
+            'super._(name: name, age: age)',
+          );
+        });
+      });
+    });
+  });
+
+  group(
       'Given a child-class named $childClassName with one primitive var and a var with default value extending a parent-class named $parentClassName with one primitive var and a var with default value when generating code',
       () {
     var models = [
@@ -92,14 +291,6 @@ void main() {
           name: null,
         );
 
-        test('defined', () {
-          expect(publicConstructor, isNotNull);
-        });
-
-        test('without the factory keyword', () {
-          expect(publicConstructor?.factoryKeyword, isNull);
-        });
-
         group('with the initializers', () {
           test('containing 1 entry', () {
             expect(publicConstructor?.initializers.length, 1);
@@ -127,10 +318,6 @@ void main() {
           childClass!,
           name: '_',
         );
-
-        test('defined', () {
-          expect(privateConstructor, isNotNull);
-        });
 
         test('with both classes vars as params', () {
           expect(
@@ -160,25 +347,6 @@ void main() {
           name: null,
         );
 
-        test('defined', () {
-          expect(factoryConstructor, isNotNull);
-        });
-
-        test('with the factory keyword', () {
-          expect(
-            factoryConstructor?.factoryKeyword,
-            isNotNull,
-            reason: 'No factory keyword found on $childClassName',
-          );
-        }, skip: factoryConstructor == null);
-
-        test('passed to private implementing class', () {
-          expect(
-            factoryConstructor?.redirectedConstructor?.toSource(),
-            '_${childClassName}Impl',
-          );
-        }, skip: factoryConstructor == null);
-
         test('with the class vars as params', () {
           expect(
             factoryConstructor?.parameters.toSource(),
@@ -193,28 +361,11 @@ void main() {
           name: 'copyWith',
         );
 
-        test('declared', () {
-          expect(
-            copyWithMethod,
-            isNotNull,
-            reason: 'No copyWith method found on $childClassName',
-          );
-        });
-
-        test('with the return type of $childClassName.', () {
-          expect(copyWithMethod?.returnType?.toSource(), childClassName);
-        }, skip: copyWithMethod == null);
-
         test('with the named params set where all variables are nullable.', () {
           expect(
             copyWithMethod?.parameters?.toSource(),
             '({String? name, int? parentDefault, int? age, int? childDefault})',
           );
-        }, skip: copyWithMethod == null);
-
-        test('with no code body.', () {
-          var sourceCode = copyWithMethod?.body.toSource();
-          expect(sourceCode, ';');
         }, skip: copyWithMethod == null);
       }, skip: childClass == null);
     });
@@ -222,13 +373,6 @@ void main() {
       childCompilationUnit,
       name: '_${childClassName}Impl',
     );
-    test('then a class named _${childClassName}Impl is generated.', () {
-      expect(
-        copyWithClass,
-        isNotNull,
-        reason: 'Missing definition for class named _${childClassName}Impl',
-      );
-    });
 
     group('then the class named _${childClassName}Impl', () {
       group('has a constructor', () {
@@ -237,14 +381,6 @@ void main() {
           copyWithClass!,
           name: null,
         );
-
-        test('without a name.', () {
-          expect(
-            defaultConstructor,
-            isNotNull,
-            reason: 'Missing declaration for base private constructor.',
-          );
-        });
 
         test('with the params set to the same as the parent class.', () {
           expect(
