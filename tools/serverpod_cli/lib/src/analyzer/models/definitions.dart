2e98a4f59b3596aa2bfbc0d118c360c314f157a8
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-01-21 20:26:03 -0300

    Revert "Implement getters to retrieve inherited indexes from classes"
    This reverts commit 36a0da37c01ee953a6df76c693ba0c3dcc9defd3.

--- tools/serverpod_cli/lib/src/analyzer/models/definitions.dart
+++ tools/serverpod_cli/lib/src/analyzer/models/definitions.dart
@@ -123,26 +123,6 @@ class ClassDefinition extends SerializableModelDefinition {
     ];
   }
 
-  /// Returns a list of all indexes declared in the parent class.
-  /// If there is no parent class, an empty list is returned.
-  /// Inherited indexes act as index generators for child classes.
-  /// They are created with the table name as prefix to the original name.
-  List<SerializableModelIndexDefinition> get inheritedIndexes {
-    var inherited = parentClass?.indexesIncludingInherited ?? [];
-    if (tableName == null) return inherited;
-    return [
-      for (var index in inherited) index.copyWithPrefix(tableName!),
-    ];
-  }
-
-  /// Returns a list of all indexes in this class, including inherited indexes.
-  List<SerializableModelIndexDefinition> get indexesIncludingInherited {
-    return [
-      ...inheritedIndexes,
-      ...indexes,
-    ];
-  }
-
   /// Returns `true` if this class is a parent class or sealed.
   bool get isParentClass => childClasses.isNotEmpty || isSealed;
 
@@ -332,16 +312,6 @@ class SerializableModelIndexDefinition {
     required this.unique,
     required this.fields,
   });
-
-  /// Copy the index with a new name that is prefixed with [prefix].
-  SerializableModelIndexDefinition copyWithPrefix(String prefix) {
-    return SerializableModelIndexDefinition(
-      name: '${prefix}_$name',
-      type: type,
-      unique: unique,
-      fields: fields,
-    );
-  }
 }
 
 /// A representation of a yaml file in the protocol directory defining an enum.
