2ab56bdce63a7a7f7919abcb7e37064d636d0208
Timm Preetz <timm@preetz.us>
2025-01-29 10:29:07 +0100

    Switch to multi-line strings for the doc comments

--- tools/serverpod_cli/lib/src/generator/dart/library_generators/class_generators/repository_classes.dart
+++ tools/serverpod_cli/lib/src/generator/dart/library_generators/class_generators/repository_classes.dart
@@ -209,29 +209,29 @@ class BuildRepositoryClass {
   Method _buildFindMethod(String className,
       Iterable<SerializableModelFieldDefinition> objectRelationFields) {
     return Method((m) => m
-      ..docs.add(
-          '/// Returns a list of [$className]s matching the given query parameters.\n'
-          '/// \n'
-          '/// Use [where] to specify which items to include in the return value.\n'
-          '/// If none is specified, all items will be returned.\n'
-          '///\n'
-          '/// To specify the order of the items use [orderBy] or [orderByList]\n'
-          '/// when sorting by multiple columns.\n'
-          '///\n'
-          '/// The maximum number of items can be set by [limit]. If no limit is set,\n'
-          '/// all items matching the query will be returned.\n'
-          '///\n'
-          '/// [offset] defines how many items to skip, after which [limit] (or all)\n'
-          '/// items are read from the database.\n'
-          '///\n'
-          '/// ```dart\n'
-          '/// var persons = await Persons.db.find(\n'
-          '///   session,\n'
-          '///   where: (t) => t.lastName.equals(\'Jones\'),\n'
-          '///   orderBy: (t) => t.firstName,\n'
-          '///   limit: 100,\n'
-          '/// );\n'
-          '/// ```')
+      ..docs.add('''
+/// Returns a list of [$className]s matching the given query parameters.
+/// 
+/// Use [where] to specify which items to include in the return value.
+/// If none is specified, all items will be returned.
+///
+/// To specify the order of the items use [orderBy] or [orderByList]
+/// when sorting by multiple columns.
+///
+/// The maximum number of items can be set by [limit]. If no limit is set,
+/// all items matching the query will be returned.
+///
+/// [offset] defines how many items to skip, after which [limit] (or all)
+/// items are read from the database.
+///
+/// ```dart
+/// var persons = await Persons.db.find(
+///   session,
+///   where: (t) => t.lastName.equals('Jones'),
+///   orderBy: (t) => t.firstName,
+///   limit: 100,
+/// );
+/// ```''')
       ..name = 'find'
       ..returns = TypeReference(
         (r) => r
@@ -330,24 +330,24 @@ class BuildRepositoryClass {
     Iterable<SerializableModelFieldDefinition> objectRelationFields,
   ) {
     return Method((m) => m
-      ..docs.add(
-          '/// Returns the first matching [$className] matching the given query parameters.\n'
-          '/// \n'
-          '/// Use [where] to specify which items to include in the return value.\n'
-          '/// If none is specified, all items will be returned.\n'
-          '///\n'
-          '/// To specify the order use [orderBy] or [orderByList]\n'
-          '/// when sorting by multiple columns.\n'
-          '///\n'
-          '/// [offset] defines how many items to skip, after which the next one will be picked.\n'
-          '///\n'
-          '/// ```dart\n'
-          '/// var youngestPerson = await Persons.db.findFirstRow(\n'
-          '///   session,\n'
-          '///   where: (t) => t.lastName.equals(\'Jones\'),\n'
-          '///   orderBy: (t) => t.age,\n'
-          '/// );\n'
-          '/// ```')
+      ..docs.add('''
+/// Returns the first matching [$className] matching the given query parameters.
+/// 
+/// Use [where] to specify which items to include in the return value.
+/// If none is specified, all items will be returned.
+///
+/// To specify the order use [orderBy] or [orderByList]
+/// when sorting by multiple columns.
+///
+/// [offset] defines how many items to skip, after which the next one will be picked.
+///
+/// ```dart
+/// var youngestPerson = await Persons.db.findFirstRow(
+///   session,
+///   where: (t) => t.lastName.equals('Jones'),
+///   orderBy: (t) => t.age,
+/// );
+/// ```''')
       ..name = 'findFirstRow'
       ..returns = TypeReference(
         (r) => r
@@ -491,13 +491,13 @@ class BuildRepositoryClass {
   Method _buildInsertMethod(String className) {
     return Method((methodBuilder) {
       methodBuilder
-        ..docs.add(
-            '/// Inserts all [$className]s in the list and returns the inserted rows.\n'
-            '///\n'
-            '/// The returned [$className]s will have their `id` fields set.\n'
-            '///\n'
-            '/// This is an atomic operation, meaning that if one of the rows fails to\n'
-            '/// insert, none of the rows will be inserted.')
+        ..docs.add('''
+/// Inserts all [$className]s in the list and returns the inserted rows.
+///
+/// The returned [$className]s will have their `id` fields set.
+///
+/// This is an atomic operation, meaning that if one of the rows fails to
+/// insert, none of the rows will be inserted.''')
         ..name = 'insert'
         ..returns = TypeReference(
           (r) => r
@@ -540,10 +540,10 @@ class BuildRepositoryClass {
   Method _buildInsertRowMethod(String className) {
     return Method((methodBuilder) {
       methodBuilder
-        ..docs.add(
-            '/// Inserts a single [$className] and returns the inserted row.\n'
-            '///\n'
-            '/// The returned [$className] will have its `id` field set.')
+        ..docs.add('''
+/// Inserts a single [$className] and returns the inserted row.
+///
+/// The returned [$className] will have its `id` field set.''')
         ..name = 'insertRow'
         ..returns = TypeReference(
           (r) => r
@@ -586,12 +586,12 @@ class BuildRepositoryClass {
   Method _buildUpdateMethod(String className) {
     return Method((methodBuilder) {
       methodBuilder
-        ..docs.add(
-            '/// Updates all [$className]s in the list and returns the updated rows. If\n'
-            '/// [columns] is provided, only those columns will be updated. Defaults to\n'
-            '/// all columns.\n'
-            '/// This is an atomic operation, meaning that if one of the rows fails to\n'
-            '/// update, none of the rows will be updated.')
+        ..docs.add('''
+/// Updates all [$className]s in the list and returns the updated rows. If
+/// [columns] is provided, only those columns will be updated. Defaults to
+/// all columns.
+/// This is an atomic operation, meaning that if one of the rows fails to
+/// update, none of the rows will be updated.''')
         ..name = 'update'
         ..returns = TypeReference(
           (r) => r
@@ -644,10 +644,10 @@ class BuildRepositoryClass {
   Method _buildUpdateRowMethod(String className) {
     return Method((methodBuilder) {
       methodBuilder
-        ..docs.add(
-            '/// Updates a single [$className]. The row needs to have its id set.\n'
-            '/// Optionally, a list of [columns] can be provided to only update those\n'
-            '/// columns. Defaults to all columns.')
+        ..docs.add('''
+/// Updates a single [$className]. The row needs to have its id set.
+/// Optionally, a list of [columns] can be provided to only update those
+/// columns. Defaults to all columns.''')
         ..name = 'updateRow'
         ..returns = TypeReference(
           (r) => r
@@ -700,10 +700,10 @@ class BuildRepositoryClass {
   Method _buildDeleteMethod(String className) {
     return Method((methodBuilder) {
       methodBuilder
-        ..docs.add(
-            '/// Deletes all [$className]s in the list and returns the deleted rows.\n'
-            '/// This is an atomic operation, meaning that if one of the rows fail to\n'
-            '/// be deleted, none of the rows will be deleted.')
+        ..docs.add('''
+/// Deletes all [$className]s in the list and returns the deleted rows.
+/// This is an atomic operation, meaning that if one of the rows fail to
+/// be deleted, none of the rows will be deleted.''')
         ..name = 'delete'
         ..returns = TypeReference(
           (r) => r
@@ -855,9 +855,9 @@ class BuildRepositoryClass {
   Method _buildCountMethod(String className) {
     return Method((methodBuilder) {
       methodBuilder
-        ..docs.add(
-            '/// Counts the number of rows matching the [where] expression. If omitted,\n'
-            '/// will return the count of all rows in the table.')
+        ..docs.add('''
+/// Counts the number of rows matching the [where] expression. If omitted,
+/// will return the count of all rows in the table.''')
         ..name = 'count'
         ..returns = TypeReference(
           (r) => r
