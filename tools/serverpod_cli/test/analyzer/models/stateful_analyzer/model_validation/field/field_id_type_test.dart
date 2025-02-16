2777b53c9478f8362a273b93891dc78a3f5bb0df
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-02-15 22:48:54 -0300

    refactor: Refactor field id type tests with int also requiring default value

--- tools/serverpod_cli/test/analyzer/models/stateful_analyzer/model_validation/field/field_id_type_test.dart
+++ tools/serverpod_cli/test/analyzer/models/stateful_analyzer/model_validation/field/field_id_type_test.dart
@@ -10,69 +10,138 @@ import 'package:test/test.dart';
 void main() {
   var config = GeneratorConfigBuilder().build();
 
-  group(
-    'Given a class with a table and an int field called "id" defined.',
-    () {
-      var models = [
-        ModelSourceBuilder().withYaml(
-          '''
-        class: Example
-        table: example
-        fields:
-          id: int, default=serial
-        ''',
-        ).build()
-      ];
-
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      var definition = definitions.first as ClassDefinition;
-
-      test('then no errors are collected,', () {
-        expect(collector.errors, isEmpty);
-      });
-
-      test('then only one int id field is defined.', () {
-        expect(definition.fields, hasLength(1));
-        expect(definition.fields.first.name, 'id');
-        expect(definition.fields.first.type.className, 'int');
-      });
-
-      test('then the id field is nullable despite the declaration.', () {
-        expect(definition.fields.first.type.nullable, true);
-      });
-    },
-  );
-
-  test(
-    'Given a class with a table defined, then add an id field to the generated model.',
-    () {
-      var models = [
-        ModelSourceBuilder().withYaml(
-          '''
+  group('Given a class with a table defined and no id field', () {
+    var models = [
+      ModelSourceBuilder().withYaml(
+        '''
         class: Example
         table: example
         fields:
           name: String
         ''',
+      ).build()
+    ];
+
+    var collector = CodeGenerationCollector();
+    var analyzer =
+        StatefulAnalyzer(config, models, onErrorsCollector(collector));
+    var definitions = analyzer.validateAll();
+
+    var definition = definitions.first as ClassDefinition;
+
+    test('then an id field is added to the generated model.', () {
+      expect(definition.fields.first.name, 'id');
+    });
+
+    test('then the id type is "int".', () {
+      expect(definition.fields.first.type.className, 'int');
+    });
+
+    test('then the id type is nullable.', () {
+      expect(definition.fields.first.type.nullable, true);
+    });
+
+    test('then the default persist is "serial".', () {
+      expect(definition.fields.first.defaultPersistValue, defaultIntSerial);
+    });
+  });
+
+  for (var idType in SupportedIdType.all) {
+    var idClassName = idType.className;
+    var defaultValue = idType.defaultValue;
+
+    group('Given a class with the $idClassName id type', () {
+      test('and no default value, then an error is collected.', () {
+      var models = [
+        ModelSourceBuilder().withYaml(
+          '''
+          class: Example
+          table: example
+          fields:
+            id: $idClassName
+          ''',
         ).build()
       ];
 
       var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
+        StatefulAnalyzer(config, models, onErrorsCollector(collector))
+            .validateAll();
+
+        expect(
+          collector.errors.first.message,
+          contains('The type "$idClassName" must have a default value.'),
+        );
+      });
+
+      group('and default value set to $defaultValue', () {
+        var yamlSource = ModelSourceBuilder().withYaml(
+          '''
+        class: Example
+        table: example
+        fields:
+          id: $idClassName, default=$defaultValue
+        ''',
+        ).build();
+
+      var collector = CodeGenerationCollector();
+        var statefulAnalyzer = StatefulAnalyzer(config, [yamlSource]);
+        var definitions = statefulAnalyzer.validateAll();
+        var errors = collector.errors;
+
+        test('then no errors are collected.', () {
+          expect(errors, isEmpty);
+        });
 
       var definition = definitions.first as ClassDefinition;
 
-      expect(definition.fields.first.name, 'id');
-      expect(definition.fields.first.type.className, 'int');
-      expect(definition.fields.first.defaultPersistValue, defaultIntSerial);
-      expect(definition.fields.first.type.nullable, true);
-    },
-  );
+        test("then the id of the table is '$idClassName'.", () {
+          expect(definition.idField.type.className, idClassName);
+        }, skip: errors.isNotEmpty);
+
+        test('then the id type is nullable.', () {
+          expect(definition.idField.type.nullable, true);
+        }, skip: errors.isNotEmpty);
+
+        test("then the default persist value is '$defaultValue'", () {
+          expect(definition.idField.defaultPersistValue, defaultValue);
+        }, skip: errors.isNotEmpty);
+      });
+
+      for (var forbiddenKey in [
+        Keyword.defaultModelKey,
+        Keyword.defaultPersistKey,
+        Keyword.persist,
+        Keyword.scope,
+      ]) {
+        test(
+          'and the forbidden $forbiddenKey key, then an error is collected.',
+          () {
+            var models = [
+              ModelSourceBuilder().withYaml(
+                '''
+            class: Example
+            table: example
+            fields:
+              id: $idClassName, default=$defaultValue, $forbiddenKey
+            ''',
+              ).build()
+            ];
+
+            var collector = CodeGenerationCollector();
+            StatefulAnalyzer(config, models, onErrorsCollector(collector))
+                .validateAll();
+
+            expect(
+              collector.errors.first.message,
+              contains(
+                'The "$forbiddenKey" key is not allowed on the "id" field.',
+              ),
+            );
+          },
+        );
+      }
+    });
+  }
 
   test(
     'Given a class without a table defined, then no id field is added.',
@@ -125,99 +194,6 @@ void main() {
       );
     },
   );
-
-  for (var forbiddenKey in [
-    Keyword.defaultModelKey,
-    Keyword.defaultPersistKey,
-    Keyword.persist,
-    Keyword.scope,
-  ]) {
-    test(
-      'Given a class with the int id field and the forbidden $forbiddenKey key, then an error is collected.',
-      () {
-        var models = [
-          ModelSourceBuilder().withYaml(
-            '''
-            class: Example
-            table: example
-            fields:
-              id: int, default=serial, $forbiddenKey
-            ''',
-          ).build()
-        ];
-
-        var collector = CodeGenerationCollector();
-        StatefulAnalyzer(config, models, onErrorsCollector(collector))
-            .validateAll();
-
-        expect(
-          collector.errors.first.message,
-          contains('The "$forbiddenKey" key is not allowed on the "id" field.'),
-        );
-      },
-    );
-  }
-
-  for (var idType in SupportedIdType.all) {
-    var idClassName = idType.className;
-    var defaultValue = idType.defaultValue;
-
-    test(
-        'Given a class with the $idClassName id type and no default value, then an error is collected.',
-        () {
-      var models = [
-        ModelSourceBuilder().withYaml(
-          '''
-          class: Example
-          table: example
-          fields:
-            id: $idClassName
-          ''',
-        ).build()
-      ];
-
-      var collector = CodeGenerationCollector();
-      StatefulAnalyzer(config, models, onErrorsCollector(collector))
-          .validateAll();
-
-      expect(
-        collector.errors.first.message,
-        contains('The type "$idClassName" must have a default value.'),
-      );
-    });
-
-    group(
-        'Given a class with the $idClassName id type and default=$defaultValue',
-        () {
-      var yamlSource = ModelSourceBuilder().withYaml(
-        '''
-        class: Example
-        table: example
-        fields:
-          id: $idClassName, default=$defaultValue
-        ''',
-      ).build();
-
-      var collector = CodeGenerationCollector();
-      var statefulAnalyzer = StatefulAnalyzer(config, [yamlSource]);
-      var definitions = statefulAnalyzer.validateAll();
-      var errors = collector.errors;
-
-      test('then no errors are collected.', () {
-        expect(errors, isEmpty);
-      });
-
-      var definition = definitions.first as ClassDefinition;
-
-      test("then the id of the table is '$idClassName'.", () {
-        expect(definition.idField.type.className, idClassName);
-      }, skip: errors.isNotEmpty);
-
-      test("then the default persist value is '$defaultValue'", () {
-        expect(definition.idField.defaultPersistValue, defaultValue);
-      }, skip: errors.isNotEmpty);
-    });
-  }
 
   test(
       'Given a class with the UuidValue id type and an invalid default value, then an error is collected.',
