deca3928496623e18665072ea6f9f81c40ccd177
Timm Preetz <timm@preetz.us>
2025-02-19 13:49:03 +0100

    Remove Stream methods from record test endpoint

--- tests/serverpod_test_server/lib/src/endpoints/record_parameters.dart
+++ tests/serverpod_test_server/lib/src/endpoints/record_parameters.dart
@@ -15,29 +15,11 @@ class RecordParametersEndpoint extends Endpoint {
     return record;
   }
 
-  Stream<(int,)> streamIntRecordEcho(
-    Session session,
-    Stream<(int,)> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<(int,)?> returnNullableIntRecord(
     Session session,
     (int,)? record,
   ) async {
     return record;
-  }
-
-  Stream<(int,)?> streamNullableIntRecordEcho(
-    Session session,
-    Stream<(int,)?> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<(int, String)> returnIntStringRecord(
@@ -47,27 +29,9 @@ class RecordParametersEndpoint extends Endpoint {
     return record;
   }
 
-  Stream<(int, String)> streamIntStringRecordEcho(
-    Session session,
-    Stream<(int, String)> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<(int, String)?> returnNullableIntStringRecord(
       Session session, (int, String)? record) async {
     return record;
-  }
-
-  Stream<(int, String)?> streamNullableIntStringRecordEcho(
-    Session session,
-    Stream<(int, String)?> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<(int, SimpleData)> returnIntSimpleDataRecord(
@@ -77,27 +41,9 @@ class RecordParametersEndpoint extends Endpoint {
     return record;
   }
 
-  Stream<(int, SimpleData)> streamIntSimpleDataRecordEcho(
-    Session session,
-    Stream<(int, SimpleData)> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<(int, SimpleData)?> returnNullableIntSimpleDataRecord(
       Session session, (int, SimpleData)? record) async {
     return record;
-  }
-
-  Stream<(int, SimpleData)?> streamNullableIntSimpleDataRecordEcho(
-    Session session,
-    Stream<(int, SimpleData)?> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<({int number, String text})> returnNamedIntStringRecord(
@@ -107,15 +53,6 @@ class RecordParametersEndpoint extends Endpoint {
     return record;
   }
 
-  Stream<({int number, String text})> streamNamedIntStringRecordEcho(
-    Session session,
-    Stream<({int number, String text})> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<({int number, String text})?> returnNamedNullableIntStringRecord(
     Session session,
     ({int number, String text})? record,
@@ -123,29 +60,11 @@ class RecordParametersEndpoint extends Endpoint {
     return record;
   }
 
-  Stream<(int, String)?> streamNamedNullableIntStringRecordEcho(
-    Session session,
-    Stream<(int, String)?> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<({int number, SimpleData data})> returnNamedIntSimpleDataRecord(
     Session session,
     ({int number, SimpleData data}) record,
   ) async {
     return record;
-  }
-
-  Stream<({int number, SimpleData data})> streamNamedIntSimpleDataRecordEcho(
-    Session session,
-    Stream<({int number, SimpleData data})> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<({int number, SimpleData data})?>
@@ -156,30 +75,11 @@ class RecordParametersEndpoint extends Endpoint {
     return record;
   }
 
-  Stream<({int number, SimpleData data})?>
-      streamNamedNullableIntSimpleDataRecordEcho(
-    Session session,
-    Stream<({int number, SimpleData data})?> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<_MixedRecordTypeDef> returnRecordTypedef(
     Session session,
     _MixedRecordTypeDef record,
   ) async {
     return record;
-  }
-
-  Stream<_MixedRecordTypeDef> streamRecordTypedef(
-    Session session,
-    Stream<_MixedRecordTypeDef> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<_MixedRecordTypeDef?> returnNullableRecordTypedef(
@@ -189,15 +89,6 @@ class RecordParametersEndpoint extends Endpoint {
     return record;
   }
 
-  Stream<_MixedRecordTypeDef?> streamNullableRecordTypedef(
-    Session session,
-    Stream<_MixedRecordTypeDef?> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<List<(int, SimpleData)>> returnListOfIntSimpleDataRecord(
     Session session,
     List<(int, SimpleData)> recordList,
@@ -205,30 +96,11 @@ class RecordParametersEndpoint extends Endpoint {
     return recordList;
   }
 
-  Stream<List<(int, SimpleData)>> streamListOfIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<List<(int, SimpleData)>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<List<(int, SimpleData)?>> returnListOfNullableIntSimpleDataRecord(
     Session session,
     List<(int, SimpleData)?> record,
   ) async {
     return record;
-  }
-
-  Stream<List<(int, SimpleData)?>>
-      streamListOfNullableIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<List<(int, SimpleData)?>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<Set<(int, SimpleData)>?> returnNullableSetOfIntSimpleDataRecord(
@@ -245,30 +117,11 @@ class RecordParametersEndpoint extends Endpoint {
     return recordSet;
   }
 
-  Stream<Set<(int, SimpleData)>> streamSetOfIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<Set<(int, SimpleData)>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<Set<(int, SimpleData)?>> returnSetOfNullableIntSimpleDataRecord(
     Session session,
     Set<(int, SimpleData)?> set,
   ) async {
     return set;
-  }
-
-  Stream<Set<(int, SimpleData)?>>
-      streamSetOfNullableIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<Set<(int, SimpleData)?>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<Map<String, (int, SimpleData)>> returnStringMapOfIntSimpleDataRecord(
@@ -293,32 +146,12 @@ class RecordParametersEndpoint extends Endpoint {
     return {};
   }
 
-  Stream<Map<String, (int, SimpleData)>>
-      streamStringMapOfIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<Map<String, (int, SimpleData)>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<Map<String, (int, SimpleData)?>>
       returnStringMapOfNullableIntSimpleDataRecord(
     Session session,
     Map<String, (int, SimpleData)?> map,
   ) async {
     return map;
-  }
-
-  Stream<Map<String, (int, SimpleData)?>>
-      streamStringMapOfNullableIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<Map<String, (int, SimpleData)?>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<Map<(String, int), (int, SimpleData)>>
@@ -329,32 +162,12 @@ class RecordParametersEndpoint extends Endpoint {
     return map;
   }
 
-  Stream<Map<(String, int), (int, SimpleData)>>
-      streamRecordMapOfIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<Map<(String, int), (int, SimpleData)>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
-  }
-
   Future<Map<String, (int, SimpleData)?>>
       returnRecordMapOfNullableIntSimpleDataRecord(
     Session session,
     Map<String, (int, SimpleData)?> map,
   ) async {
     return map;
-  }
-
-  Stream<Map<(String, int), (int, SimpleData)?>>
-      streamRecordMapOfNullableIntSimpleDataRecordEchoStream(
-    Session session,
-    Stream<Map<(String, int), (int, SimpleData)?>> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<((int, String), {(SimpleData, double) namedSubRecord})>
@@ -371,16 +184,6 @@ class RecordParametersEndpoint extends Endpoint {
     List<((int, String), {(SimpleData, double) namedSubRecord})> recordList,
   ) async {
     return recordList;
-  }
-
-  Stream<((int, String), {(SimpleData, double) namedSubRecord})>
-      streamNestedRecordEchoStream(
-    Session session,
-    Stream<((int, String), {(SimpleData, double) namedSubRecord})> value,
-  ) async* {
-    await for (var i in value) {
-      yield i;
-    }
   }
 
   Future<({(SimpleData, double)? namedSubRecord})>
