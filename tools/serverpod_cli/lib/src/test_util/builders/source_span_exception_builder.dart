97acff9c8317af77c4d844990885c0aa57fa2813
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-02-22 14:15:14 +0100

    Add builder for SourceSpanException from ModelSource

---
+++ tools/serverpod_cli/lib/src/test_util/builders/source_span_exception_builder.dart
@@ -0,0 +1,37 @@
+import 'package:serverpod_cli/analyzer.dart';
+import 'package:serverpod_cli/src/analyzer/models/stateful_analyzer.dart';
+import 'package:serverpod_cli/src/generator/code_generation_collector.dart';
+import 'package:serverpod_cli/src/test_util/builders/generator_config_builder.dart';
+import 'package:serverpod_cli/src/test_util/builders/model_source_builder.dart';
+import 'package:serverpod_cli/src/util/model_helper.dart';
+
+class SourceSpanExceptionBuilder {
+  GeneratorConfig? generatorConfig;
+  List<ModelSource> models;
+
+  SourceSpanExceptionBuilder({
+    this.generatorConfig,
+    Iterable<ModelSource>? models,
+  }) : models = models?.toList() ?? [];
+
+  void addSourceFile(String fileName, String contents) => models.add(
+      ModelSourceBuilder().withFileName(fileName).withYaml(contents).build());
+
+  List<SourceSpanException> build() =>
+      models.validateAll(generatorConfig: generatorConfig);
+}
+
+extension ModelSourceListExtension on List<ModelSource> {
+  List<SourceSpanException> validateAll({GeneratorConfig? generatorConfig}) {
+    var collector = CodeGenerationCollector();
+
+    var analyzer = StatefulAnalyzer(
+      generatorConfig ?? GeneratorConfigBuilder().build(),
+      this,
+      onErrorsCollector(collector),
+    );
+    analyzer.validateAll();
+
+    return collector.errors;
+  }
+}
