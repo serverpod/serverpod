6909f0aed3814a68dec690ea8458dbafe9cf7c02
Timm Preetz <timm@preetz.us>
2025-02-25 12:21:11 +0100

    fix(review): Add table migration test for optional relationship

--- tools/serverpod_cli/test/database/extentions/migration_with_many_to_many_relation_pgsql_code_generation_test.dart
+++ tools/serverpod_cli/test/database/extentions/migration_with_many_to_many_relation_pgsql_code_generation_test.dart
@@ -107,6 +107,103 @@ fields:
   });
 
   test(
+      'Given a table that is optionally referenced by another one, when it is renamed, then the migration code should drop both tables and recreate them in a working order',
+      () {
+    DatabaseDefinition sourceDefinition;
+    {
+      var models = [
+        ModelSourceBuilder().withFileName('target').withYaml(
+          '''
+class: Target
+table: target
+fields:
+  name: String
+          ''',
+        ).build(),
+        ModelSourceBuilder().withFileName('source').withYaml(
+          '''
+class: Source
+table: source
+fields:
+  target: Target?, relation(optional)
+          ''',
+        ).build(),
+      ];
+
+      var collector = CodeGenerationCollector();
+      var analyzer =
+          StatefulAnalyzer(config, models, onErrorsCollector(collector));
+      var definitions = analyzer.validateAll();
+      expect(collector.errors, isEmpty);
+
+      sourceDefinition = createDatabaseDefinitionFromModels(
+        definitions,
+        'example',
+        [],
+      );
+    }
+
+    DatabaseDefinition
+        targetDefinition; // renames table `target` to `target_new`
+    {
+      var models = [
+        ModelSourceBuilder().withFileName('target').withYaml(
+          '''
+class: Target
+table: target_new
+fields:
+  name: String
+          ''',
+        ).build(),
+        ModelSourceBuilder().withFileName('source').withYaml(
+          '''
+class: Source
+table: source
+fields:
+  target: Target?, relation(optional)
+          ''',
+        ).build(),
+      ];
+
+      var collector = CodeGenerationCollector();
+      var analyzer =
+          StatefulAnalyzer(config, models, onErrorsCollector(collector));
+      var definitions = analyzer.validateAll();
+      expect(collector.errors, isEmpty);
+
+      targetDefinition = createDatabaseDefinitionFromModels(
+        definitions,
+        'example',
+        [],
+      );
+    }
+
+    var migration = generateDatabaseMigration(
+      databaseSource: sourceDefinition,
+      databaseTarget: targetDefinition,
+    );
+
+    var psql = migration.toPgSql(installedModules: [], removedModules: []);
+
+    var dropTableSourceIndex = psql.indexOf('DROP TABLE "source"');
+    var dropTableTargetIndex = psql.indexOf('DROP TABLE "target"');
+    var createTableSourceIndex = psql.indexOf('CREATE TABLE "source"');
+    var createTableTargetIndex = psql.indexOf('CREATE TABLE "target_new"');
+    var addForegeinKeyIndex = psql.indexOf('ADD CONSTRAINT "source_fk_0"');
+
+    expect(dropTableSourceIndex, greaterThanOrEqualTo(0));
+    expect(dropTableTargetIndex, greaterThanOrEqualTo(0));
+    expect(createTableSourceIndex, greaterThanOrEqualTo(0));
+    expect(createTableTargetIndex, greaterThanOrEqualTo(0));
+    expect(addForegeinKeyIndex, greaterThanOrEqualTo(0));
+
+    expect(dropTableSourceIndex, lessThan(dropTableTargetIndex));
+    expect(dropTableTargetIndex, lessThan(createTableSourceIndex));
+    expect(createTableSourceIndex, lessThan(createTableTargetIndex));
+    expect(createTableTargetIndex, lessThan(addForegeinKeyIndex));
+  });
+
+  test(
       'Given two tables that reference each other, when one is renamed, then the migration code should drop both tables and recreate them in a working order',
       () {
     DatabaseDefinition sourceDefinition;
