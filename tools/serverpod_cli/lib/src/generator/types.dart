918c429d2e80900881ad7d74824860747f1f4c5c
Timm Preetz <timm@preetz.us>
2025-03-18 12:24:42 +0100

    fix(review): Create `tryParseRecord` method that combines validating the input to be a record and parsing it into a `TypeDefinition` Thus no checks need to be repeated between a "checker" and "parser" function

--- tools/serverpod_cli/lib/src/generator/types.dart
+++ tools/serverpod_cli/lib/src/generator/types.dart
@@ -6,6 +6,7 @@ import 'package:path/path.dart' as p;
 import 'package:serverpod_cli/analyzer.dart';
 import 'package:serverpod_cli/src/generator/keywords.dart';
 import 'package:serverpod_cli/src/generator/shared.dart';
+import 'package:serverpod_cli/src/generator/types_record.dart';
 import 'package:serverpod_cli/src/util/model_helper.dart';
 import 'package:serverpod_cli/src/util/string_manipulation.dart';
 import 'package:serverpod_serialization/serverpod_serialization.dart';
@@ -695,8 +696,9 @@ TypeDefinition parseType(
 }) {
   var trimmedInput = input.trim();
 
-  if (_describesRecord(trimmedInput)) {
-    return _parseRecord(trimmedInput, extraClasses: extraClasses);
+  var maybeRecord = tryParseRecord(trimmedInput, extraClasses: extraClasses);
+  if (maybeRecord != null) {
+    return maybeRecord;
   }
 
   var start = trimmedInput.indexOf('<');
@@ -732,122 +734,6 @@ TypeDefinition parseType(
     generics: generics,
     customClass: extraClasses == null,
   );
-}
-
-bool _describesRecord(String trimmedInput) {
-  if (!trimmedInput.startsWith('(')) {
-    return false;
-  }
-
-  if (!trimmedInput.endsWith(')') &&
-      !trimmedInput.replaceAll(' ', '').endsWith(')?')) {
-    return false;
-  }
-
-  var (fields, _) = _unwrapRecord(trimmedInput);
-
-  var splitFields = splitIgnoringBracketsAndBracesAndQuotes(
-    fields,
-    returnEmptyParts: true,
-  );
-
-  // Field into split from string must have at least 2 parts,
-  // meaning the record is using a `,` even if it has only 1 fields, e.g. `(int,)`
-  // or some named parameter like `({String foo})`.
-  return (splitFields.length > 1 && !splitFields.every((f) => f.isEmpty)) ||
-      (splitFields.length == 1 &&
-          splitFields.single.startsWith('{') &&
-          splitFields.single.endsWith('}'));
-}
-
-(String fields, bool nullable) _unwrapRecord(String trimmedRecordInput) {
-  var start = trimmedRecordInput.indexOf('(');
-  var end = trimmedRecordInput.lastIndexOf(')');
-  var nullable = trimmedRecordInput.endsWith('?');
-  var fields = trimmedRecordInput.substring(start + 1, end);
-
-  return (fields, nullable);
-}
-
-TypeDefinition _parseRecord(
-  String trimmedRecordInput, {
-  List<TypeDefinition>? extraClasses,
-}) {
-  if (!(trimmedRecordInput.startsWith('(') &&
-      (trimmedRecordInput.endsWith(')') || trimmedRecordInput.endsWith('?')))) {
-    throw Exception('"$trimmedRecordInput" does not describe a Record');
-  }
-
-  var (fields, nullable) = _unwrapRecord(trimmedRecordInput);
-
-  var splitFields = splitIgnoringBracketsAndBracesAndQuotes(fields);
-
-  var recordFields = splitFields.expand((splitField) {
-    if (splitField.startsWith('{')) {
-      return _parseNamedRecordFields(
-        splitField,
-        extraClasses: extraClasses,
-      );
-    }
-
-    // could be either just a positional type, or a named positional type (like `int` or `int someNumber`, or even `Set<String> someSet`)
-    var parts =
-        splitIgnoringBracketsAndBracesAndQuotes(splitField, separator: ' ');
-
-    if (parts.length > 1 && !parts.last.startsWith('<')) {
-      // if the last part is a name (and not a generic parameter), then we need to drop that
-      splitField = parts.take(parts.length - 1).join();
-    }
-
-    return [
-      parseType(
-        splitField,
-        extraClasses: extraClasses,
-      ),
-    ];
-  }).toList();
-
-  return TypeDefinition(
-    className: RecordKeyword.className,
-    generics: recordFields,
-    nullable: nullable,
-  );
-}
-
-Iterable<TypeDefinition> _parseNamedRecordFields(
-  String namedRecordFieldsPart, {
-  List<TypeDefinition>? extraClasses,
-}) sync* {
-  if (!namedRecordFieldsPart.startsWith('{') ||
-      !namedRecordFieldsPart.endsWith('}')) {
-    throw Exception(
-      '"$namedRecordFieldsPart" does not described the named parameters of a Record',
-    );
-  }
-
-  var start = namedRecordFieldsPart.indexOf('{');
-  var end = namedRecordFieldsPart.lastIndexOf('}');
-  var typesMap = namedRecordFieldsPart.substring(start + 1, end);
-
-  var namedFieldWithTypes = splitIgnoringBracketsAndBracesAndQuotes(typesMap);
-
-  for (var namedFieldWithType in namedFieldWithTypes) {
-    namedFieldWithType = namedFieldWithType.trim();
-
-    var typeDescription = namedFieldWithType
-        .substring(0, namedFieldWithType.lastIndexOf(' '))
-        .trim();
-    var name = namedFieldWithType
-        .substring(namedFieldWithType.lastIndexOf(' '))
-        .trim();
-
-    var type = parseType(
-      typeDescription,
-      extraClasses: extraClasses,
-    );
-
-    yield type.asNamedRecordField(name);
-  }
 }
 
 int _findLastClassToken(int start, String input, bool isNullable) {
