7626e36eb79e2cfba176e730b705b62237b6dab5
Christer <christer@serverpod.dev>
2025-04-02 09:01:28 +0200

    test: Added exception event test with missing DB

--- tests/serverpod_test_server/test_integration/diagnostics/diagnostic_events_test.dart
+++ tests/serverpod_test_server/test_integration/diagnostics/diagnostic_events_test.dart
@@ -281,4 +281,83 @@ void main() {
       expect(record.context, isA<WebCallOpContext>());
     });
   });
+
+  group(
+      'Given a serverpod server with a missing database and a diagnostic event handler, '
+      'when starting serverpod', () {
+    var exceptionHandler = TestExceptionHandler();
+    late Serverpod pod;
+    late DiagnosticEventRecord<ExceptionEvent> record;
+
+    setUpAll(() async {
+      exceptionHandler = TestExceptionHandler();
+
+      final config = ServerpodConfig(
+        database: DatabaseConfig(
+          host: 'localhost',
+          port: 9999,
+          user: 'postgres',
+          password: 'postgres',
+          name: 'postgres',
+        ),
+        apiServer: ServerConfig(
+          port: 8080,
+          publicHost: 'localhost',
+          publicPort: 8080,
+          publicScheme: 'http',
+        ),
+      );
+      pod = IntegrationTestServer.create(
+        config: config,
+        experimentalFeatures: ExperimentalFeatures(
+          diagnosticEventHandlers: [exceptionHandler],
+        ),
+      );
+      final result = pod
+          .start(runInGuardedZone: false)
+          .timeout(const Duration(seconds: 2));
+      await expectLater(result, throwsA(isA<TimeoutException>()));
+      record = await exceptionHandler.events.first.timeout(timeout);
+    });
+
+    tearDownAll(() async {
+      await pod.shutdown(exitProcess: false);
+      exceptionHandler.eventsStreamController.close();
+    });
+
+    test('then the diagnostic event handler gets called with a Exception',
+        () async {
+      expect(record.event.exception, isA<SocketException>());
+    });
+
+    test('then the diagnostic event exception message is correct', () async {
+      expect((record.event.exception as SocketException).message,
+          contains('Connection refused'));
+    });
+
+    test('then the diagnostic event message is correct', () async {
+      expect(record.event.message,
+          startsWith('Failed to connect to the database'));
+    });
+
+    test('then the diagnostic event space is framework', () async {
+      expect(record.space, equals(OriginSpace.framework));
+    });
+
+    test('then the diagnostic event context is a DiagnosticEventContext',
+        () async {
+      expect(record.context.runtimeType, DiagnosticEventContext);
+    });
+
+    test('then the diagnostic event context has the expected content',
+        () async {
+      expect(
+          record.context.toJson(),
+          allOf([
+            containsPair('serverId', 'default'),
+            containsPair('serverName', ''),
+            containsPair('serverRunMode', 'production'),
+          ]));
+    });
+  });
 }
