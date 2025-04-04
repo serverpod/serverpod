6941898ad1719d3d5b7251588b91d7cc2ff625b1
Timm Preetz <timm@preetz.us>
2025-04-04 18:06:45 +0200

    Implement `serverpod_auth2_test_server`

--- modules/serverpod_auth2/serverpod_auth2_test_server/serverpod_auth2_test_server_server/lib/server.dart
+++ modules/serverpod_auth2/serverpod_auth2_test_server/serverpod_auth2_test_server_server/lib/server.dart
@@ -1,21 +1,84 @@
 import 'package:serverpod/serverpod.dart';
-
 import 'package:serverpod_auth2_test_server_server/src/web/routes/root.dart';
+import 'package:serverpod_auth_email_account_server/serverpod_auth_email_account_server.dart'
+    as email_account;
+import 'package:serverpod_auth_migration_server/serverpod_auth_migration_server.dart'
+    as auth_migration;
+import 'package:serverpod_auth_profile_server/serverpod_auth_profile_server.dart'
+    as auth_profile;
+import 'package:serverpod_auth_server/serverpod_auth_server.dart' as auth;
+import 'package:serverpod_auth_session_server/serverpod_auth_session_server.dart'
+    as auth_session;
 
-import 'src/generated/protocol.dart';
 import 'src/generated/endpoints.dart';
+import 'src/generated/protocol.dart';
 
-// This is the starting point of your Serverpod server. In most cases, you will
-// only need to make additions to this file if you add future calls,  are
-// configuring Relic (Serverpod's web-server), or need custom setup work.
+enum AuthMode {
+  /// Project updating from Serverpod Auth v1 to v2
+  updateProject,
+
+  /// Project starting with the Auth2 modules
+  freshProject,
+}
+
+var authMode = AuthMode.updateProject;
 
 void run(List<String> args) async {
-  // Initialize Serverpod and connect it with your generated code.
-  final pod = Serverpod(
-    args,
-    Protocol(),
-    Endpoints(),
-  );
+  final Serverpod pod;
+  switch (authMode) {
+    case AuthMode.updateProject:
+      pod = Serverpod(
+        args,
+        Protocol(),
+        Endpoints(),
+        authenticationHandler: auth_session.authenticationHandlerWithMigration(
+          auth_migration.sessionMigrationFunction,
+        ),
+      );
+
+      // maybe put into config, where we might also handle lifetime
+      // auth_session.Config.update(migrationCallback: auth_migration.sessionMigrationFunction);
+
+      email_account.EmailAccountConfig.update(
+          // TODO: Configure email sending etc.
+          //   existingUserImportFunction: () {
+          //   }
+          // by default would be based on the profile (which the endpoint knows, but could be overwritten here)
+          // TODO: This was kept out of the core "email account", so there is no user creation in there, just on the higher level, and users get linked to the core account
+          // consolidateUser: auth_profile.findExistingUserByEmail,
+          );
+
+      auth_migration.AuthMigrationConfig.update(
+        // TODO: Here we could switch to records and fully align the type, so the package can provide a nice, full default implementation without relying on the old modules
+        afterAuthUserMigration: (session, authUser, userInfo) async {
+          // TODO: get from package once types are aligned
+          await auth_profile.createUserProfile(session,
+              userId: authUser.id!,
+              email: userInfo.email,
+              fullName: userInfo.fullName /* … */
+              );
+        },
+
+        // afterAuthUserMigration: auth_profile.createUserProfile,
+      );
+
+    // TODO: Support "background migration" for inactive accounts
+    // pod.registerFutureCall(ExampleFutureCall(), 'exampleFutureCall');
+
+    case AuthMode.freshProject:
+      pod = Serverpod(
+        args,
+        Protocol(),
+        Endpoints(),
+        authenticationHandler: auth_session.authenticationHandler,
+
+        ///
+      );
+
+      email_account.EmailAccountConfig.update(
+          // TODO: Configure email sending
+          );
+  }
 
   // If you are using any future calls, they need to be registered here.
   // pod.registerFutureCall(ExampleFutureCall(), 'exampleFutureCall');
@@ -28,6 +91,15 @@ void run(List<String> args) async {
     RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
     '/*',
   );
+
+  if (DateTime.now().microsecondsSinceEpoch < 0) {
+    AuthenticationInfo x = null as dynamic;
+    // Just showcasing how `dynamic` should be fine here to keep using the user ID,
+    // at least while staying on the legacy auth package.
+    // When migration to the new users, some code that would still compile would need modification
+    // (as otherwise it might pass a UUID where and int is expected).
+    await auth.UserInfo.db.findById(await pod.createSession(), x.userId);
+  }
 
   // Start the server.
   await pod.start();
---
+++ modules/serverpod_auth2/serverpod_auth2_test_server/serverpod_auth2_test_server_server/lib/src/endpoints/email_login_endpoint.dart
@@ -0,0 +1,4 @@
+// ignore: implementation_imports
+import 'package:serverpod_auth_email_server/src/endpoints/email_account_endpoint.dart';
+
+class EmailLoginEndpoint extends EmailAccountEndpoint {}
