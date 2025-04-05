ae55ac3b1bd745d918b0db6044aea8ef4924c0aa
Timm Preetz <timm@preetz.us>
2025-04-05 11:47:24 +0200

    Add session migration example to `serverpod_auth2_test_server_server`

--- modules/serverpod_auth2/serverpod_auth2_test_server/serverpod_auth2_test_server_server/lib/server.dart
+++ modules/serverpod_auth2/serverpod_auth2_test_server/serverpod_auth2_test_server_server/lib/server.dart
@@ -1,9 +1,13 @@
 import 'package:serverpod/serverpod.dart';
-
 import 'package:serverpod_auth2_test_server_server/src/web/routes/root.dart';
+import 'package:serverpod_auth_migration_server/serverpod_auth_migration_server.dart'
+    as auth_migration;
+import 'package:serverpod_auth_server/serverpod_auth_server.dart';
+import 'package:serverpod_auth_session_server/serverpod_auth_session_server.dart'
+    as auth_session;
 
-import 'src/generated/protocol.dart';
 import 'src/generated/endpoints.dart';
+import 'src/generated/protocol.dart';
 
 // This is the starting point of your Serverpod server. In most cases, you will
 // only need to make additions to this file if you add future calls,  are
@@ -15,6 +19,9 @@ void run(List<String> args) async {
     args,
     Protocol(),
     Endpoints(),
+    authenticationHandler: auth_session.authenticationHandlerWithMigration(
+      auth_migration.sessionMigrationFunction,
+    ),
   );
 
   // If you are using any future calls, they need to be registered here.
@@ -28,6 +35,15 @@ void run(List<String> args) async {
     RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
     '/*',
   );
+
+  if (DateTime.now().microsecondsSinceEpoch < 0) {
+    AuthenticationInfo x = null as dynamic;
+    // Just showcasing how `dynamic` should be fine here to keep using the user ID,
+    // at least while staying on the legacy auth package.
+    // When migration to the new users, some code that would still compile would need modification
+    // (as otherwise it might pass a UUID where and int is expected).
+    await UserInfo.db.findById(await pod.createSession(), x.userId);
+  }
 
   // Start the server.
   await pod.start();
