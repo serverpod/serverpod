26cf634b2e5f27debc91992779c3a940794b221f
Timm Preetz <timm@preetz.us>
2025-04-11 09:57:55 +0200

    Create module `serverpod_auth_profile`

---
+++ modules/serverpod_auth2/serverpod_auth_profile/serverpod_auth_profile_client/lib/src/protocol/client.dart
@@ -0,0 +1,39 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: implementation_imports
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: type_literal_in_constant_pattern
+// ignore_for_file: use_super_parameters
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod_client/serverpod_client.dart' as _i1;
+import 'dart:async' as _i2;
+
+/// {@category Endpoint}
+class EndpointModule extends _i1.EndpointRef {
+  EndpointModule(_i1.EndpointCaller caller) : super(caller);
+
+  @override
+  String get name => 'serverpod_auth_profile.module';
+
+  _i2.Future<String> hello(String name) => caller.callServerEndpoint<String>(
+        'serverpod_auth_profile.module',
+        'hello',
+        {'name': name},
+      );
+}
+
+class Caller extends _i1.ModuleEndpointCaller {
+  Caller(_i1.ServerpodClientShared client) : super(client) {
+    module = EndpointModule(this);
+  }
+
+  late final EndpointModule module;
+
+  @override
+  Map<String, _i1.EndpointRef> get endpointRefLookup =>
+      {'serverpod_auth_profile.module': module};
+}
