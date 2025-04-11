578814a82f42d17e2419fcac113f8756f6bb2bb2
Damian Bast <dkbast@users.noreply.github.com>
2025-04-11 13:59:23 +0200

    Slim down GreetingEndpoint

--- templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
+++ templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
@@ -8,40 +8,21 @@ import 'package:serverpod/serverpod.dart';
 
 // After adding or modifying an endpoint, you will need to run
 // `serverpod generate` to update the server and client code.
+
+/// This is an example endpoint that returns a greeting message.
 class GreetingEndpoint extends Endpoint {
-  // You create methods in your endpoint which are accessible from the client by
-  // creating a public method with `Session` as its first parameter.
-  // `bool`, `int`, `double`, `String`, `UuidValue`, `Duration`, `DateTime`, `ByteData`,
-  // and other serializable classes, exceptions and enums from your from your `protocol` directory.
-  // The methods should return a typed future; the same types as for the parameters are
-  // supported. The `session` object provides access to the database, logging,
-  // passwords, and information about the request being made to the server.
+  /// This method is called when the client calls the `hello` method on the
+  /// `greeting` endpoint.
+  /// The `Session` parameter is used to access the session information of the
+  /// client that called the endpoint. Think of it as the `context` parameter in
+  /// a Flutter widget.
+  /// The session object provides access to the database and other server-side
+  /// resources like the secrets from your passwords file, the cache and the
+  /// storage.
+  /// You can use any serializable type as a parameter or return type.
+  /// Read more in the [docs](https://docs.serverpod.dev/concepts/working-with-endpoints).
   Future<Greeting> hello(Session session, String name) async {
-    final greeter = Greeter();
-    return greeter.call(name);
-  }
-}
-
-/// This is a simple class that generates a greeting message.
-/// It can be configured with a custom greeting message. E.g. if reusing between
-/// multiple endpoints.
-/// The `call` method takes a name and returns a `Greeting` object.
-/// When building endpoints, think of them as functions which take the
-/// input they are called with plus anything they fetch from the database etc
-/// and return a result. But they should not maintain any state between calls.
-class Greeter {
-  final String greeting;
-
-  Greeter({this.greeting = 'Hello'});
-
-  /// This method takes a name and returns a `Greeting` object.
-  /// call is a special method in Dart that allows an object to be called as a function.
-  /// see https://dart.dev/language/callable-objects
-  Greeting call(String name) {
     return Greeting(
-      message: '$greeting $name',
-      author: 'Serverpod',
-      timestamp: DateTime.now(),
-    );
+        message: 'Hello $name', author: 'Serverpod', timestamp: DateTime.now());
   }
 }
