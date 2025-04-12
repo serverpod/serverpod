a8e49b873848168cc47d4e7cd92686359f5f5222
Timm Preetz <timm@preetz.us>
2025-04-12 11:06:44 +0200

    Add endpoints hierarchy E2E tests for logged-in and admin user

--- tests/serverpod_test_server/test_e2e/endpoint_hierarchy_test.dart
+++ tests/serverpod_test_server/test_e2e/endpoint_hierarchy_test.dart
@@ -1,9 +1,30 @@
+import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_test_client/serverpod_test_client.dart';
 import 'package:serverpod_test_server/test_util/config.dart';
+import 'package:serverpod_test_server/test_util/test_key_manager.dart';
 import 'package:test/test.dart';
 
 void main() {
-  var client = Client(serverUrl);
+  var client = Client(
+    serverUrl,
+    authenticationKeyManager: TestAuthKeyManager(),
+  );
+
+  setUp(() async {
+    await client.authentication.removeAllUsers();
+    var userCount = await client.authentication.countUsers();
+    expect(userCount, equals(0));
+  });
+
+  tearDown(() async {
+    await client.authenticationKeyManager?.remove();
+    await client.authentication.removeAllUsers();
+    await client.authentication.signOut();
+    assert(
+      await client.modules.auth.status.isSignedIn() == false,
+      'Still signed in after teardown',
+    );
+  });
 
   group(
       'Given an endpoint which subclasses a base class which requires login, ',
@@ -19,6 +40,21 @@ void main() {
               .having((e) => e.statusCode, 'statusCode', 401),
         ),
       );
+    });
+
+    test(
+        'when calling `echo` as a logged-in user, then the request returns the expected value',
+        () async {
+      var loginResponse = await client.authentication.authenticate(
+        'test@foo.bar',
+        'password',
+      );
+      await client.authenticationKeyManager!
+          .put('${loginResponse.keyId}:${loginResponse.key}');
+
+      final response = await client.myLoggedIn.echo('hello');
+
+      expect(response, 'hello');
     });
   });
 
@@ -36,6 +72,42 @@ void main() {
               .having((e) => e.statusCode, 'statusCode', 401),
         ),
       );
+    });
+
+    test(
+        'when calling `echo` as a logged-in user, then the request errs with "Forbidden" due to the missing scope',
+        () async {
+      var loginResponse = await client.authentication.authenticate(
+        'test@foo.bar',
+        'password',
+      );
+      await client.authenticationKeyManager!
+          .put('${loginResponse.keyId}:${loginResponse.key}');
+
+      await expectLater(
+        () async => await client.myAdmin.echo('hello'),
+        throwsA(
+          isA<ServerpodClientException>()
+              .having((e) => e.message, 'statusCode', contains('Forbidden'))
+              .having((e) => e.statusCode, 'statusCode', 403),
+        ),
+      );
+    });
+
+    test(
+        'when calling `echo` as an admin user, then the request returns the expected value',
+        () async {
+      var loginResponse = await client.authentication.authenticate(
+        'test@foo.bar',
+        'password',
+        [Scope.admin.name!],
+      );
+      await client.authenticationKeyManager!
+          .put('${loginResponse.keyId}:${loginResponse.key}');
+
+      final response = await client.myAdmin.echo('hello');
+
+      expect(response, 'hello');
     });
   });
 }
