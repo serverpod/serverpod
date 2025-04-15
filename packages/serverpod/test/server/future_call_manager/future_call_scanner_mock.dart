a95995ecc7afb1bd2ef951ee4ff15e51c8196edb
inf0rmatix <kontakt@paul-koehler.dev>
2025-04-15 15:15:46 +0200

    refactor: remove unused mock classes for FutureCallManager tests

--- packages/serverpod/test/server/future_call_manager/future_call_scanner_mock.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:serverpod/src/server/future_call_manager/future_call_scanner.dart';
-
-class MockFutureCallScanner extends Mock implements FutureCallScanner {}
--- packages/serverpod/test/server/future_call_manager/future_call_scheduler_mock.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:serverpod/src/server/future_call_manager/serverpod_task_scheduler.dart';
-
-class MockFutureCallScheduler extends Mock implements ServerpodTaskScheduler {}
