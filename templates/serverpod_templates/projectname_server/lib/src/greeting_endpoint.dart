82f1b793cd100bba59e45bf42568a9092bef8d62
Damian Bast <dkbast@users.noreply.github.com>
2025-04-16 14:22:15 +0200

    Address PR feedback

--- templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
+++ templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
@@ -4,7 +4,7 @@ import 'package:serverpod/serverpod.dart';
 // This is an example endpoint of your server. It's best practice to use the
 // `Endpoint` ending of the class name, but it will be removed when accessing
 // the endpoint from the client. I.e., this endpoint can be accessed through
-// `client.example` on the client side.
+// `client.greeting` on the client side.
 
 // After adding or modifying an endpoint, you will need to run
 // `serverpod generate` to update the server and client code.
@@ -13,12 +13,11 @@ import 'package:serverpod/serverpod.dart';
 class GreetingEndpoint extends Endpoint {
   // This method is called when the client calls the `hello` method on the
   // `greeting` endpoint.
-  // The `Session` parameter is used to access the session information of the
-  // client that called the endpoint. Think of it as the `context` parameter in
-  // a Flutter widget.
-  // The session object provides access to the database and other server-side
-  // resources like the secrets from your passwords file, the cache and the
-  // storage.
+  //
+  // The `Session` parameter is a context object for the client request.
+  // It provides access to the database and other server-side resources like
+  // secrets from your password file, the cache, storage, and server-event messaging
+  //
   // You can use any serializable type as a parameter or return type.
   // Read more in the [docs](https://docs.serverpod.dev/concepts/working-with-endpoints).
 
