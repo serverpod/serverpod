6fece6570c5197eacc4df92219fb47766339aef4
Timm Preetz <timm@preetz.us>
2025-04-16 14:50:02 +0200

    Generate module `serverpod_auth_user`

---
+++ modules/serverpod_auth2/serverpod_auth_user/serverpod_auth_user_server/test/unit/module_util_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+import 'package:serverpod_auth_user_server/src/module_util.dart';
+
+void main() {
+  test(
+      'Given ModuleUtil '
+      'when calling `buildGreeting` with name '
+      'then returned greeting includes name', () async {
+    final greeting = ModuleUtil.buildGreeting('Alice');
+    expect(greeting, 'Hello Alice');
+  });
+}
