b31b6e9538d59165481decba76b0309dfeab6681
Damian Bast <dkbast@users.noreply.github.com>
2025-04-16 12:38:41 +0200

    Update templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
    Co-authored-by: Viktor Lidholt <viktorlidholt@gmail.com>

--- templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
+++ templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
@@ -4,7 +4,7 @@ import 'package:serverpod/serverpod.dart';
 // This is an example endpoint of your server. It's best practice to use the
 // `Endpoint` ending of the class name, but it will be removed when accessing
 // the endpoint from the client. I.e., this endpoint can be accessed through
-// `client.example` on the client side.
+// `client.greeting` on the client side.
 
 // After adding or modifying an endpoint, you will need to run
 // `serverpod generate` to update the server and client code.
