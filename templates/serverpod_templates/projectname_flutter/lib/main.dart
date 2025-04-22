0f5e068bfd235f42c8449f4f6bfc059c5ee86ec9
Damian Bast <dkbast@users.noreply.github.com>
2025-04-22 17:38:10 +0200

    Update templates/serverpod_templates/projectname_flutter/lib/main.dart
    Co-authored-by: Viktor Lidholt <viktorlidholt@gmail.com>

--- templates/serverpod_templates/projectname_flutter/lib/main.dart
+++ templates/serverpod_templates/projectname_flutter/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 /// Sets up a global client object that can be used to talk to the server from
 /// anywhere in our app. The client is generated from your server code.
-/// The client is set up to connect to a Serverpod running on a local server on
+/// and is set up to connect to a Serverpod running on a local server on
 /// the default port. You will need to modify this to connect to staging or
 /// production servers.
 /// You might want to use the dependency injection of your choice instead of
