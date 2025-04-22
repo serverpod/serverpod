c76609c1e8718a2bbacca9e94520f92115096a2c
Damian Bast <dkbast@users.noreply.github.com>
2025-04-22 18:13:58 +0200

    Adds example to passing env variable to Flutter app

--- templates/serverpod_templates/projectname_flutter/lib/main.dart
+++ templates/serverpod_templates/projectname_flutter/lib/main.dart
@@ -17,6 +17,8 @@ void main() {
   // When you are running the app on a physical device, you need to set the
   // server URL to the IP address of your computer. You can find the IP
   // address by running `ipconfig` on Windows or `ifconfig` on Mac/Linux.
+  // You can set the variable when running or building your app like this:
+  // E.g. `flutter run --dart-define=SERVER_URL=https://api.example.com/`
   const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
   final serverUrl =
       serverUrlFromEnv.isEmpty ? 'http://$localhost:8080/' : serverUrlFromEnv;
