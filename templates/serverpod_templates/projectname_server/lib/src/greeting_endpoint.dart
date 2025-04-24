1e65858b405c52fa186d5ea2da478aef1ce3ef3f
Damian Bast <dkbast@users.noreply.github.com>
2025-04-24 10:51:24 +0200

    Update templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
    Co-authored-by: Viktor Lidholt <viktorlidholt@gmail.com>

--- templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
+++ templates/serverpod_templates/projectname_server/lib/src/greeting_endpoint.dart
@@ -9,7 +9,7 @@ import 'package:serverpod/serverpod.dart';
 // After adding or modifying an endpoint, you will need to run
 // `serverpod generate` to update the server and client code.
 
-/// This is an example endpoint that returns a greeting message.
+/// This is an example endpoint that returns a greeting message through its [hello] method.
 class GreetingEndpoint extends Endpoint {
   // This method is called when the client calls the `hello` method on the
   // `greeting` endpoint.
