397b1bf18d44c993cd7189b1f593a7db0e961de1
Alexander Sandor <alexander@serverpod.dev>
2025-04-25 09:51:45 +0200

    test(style): Remove group and clarify that it is non-android platform.

--- packages/serverpod_flutter/test/localhost_test.dart
+++ packages/serverpod_flutter/test/localhost_test.dart
@@ -3,28 +3,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:serverpod_flutter/src/localhost.dart';
 
 void main() {
-  group('localhost', () {
-    tearDown(() => debugDefaultTargetPlatformOverride = null);
+  tearDown(() => debugDefaultTargetPlatformOverride = null);
 
-    test(
-        'Given target platform is android when reading localhost then 10.0.2.2 is returned',
-        () {
-      debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  test(
+      'Given target platform is android when reading localhost then 10.0.2.2 is returned',
+      () {
+    debugDefaultTargetPlatformOverride = TargetPlatform.android;
 
-      expect(localhost, '10.0.2.2');
-    });
-
-    final nonAndroidPlatforms = TargetPlatform.values.toList()
-      ..removeWhere((platform) => platform == TargetPlatform.android);
-
-    for (final testPlatform in nonAndroidPlatforms) {
-      test(
-          'Given target platform is ${testPlatform.name} when reading localhost then localhost is returned',
-          () {
-        debugDefaultTargetPlatformOverride = testPlatform;
-
-        expect(localhost, 'localhost');
-      });
-    }
+    expect(localhost, '10.0.2.2');
   });
+
+  final nonAndroidPlatforms = TargetPlatform.values.toList()
+    ..removeWhere((platform) => platform == TargetPlatform.android);
+
+  for (final testPlatform in nonAndroidPlatforms) {
+    test(
+        'Given non-android target platform "${testPlatform.name}" when reading localhost then localhost is returned',
+        () {
+      debugDefaultTargetPlatformOverride = testPlatform;
+
+      expect(localhost, 'localhost');
+    });
+  }
 }
