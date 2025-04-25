ca6c9f6cb54ba8ced30d0600967be2f611dd7cf9
Ellet <echo.ellet@gmail.com>
2025-04-25 11:25:37 +0300

    refactor(serverpod_flutter): Avoid using conditional import for localhost (#3439)
    Co-authored-by: Alexander Sandor <137198655+SandPod@users.noreply.github.com>
    Co-authored-by: Alexander Sandor <alexander@serverpod.dev>

---
+++ packages/serverpod_flutter/test/localhost_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:serverpod_flutter/src/localhost.dart';
+
+void main() {
+  tearDown(() => debugDefaultTargetPlatformOverride = null);
+
+  test(
+      'Given target platform is android when reading localhost then 10.0.2.2 is returned',
+      () {
+    debugDefaultTargetPlatformOverride = TargetPlatform.android;
+
+    expect(localhost, '10.0.2.2');
+  });
+
+  final nonAndroidPlatforms = TargetPlatform.values.toList()
+    ..removeWhere((platform) => platform == TargetPlatform.android);
+
+  for (final testPlatform in nonAndroidPlatforms) {
+    test(
+        'Given non-android target platform "${testPlatform.name}" when reading localhost then localhost is returned',
+        () {
+      debugDefaultTargetPlatformOverride = testPlatform;
+
+      expect(localhost, 'localhost');
+    });
+  }
+}
