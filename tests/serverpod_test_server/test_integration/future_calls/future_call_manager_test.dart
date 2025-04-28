ff39ce07ea892ebb47c4aded952f06237e7e4125
inf0rmatix <kontakt@paul-koehler.dev>
2025-04-28 15:13:35 +0200

    test: extending FutureCallManager integration tests

--- tests/serverpod_test_server/test_integration/future_calls/future_call_manager_test.dart
+++ tests/serverpod_test_server/test_integration/future_calls/future_call_manager_test.dart
@@ -25,8 +25,145 @@ class CompleterTestCall extends FutureCall<SimpleData> {
 }
 
 void main() async {
+  withServerpod('Given FutureCallManager', (sessionBuilder, _) {
+    late FutureCallManager futureCallManager;
+
+    setUp(() async {
+      futureCallManager =
+          FutureCallManagerBuilder.fromTestSessionBuilder(sessionBuilder)
+              .build();
+
+      futureCallManager.registerFutureCall(
+          CompleterTestCall(), 'test-db-entry-call');
+    });
+
+    tearDown(() async {
+      await futureCallManager.stop();
+    });
+
+    group('when scheduling a FutureCall', () {
+      setUp(() async {
+        await futureCallManager.scheduleFutureCall(
+          'test-db-entry-call',
+          SimpleData(num: 4),
+          DateTime.now(),
+          '1',
+          'unique-identifier-42',
+        );
+      });
+
+      test('then a FutureCallEntry is added to the database', () async {
+        final futureCallEntries = await FutureCallEntry.db.find(
+          sessionBuilder.build(),
+          where: (entry) => entry.name.equals('test-db-entry-call'),
+        );
+
+        expect(futureCallEntries, hasLength(1));
+      });
+    });
+  });
+
+  withServerpod('Given FutureCallManager with a scheduled FutureCall',
+      (sessionBuilder, _) {
+    late FutureCallManager futureCallManager;
+
+    setUp(() async {
+      futureCallManager =
+          FutureCallManagerBuilder.fromTestSessionBuilder(sessionBuilder)
+              .build();
+
+      futureCallManager.registerFutureCall(
+          CompleterTestCall(), 'test-cancel-future-call');
+
+      futureCallManager.scheduleFutureCall(
+        'test-cancel-future-call',
+        SimpleData(num: 4),
+        DateTime.now().add(Duration(days: 42)),
+        '1',
+        'unique-identifier-1337',
+      );
+    });
+
+    tearDown(() async {
+      await futureCallManager.stop();
+    });
+
+    group('when cancelling the scheduled FutureCall', () {
+      setUp(() async {
+        await futureCallManager.cancelFutureCall('unique-identifier-1337');
+      });
+
+      test('then the FutureCallEntry is removed from the database', () async {
+        final futureCallEntries = await FutureCallEntry.db.find(
+          sessionBuilder.build(),
+          where: (entry) => entry.name.equals('test-cancel-future-call'),
+        );
+
+        expect(futureCallEntries, hasLength(0));
+      });
+    });
+
+    group('when cancelling a non-scheduled FutureCall', () {
+      setUp(() async {
+        await futureCallManager.cancelFutureCall('non-existing-identifier');
+      });
+
+      test('then no error is thrown and the database is not affected',
+          () async {
+        await expectLater(
+          () => futureCallManager.cancelFutureCall('non-existing-identifier'),
+          returnsNormally,
+        );
+
+        final futureCallEntries = await FutureCallEntry.db.find(
+          sessionBuilder.build(),
+          where: (entry) => entry.name.equals('test-cancel-future-call'),
+        );
+
+        expect(futureCallEntries, hasLength(1));
+      });
+    });
+  });
+
   withServerpod(
-      'Given FutureCallManager with scheduled future call that is due',
+      'Given FutureCallManager with a scheduled not-registered FutureCall',
+      (sessionBuilder, _) {
+    late FutureCallManager futureCallManager;
+
+    setUp(() async {
+      futureCallManager =
+          FutureCallManagerBuilder.fromTestSessionBuilder(sessionBuilder)
+              .build();
+
+      futureCallManager.scheduleFutureCall(
+        'non-registered-future-call',
+        SimpleData(num: 4),
+        DateTime.now().add(Duration(days: 42)),
+        '1',
+        'very-unique-identifier',
+      );
+    });
+
+    tearDown(() async {
+      await futureCallManager.stop();
+    });
+
+    group('when executing a scheduled but unregistered FutureCall', () {
+      test('then no error is thrown and the FutureCallEntry is removed',
+          () async {
+        await futureCallManager.runScheduledFutureCalls();
+
+        final futureCallEntries = await FutureCallEntry.db.find(
+          sessionBuilder.build(),
+          where: (entry) => entry.name.equals('non-registered-future-call'),
+        );
+
+        expect(futureCallEntries, hasLength(0));
+      });
+    });
+  });
+
+  withServerpod('Given FutureCallManager with scheduled FutureCall that is due',
       (sessionBuilder, _) {
     late FutureCallManager futureCallManager;
     late CompleterTestCall testCall;
@@ -53,16 +190,16 @@ void main() async {
       await futureCallManager.stop();
     });
 
-    group('when running scheduled future calls', () {
+    group('when running scheduled FutureCalls', () {
       setUp(() async {
         await futureCallManager.runScheduledFutureCalls();
       });
 
-      test('then call is executed', () async {
-        expect(testCall.completer.future, completes);
+      test('then the FutureCall is executed', () async {
+        await expectLater(testCall.completer.future, completes);
       });
 
-      test('then call is removed from database', () async {
+      test('then the FutureCallEntry gets deleted from database', () async {
         final futureCallEntries = await FutureCallEntry.db.find(
           sessionBuilder.build(),
         );
