032a54c1f49442be37b5014ec1c876ea33b64599
Timm Preetz <timm@preetz.us>
2025-04-29 10:49:56 +0200

    Update `serverpod_auth_user_server` SQL migration to include model

--- modules/new_serverpod_auth/serverpod_auth_user/serverpod_auth_user_server/migrations/20250506070330492/migration.sql
+++ modules/new_serverpod_auth/serverpod_auth_user/serverpod_auth_user_server/migrations/20250506070330492/migration.sql
@@ -1,6 +1,45 @@
 BEGIN;
 
 --
+-- Function: gen_random_uuid_v7()
+-- Source: https://gist.github.com/kjmph/5bd772b2c2df145aa645b837da7eca74
+-- License: MIT (copyright notice included on the generator source code).
+--
+create or replace function gen_random_uuid_v7()
+returns uuid
+as $$
+begin
+  -- use random v4 uuid as starting point (which has the same variant we need)
+  -- then overlay timestamp
+  -- then set version 7 by flipping the 2 and 1 bit in the version 4 string
+  return encode(
+    set_bit(
+      set_bit(
+        overlay(uuid_send(gen_random_uuid())
+                placing substring(int8send(floor(extract(epoch from clock_timestamp()) * 1000)::bigint) from 3)
+                from 1 for 6
+        ),
+        52, 1
+      ),
+      53, 1
+    ),
+    'hex')::uuid;
+end
+$$
+language plpgsql
+volatile;
+
+--
+-- ACTION CREATE TABLE
+--
+CREATE TABLE "serverpod_auth_user" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    "created" timestamp without time zone NOT NULL,
+    "scopeNames" json NOT NULL,
+    "blocked" boolean NOT NULL
+);
+
+--
 -- ACTION CREATE TABLE
 --
 CREATE TABLE "serverpod_cloud_storage" (
@@ -241,9 +280,9 @@ ALTER TABLE ONLY "serverpod_query_log"
 -- MIGRATION VERSION FOR serverpod_auth_user
 --
 INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
-    VALUES ('serverpod_auth_user', '20250416124930715', now())
+    VALUES ('serverpod_auth_user', '20250506070330492', now())
     ON CONFLICT ("module")
-    DO UPDATE SET "version" = '20250416124930715', "timestamp" = now();
+    DO UPDATE SET "version" = '20250506070330492', "timestamp" = now();
 
 --
 -- MIGRATION VERSION FOR serverpod
