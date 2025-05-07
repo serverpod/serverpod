3ff340357d4859c400f6afa106f5f2b041c61e31
Alexander Sandor <137198655+SandPod@users.noreply.github.com>
2025-05-07 08:46:17 +0300

    fix: Ensure foreign key relations are always defined after table creation. (#3516)

--- tools/serverpod_cli/test/database/extentions/migration_pgsql_code_generation_test.dart
+++ tools/serverpod_cli/test/database/extentions/migration_pgsql_code_generation_test.dart
@@ -1,9 +1,10 @@
-import 'package:serverpod_cli/src/database/extensions.dart';
-import 'package:serverpod_cli/src/database/migration.dart';
+import 'package:serverpod_cli/analyzer.dart';
 import 'package:test/test.dart';
 
 import '../../test_util/builders/database/database_definition_builder.dart';
 import '../../test_util/builders/database/table_definition_builder.dart';
+import '../../test_util/builders/model_source_builder.dart';
+import '../../test_util/database_definition_helpers.dart';
 
 void main() {
   group(
@@ -39,5 +40,62 @@ void main() {
         () {
       expect(psql, contains('CREATE TABLE IF NOT EXISTS "example_table"'));
     });
+  });
+
+  /// Issue: https://github.com/serverpod/serverpod/issues/3503
+  test(
+      'Given an existing table that that references a new table with a name lexically sorted before the existing one, when creating migraion sql then the migration code should create the table before defining the foreign key',
+      () {
+    var sourceModels = [
+      ModelSourceBuilder().withFileName('existing_table').withYaml(
+        '''
+class: ExistingModel
+table: a_existing_table
+fields:
+  name: String
+          ''',
+      ).build(),
+    ];
+
+    var targetModels = [
+      ModelSourceBuilder().withFileName('new_model').withYaml(
+        '''
+class: NewModel
+table: z_new_model
+fields:
+  name: String
+          ''',
+      ).build(),
+      ModelSourceBuilder().withFileName('existing_table').withYaml(
+        '''
+class: ExistingModel
+table: a_existing_table
+fields:
+  name: String
+  newModel: NewModel?, relation(optional)
+          ''',
+      ).build(),
+    ];
+
+    var (:sourceDefinition, :targetDefinition) = databaseDefinitionsFromModels(
+      sourceModels,
+      targetModels,
+    );
+
+    var migration = generateDatabaseMigration(
+      databaseSource: sourceDefinition,
+      databaseTarget: targetDefinition,
+    );
+
+    var psql = migration.toPgSql(installedModules: [], removedModules: []);
+
+    var createNewModelTable = psql.indexOf('CREATE TABLE "z_new_model"');
+    var addForeignKeyToExistingTable =
+        psql.indexOf('ADD CONSTRAINT "a_existing_table_fk_0"');
+
+    expect(createNewModelTable, greaterThanOrEqualTo(0));
+    expect(addForeignKeyToExistingTable, greaterThanOrEqualTo(0));
+
+    expect(createNewModelTable, lessThan(addForeignKeyToExistingTable));
   });
 }
--- tools/serverpod_cli/test/database/extentions/migration_with_many_to_many_relation_pgsql_code_generation_test.dart
+++ tools/serverpod_cli/test/database/extentions/migration_with_many_to_many_relation_pgsql_code_generation_test.dart
@@ -1,85 +1,56 @@
 import 'package:serverpod_cli/analyzer.dart';
-import 'package:serverpod_cli/src/analyzer/models/stateful_analyzer.dart';
-import 'package:serverpod_cli/src/database/create_definition.dart';
-import 'package:serverpod_cli/src/generator/code_generation_collector.dart';
-import 'package:serverpod_service_client/serverpod_service_client.dart';
 import 'package:test/test.dart';
 
-import '../../generator/dart/client_code_generator/class_constructors_test.dart';
 import '../../test_util/builders/model_source_builder.dart';
+import '../../test_util/database_definition_helpers.dart';
 
 void main() {
   test(
       'Given a table that is referenced by another one, when it is renamed, then the migration code should drop both tables and recreate them in a working order',
       () {
-    DatabaseDefinition sourceDefinition;
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('target').withYaml(
-          '''
+    var sourceModels = [
+      ModelSourceBuilder().withFileName('target').withYaml(
+        '''
 class: Target
 table: target
 fields:
   name: String
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('source').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('source').withYaml(
+        '''
 class: Source
 table: source
 fields:
   target: Target?, relation
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      sourceDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
-
-    DatabaseDefinition
-        targetDefinition; // renames table `target` to `target_new`
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('target').withYaml(
-          '''
+    var targetModels = [
+      // renames table `target` to `target_new`
+      ModelSourceBuilder().withFileName('target').withYaml(
+        '''
 class: Target
 table: target_new
 fields:
   name: String
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('source').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('source').withYaml(
+        '''
 class: Source
 table: source
 fields:
   target: Target?, relation
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      targetDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
+    var (:sourceDefinition, :targetDefinition) = databaseDefinitionsFromModels(
+      sourceModels,
+      targetModels,
+    );
 
     var migration = generateDatabaseMigration(
       databaseSource: sourceDefinition,
@@ -109,74 +80,49 @@ fields:
   test(
       'Given a table that is optionally referenced by another one, when it is renamed, then the migration code should drop both tables and recreate them in a working order',
       () {
-    DatabaseDefinition sourceDefinition;
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('target').withYaml(
-          '''
+    var sourceModels = [
+      ModelSourceBuilder().withFileName('target').withYaml(
+        '''
 class: Target
 table: target
 fields:
   name: String
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('source').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('source').withYaml(
+        '''
 class: Source
 table: source
 fields:
   target: Target?, relation(optional)
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      sourceDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
-
-    DatabaseDefinition
-        targetDefinition; // renames table `target` to `target_new`
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('target').withYaml(
-          '''
+    var targetModels = [
+      // renames table `target` to `target_new`
+      ModelSourceBuilder().withFileName('target').withYaml(
+        '''
 class: Target
 table: target_new
 fields:
   name: String
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('source').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('source').withYaml(
+        '''
 class: Source
 table: source
 fields:
   target: Target?, relation(optional)
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      targetDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
+    var (:sourceDefinition, :targetDefinition) = databaseDefinitionsFromModels(
+      sourceModels,
+      targetModels,
+    );
 
     var migration = generateDatabaseMigration(
       databaseSource: sourceDefinition,
@@ -206,74 +152,49 @@ fields:
   test(
       'Given two tables that reference each other, when one is renamed, then the migration code should drop both tables and recreate them in a working order',
       () {
-    DatabaseDefinition sourceDefinition;
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('a').withYaml(
-          '''
+    var sourceModels = [
+      ModelSourceBuilder().withFileName('a').withYaml(
+        '''
 class: A
 table: a
 fields:
   b: B?, relation
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('b').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('b').withYaml(
+        '''
 class: B
 table: b
 fields:
   a: A?, relation
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      sourceDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
-
-    // renames table `target` to `target_new`
-    DatabaseDefinition targetDefinition;
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('a').withYaml(
-          '''
+    var targetModels = [
+      // renames table `target` to `target_new`
+      ModelSourceBuilder().withFileName('a').withYaml(
+        '''
 class: A
 table: a_new
 fields:
   b: B?, relation
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('b').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('b').withYaml(
+        '''
 class: B
 table: b
 fields:
   a: A?, relation
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      targetDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
+    var (:sourceDefinition, :targetDefinition) = databaseDefinitionsFromModels(
+      sourceModels,
+      targetModels,
+    );
 
     var migration = generateDatabaseMigration(
       databaseSource: sourceDefinition,
@@ -306,90 +227,65 @@ fields:
   test(
       'Given two tables that reference each other, when one is renamed, then the migration code should not mention an unrelated table',
       () {
-    DatabaseDefinition sourceDefinition;
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('a').withYaml(
-          '''
+    var sourceModels = [
+      ModelSourceBuilder().withFileName('a').withYaml(
+        '''
 class: A
 table: a
 fields:
   b: B?, relation
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('b').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('b').withYaml(
+        '''
 class: B
 table: b
 fields:
   a: A?, relation
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('c').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('c').withYaml(
+        '''
 class: C
 table: c
 fields:
   name: String?
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      sourceDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
-
-    // renames table `target` to `target_new`
-    DatabaseDefinition targetDefinition;
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('a').withYaml(
-          '''
+    var targetModels = [
+      // renames table `target` to `target_new`
+      ModelSourceBuilder().withFileName('a').withYaml(
+        '''
 class: A
 table: a_new
 fields:
   b: B?, relation
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('b').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('b').withYaml(
+        '''
 class: B
 table: b
 fields:
   a: A?, relation
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('c').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('c').withYaml(
+        '''
 class: C
 table: c
 fields:
   name: String?
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      targetDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
+    var (:sourceDefinition, :targetDefinition) = databaseDefinitionsFromModels(
+      sourceModels,
+      targetModels,
+    );
 
     var migration = generateDatabaseMigration(
       databaseSource: sourceDefinition,
@@ -404,75 +300,50 @@ fields:
   test(
       'Given a table that is referenced by another one, when it is renamed while the pointing table is dropping the reference column, then the migration code should only drop and recreate the renamed table and drop just the column on the "pointing" one',
       () {
-    DatabaseDefinition sourceDefinition;
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('target').withYaml(
-          '''
+    var sourceModels = [
+      ModelSourceBuilder().withFileName('target').withYaml(
+        '''
 class: Target
 table: target
 fields:
   name: String
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('source').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('source').withYaml(
+        '''
 class: Source
 table: source
 fields:
   name: String
   target: Target?, relation
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      sourceDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
-
-    DatabaseDefinition
-        targetDefinition; // renames table `target` to `target_new` and `Source` drops the reference to `Target`
-    {
-      var models = [
-        ModelSourceBuilder().withFileName('target').withYaml(
-          '''
+    var targetModels = [
+      // renames table `target` to `target_new` and `Source` drops the reference to `Target`
+      ModelSourceBuilder().withFileName('target').withYaml(
+        '''
 class: Target
 table: target_new
 fields:
   name: String
           ''',
-        ).build(),
-        ModelSourceBuilder().withFileName('source').withYaml(
-          '''
+      ).build(),
+      ModelSourceBuilder().withFileName('source').withYaml(
+        '''
 class: Source
 table: source
 fields:
   name: String
           ''',
-        ).build(),
-      ];
+      ).build(),
+    ];
 
-      var collector = CodeGenerationCollector();
-      var analyzer =
-          StatefulAnalyzer(config, models, onErrorsCollector(collector));
-      var definitions = analyzer.validateAll();
-      expect(collector.errors, isEmpty);
-
-      targetDefinition = createDatabaseDefinitionFromModels(
-        definitions,
-        'example',
-        [],
-      );
-    }
+    var (:sourceDefinition, :targetDefinition) = databaseDefinitionsFromModels(
+      sourceModels,
+      targetModels,
+    );
 
     var migration = generateDatabaseMigration(
       databaseSource: sourceDefinition,
