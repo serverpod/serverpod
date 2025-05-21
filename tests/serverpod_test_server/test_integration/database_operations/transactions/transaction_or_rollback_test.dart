97be99363562eae2f53055f7c80928ae0318387c
Timm Preetz <timm@preetz.us>
2025-05-21 15:13:13 +0200

    fix(review): Remove tests using `IntegrationTestServer` and instead only use `withServerpod(rollbackDatabase: RollbackDatabase.disabled)`, which also gives us production-like transaction behavior

--- tests/serverpod_test_server/test_integration/database_operations/transactions/transaction_or_rollback_test.dart
+++ tests/serverpod_test_server/test_integration/database_operations/transactions/transaction_or_rollback_test.dart
@@ -1,235 +1,10 @@
 import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_test_server/src/generated/protocol.dart';
-import 'package:serverpod_test_server/test_util/test_serverpod.dart';
 import 'package:test/test.dart';
 
 import '../../test_tools/serverpod_test_tools.dart';
 
 void main() async {
-  group('Given a non-test `Session` implementation,', () {
-    late Session session;
-
-    setUp(() async {
-      session = await IntegrationTestServer().session();
-    });
-
-    tearDown(() async {
-      await SimpleData.db.deleteWhere(
-        session,
-        where: (_) => Constant.bool(true),
-      );
-    });
-
-    test(
-        'when inspecting the session, '
-        'then it does not have an implicit transaction attached to it.',
-        () async {
-      // ignore: invalid_use_of_visible_for_testing_member
-      expect(session.transaction, isNull);
-    });
-
-    test(
-        'when creating a transaction with `transactionOrSavepoint`, '
-        'then the data is visible only on the transaction until the closure completes.',
-        () async {
-      await DatabaseUtil.transactionOrSavepoint(
-        session.db,
-        (final transaction) async {
-          await SimpleData.db.insertRow(
-            session,
-            SimpleData(num: 1),
-            transaction: transaction,
-          );
-
-          expect(
-            await SimpleData.db.count(session, transaction: transaction),
-            1,
-          );
-          expect(
-            await SimpleData.db.count(session),
-            0,
-          );
-        },
-        transaction: null,
-      );
-
-      expect(
-        await SimpleData.db.count(session),
-        1,
-      );
-    });
-
-    test(
-        'when creating two independent transactions with `transactionOrSavepoint`, '
-        'then the data is visible only to each transaction that wrote it.',
-        () async {
-      await DatabaseUtil.transactionOrSavepoint(
-        session.db,
-        (final transaction1) async {
-          await DatabaseUtil.transactionOrSavepoint(
-            session.db,
-            (final transaction2) async {
-              await SimpleData.db.insertRow(
-                session,
-                SimpleData(num: 1),
-                transaction: transaction2,
-              );
-              await SimpleData.db.insertRow(
-                session,
-                SimpleData(num: 2),
-                transaction: transaction1,
-              );
-
-              expect(
-                await SimpleData.db.count(session, transaction: transaction1),
-                1,
-              );
-              expect(
-                await SimpleData.db.count(session, transaction: transaction2),
-                1,
-              );
-            },
-            transaction: null,
-          );
-        },
-        transaction: null,
-      );
-
-      expect(
-        await SimpleData.db.count(session),
-        2,
-      );
-    });
-
-    test(
-        'when creating savepoint under an existing transaction with `transactionOrSavepoint`, '
-        'then the data is visible only to each transaction argument given to the callback.',
-        () async {
-      await DatabaseUtil.transactionOrSavepoint(
-        session.db,
-        (final transaction1) async {
-          await SimpleData.db.insertRow(
-            session,
-            SimpleData(num: 1),
-            transaction: transaction1,
-          );
-
-          await DatabaseUtil.transactionOrSavepoint(
-            session.db,
-            (final transaction2) async {
-              expect(identical(transaction1, transaction2), isTrue);
-
-              await SimpleData.db.insertRow(
-                session,
-                SimpleData(num: 2),
-                transaction: transaction2,
-              );
-
-              expect(
-                await SimpleData.db.count(session, transaction: transaction1),
-                2,
-              );
-              expect(
-                await SimpleData.db.count(session, transaction: transaction2),
-                2,
-              );
-            },
-            transaction: transaction1,
-          );
-
-          expect(
-            await SimpleData.db.count(session, transaction: transaction1),
-            2,
-          );
-        },
-        transaction: null,
-      );
-
-      expect(
-        await SimpleData.db.count(session),
-        2,
-      );
-    });
-
-    test(
-        'when throwing in a nested savepoint callback, '
-        'then the parent transaction can still be completed and its data be visible on the outside.',
-        () async {
-      await DatabaseUtil.transactionOrSavepoint(
-        session.db,
-        (final transaction1) async {
-          await SimpleData.db.insertRow(
-            session,
-            SimpleData(num: 1),
-            transaction: transaction1,
-          );
-
-          try {
-            await DatabaseUtil.transactionOrSavepoint(
-              session.db,
-              (final transaction2) async {
-                await SimpleData.db.insertRow(
-                  session,
-                  SimpleData(num: 2),
-                  transaction: transaction1,
-                );
-
-                expect(
-                  await SimpleData.db.count(session, transaction: transaction2),
-                  2,
-                );
-
-                throw _ForcedTestException();
-              },
-              transaction: transaction1,
-            );
-          } on _ForcedTestException catch (_) {}
-        },
-        transaction: null,
-      );
-
-      expect(
-        await SimpleData.db.count(session),
-        1,
-      );
-    });
-
-    test(
-        'when throwing in a nested transaction callback, '
-        'then the the data written with the transaction is not visible on the outside.',
-        () async {
-      try {
-        await DatabaseUtil.transactionOrSavepoint(
-          session.db,
-          (final transaction) async {
-            await SimpleData.db.insertRow(
-              session,
-              SimpleData(num: 1),
-              transaction: transaction,
-            );
-            await SimpleData.db.insertRow(
-              session,
-              SimpleData(num: 2),
-            );
-
-            expect(
-              await SimpleData.db.count(session, transaction: transaction),
-              2,
-            );
-
-            throw _ForcedTestException();
-          },
-          transaction: null,
-        );
-      } on _ForcedTestException catch (_) {}
-
-      expect(
-        await SimpleData.db.count(session),
-        1,
-      );
-    });
-  });
-
   withServerpod(
     'Given `withServerpod` with rollback after each test,`',
     (final sessionBuilder, final endpoints) {
@@ -237,14 +12,9 @@ void main() async {
 
       setUp(() async {
         session = sessionBuilder.build();
-      });
 
-      test(
-          'when inspecting the session, '
-          'then it does have an implicit transaction attached to it.',
-          () async {
         // ignore: invalid_use_of_visible_for_testing_member
-        expect(session.transaction, isNotNull);
+        assert(session.transaction != null);
       });
 
       test(
@@ -413,6 +183,9 @@ void main() async {
 
       setUp(() async {
         session = sessionBuilder.build();
+
+        // ignore: invalid_use_of_visible_for_testing_member
+        expect(session.transaction, isNull);
       });
 
       tearDown(() async {
@@ -420,14 +193,6 @@ void main() async {
           session,
           where: (_) => Constant.bool(true),
         );
-      });
-
-      test(
-          'when inspecting the session, '
-          'then it does not have an implicit transaction attached to it.',
-          () async {
-        // ignore: invalid_use_of_visible_for_testing_member
-        expect(session.transaction, isNull);
       });
 
       test(
