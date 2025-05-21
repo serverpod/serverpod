619059070260ffb778bdfc0bce873c66e6d16128
Timm Preetz <timm@preetz.us>
2025-05-21 21:29:27 +0200

    fix(review): Use `TestTags.concurrencyOneTestTag` for tests

--- tests/serverpod_test_server/test_integration/database_operations/transactions/transaction_or_rollback_test.dart
+++ tests/serverpod_test_server/test_integration/database_operations/transactions/transaction_or_rollback_test.dart
@@ -1,5 +1,6 @@
 import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_test_server/src/generated/protocol.dart';
+import 'package:serverpod_test_server/test_util/test_tags.dart';
 import 'package:test/test.dart';
 
 import '../../test_tools/serverpod_test_tools.dart';
@@ -174,6 +175,7 @@ void main() async {
       });
     },
     rollbackDatabase: RollbackDatabase.afterEach,
+    testGroupTagsOverride: [TestTags.concurrencyOneTestTag],
   );
 
   withServerpod(
@@ -398,6 +400,7 @@ void main() async {
       });
     },
     rollbackDatabase: RollbackDatabase.disabled,
+    testGroupTagsOverride: [TestTags.concurrencyOneTestTag],
   );
 }
 
