434cddeed6b053ac2897591eecdc879777ee309c
Timm Preetz <timm@preetz.us>
2025-05-22 12:07:04 +0200

    Generate code

--- modules/new_serverpod_auth/serverpod_auth_jwt/serverpod_auth_jwt_client/lib/src/protocol/protocol.dart
+++ modules/new_serverpod_auth/serverpod_auth_jwt/serverpod_auth_jwt_client/lib/src/protocol/protocol.dart
@@ -10,8 +10,18 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:serverpod_client/serverpod_client.dart' as _i1;
-import 'module_class.dart' as _i2;
-export 'module_class.dart';
+import 'refresh_token_expired_exception.dart' as _i2;
+import 'refresh_token_invalid_secret_exception.dart' as _i3;
+import 'refresh_token_malformed_exception.dart' as _i4;
+import 'refresh_token_not_found_exception.dart' as _i5;
+import 'token_pair.dart' as _i6;
+import 'package:serverpod_auth_user_client/serverpod_auth_user_client.dart'
+    as _i7;
+export 'refresh_token_expired_exception.dart';
+export 'refresh_token_invalid_secret_exception.dart';
+export 'refresh_token_malformed_exception.dart';
+export 'refresh_token_not_found_exception.dart';
+export 'token_pair.dart';
 export 'client.dart';
 
 class Protocol extends _i1.SerializationManager {
@@ -27,12 +37,47 @@ class Protocol extends _i1.SerializationManager {
     Type? t,
   ]) {
     t ??= T;
-    if (t == _i2.ModuleClass) {
-      return _i2.ModuleClass.fromJson(data) as T;
+    if (t == _i2.RefreshTokenExpiredException) {
+      return _i2.RefreshTokenExpiredException.fromJson(data) as T;
     }
-    if (t == _i1.getType<_i2.ModuleClass?>()) {
-      return (data != null ? _i2.ModuleClass.fromJson(data) : null) as T;
+    if (t == _i3.RefreshTokenInvalidSecretException) {
+      return _i3.RefreshTokenInvalidSecretException.fromJson(data) as T;
     }
+    if (t == _i4.RefreshTokenMalformedException) {
+      return _i4.RefreshTokenMalformedException.fromJson(data) as T;
+    }
+    if (t == _i5.RefreshTokenNotFoundException) {
+      return _i5.RefreshTokenNotFoundException.fromJson(data) as T;
+    }
+    if (t == _i6.TokenPair) {
+      return _i6.TokenPair.fromJson(data) as T;
+    }
+    if (t == _i1.getType<_i2.RefreshTokenExpiredException?>()) {
+      return (data != null
+          ? _i2.RefreshTokenExpiredException.fromJson(data)
+          : null) as T;
+    }
+    if (t == _i1.getType<_i3.RefreshTokenInvalidSecretException?>()) {
+      return (data != null
+          ? _i3.RefreshTokenInvalidSecretException.fromJson(data)
+          : null) as T;
+    }
+    if (t == _i1.getType<_i4.RefreshTokenMalformedException?>()) {
+      return (data != null
+          ? _i4.RefreshTokenMalformedException.fromJson(data)
+          : null) as T;
+    }
+    if (t == _i1.getType<_i5.RefreshTokenNotFoundException?>()) {
+      return (data != null
+          ? _i5.RefreshTokenNotFoundException.fromJson(data)
+          : null) as T;
+    }
+    if (t == _i1.getType<_i6.TokenPair?>()) {
+      return (data != null ? _i6.TokenPair.fromJson(data) : null) as T;
+    }
+    try {
+      return _i7.Protocol().deserialize<T>(data, t);
+    } on _i1.DeserializationTypeNotFoundException catch (_) {}
     return super.deserialize<T>(data, t);
   }
 
@@ -40,8 +85,24 @@ class Protocol extends _i1.SerializationManager {
   String? getClassNameForObject(Object? data) {
     String? className = super.getClassNameForObject(data);
     if (className != null) return className;
-    if (data is _i2.ModuleClass) {
-      return 'ModuleClass';
+    if (data is _i2.RefreshTokenExpiredException) {
+      return 'RefreshTokenExpiredException';
+    }
+    if (data is _i3.RefreshTokenInvalidSecretException) {
+      return 'RefreshTokenInvalidSecretException';
+    }
+    if (data is _i4.RefreshTokenMalformedException) {
+      return 'RefreshTokenMalformedException';
+    }
+    if (data is _i5.RefreshTokenNotFoundException) {
+      return 'RefreshTokenNotFoundException';
+    }
+    if (data is _i6.TokenPair) {
+      return 'TokenPair';
+    }
+    className = _i7.Protocol().getClassNameForObject(data);
+    if (className != null) {
+      return 'serverpod_auth_user.$className';
     }
     return null;
   }
@@ -52,8 +113,24 @@ class Protocol extends _i1.SerializationManager {
     if (dataClassName is! String) {
       return super.deserializeByClassName(data);
     }
-    if (dataClassName == 'ModuleClass') {
-      return deserialize<_i2.ModuleClass>(data['data']);
+    if (dataClassName == 'RefreshTokenExpiredException') {
+      return deserialize<_i2.RefreshTokenExpiredException>(data['data']);
+    }
+    if (dataClassName == 'RefreshTokenInvalidSecretException') {
+      return deserialize<_i3.RefreshTokenInvalidSecretException>(data['data']);
+    }
+    if (dataClassName == 'RefreshTokenMalformedException') {
+      return deserialize<_i4.RefreshTokenMalformedException>(data['data']);
+    }
+    if (dataClassName == 'RefreshTokenNotFoundException') {
+      return deserialize<_i5.RefreshTokenNotFoundException>(data['data']);
+    }
+    if (dataClassName == 'TokenPair') {
+      return deserialize<_i6.TokenPair>(data['data']);
+    }
+    if (dataClassName.startsWith('serverpod_auth_user.')) {
+      data['className'] = dataClassName.substring(20);
+      return _i7.Protocol().deserializeByClassName(data);
     }
     return super.deserializeByClassName(data);
   }
--- modules/new_serverpod_auth/serverpod_auth_jwt/serverpod_auth_jwt_server/migrations/20250522114939016/definition.sql
+++ modules/new_serverpod_auth/serverpod_auth_jwt/serverpod_auth_jwt_server/migrations/20250522114939016/definition.sql
@@ -1,6 +1,18 @@
 BEGIN;
 
 --
+-- Class RefreshToken as table serverpod_auth_jwt_refresh_token
+--
+CREATE TABLE "serverpod_auth_jwt_refresh_token" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    "authUserId" uuid NOT NULL,
+    "scopeNames" json NOT NULL,
+    "secret" text NOT NULL,
+    "lastUpdated" timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created" timestamp without time zone NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+--
 -- Class CloudStorageEntry as table serverpod_cloud_storage
 --
 CREATE TABLE "serverpod_cloud_storage" (
@@ -207,6 +219,26 @@ CREATE INDEX "serverpod_session_log_touched_idx" ON "serverpod_session_log" USIN
 CREATE INDEX "serverpod_session_log_isopen_idx" ON "serverpod_session_log" USING btree ("isOpen");
 
 --
+-- Class AuthUser as table serverpod_auth_user
+--
+CREATE TABLE "serverpod_auth_user" (
+    "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    "created" timestamp without time zone NOT NULL,
+    "scopeNames" json NOT NULL,
+    "blocked" boolean NOT NULL
+);
+
+--
+-- Foreign relations for "serverpod_auth_jwt_refresh_token" table
+--
+ALTER TABLE ONLY "serverpod_auth_jwt_refresh_token"
+    ADD CONSTRAINT "serverpod_auth_jwt_refresh_token_fk_0"
+    FOREIGN KEY("authUserId")
+    REFERENCES "serverpod_auth_user"("id")
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION;
+
+--
 -- Foreign relations for "serverpod_log" table
 --
 ALTER TABLE ONLY "serverpod_log"
@@ -241,9 +273,9 @@ ALTER TABLE ONLY "serverpod_query_log"
 -- MIGRATION VERSION FOR serverpod_auth_jwt
 --
 INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
-    VALUES ('serverpod_auth_jwt', '20250522073302514', now())
+    VALUES ('serverpod_auth_jwt', '20250522114939016', now())
     ON CONFLICT ("module")
-    DO UPDATE SET "version" = '20250522073302514', "timestamp" = now();
+    DO UPDATE SET "version" = '20250522114939016', "timestamp" = now();
 
 --
 -- MIGRATION VERSION FOR serverpod
@@ -252,6 +284,14 @@ INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
     VALUES ('serverpod', '20240516151843329', now())
     ON CONFLICT ("module")
     DO UPDATE SET "version" = '20240516151843329', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod_auth_user
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_auth_user', '20250506070330492', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20250506070330492', "timestamp" = now();
 
 
 COMMIT;
