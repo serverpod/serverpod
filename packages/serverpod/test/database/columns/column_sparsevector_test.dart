b91b7ea5d7bdc85c53068ddd13a3a8c3a6973025
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-06-04 00:28:43 -0300

    feat: Add column vector classes

--- packages/serverpod/test/database/columns/column_sparsevector_test.dart
+++ packages/serverpod/test/database/columns/column_sparsevector_test.dart
@@ -2,10 +2,10 @@ import 'package:serverpod/serverpod.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Given a ColumnVector', () {
-    var columnName = 'embedding';
-    var dimension = 3;
-    var column = ColumnVector(
+  group('Given a ColumnSparseVector', () {
+    var columnName = 'sparse_data';
+    var dimension = 8;
+    var column = ColumnSparseVector(
       columnName,
       Table<int?>(tableName: 'test'),
       dimension: dimension,
@@ -21,8 +21,8 @@ void main() {
       expect(column.columnName, columnName);
     });
 
-    test('when type is called then Vector is returned.', () {
-      expect(column.type, Vector);
+    test('when type is called then SparseVector is returned.', () {
+      expect(column.type, SparseVector);
     });
 
     test('when dimension is accessed then correct dimension is returned.', () {
@@ -31,48 +31,48 @@ void main() {
 
     group('with _ColumnDefaultOperations mixin', () {
       test(
-          'when equals compared to vector value then output is equals expression.',
+          'when equals compared to SparseVector value then output is equals expression.',
           () {
-        var testVector = const Vector([1.0, 2.0, 3.0]);
+        var testVector = SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension);
         var comparisonExpression = column.equals(testVector);
 
         expect(
           comparisonExpression.toString(),
-          "$column = '[1.0,2.0,3.0]'",
+          "$column = '{1:1.0,2:3.0}/8'",
         );
       });
 
       test(
-          'when NOT equals compared to vector value then output is NOT equals expression.',
+          'when NOT equals compared to SparseVector value then output is NOT equals expression.',
           () {
-        var testVector = const Vector([1.0, 2.0, 3.0]);
+        var testVector = SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension);
         var comparisonExpression = column.notEquals(testVector);
 
         expect(
           comparisonExpression.toString(),
-          "$column != '[1.0,2.0,3.0]'",
+          "$column != '{1:1.0,2:3.0}/8'",
         );
       });
 
       test(
           'when checking if expression is in value set then output is IN expression.',
           () {
-        var comparisonExpression = column.inSet(<Vector>{
-          const Vector([1.0, 2.0, 3.0]),
-          const Vector([4.0, 5.0, 6.0]),
-          const Vector([7.0, 8.0, 9.0]),
+        var comparisonExpression = column.inSet(<SparseVector>{
+          SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension),
+          SparseVector.fromMap({2: 4.0, 3: 6.0}, dimension),
+          SparseVector.fromMap({1: 7.0, 4: 9.0}, dimension),
         });
 
         expect(
           comparisonExpression.toString(),
-          "$column IN ('[1.0,2.0,3.0]', '[4.0,5.0,6.0]', '[7.0,8.0,9.0]')",
+          "$column IN ('{1:1.0,2:3.0}/8', '{2:4.0,3:6.0}/8', '{1:7.0,4:9.0}/8')",
         );
       });
 
       test(
           'when checking if expression is in empty value set then output is FALSE expression.',
           () {
-        var comparisonExpression = column.inSet(<Vector>{});
+        var comparisonExpression = column.inSet(<SparseVector>{});
 
         expect(comparisonExpression.toString(), 'FALSE');
       });
@@ -80,22 +80,22 @@ void main() {
       test(
           'when checking if expression is NOT in value set then output is NOT IN expression.',
           () {
-        var comparisonExpression = column.notInSet(<Vector>{
-          const Vector([1.0, 2.0, 3.0]),
-          const Vector([4.0, 5.0, 6.0]),
-          const Vector([7.0, 8.0, 9.0]),
+        var comparisonExpression = column.notInSet(<SparseVector>{
+          SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension),
+          SparseVector.fromMap({2: 4.0, 3: 6.0}, dimension),
+          SparseVector.fromMap({1: 7.0, 4: 9.0}, dimension),
         });
 
         expect(
           comparisonExpression.toString(),
-          "$column NOT IN ('[1.0,2.0,3.0]', '[4.0,5.0,6.0]', '[7.0,8.0,9.0]')",
+          "$column NOT IN ('{1:1.0,2:3.0}/8', '{2:4.0,3:6.0}/8', '{1:7.0,4:9.0}/8')",
         );
       });
 
       test(
           'when checking if expression is NOT in empty value set then output is TRUE expression.',
           () {
-        var comparisonExpression = column.notInSet(<Vector>{});
+        var comparisonExpression = column.notInSet(<SparseVector>{});
 
         expect(comparisonExpression.toString(), 'TRUE');
       });
@@ -105,60 +105,60 @@ void main() {
       test(
           'when distanceL2 is called then output is correct operator expression.',
           () {
-        var testVector = const Vector([1.0, 2.0, 3.0]);
+        var testVector = SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension);
         var comparisonExpression = column.distanceL2(testVector);
 
         expect(
           comparisonExpression.toString(),
-          "$column <-> '[1.0,2.0,3.0]'",
+          "$column <-> '{1:1.0,2:3.0}/8'",
         );
       });
 
       test(
           'when distanceInnerProduct is called then output is correct operator expression.',
           () {
-        var testVector = const Vector([1.0, 2.0, 3.0]);
+        var testVector = SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension);
         var comparisonExpression = column.distanceInnerProduct(testVector);
 
         expect(
           comparisonExpression.toString(),
-          "$column <#> '[1.0,2.0,3.0]'",
+          "$column <#> '{1:1.0,2:3.0}/8'",
         );
       });
 
       test(
           'when distanceCosine is called then output is correct operator expression.',
           () {
-        var testVector = const Vector([1.0, 2.0, 3.0]);
+        var testVector = SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension);
         var comparisonExpression = column.distanceCosine(testVector);
 
         expect(
           comparisonExpression.toString(),
-          "$column <=> '[1.0,2.0,3.0]'",
+          "$column <=> '{1:1.0,2:3.0}/8'",
         );
       });
 
       test(
           'when distanceL1 is called then output is correct operator expression.',
           () {
-        var testVector = const Vector([1.0, 2.0, 3.0]);
+        var testVector = SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension);
         var comparisonExpression = column.distanceL1(testVector);
 
         expect(
           comparisonExpression.toString(),
-          "$column <+> '[1.0,2.0,3.0]'",
+          "$column <+> '{1:1.0,2:3.0}/8'",
         );
       });
 
       test(
           'when comparing a distance to a value then output is correct comparison expression.',
           () {
-        var testVector = const Vector([1.0, 2.0, 3.0]);
+        var testVector = SparseVector.fromMap({1: 1.0, 2: 3.0}, dimension);
         var comparisonExpression = column.distanceL2(testVector) < 0.5;
 
         expect(
           comparisonExpression.toString(),
-          "$column <-> '[1.0,2.0,3.0]' < 0.5",
+          "$column <-> '{1:1.0,2:3.0}/8' < 0.5",
         );
       });
     });
