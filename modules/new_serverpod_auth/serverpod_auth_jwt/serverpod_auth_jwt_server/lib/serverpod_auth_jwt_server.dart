c3323d5b9e2f4de3a3c3e4e72edd88febc598a81
Timm Preetz <timm@preetz.us>
2025-06-04 10:54:15 +0200

    fix(review): Export `AuthenticationTokensAdmin` class

--- modules/new_serverpod_auth/serverpod_auth_jwt/serverpod_auth_jwt_server/lib/serverpod_auth_jwt_server.dart
+++ modules/new_serverpod_auth/serverpod_auth_jwt/serverpod_auth_jwt_server/lib/serverpod_auth_jwt_server.dart
@@ -2,5 +2,7 @@ export 'src/business/authentication_info_from_jwt.dart'
     show AuthenticationInfoJwt;
 export 'src/business/authentication_token_config.dart';
 export 'src/business/authentication_tokens.dart';
+export 'src/business/authentication_tokens_admin.dart'
+    show AuthenticationTokensAdmin;
 export 'src/generated/endpoints.dart';
 export 'src/generated/protocol.dart' hide RefreshToken, RefreshTokenRepository;
