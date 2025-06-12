45ba9a18539221920b8020eb755e8410553a1c1d
Timm Preetz <timm@preetz.us>
2025-06-12 14:25:57 +0200

    fix(review): Keep exporting the `AuthUser` & database related classes So we don't have to make analyzer exceptions that could break in other configuration

--- modules/new_serverpod_auth/serverpod_auth_user/serverpod_auth_user_server/lib/serverpod_auth_user_server.dart
+++ modules/new_serverpod_auth/serverpod_auth_user/serverpod_auth_user_server/lib/serverpod_auth_user_server.dart
@@ -1,16 +1,13 @@
-import 'package:meta/meta.dart';
-
 export 'src/business/auth_users.dart' show AuthUsers;
 export 'src/generated/endpoints.dart' show Endpoints;
-// We don't want to expose these, but have to for the generated database code in consuming packages.
-@visibleForTesting
-export 'src/generated/protocol.dart'
-    show AuthUser, AuthUserInclude, AuthUserTable;
 export 'src/generated/protocol.dart'
     show
         Protocol,
-        AuthUserModel,
+        AuthUserTable,
+        AuthUserInclude,
+        AuthUser,
         AuthUserBlockedException,
+        AuthUserModel,
         AuthUserNotFoundException;
 export 'src/util/auth_user_scopes_extension.dart' show AuthUserScopes;
 export 'src/util/authentication_info_extension.dart'
