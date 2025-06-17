81db90ca2670401ae098190a97c37544df041a44
Timm Preetz <timm@preetz.us>
2025-06-17 10:33:10 +0200

    Add `AuthMigrationEmail` class providing helpers to trigger the import of email-based accounts - `migrateOnLogin`: For importing a user during the login call, if the credentials match in the old system - `migrateWithoutPassword`: For background processes and any other occasion (e.g. account creation, password reset) where the plain-text password from the client is not available, and thus the user can only be migrated without password

--- modules/new_serverpod_auth/serverpod_auth_migration/serverpod_auth_migration_server/lib/serverpod_auth_migration_server.dart
+++ modules/new_serverpod_auth/serverpod_auth_migration/serverpod_auth_migration_server/lib/serverpod_auth_migration_server.dart
@@ -1,2 +1,4 @@
+export 'src/business/auth_migration_email.dart';
+export 'src/business/auth_migration_email_config.dart';
 export 'src/generated/endpoints.dart';
 export 'src/generated/protocol.dart';
