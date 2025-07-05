5bb1e6578a4b75d97cd2e5fda6fcd026255f89ab
Timm Preetz <timm@preetz.us>
2025-07-05 12:25:33 +0200

    Implement backwards compatibility package (testing to follow in E2E package)

--- modules/new_serverpod_auth/serverpod_auth_backwards_compatibility/serverpod_auth_backwards_compatibility_server/lib/serverpod_auth_backwards_compatibility_server.dart
+++ modules/new_serverpod_auth/serverpod_auth_backwards_compatibility/serverpod_auth_backwards_compatibility_server/lib/serverpod_auth_backwards_compatibility_server.dart
@@ -1,2 +1,4 @@
+export 'src/business/auth_backwards_compatibility.dart';
+export 'src/business/config.dart';
 export 'src/generated/endpoints.dart';
-export 'src/generated/protocol.dart';
+export 'src/generated/protocol.dart' show Protocol;
---
+++ modules/new_serverpod_auth/serverpod_auth_backwards_compatibility/serverpod_auth_backwards_compatibility_server/lib/src/business/auth_backwards_compatibility.dart
@@ -0,0 +1,126 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_auth_backwards_compatibility_server/serverpod_auth_backwards_compatibility_server.dart';
+import 'package:serverpod_auth_backwards_compatibility_server/src/business/legacy_authentication_handler.dart';
+import 'package:serverpod_auth_backwards_compatibility_server/src/business/legacy_email_password_validator.dart';
+import 'package:serverpod_auth_backwards_compatibility_server/src/generated/protocol.dart';
+
+/// Collections of helper functions to work with legacy authentication data.
+abstract final class AuthBackwardsCompatibility {
+  /// The configuration used for the backwards compatibility.
+  ///
+  /// Should match the previous `AuthConfig`.
+  static var config = AuthBackwardsCompatibilityConfig();
+
+  /// Set a legacy `serverpod_auth` `EmailAuth` "hash" as a fallback password
+  /// for a `serverpod_auth_email_account` `EmailAccount`.
+  ///
+  /// In case the latter doesn't have a password set, it can look up the
+  /// previous one as a fallback here using [isLegacyPasswordValid].
+  static Future<void> storeLegacyPassword(
+    final Session session, {
+    required final UuidValue emailAccountId,
+
+    /// The legacy password hash
+    required final String passwordHash,
+    final Transaction? transaction,
+  }) async {
+    await LegacyEmailPassword.db.insertRow(
+      session,
+      LegacyEmailPassword(
+        emailAccountId: emailAccountId,
+        hash: passwordHash,
+      ),
+      transaction: transaction,
+    );
+  }
+
+  /// Checks whether the password was valid for the email account in the legacy
+  /// `serverpod_auth` module.
+  static Future<bool> isLegacyPasswordValid(
+    final Session session, {
+    required final UuidValue emailAccountId,
+    required final String email,
+    required final String password,
+    final Transaction? transaction,
+  }) async {
+    final legacyPassword = await LegacyEmailPassword.db.findFirstRow(
+      session,
+      where: (final t) => t.emailAccountId.equals(emailAccountId),
+      transaction: transaction,
+    );
+    if (legacyPassword == null) {
+      return false;
+    }
+
+    return legacyEmailPasswordValidator(
+      session,
+      email: email,
+      password: password,
+      passwordHash: legacyPassword.hash,
+    );
+  }
+
+  /// Imports a legacy session from `serverpod_auth` mapped to the new
+  ///  `AuthUser`.
+  ///
+  /// This can later be checked using [authenticationHandler].
+  static Future<void> storeLegacySession(
+    final Session session, {
+    required final UuidValue authUserId,
+
+    /// The legacy session key hash
+    required final String sessionKeyHash,
+    required final String method,
+    required final Iterable<String> scopeNames,
+    final Transaction? transaction,
+  }) async {
+    await LegacySession.db.insertRow(
+      session,
+      LegacySession(
+        authUserId: authUserId,
+        hash: sessionKeyHash,
+        method: method,
+        scopeNames: scopeNames.toSet(),
+      ),
+      transaction: transaction,
+    );
+  }
+
+  /// Check whether the `sessionKey` matches an imported legacy session.
+  static Future<AuthenticationInfo?> authenticationHandler(
+    final Session session,
+    final String sessionKey,
+  ) async {
+    return legacyAuthenticationHandler(session, sessionKey);
+  }
+
+  /// Imports a legacy (external) "user identifier".
+  static Future<void> storeLegacyExternalUserIdentifier(
+    final Session session, {
+    required final UuidValue authUserId,
+    required final String userIdentifier,
+    final Transaction? transaction,
+  }) async {
+    await LegacyExternalUserIdentifier.db.insertRow(
+      session,
+      LegacyExternalUserIdentifier(
+          authUserId: authUserId, userIdentifier: userIdentifier),
+      transaction: transaction,
+    );
+  }
+
+  /// Looks up a legacy (external) "user identifier".
+  static Future<UuidValue?> lookUpLegacyExternalUserIdentifier(
+    final Session session, {
+    required final String userIdentifier,
+    final Transaction? transaction,
+  }) async {
+    final row = await LegacyExternalUserIdentifier.db.findFirstRow(
+      session,
+      where: (final t) => t.userIdentifier.equals(userIdentifier),
+      transaction: transaction,
+    );
+
+    return row?.authUserId;
+  }
+}
