36777079b43a48897ea5e07b38c1996c9a546870
Timm Preetz <timm@preetz.us>
2025-07-07 09:53:41 +0200

    fix(review): Fix typo

--- modules/new_serverpod_auth/serverpod_auth_backwards_compatibility/serverpod_auth_backwards_compatibility_server/lib/src/business/config.dart
+++ modules/new_serverpod_auth/serverpod_auth_backwards_compatibility/serverpod_auth_backwards_compatibility_server/lib/src/business/config.dart
@@ -1,6 +1,6 @@
 /// Configuration for the `servpod_auth` backwards compability module.
 class AuthBackwardsCompatibilityConfig {
-  /// True if the server should use the accounts email address as part of the
+  /// True if the server should use the account's email address as part of the
   /// salt when storing password hashes (strongly recommended). Default is true.
   final bool extraSaltyHash;
 
