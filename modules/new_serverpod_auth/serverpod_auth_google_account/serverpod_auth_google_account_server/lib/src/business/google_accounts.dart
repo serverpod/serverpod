c27a09db35fa713ddc6f788f777d6e8a5b0b7be0
Timm Preetz <timm@preetz.us>
2025-07-10 15:23:49 +0200

    feat(auth2): Add support for Google account in backwards compatibility package

--- modules/new_serverpod_auth/serverpod_auth_google_account/serverpod_auth_google_account_server/lib/src/business/google_accounts.dart
+++ modules/new_serverpod_auth/serverpod_auth_google_account/serverpod_auth_google_account_server/lib/src/business/google_accounts.dart
@@ -1,13 +1,12 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
 import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_auth_google_account_server/serverpod_auth_google_account_server.dart';
-import 'package:serverpod_auth_google_account_server/src/business/google_auth.dart';
 import 'package:serverpod_auth_user_server/serverpod_auth_user_server.dart';
 
 /// Management functions to work with Google-backed accounts.
 abstract final class GoogleAccounts {
+  /// Admin operations to work with Google-backed accounts.
+  static final admin = GoogleAccountsAdmin();
+
   /// Authenticates a user using an ID token.
   ///
   /// If the external user ID is not yet known in the system, a new `AuthUser`
@@ -17,7 +16,10 @@ abstract final class GoogleAccounts {
     required final String idToken,
     final Transaction? transaction,
   }) async {
-    final accountDetails = await _getAccount(session, idToken: idToken);
+    final accountDetails = await admin.fetchAccountDetails(
+      session,
+      idToken: idToken,
+    );
 
     var googleAccount = await GoogleAccount.db.findFirstRow(
       session,
@@ -38,13 +40,10 @@ abstract final class GoogleAccounts {
             transaction: transaction,
           );
 
-          googleAccount = await GoogleAccount.db.insertRow(
+          googleAccount = await admin.linkGoogleAuthentication(
             session,
-            GoogleAccount(
-              userIdentifier: accountDetails.userIdentifier,
-              email: accountDetails.email,
-              authUserId: authUser.id,
-            ),
+            authUserId: authUser.id,
+            accountDetails: accountDetails,
             transaction: transaction,
           );
         },
@@ -58,95 +57,6 @@ abstract final class GoogleAccounts {
       authUserNewlyCreated: authUserNewlyCreated,
     );
   }
-
-  static Future<GoogleAccountDetails> _getAccount(
-    final Session session, {
-    required final String idToken,
-  }) async {
-    final clientSecret = GoogleAuth.clientSecret;
-    if (clientSecret == null) {
-      throw StateError('The server side Google client secret is not loaded.');
-    }
-
-    final String clientId = clientSecret.clientId;
-
-    // Verify the ID token with Google's servers.
-    final response = await http.get(
-      Uri.https(
-        'www.googleapis.com',
-        '/oauth2/v3/tokeninfo',
-        {'id_token': idToken},
-      ),
-    );
-
-    if (response.statusCode != 200) {
-      session.log(
-        'Invalid token response status: ${response.statusCode}',
-        level: LogLevel.debug,
-      );
-
-      throw GoogleIdTokenVerificationException();
-    }
-
-    final data = jsonDecode(response.body);
-    if (data is! Map<String, dynamic>) {
-      session.log(
-        'Invalid token response, content was not a Map.',
-        level: LogLevel.debug,
-      );
-
-      throw GoogleIdTokenVerificationException();
-    }
-
-    final issuer = data['iss'] as String?;
-    if (issuer != 'accounts.google.com' &&
-        issuer != 'https://accounts.google.com') {
-      session.log(
-        'Invalid token response, unexpected issuer "$issuer"',
-        level: LogLevel.debug,
-      );
-
-      throw GoogleIdTokenVerificationException();
-    }
-
-    if (data['aud'] != clientId) {
-      session.log(
-        'Invalid token response, client ID does not match',
-        level: LogLevel.debug,
-      );
-
-      throw GoogleIdTokenVerificationException();
-    }
-
-    final userId = data['sub'] as String?;
-    final email = data['email'] as String?;
-    final verifiedEmail = data['verified_email'] as bool?;
-    final fullName = data['name'] as String?;
-    final image = data['picture'] as String?;
-    final name = data['given_name'] as String?;
-
-    if (userId == null ||
-        email == null ||
-        verifiedEmail != true ||
-        fullName == null ||
-        image == null ||
-        name == null) {
-      session.log(
-        'Invalid token response, missing required data: $data',
-        level: LogLevel.debug,
-      );
-
-      throw GoogleIdTokenVerificationException();
-    }
-
-    return (
-      userIdentifier: userId,
-      email: email,
-      name: name,
-      fullName: fullName,
-      image: image,
-    );
-  }
 }
 
 /// Details of a successful Google-based authentication.
@@ -155,16 +65,4 @@ typedef GoogleAuthSuccess = ({
   UuidValue authUserId,
   GoogleAccountDetails details,
   bool authUserNewlyCreated,
-});
-
-/// Details of the Google Account.
-typedef GoogleAccountDetails = ({
-  /// Google's user identifier for this account
-  String userIdentifier,
-
-  /// The verified email received from Google
-  String email,
-  String name,
-  String fullName,
-  String image
 });
