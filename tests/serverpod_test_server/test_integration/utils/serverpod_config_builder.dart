9de8852de2f1611af445b915e695c1a63b7ccace
exaby73 <nabeel@ex3.dev>
2025-07-10 15:49:59 +0530

    feat: Introduce builders for Database, Redis, Server, SessionLog, and FutureCall configurations in ServerpodConfigBuilder; Add defaults for the rest of the fields in ServerpodConfigBuilder

--- tests/serverpod_test_server/test_integration/utils/serverpod_config_builder.dart
+++ tests/serverpod_test_server/test_integration/utils/serverpod_config_builder.dart
@@ -1,5 +1,216 @@
 import 'package:serverpod_shared/serverpod_shared.dart';
 
+class DatabaseConfigBuilder {
+  String _host = 'localhost';
+  int _port = 8090;
+  String _name = 'serverpod_test';
+  String _user = 'postgres';
+  String _password = 'password';
+  bool _requireSsl = false;
+  bool _isUnixSocket = false;
+  List<String>? _searchPaths;
+
+  DatabaseConfigBuilder();
+
+  DatabaseConfig build() {
+    return DatabaseConfig(
+      host: _host,
+      port: _port,
+      name: _name,
+      user: _user,
+      password: _password,
+      requireSsl: _requireSsl,
+      isUnixSocket: _isUnixSocket,
+      searchPaths: _searchPaths,
+    );
+  }
+
+  DatabaseConfigBuilder withHost(String host) {
+    _host = host;
+    return this;
+  }
+
+  DatabaseConfigBuilder withPort(int port) {
+    _port = port;
+    return this;
+  }
+
+  DatabaseConfigBuilder withName(String name) {
+    _name = name;
+    return this;
+  }
+
+  DatabaseConfigBuilder withUser(String user) {
+    _user = user;
+    return this;
+  }
+
+  DatabaseConfigBuilder withPassword(String password) {
+    _password = password;
+    return this;
+  }
+
+  DatabaseConfigBuilder withRequireSsl(bool requireSsl) {
+    _requireSsl = requireSsl;
+    return this;
+  }
+
+  DatabaseConfigBuilder withIsUnixSocket(bool isUnixSocket) {
+    _isUnixSocket = isUnixSocket;
+    return this;
+  }
+
+  DatabaseConfigBuilder withSearchPaths(List<String>? searchPaths) {
+    _searchPaths = searchPaths;
+    return this;
+  }
+}
+
+class RedisConfigBuilder {
+  bool _enabled = false;
+  String _host = 'localhost';
+  int _port = 8091;
+  String? _user;
+  String? _password = 'password';
+  bool _requireSsl = false;
+
+  RedisConfigBuilder();
+
+  RedisConfig build() {
+    return RedisConfig(
+      enabled: _enabled,
+      host: _host,
+      port: _port,
+      user: _user,
+      password: _password,
+      requireSsl: _requireSsl,
+    );
+  }
+
+  RedisConfigBuilder withEnabled(bool enabled) {
+    _enabled = enabled;
+    return this;
+  }
+
+  RedisConfigBuilder withHost(String host) {
+    _host = host;
+    return this;
+  }
+
+  RedisConfigBuilder withPort(int port) {
+    _port = port;
+    return this;
+  }
+
+  RedisConfigBuilder withUser(String? user) {
+    _user = user;
+    return this;
+  }
+
+  RedisConfigBuilder withPassword(String? password) {
+    _password = password;
+    return this;
+  }
+
+  RedisConfigBuilder withRequireSsl(bool requireSsl) {
+    _requireSsl = requireSsl;
+    return this;
+  }
+}
+
+class ServerConfigBuilder {
+  int _port = 8080;
+  String _publicHost = 'localhost';
+  int _publicPort = 8080;
+  String _publicScheme = 'http';
+
+  ServerConfigBuilder();
+
+  ServerConfig build() {
+    return ServerConfig(
+      port: _port,
+      publicHost: _publicHost,
+      publicPort: _publicPort,
+      publicScheme: _publicScheme,
+    );
+  }
+
+  ServerConfigBuilder withPort(int port) {
+    _port = port;
+    return this;
+  }
+
+  ServerConfigBuilder withPublicHost(String publicHost) {
+    _publicHost = publicHost;
+    return this;
+  }
+
+  ServerConfigBuilder withPublicPort(int publicPort) {
+    _publicPort = publicPort;
+    return this;
+  }
+
+  ServerConfigBuilder withPublicScheme(String publicScheme) {
+    _publicScheme = publicScheme;
+    return this;
+  }
+}
+
+class SessionLogConfigBuilder {
+  bool _persistentEnabled = true;
+  bool _consoleEnabled = true;
+  ConsoleLogFormat _consoleLogFormat = ConsoleLogFormat.text;
+
+  SessionLogConfigBuilder();
+
+  SessionLogConfig build() {
+    return SessionLogConfig(
+      persistentEnabled: _persistentEnabled,
+      consoleEnabled: _consoleEnabled,
+      consoleLogFormat: _consoleLogFormat,
+    );
+  }
+
+  SessionLogConfigBuilder withPersistentEnabled(bool persistentEnabled) {
+    _persistentEnabled = persistentEnabled;
+    return this;
+  }
+
+  SessionLogConfigBuilder withConsoleEnabled(bool consoleEnabled) {
+    _consoleEnabled = consoleEnabled;
+    return this;
+  }
+
+  SessionLogConfigBuilder withConsoleLogFormat(ConsoleLogFormat consoleLogFormat) {
+    _consoleLogFormat = consoleLogFormat;
+    return this;
+  }
+}
+
+class FutureCallConfigBuilder {
+  int? _concurrencyLimit = 1;
+  Duration _scanInterval = const Duration(milliseconds: 5000);
+
+  FutureCallConfigBuilder();
+
+  FutureCallConfig build() {
+    return FutureCallConfig(
+      concurrencyLimit: _concurrencyLimit,
+      scanInterval: _scanInterval,
+    );
+  }
+
+  FutureCallConfigBuilder withConcurrencyLimit(int? concurrencyLimit) {
+    _concurrencyLimit = concurrencyLimit;
+    return this;
+  }
+
+  FutureCallConfigBuilder withScanInterval(Duration scanInterval) {
+    _scanInterval = scanInterval;
+    return this;
+  }
+}
+
 class ServerpodConfigBuilder {
   String _runMode = 'development';
   String _serverId = 'default';
@@ -13,7 +224,7 @@ class ServerpodConfigBuilder {
   ServerConfig? _webServer;
   DatabaseConfig? _database;
   RedisConfig? _redis;
-  String? _serviceSecret;
+  String? _serviceSecret = 'test_service_secret';
   SessionLogConfig? _sessionLogs;
   Duration? _experimentalDiagnosticHandlerTimeout = const Duration(seconds: 30);
   FutureCallConfig _futureCall = const FutureCallConfig();
@@ -33,12 +244,12 @@ class ServerpodConfigBuilder {
       apiServer: _apiServer ?? ServerpodConfig.createDefaultApiServer(),
       insightsServer: _insightsServer ?? _createDefaultInsightsServer(),
       webServer: _webServer ?? _createDefaultWebServer(),
-      database: _database,
-      redis: _redis,
+      database: _database ?? DatabaseConfigBuilder().build(),
+      redis: _redis ?? RedisConfigBuilder().build(),
       serviceSecret: _serviceSecret,
       sessionLogs: _sessionLogs ??
           SessionLogConfig.buildDefault(
-            databaseEnabled: _database != null,
+            databaseEnabled: true,
             runMode: _runMode,
           ),
       experimentalDiagnosticHandlerTimeout:
@@ -88,6 +299,11 @@ class ServerpodConfigBuilder {
     return this;
   }
 
+  ServerpodConfigBuilder withApiServerBuilder(ServerConfigBuilder Function(ServerConfigBuilder) builder) {
+    _apiServer = builder(ServerConfigBuilder()).build();
+    return this;
+  }
+
   ServerpodConfigBuilder withInsightsServer(ServerConfig? insightsServer) {
     _insightsServer = insightsServer;
     return this;
@@ -103,8 +319,18 @@ class ServerpodConfigBuilder {
     return this;
   }
 
+  ServerpodConfigBuilder withDatabaseBuilder(DatabaseConfigBuilder Function(DatabaseConfigBuilder) builder) {
+    _database = builder(DatabaseConfigBuilder()).build();
+    return this;
+  }
+
   ServerpodConfigBuilder withRedis(RedisConfig? redis) {
     _redis = redis;
+    return this;
+  }
+
+  ServerpodConfigBuilder withRedisBuilder(RedisConfigBuilder Function(RedisConfigBuilder) builder) {
+    _redis = builder(RedisConfigBuilder()).build();
     return this;
   }
 
@@ -118,6 +344,11 @@ class ServerpodConfigBuilder {
     return this;
   }
 
+  ServerpodConfigBuilder withSessionLogsBuilder(SessionLogConfigBuilder Function(SessionLogConfigBuilder) builder) {
+    _sessionLogs = builder(SessionLogConfigBuilder()).build();
+    return this;
+  }
+
   ServerpodConfigBuilder withExperimentalDiagnosticHandlerTimeout(
       Duration? timeout) {
     _experimentalDiagnosticHandlerTimeout = timeout;
@@ -126,6 +357,11 @@ class ServerpodConfigBuilder {
 
   ServerpodConfigBuilder withFutureCall(FutureCallConfig futureCall) {
     _futureCall = futureCall;
+    return this;
+  }
+
+  ServerpodConfigBuilder withFutureCallBuilder(FutureCallConfigBuilder Function(FutureCallConfigBuilder) builder) {
+    _futureCall = builder(FutureCallConfigBuilder()).build();
     return this;
   }
 
