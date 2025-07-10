410423d9585355780a8aa5dd86e815d91dcff6f2
Timm Preetz <timm@preetz.us>
2025-07-10 14:10:19 +0200

    Implement basic Google Account Endpoint

---
+++ modules/new_serverpod_auth/serverpod_auth_google/serverpod_auth_google_server/lib/src/endpoints/google_account_base_endpoint.dart
@@ -0,0 +1,46 @@
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_auth_google_account_server/serverpod_auth_google_account_server.dart';
+import 'package:serverpod_auth_session_server/serverpod_auth_session_server.dart';
+import 'package:serverpod_auth_user_server/serverpod_auth_user_server.dart';
+
+abstract class GoogleAccountBaseEndpoint extends Endpoint {
+  static const String _method = 'google';
+
+  Future<AuthSuccess> authenticate(
+    Session session, {
+    required final String idToken,
+  }) async {
+    final account = await GoogleAccounts.authenticate(
+      session,
+      idToken: idToken,
+    );
+
+    return _createSession(session, account.authUserId);
+  }
+
+  Future<AuthSuccess> _createSession(
+    final Session session,
+    final UuidValue authUserId, {
+    final Transaction? transaction,
+  }) async {
+    final authUser = await AuthUsers.get(
+      session,
+      authUserId: authUserId,
+      transaction: transaction,
+    );
+
+    if (authUser.blocked) {
+      throw AuthUserBlockedException();
+    }
+
+    final sessionKey = await AuthSessions.createSession(
+      session,
+      authUserId: authUserId,
+      method: _method,
+      scopes: authUser.scopes,
+      transaction: transaction,
+    );
+
+    return sessionKey;
+  }
+}
