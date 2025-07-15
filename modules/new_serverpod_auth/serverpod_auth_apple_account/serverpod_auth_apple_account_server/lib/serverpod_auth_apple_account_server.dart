cce26c49ddab47be3f4f2d1dd42d2a4178aca3c9
Timm Preetz <timm@preetz.us>
2025-07-15 09:31:28 +0200

    feat(auth2): Implement "Sign in with Apple" account package

--- modules/new_serverpod_auth/serverpod_auth_apple_account/serverpod_auth_apple_account_server/lib/serverpod_auth_apple_account_server.dart
+++ modules/new_serverpod_auth/serverpod_auth_apple_account/serverpod_auth_apple_account_server/lib/serverpod_auth_apple_account_server.dart
@@ -1,2 +1,5 @@
+export 'src/business/apple_accounts.dart';
+export 'src/business/apple_accounts_admin.dart';
+export 'src/business/config.dart';
 export 'src/generated/endpoints.dart';
 export 'src/generated/protocol.dart';
---
+++ modules/new_serverpod_auth/serverpod_auth_apple_account/serverpod_auth_apple_account_server/lib/src/business/apple_accounts_admin.dart
@@ -0,0 +1,62 @@
+import 'package:clock/clock.dart';
+import 'package:meta/meta.dart';
+import 'package:serverpod/serverpod.dart';
+import 'package:serverpod_auth_apple_account_server/serverpod_auth_apple_account_server.dart';
+import 'package:sign_in_with_apple_server/sign_in_with_apple_server.dart';
+
+/// Administrative Apple account management functions.
+final class AppleAccountsAdmin {
+  final AppleAccountConfig _config;
+  final SignInWithApple _siwa;
+
+  /// Creates a new instance of the admin utilities.
+  @internal
+  AppleAccountsAdmin(this._config, this._siwa);
+
+  /// Checks whether all accounts are in good standing with Apple and that the
+  /// authorization has not been revoked.
+  ///
+  /// Accounts are checked at most every 24 hours.
+  ///
+  /// In case a deactivated account is encountered, the configuration's
+  /// `expiredUserAuthenticationCallback` is invoked with the auth user's ID.
+  /// Then all their sessions created through Sign in with Apple should be
+  /// revoked.
+  Future<void> checkAccountStatus(
+    final Session session, {
+    final Transaction? transaction,
+    final int databaseBatchSize = 100,
+  }) async {
+    final expiredUserAuthenticationCallback =
+        _config.expiredUserAuthenticationCallback;
+    if (expiredUserAuthenticationCallback == null) {
+      throw StateError(
+        'No `expiredUserAuthenticationCallback` is configured in the `AppleAccountConfig`.',
+      );
+    }
+
+    while (true) {
+      final appleAccounts = await AppleAccount.db.find(
+        session,
+        where: (final t) =>
+            t.lastRefreshed < DateTime.now().subtract(const Duration(days: 1)),
+        limit: 10,
+        transaction: transaction,
+      );
+
+      for (final appleAccount in appleAccounts) {
+        try {
+          // TODO: Get proper status from library in error cases.
+          await _siwa.validateRefreshToken(appleAccount.refreshToken);
+        } catch (_) {
+          expiredUserAuthenticationCallback(appleAccount.authUserId);
+        }
+
+        await AppleAccount.db.updateRow(
+          session,
+          appleAccount.copyWith(lastRefreshed: clock.now()),
+        );
+      }
+    }
+  }
+}
