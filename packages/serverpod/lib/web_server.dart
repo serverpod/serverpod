ecae50807db2fd404d28842f5636c2f6f83ff693
exaby73 <nabeel@ex3.dev>
2025-08-05 15:26:48 +0530

    chore: Rename widget.dart to legacy_widget.dart

--- packages/serverpod/lib/web_server.dart
+++ packages/serverpod/lib/web_server.dart
@@ -2,4 +2,4 @@ export 'src/web_server/routes/static_directory.dart';
 export 'src/web_server/templates.dart';
 export 'src/web_server/web_server.dart';
 export 'src/web_server/widgets/web_widget.dart';
-export 'src/web_server/widgets/widget.dart';
+export 'src/web_server/widgets/legacy_widget.dart';
