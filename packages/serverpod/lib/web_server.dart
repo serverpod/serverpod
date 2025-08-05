4398f28885f4621d946684f584a27ee7605b088c
exaby73 <nabeel@ex3.dev>
2025-08-05 15:02:23 +0530

    chore: Move widget related files into their own dir

--- packages/serverpod/lib/web_server.dart
+++ packages/serverpod/lib/web_server.dart
@@ -1,5 +1,5 @@
 export 'src/web_server/routes/static_directory.dart';
 export 'src/web_server/templates.dart';
-export 'src/web_server/web_widget.dart';
 export 'src/web_server/web_server.dart';
-export 'src/web_server/widget.dart';
+export 'src/web_server/widgets/web_widget.dart';
+export 'src/web_server/widgets/widget.dart';
