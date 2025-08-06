f8221220a38e7ded4e5d97cbfda4e69fb00d8262
Alexander Sandor <alexander@serverpod.dev>
2025-08-06 16:12:40 +0200

    docs: Add library description.

--- modules/new_serverpod_auth/serverpod_auth_bridge/serverpod_auth_bridge_server/lib/serverpod_auth_bridge_server.dart
+++ modules/new_serverpod_auth/serverpod_auth_bridge/serverpod_auth_bridge_server/lib/serverpod_auth_bridge_server.dart
@@ -1,3 +1,10 @@
+/// This library provides backwards compatibility for the new authentication
+/// module with the legacy `serverpod_auth` module.
+///
+/// It includes utilities to convert legacy sessions, import legacy passwords,
+/// and manage legacy authentication data.
+library;
+
 export 'src/business/auth_backwards_compatibility.dart';
 export 'src/business/config.dart';
 export 'src/generated/endpoints.dart';
