5f3b31a868445efc3ed5c8b22b5092bde44822b0
Alexander Sandor <alexander@serverpod.dev>
2025-08-06 16:39:20 +0200

    docs: Add documentation to flutter package.

--- modules/new_serverpod_auth/serverpod_auth_bridge/serverpod_auth_bridge_flutter/lib/serverpod_auth_bridge_flutter.dart
+++ modules/new_serverpod_auth/serverpod_auth_bridge/serverpod_auth_bridge_flutter/lib/serverpod_auth_bridge_flutter.dart
@@ -1,1 +1,7 @@
+/// This library provides backwards compatibility for the new authentication
+/// module with the legacy `serverpod_auth` module.
+///
+/// It includes tools to import legacy sessions and manage authentication data.
+library;
+
 export './src/business/session_manager_legacy_import.dart';
