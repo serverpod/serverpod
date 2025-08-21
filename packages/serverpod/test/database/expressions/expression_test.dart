e2bf62a4c70c1e533788cc15319265ca70eb2468
Marcelo Mendonça Soares <marcelo.me.soares@gmail.com>
2025-08-21 13:59:56 -0300

    feat: Add a `~` operator on expressions to perform `NOT` expression (#3889)

--- packages/serverpod/test/database/expressions/expression_test.dart
+++ packages/serverpod/test/database/expressions/expression_test.dart
@@ -90,6 +90,72 @@ void main() {
     });
   });
 
+  group('Given expressions using the ~ (NOT) operator', () {
+    group('Given one expression wrapped using ~ operator', () {
+      var expression = const Expression('TRUE');
+      var notWrappedExpression = ~expression;
+
+      test('when toString is called then string matches expected.', () {
+        expect(notWrappedExpression.toString(), 'NOT TRUE');
+      });
+
+      test('when subExpression is called then wrapped expression is returned',
+          () {
+        expect(
+            (notWrappedExpression as NotExpression).subExpression, expression);
+      });
+
+      group('when iterating not wrapped expression', () {
+        test('then both expressions are represented.', () {
+          expect(notWrappedExpression.depthFirst, hasLength(2));
+        });
+
+        test('then first expression is NOT expression.', () {
+          expect(notWrappedExpression.depthFirst.first, notWrappedExpression);
+        });
+
+        test('then last expression is wrapped expression.', () {
+          expect(notWrappedExpression.depthFirst.last, expression);
+        });
+      });
+    });
+
+    group('Given a combined expression wrapped using ~ operator', () {
+      var expression1 = const Expression('true = true');
+      var expression2 = const Expression('"A" = "A"');
+      var combinedExpression = expression1 & expression2;
+      var notWrappedExpression = ~combinedExpression;
+
+      test('when toString is called then expression is returned', () {
+        expect(
+          notWrappedExpression.toString(),
+          'NOT (true = true AND "A" = "A")',
+        );
+      });
+
+      group('when iterating not wrapped expression', () {
+        test('then all expressions are represented.', () {
+          expect(notWrappedExpression.depthFirst, hasLength(4));
+        });
+
+        test('then order matches expressions.', () {
+          var expectedExpressions = [
+            notWrappedExpression,
+            combinedExpression,
+            expression1,
+            expression2,
+          ];
+
+          var i = 0;
+          for (var expression in notWrappedExpression.depthFirst) {
+            expect(expression, expectedExpressions[i]);
+            i++;
+          }
+        });
+      });
+    });
+  });
+
   group('Given two AND operator combined expressions', () {
     var expression1 = const Expression('true = true');
     var expression2 = const Expression('"A" = "A"');
