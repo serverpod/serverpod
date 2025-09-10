ec479e6241e81da17ee4431bd37a03fdd6b0f20a
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-09-09 22:50:39 -0300

    refactor: Refactor `serverpod_new_auth_test` project to support JWT and SAS tokens

--- modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/core.dart
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/core.dart
@@ -4,5 +4,6 @@
 library;
 
 export 'package:serverpod_auth_core_server/auth_user.dart';
+export 'package:serverpod_auth_core_server/jwt.dart';
 export 'package:serverpod_auth_core_server/profile.dart';
 export 'package:serverpod_auth_core_server/session.dart';
