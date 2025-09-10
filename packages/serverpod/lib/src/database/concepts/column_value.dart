60a59d4a003fe670c09e06f8bed576a74d8c360f
exaby73 <nabeel@ex3.dev>
2025-09-10 18:36:22 +0530

    feat: Make ColumnValue generic over the Column's underlying type and the value to support records' encoding

--- packages/serverpod/lib/src/database/concepts/column_value.dart
+++ packages/serverpod/lib/src/database/concepts/column_value.dart
@@ -2,22 +2,16 @@ import 'package:serverpod/src/database/concepts/columns.dart';
 import 'package:serverpod/src/database/concepts/table.dart';
 
 /// A function that returns a list of [ColumnValue]s for a [Table].
-typedef ColumnValueListBuilder<T extends Table> = List<ColumnValue> Function(T);
+typedef ColumnValueListBuilder<T> = List<ColumnValue> Function(T);
 
 /// Represents a column-value pair for database updates.
-class ColumnValue<T> {
+class ColumnValue<T, V> {
   /// The column to update.
   final Column<T> column;
 
   /// The value to set.
-  final T? value;
+  final V? value;
 
   /// Creates a new [ColumnValue] with the specified column and value.
   const ColumnValue(this.column, this.value);
-}
-
-/// Extension to add value setting methods to columns.
-extension ColumnValueExtension<T> on Column<T> {
-  /// Creates a [ColumnValue] with the specified value for this column.
-  ColumnValue<T> call(T? value) => ColumnValue(this, value);
 }
