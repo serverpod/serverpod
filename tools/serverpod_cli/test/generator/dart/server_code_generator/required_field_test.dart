73400499ef70244b2612c497d18f06028c5475b6
Alexander Sandor <alexander@serverpod.dev>
2025-09-11 09:02:09 +0200

    feat: Support required keyword in exceptions.

--- tools/serverpod_cli/test/generator/dart/server_code_generator/required_field_test.dart
+++ tools/serverpod_cli/test/generator/dart/server_code_generator/required_field_test.dart
@@ -1,6 +1,7 @@
 import 'package:serverpod_cli/src/generator/dart/server_code_generator.dart';
 import 'package:test/test.dart';
 
+import '../../../test_util/builders/exception_class_definition_builder.dart';
 import '../../../test_util/builders/generator_config_builder.dart';
 import '../../../test_util/builders/model_class_definition_builder.dart';
 import '../../../test_util/builders/serializable_entity_field_definition_builder.dart';
@@ -12,7 +13,9 @@ const generator = DartServerCodeGenerator();
 
 void main() {
   group('Given a model with required nullable fields', () {
-    test('then constructor parameters are marked as required for nullable required fields', () {
+    test(
+        'then constructor parameters are marked as required for nullable required fields',
+        () {
       var models = [
         ModelClassDefinitionBuilder()
             .withClassName('Example')
@@ -63,7 +66,71 @@ void main() {
 
       // Check that the generated code has the correct constructor
       var generatedCode = codeMap.values.first;
-      
+
+      // The required nullable field should be marked as required
+      expect(generatedCode, contains('required String? email'));
+      // The non-required nullable field should not be marked as required
+      expect(generatedCode, contains('String? phone'));
+      // The non-nullable field should be marked as required (default behavior)
+      expect(generatedCode, contains('required String name'));
+    });
+  });
+
+  group('Given a exception with required nullable fields', () {
+    test(
+        'then constructor parameters are marked as required for nullable required fields',
+        () {
+      var models = [
+        ExceptionClassDefinitionBuilder()
+            .withClassName('Example')
+            .withFileName('example')
+            .withField(
+              FieldDefinitionBuilder()
+                  .withName('name')
+                  .withType(
+                    TypeDefinitionBuilder()
+                        .withClassName('String')
+                        .withNullable(false)
+                        .build(),
+                  )
+                  .withIsRequired(false)
+                  .build(),
+            )
+            .withField(
+              FieldDefinitionBuilder()
+                  .withName('email')
+                  .withType(
+                    TypeDefinitionBuilder()
+                        .withClassName('String')
+                        .withNullable(true)
+                        .build(),
+                  )
+                  .withIsRequired(true)
+                  .build(),
+            )
+            .withField(
+              FieldDefinitionBuilder()
+                  .withName('phone')
+                  .withType(
+                    TypeDefinitionBuilder()
+                        .withClassName('String')
+                        .withNullable(true)
+                        .build(),
+                  )
+                  .withIsRequired(false)
+                  .build(),
+            )
+            .build()
+      ];
+
+      var codeMap = generator.generateSerializableModelsCode(
+        models: models,
+        config: config,
+      );
+
+      // Check that the generated code has the correct constructor
+      var generatedCode = codeMap.values.first;
+
       // The required nullable field should be marked as required
       expect(generatedCode, contains('required String? email'));
       // The non-required nullable field should not be marked as required
