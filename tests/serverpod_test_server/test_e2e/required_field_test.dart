9b7548cf0243789fd8df0e48fd1bd51873abcd1a
Alexander Sandor <alexander@serverpod.dev>
2025-09-12 08:49:59 +0200

    test: Add protocol serialization tests.

---
+++ tests/serverpod_test_server/test_e2e/required_field_test.dart
@@ -0,0 +1,25 @@
+import 'package:serverpod_test_client/serverpod_test_client.dart';
+import 'package:serverpod_test_server/test_util/config.dart';
+import 'package:test/test.dart';
+
+void main() {
+  var client = Client(serverUrl);
+
+  test(
+      'Given a model with a required filed when sending it to the echo server it is returned unmodified',
+      () async {
+    var model = ModelWithRequiredField(name: 'John Doe', email: null);
+    var result = await client.echoRequiredField.echoModel(model);
+
+    expect(result, isNotNull);
+    expect(result.name, model.name);
+    expect(result.email, model.email);
+  });
+
+  test(
+      'Given endpoint that throws an exception with a required field, when calling it, the exception is received',
+      () async {
+    await expectLater(client.echoRequiredField.throwException(),
+        throwsA(isA<ExceptionWithRequiredField>()));
+  });
+}
