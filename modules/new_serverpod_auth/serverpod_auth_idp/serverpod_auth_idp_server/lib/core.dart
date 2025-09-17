a104eceb1f872715047240b4aedc359fc63211d9
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-09-16 23:17:01 -0300

    feat: Add an `UnifiedAuthTokens` to handle both SAS and JWT tokens

--- modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/core.dart
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/core.dart
@@ -7,3 +7,4 @@ export 'package:serverpod_auth_core_server/auth_user.dart';
 export 'package:serverpod_auth_core_server/jwt.dart';
 export 'package:serverpod_auth_core_server/profile.dart';
 export 'package:serverpod_auth_core_server/session.dart';
+export 'package:serverpod_auth_core_server/unified.dart';
