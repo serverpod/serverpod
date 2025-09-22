a467e7ba95448cb9574f3d6553f6887a0960cbf8
Marcelo Mendonça Soares <marcelo.me.soares@gmail.com>
2025-09-22 10:10:07 -0300

    feat: Implement `ClientAuthSessionManager` completely with JWT and SAS support (#3982)

--- modules/new_serverpod_auth/serverpod_auth_core/serverpod_auth_core_server/lib/src/generated/endpoints.dart
+++ modules/new_serverpod_auth/serverpod_auth_core/serverpod_auth_core_server/lib/src/generated/endpoints.dart
@@ -11,8 +11,85 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:serverpod/serverpod.dart' as _i1;
+import '../common/endpoints/status_endpoint.dart' as _i2;
+import '../jwt/endpoints/jwt_tokens_endpoint.dart' as _i3;
 
 class Endpoints extends _i1.EndpointDispatch {
   @override
-  void initializeEndpoints(_i1.Server server) {}
+  void initializeEndpoints(_i1.Server server) {
+    var endpoints = <String, _i1.Endpoint>{
+      'status': _i2.StatusEndpoint()
+        ..initialize(
+          server,
+          'status',
+          'serverpod_auth_core',
+        ),
+      'refreshJwtTokens': _i3.RefreshJwtTokensEndpoint()
+        ..initialize(
+          server,
+          'refreshJwtTokens',
+          'serverpod_auth_core',
+        ),
+    };
+    connectors['status'] = _i1.EndpointConnector(
+      name: 'status',
+      endpoint: endpoints['status']!,
+      methodConnectors: {
+        'isSignedIn': _i1.MethodConnector(
+          name: 'isSignedIn',
+          params: {},
+          call: (
+            _i1.Session session,
+            Map<String, dynamic> params,
+          ) async =>
+              (endpoints['status'] as _i2.StatusEndpoint).isSignedIn(session),
+        ),
+        'signOutDevice': _i1.MethodConnector(
+          name: 'signOutDevice',
+          params: {},
+          call: (
+            _i1.Session session,
+            Map<String, dynamic> params,
+          ) async =>
+              (endpoints['status'] as _i2.StatusEndpoint)
+                  .signOutDevice(session),
+        ),
+        'signOutAllDevices': _i1.MethodConnector(
+          name: 'signOutAllDevices',
+          params: {},
+          call: (
+            _i1.Session session,
+            Map<String, dynamic> params,
+          ) async =>
+              (endpoints['status'] as _i2.StatusEndpoint)
+                  .signOutAllDevices(session),
+        ),
+      },
+    );
+    connectors['refreshJwtTokens'] = _i1.EndpointConnector(
+      name: 'refreshJwtTokens',
+      endpoint: endpoints['refreshJwtTokens']!,
+      methodConnectors: {
+        'refreshAccessToken': _i1.MethodConnector(
+          name: 'refreshAccessToken',
+          params: {
+            'refreshToken': _i1.ParameterDescription(
+              name: 'refreshToken',
+              type: _i1.getType<String>(),
+              nullable: false,
+            )
+          },
+          call: (
+            _i1.Session session,
+            Map<String, dynamic> params,
+          ) async =>
+              (endpoints['refreshJwtTokens'] as _i3.RefreshJwtTokensEndpoint)
+                  .refreshAccessToken(
+            session,
+            refreshToken: params['refreshToken'],
+          ),
+        )
+      },
+    );
+  }
 }
