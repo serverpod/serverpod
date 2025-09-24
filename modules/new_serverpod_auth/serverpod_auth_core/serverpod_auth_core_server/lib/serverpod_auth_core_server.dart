67ea5460050618335656b396906592b9d0e9efd5
exaby73 <nabeel@ex3.dev>
2025-09-24 05:45:31 +0530

    feat: Export new classes

--- modules/new_serverpod_auth/serverpod_auth_core/serverpod_auth_core_server/lib/serverpod_auth_core_server.dart
+++ modules/new_serverpod_auth/serverpod_auth_core/serverpod_auth_core_server/lib/serverpod_auth_core_server.dart
@@ -3,5 +3,12 @@
 /// Serverpod IDP module.
 library;
 
+export 'src/common/business/auth_config.dart';
+export 'src/common/business/provider_factory.dart';
+export 'src/common/business/token_issuer.dart';
+export 'src/common/business/token_manager.dart';
+export 'src/common/business/token_provider.dart';
+export 'src/common/business/token_providers/jwt_provider.dart';
+export 'src/common/business/token_providers/sas_provider.dart';
 export 'src/generated/endpoints.dart';
 export 'src/generated/protocol.dart';
