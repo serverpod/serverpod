7129442c963e90f846949a7d720baa9cf79a96e7
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-10-01 08:20:15 +0200

    refactor: Force immediate deprecation of RouteStaticDirectory by making it abstract and clearing out implementation

--- packages/serverpod/lib/src/web_server/routes/static_directory.dart
+++ packages/serverpod/lib/src/web_server/routes/static_directory.dart
@@ -40,50 +40,9 @@ class PathCacheMaxAge {
   }
 }
 
+// This class has been cleared out and marked abstract.
+// It is only left behind to add a deprecation annotation
+// to help people move to use StaticRoute.
 /// Route for serving a directory of static files.
 @Deprecated('Use StaticRoute.directory instead')
-class RouteStaticDirectory extends Route {
-  /// The path to the directory to serve relative to the web/ directory.
-  final String serverDirectory;
-
-  /// A regular expression that will be used to determine if a path should not
-  /// be cached.
-  late final List<PathCacheMaxAge>? _pathCachePatterns;
-
-  /// Creates a static directory with the [serverDirectory] as its root.
-  ///
-  /// If [pathCachePatterns] is provided, paths matching the requested
-  /// patterns will be cached for the requested amount of time. Paths that
-  /// are do not match any provided pattern are cached for one year.
-  RouteStaticDirectory({
-    required this.serverDirectory,
-    List<PathCacheMaxAge>? pathCachePatterns,
-  }) : super(methods: {Method.get, Method.head}) {
-    _pathCachePatterns = pathCachePatterns;
-    _handler = createStaticHandler(
-      p.join('web', serverDirectory),
-      cacheControl: (ctx, fileInfo) {
-        // Get the max age for the path
-        var pathCacheMaxAge = _pathCachePatterns
-                ?.firstWhereOrNull(
-                    (pattern) => pattern._shouldCache(ctx.remainingPath.path))
-                ?.maxAge ??
-            // Default to a max age of one year if no pattern matched
-            PathCacheMaxAge.oneYear;
-
-        // Set Cache-Control header
-        return pathCacheMaxAge == PathCacheMaxAge.noCache
-            // Don't cache this path
-            ? CacheControlHeader(
-                maxAge: 0, sMaxAge: 0, noCache: true, noStore: true)
-            : CacheControlHeader(maxAge: pathCacheMaxAge.inSeconds);
-      },
-    );
-  }
-
-  late final Handler _handler;
-
-  @override
-  FutureOr<HandledContext> handleCall(Session session, NewContext context) =>
-      _handler(context);
-}
+abstract class RouteStaticDirectory extends Route {}
