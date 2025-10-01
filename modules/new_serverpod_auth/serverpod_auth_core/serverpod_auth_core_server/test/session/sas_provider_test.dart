304a6de5d43b95a080e8dd13fb735d1f40dbdc92
exaby73 <nabeel@ex3.dev>
2025-10-01 15:53:16 +0530

    refactor: Move provider tests to the correct directory

--- modules/new_serverpod_auth/serverpod_auth_core/serverpod_auth_core_server/test/session/sas_provider_test.dart
+++ modules/new_serverpod_auth/serverpod_auth_core/serverpod_auth_core_server/test/session/sas_provider_test.dart
@@ -3,7 +3,7 @@ import 'package:serverpod_auth_core_server/serverpod_auth_core_server.dart';
 import 'package:serverpod_auth_core_server/session.dart';
 import 'package:test/test.dart';
 
-import '../../../serverpod_test_tools.dart';
+import '../serverpod_test_tools.dart';
 
 void main() {
   group('SASTokenIssuer', () {
