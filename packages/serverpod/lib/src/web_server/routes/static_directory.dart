624977b1225ff446b09f49009b2b122287aad060
Kasper Overgård Nielsen <kasper@byolimit.com>
2025-10-01 10:11:11 +0200

    fix(pr-review): Doudle deprecation warning in doc comments for easy navigation in generated docs

--- packages/serverpod/lib/src/web_server/routes/static_directory.dart
+++ packages/serverpod/lib/src/web_server/routes/static_directory.dart
@@ -3,6 +3,7 @@ import 'package:serverpod/serverpod.dart';
 // NOTE: This class has been cleared out and marked abstract.
 // It is only left behind to add a deprecation annotation
 // to help people move to use StaticRoute.
+/// DEPRECATED: Use [StaticRoute.directory] with [CacheControlFactory] instead
 /// A path pattern to match, and the max age that paths that match the pattern
 /// should be cached for, in seconds.
 @Deprecated('Use StaticRoute.directory and CacheControlFactory instead')
@@ -11,6 +12,7 @@ class PathCacheMaxAge {}
 // NOTE: This class has been cleared out and marked abstract.
 // It is only left behind to add a deprecation annotation
 // to help people move to use StaticRoute.
+/// DEPRECATED: Use [StaticRoute.directory] instead
 /// Route for serving a directory of static files.
 @Deprecated('Use StaticRoute.directory instead')
 abstract class RouteStaticDirectory extends Route {}
