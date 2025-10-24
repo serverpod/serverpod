dc4b683491fe0566962d3e3a9ace439c2235b265
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-10-24 01:11:17 -0400

    feat: map column name to serialized field name variable when set

--- tools/serverpod_cli/lib/src/generator/dart/library_generators/model_library_generator.dart
+++ tools/serverpod_cli/lib/src/generator/dart/library_generators/model_library_generator.dart
@@ -1620,11 +1620,13 @@ class SerializableModelLibraryGenerator {
         nullCheckedReference: field.hiddenSerializableField(serverCode),
       );
 
+      final fieldKey = field.column ?? field.name;
+
       return {
         ...map,
         if (field.type.nullable)
-          Code("if (${fieldName.symbol} != null) '${field.name}'"): fieldRef,
-        if (!field.type.nullable) Code("'${field.name}'"): fieldRef,
+          Code("if (${fieldName.symbol} != null) '$fieldKey'"): fieldRef,
+        if (!field.type.nullable) Code("'$fieldKey'"): fieldRef,
       };
     });
 
