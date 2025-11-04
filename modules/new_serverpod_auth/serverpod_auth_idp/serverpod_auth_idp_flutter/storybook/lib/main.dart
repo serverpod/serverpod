87468ca131c606357c15b4fad6f6efdcf8e78f3a
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-11-03 23:44:00 -0300

    feat: Add a storybook app to visualize and test IDP widgets

---
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/storybook/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
+import 'package:storybook_toolkit/storybook_toolkit.dart';
+
+import 'utils/client.dart';
+import 'stories/google.dart';
+
+final client = Client('http://localhost:8080/')
+  ..authSessionManager = ClientAuthSessionManager();
+
+void main() {
+  client.auth.initialize();
+  client.auth.initializeGoogleSignIn();
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Storybook(
+      canvasColor: Colors.white,
+      initialStory: 'Home',
+      plugins: StorybookPlugins(
+        enableTimeDilation: true,
+        initialDeviceFrameData: DeviceFrameData(
+          visibility: DeviceFrameVisibility.visible,
+          orientation: Orientation.portrait,
+        ),
+      ),
+      stories: [...googleStories],
+    );
+  }
+}
