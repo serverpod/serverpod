89cc99cf8e609404cb578401b71cfc06032562c1
Marcelo Soares <marcelo.me.soares@gmail.com>
2025-11-04 13:52:17 -0300

    fix: Fix publishing requirements for the new flutter package

--- modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/lib/src/google/web/wrapper_web.dart
+++ modules/new_serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/lib/src/google/web/wrapper_web.dart
@@ -1,4 +1,1 @@
-// No need to add the dependency on this package since it is endorsed and will
-// always be available on web if `google_sign_in` is present.
-// ignore: depend_on_referenced_packages
 export 'package:google_sign_in_web/web_only.dart';
