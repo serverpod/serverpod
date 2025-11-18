0d491c2e7216f264bf753e7141695c33bb030b49
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-18 12:21:44 -0500

    test: update folder names for explicit column name relations

--- tests/serverpod_test_client/lib/src/protocol/explicit_column_name/relations/one_to_one/contractor.dart
+++ tests/serverpod_test_client/lib/src/protocol/explicit_column_name/relations/one_to_one/contractor.dart
@@ -11,7 +11,7 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:serverpod_client/serverpod_client.dart' as _i1;
-import '../../../explicit_column_name/relations/index/service.dart' as _i2;
+import '../../../explicit_column_name/relations/one_to_one/service.dart' as _i2;
 import 'package:serverpod_test_client/src/protocol/protocol.dart' as _i3;
 
 abstract class Contractor implements _i1.SerializableModel {
