72ef139fd272d4a6158e4cf8b5b6378547f8b3a7
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-26 21:51:48 -0500

    test: reorganize explicit column name CRUD tests

--- tests/serverpod_test_server/test_integration/database_operations/explicit_column_name/crud_test.dart
+++ tests/serverpod_test_server/test_integration/database_operations/explicit_column_name/crud_test.dart
@@ -20,45 +20,61 @@ void main() {
         db = TableWithExplicitColumnName.db;
       });
 
-      group('when inserting', () {
-        test('then it is created', () async {
-          final inserted = await db.insertRow(session, data);
+      test('when inserting an entry then it is created', () async {
+        final inserted = await db.insertRow(session, data);
 
-          expect(inserted.id, isNotNull);
-        });
+        expect(inserted.id, isNotNull);
+      });
+    },
+  );
+
+  withServerpod(
+    'Given an entry in the database for a table with an explicit column name',
+    (sessionBuilder, endpoints) {
+      var session = sessionBuilder.build();
+
+      late TableWithExplicitColumnName data;
+      late TableWithExplicitColumnName inserted;
+      late TableWithExplicitColumnNameRepository db;
+
+      setUp(() async {
+        data = TableWithExplicitColumnName(
+          userName: 'userName',
+          description: 'description',
+        );
+        db = TableWithExplicitColumnName.db;
+        inserted = await db.insertRow(session, data);
       });
 
-      group('when an object is retrieved by its id', () {
-        test('it returns the same object after insertion', () async {
-          final inserted = await db.insertRow(session, data);
-          final retrieved = await db.findById(session, (await inserted).id!);
+      test(
+        'when fetching the entry by id then the same object is returned',
+        () async {
+          final retrieved = await db.findById(session, inserted.id!);
 
           expect(retrieved, isNotNull);
           expect(retrieved?.id, inserted.id);
           expect(retrieved?.userName, data.userName);
           expect(retrieved?.description, data.description);
-        });
-      });
+        },
+      );
 
-      test('when an object is previously inserted and then updated, '
-          'the updated object is returned', () async {
-        const newUserName = 'newUserName';
-        final inserted = await db.insertRow(session, data);
-        inserted.userName = newUserName;
-        final updated = await db.updateRow(session, inserted);
+      test(
+        'when updating the entry then the updated object is returned',
+        () async {
+          const newUserName = 'newUserName';
+          inserted.userName = newUserName;
+          final updated = await db.updateRow(session, inserted);
 
-        expect(updated.userName, newUserName);
-      });
+          expect(updated.userName, newUserName);
+        },
+      );
 
-      group('when deleting', () {
-        test('then it is deleted', () async {
-          final inserted = await db.insertRow(session, data);
-          final result = await db.deleteRow(session, inserted);
+      test('when deleting the entry then it is deleted', () async {
+        final result = await db.deleteRow(session, inserted);
 
-          expect(result, isNotNull);
-          final retrieved = await db.findById(session, inserted.id!);
-          expect(retrieved, isNull);
-        });
+        expect(result, isNotNull);
+        final retrieved = await db.findById(session, inserted.id!);
+        expect(retrieved, isNull);
       });
     },
   );
