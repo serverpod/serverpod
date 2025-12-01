1e187a99f6f7b6b03c2f62d77640d1241f400347
mathis6787 <mathisdesmeules@protonmail.com>
2025-12-01 12:00:15 -0500

    refactor: add missing exports for AWS and MinIO S3 options

--- integrations/serverpod_cloud_storage_s3/lib/serverpod_cloud_storage_s3.dart
+++ integrations/serverpod_cloud_storage_s3/lib/serverpod_cloud_storage_s3.dart
@@ -5,3 +5,5 @@ library;
 
 export 'src/cloud_storage/s3_cloud_storage.dart';
 export 'src/providers/s3_options.dart';
+export 'src/providers/aws_s3_options.dart';
+export 'src/providers/minio_s3_options.dart';
