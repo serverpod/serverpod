a0810cb192a42e3c193cf79cd53d5b9f2e92e246
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-11-30 21:32:36 -0500

    feat: mark column override as experimental feature

--- tools/serverpod_cli/lib/src/analyzer/models/yaml_definitions/class_yaml_definition.dart
+++ tools/serverpod_cli/lib/src/analyzer/models/yaml_definitions/class_yaml_definition.dart
@@ -5,6 +5,7 @@ import 'package:serverpod_cli/src/analyzer/models/validation/restrictions/base.d
 import 'package:serverpod_cli/src/analyzer/models/validation/restrictions/default.dart';
 import 'package:serverpod_cli/src/analyzer/models/validation/restrictions/scope.dart';
 import 'package:serverpod_cli/src/analyzer/models/validation/validate_node.dart';
+import 'package:serverpod_cli/src/config/experimental_feature.dart';
 import 'package:serverpod_service_client/serverpod_service_client.dart';
 
 class ClassYamlDefinition {
@@ -201,6 +202,9 @@ class ClassYamlDefinition {
               ValidateNode(
                 Keyword.columnKey,
                 valueRestriction: restrictions.validateColumnName,
+                isHidden: !restrictions.config.isExperimentalFeatureEnabled(
+                  ExperimentalFeature.columnOverride,
+                ),
               ),
             },
           ),
