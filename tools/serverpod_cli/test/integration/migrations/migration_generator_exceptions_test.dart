596b063cda4740868918588e8848a6d8f8f1be53
Nabeel Parkar <nabeel@ex3.dev>
2025-12-09 17:47:14 +0530

    feat!: Remove empty migration creation on `--force` (#4388)

--- tools/serverpod_cli/test/integration/migrations/migration_generator_exceptions_test.dart
+++ tools/serverpod_cli/test/integration/migrations/migration_generator_exceptions_test.dart
@@ -3,10 +3,18 @@ import 'dart:io';
 import 'package:path/path.dart' as path;
 import 'package:serverpod_cli/analyzer.dart';
 import 'package:serverpod_cli/src/commands/create_repair_migration.dart';
+import 'package:serverpod_serialization/serverpod_serialization.dart';
+import 'package:serverpod_service_client/serverpod_service_client.dart';
 import 'package:serverpod_shared/serverpod_shared.dart';
 import 'package:test/test.dart';
 
+import '../../test_util/builders/database/column_definition_builder.dart';
+import '../../test_util/builders/database/database_definition_builder.dart';
+import '../../test_util/builders/database/table_definition_builder.dart';
 import '../../test_util/builders/generator_config_builder.dart';
+
+const _projectName = 'test_project';
+const _migrationVersion = '00000000000000';
 
 void main() {
   var config = GeneratorConfigBuilder().build();
@@ -28,8 +36,9 @@ void main() {
       projectName: projectName,
     );
 
-    group('when creating migration', () {
-      test('then migration version load exception is thrown.', () async {
+    test(
+      'when creating migration then MigrationVersionLoadException is thrown.',
+      () async {
         expect(
           generator.createMigration(force: false, config: config),
           throwsA(
@@ -51,11 +60,12 @@ void main() {
                 ),
           ),
         );
-      });
-    });
+      },
+    );
 
-    group('when creating repair migration', () {
-      test('then null is returned.', () async {
+    test(
+      'when creating repair migration then MigrationVersionLoadException exception is thrown.',
+      () async {
         expect(
           generator.repairMigration(
             runMode:
@@ -81,7 +91,241 @@ void main() {
                 ),
           ),
         );
-      });
+      },
+    );
+  });
+
+  group('Given a migration that produces warnings due to column removal', () {
+    late Directory testDirectory;
+    late GeneratorConfig config;
+    late MigrationGenerator generator;
+
+    setUp(() async {
+      testDirectory = Directory(
+        path.join(Directory.current.path, const Uuid().v4()),
+      );
+      testDirectory.createSync(recursive: true);
+
+      var modelFile = File(
+        path.join(
+          testDirectory.path,
+          'lib',
+          'src',
+          'protocol',
+          'example.yaml',
+        ),
+      );
+      modelFile.createSync(recursive: true);
+      modelFile.writeAsStringSync('''
+class: Example
+table: example
+fields:
+  name: String
+''');
+
+      var databaseDefinition = DatabaseDefinitionBuilder()
+          .withModuleName(_projectName)
+          .withInstalledModules([
+            DatabaseMigrationVersion(
+              module: _projectName,
+              version: _migrationVersion,
+            ),
+          ])
+          .withTable(
+            TableDefinitionBuilder()
+                .withName('example')
+                .withModule(_projectName)
+                .withColumn(
+                  ColumnDefinitionBuilder()
+                      .withName('toBeDropped')
+                      .withIsNullable(true)
+                      .withDartType('String?')
+                      .build(),
+                )
+                .build(),
+          )
+          .build();
+
+      var migration = DatabaseMigration(
+        actions: [],
+        warnings: [],
+        migrationApiVersion: 1,
+      );
+
+      _writeMigrationFiles(
+        testDirectory: testDirectory,
+        databaseDefinition: databaseDefinition,
+        migration: migration,
+      );
+
+      config = GeneratorConfigBuilder()
+          .withName(_projectName)
+          .withServerPackageDirectoryPathParts(path.split(testDirectory.path))
+          .withModules([])
+          .build();
+
+      generator = MigrationGenerator(
+        directory: testDirectory,
+        projectName: _projectName,
+      );
+    });
+
+    tearDown(() {
+      if (testDirectory.existsSync()) {
+        testDirectory.deleteSync(recursive: true);
+      }
+    });
+
+    test(
+      'when creating migration with force `false` then MigrationAbortedException is thrown.',
+      () async {
+        await expectLater(
+          generator.createMigration(force: false, config: config),
+          throwsA(isA<MigrationAbortedException>()),
+        );
+      },
+    );
+
+    test(
+      'when creating migration with force `true` then migration is created.',
+      () async {
+        var result = await generator.createMigration(
+          force: true,
+          config: config,
+          write: false,
+        );
+        expect(result, isNotNull);
+      },
+    );
+  });
+
+  group('Given a project with no database changes', () {
+    late Directory testDirectory;
+    late GeneratorConfig config;
+    late MigrationGenerator generator;
+
+    setUp(() async {
+      testDirectory = Directory(
+        path.join(Directory.current.path, const Uuid().v4()),
+      );
+      testDirectory.createSync(recursive: true);
+
+      var modelFile = File(
+        path.join(
+          testDirectory.path,
+          'lib',
+          'src',
+          'protocol',
+          'example.yaml',
+        ),
+      );
+      modelFile.createSync(recursive: true);
+      modelFile.writeAsStringSync('''
+class: Example
+table: example
+fields:
+  name: String
+''');
+
+      var databaseDefinition = DatabaseDefinitionBuilder()
+          .withModuleName(_projectName)
+          .withInstalledModules([
+            DatabaseMigrationVersion(
+              module: _projectName,
+              version: _migrationVersion,
+            ),
+          ])
+          .withTable(
+            TableDefinitionBuilder()
+                .withName('example')
+                .withModule(_projectName)
+                .build(),
+          )
+          .build();
+
+      var migration = DatabaseMigration(
+        actions: [],
+        warnings: [],
+        migrationApiVersion: 1,
+      );
+
+      _writeMigrationFiles(
+        testDirectory: testDirectory,
+        databaseDefinition: databaseDefinition,
+        migration: migration,
+      );
+
+      config = GeneratorConfigBuilder()
+          .withName(_projectName)
+          .withServerPackageDirectoryPathParts(path.split(testDirectory.path))
+          .withModules([])
+          .build();
+
+      generator = MigrationGenerator(
+        directory: testDirectory,
+        projectName: _projectName,
+      );
+    });
+
+    tearDown(() {
+      if (testDirectory.existsSync()) {
+        testDirectory.deleteSync(recursive: true);
+      }
+    });
+
+    test('when creating migration then `null` is returned.', () async {
+      var result = await generator.createMigration(
+        force: false,
+        config: config,
+      );
+      expect(result, isNull);
     });
   });
+}
+
+void _writeMigrationFiles({
+  required Directory testDirectory,
+  required DatabaseDefinition databaseDefinition,
+  required DatabaseMigration migration,
+}) {
+  var migrationDir = Directory(
+    path.join(testDirectory.path, 'migrations', _migrationVersion),
+  );
+  migrationDir.createSync(recursive: true);
+
+  var definitionProjectJson = File(
+    path.join(migrationDir.path, 'definition_project.json'),
+  );
+  definitionProjectJson.writeAsStringSync(
+    SerializationManager.encode(databaseDefinition, formatted: true),
+  );
+
+  var definitionJson = File(
+    path.join(migrationDir.path, 'definition.json'),
+  );
+  definitionJson.writeAsStringSync(
+    SerializationManager.encode(databaseDefinition, formatted: true),
+  );
+
+  var migrationJson = File(
+    path.join(migrationDir.path, 'migration.json'),
+  );
+  migrationJson.writeAsStringSync(
+    SerializationManager.encode(migration, formatted: true),
+  );
+
+  var definitionSql = File(
+    path.join(migrationDir.path, 'definition.sql'),
+  );
+  definitionSql.writeAsStringSync('-- initial definition');
+
+  var migrationSql = File(
+    path.join(migrationDir.path, 'migration.sql'),
+  );
+  migrationSql.writeAsStringSync('-- initial migration');
+
+  var registryFile = File(
+    path.join(testDirectory.path, 'migrations', 'migration_registry.txt'),
+  );
+  registryFile.writeAsStringSync('$_migrationVersion\n');
 }
