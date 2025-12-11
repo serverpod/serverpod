35daca9b704821e2f72acdf2c993b41841816171
Nabeel Parkar <nabeelparkar99@gmail.com>
2025-12-11 20:07:08 +0530

    chore: Remove redundant comment

--- modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/lib/src/google/google_auth_controller.dart
+++ modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_flutter/lib/src/google/google_auth_controller.dart
@@ -203,7 +203,6 @@ class GoogleAuthController extends ChangeNotifier {
     if (error is GoogleSignInException &&
         error.code == GoogleSignInExceptionCode.canceled) {
       // The Google Sign-In package already prints these to the debug log.
-      // Reset state to idle so the button spinner stops.
       _setState(GoogleAuthState.idle);
       return;
     }
