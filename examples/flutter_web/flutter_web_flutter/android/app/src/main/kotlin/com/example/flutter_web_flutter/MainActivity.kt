70e52b82d45210a8a5a2c1f397716e473481ad67
Alexander Sandor <alexander@serverpod.dev>
2025-12-14 22:45:46 +0100

    Revert "chore: Add target project that serves flutter app."
    This reverts commit 663afd20dcde731fb99ba25fb4cd21d0df309030.

--- examples/flutter_web/flutter_web_flutter/android/app/src/main/kotlin/com/example/flutter_web_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_web_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- examples/flutter_web/flutter_web_server/lib/src/web/routes/root.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_web_server/src/web/widgets/built_with_serverpod_page.dart';
-import 'package:serverpod/serverpod.dart';
-
-class RootRoute extends WidgetRoute {
-  @override
-  Future<TemplateWidget> build(Session session, Request request) async {
-    return BuiltWithServerpodPageWidget();
-  }
-}
