a622bf88389c1b231fee24c95bf07a00d3b18bb3
Preston <preston.schwartz505@gmail.com>
2025-12-23 08:24:12 -0600

    Fixed incorrect migration order to match main, reran serverpod create migration.

--- tests/serverpod_test_server/migrations/20251223142045597/migration.sql
+++ tests/serverpod_test_server/migrations/20251223142045597/migration.sql
@@ -15,17 +15,17 @@ CREATE TABLE "object_with_enum_enhanced" (
 -- MIGRATION VERSION FOR serverpod_test
 --
 INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
-    VALUES ('serverpod_test', '20251222160858274', now())
+    VALUES ('serverpod_test', '20251223142045597', now())
     ON CONFLICT ("module")
-    DO UPDATE SET "version" = '20251222160858274', "timestamp" = now();
+    DO UPDATE SET "version" = '20251223142045597', "timestamp" = now();
 
 --
 -- MIGRATION VERSION FOR serverpod
 --
 INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
-    VALUES ('serverpod', '20250825102336032-v3-0-0', now())
+    VALUES ('serverpod', '20251208110333922-v3-0-0', now())
     ON CONFLICT ("module")
-    DO UPDATE SET "version" = '20250825102336032-v3-0-0', "timestamp" = now();
+    DO UPDATE SET "version" = '20251208110333922-v3-0-0', "timestamp" = now();
 
 --
 -- MIGRATION VERSION FOR serverpod_auth
@@ -39,9 +39,9 @@ INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
 -- MIGRATION VERSION FOR serverpod_test_module
 --
 INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
-    VALUES ('serverpod_test_module', '20250825102429343-v3-0-0', now())
+    VALUES ('serverpod_test_module', '20251208110450074-v3-0-0', now())
     ON CONFLICT ("module")
-    DO UPDATE SET "version" = '20250825102429343-v3-0-0', "timestamp" = now();
+    DO UPDATE SET "version" = '20251208110450074-v3-0-0', "timestamp" = now();
 
 
 COMMIT;
