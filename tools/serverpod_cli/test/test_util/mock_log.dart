c6afd132763ce236dd660cd5f8a29f07163e7d61
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-12-26 14:34:10 -0500

    test(cli): move mock logger to test util folder

---
+++ tools/serverpod_cli/test/test_util/mock_log.dart
@@ -0,0 +1,74 @@
+import 'package:cli_tools/cli_tools.dart';
+
+class MockLogOutput {
+  List<String> messages = [];
+
+  void info(String message) {
+    messages.add(message);
+  }
+
+  void error(String message) {
+    messages.add(message);
+  }
+
+  void debug(String message) {
+    messages.add(message);
+  }
+
+  void warning(String message) {
+    messages.add(message);
+  }
+
+  MockLogOutput reset() {
+    messages.clear();
+    return this;
+  }
+}
+
+class MockLogger extends VoidLogger {
+  final MockLogOutput output = MockLogOutput();
+
+  MockLogger();
+
+  @override
+  void info(String message, {bool newParagraph = false, LogType? type}) {
+    output.info(message);
+  }
+
+  @override
+  void error(
+    String message, {
+    bool newParagraph = false,
+    LogType? type,
+    Object? exception,
+    StackTrace? stackTrace,
+  }) {
+    output.error(message);
+  }
+
+  @override
+  void debug(String message, {bool newParagraph = false, LogType? type}) {
+    output.debug(message);
+  }
+
+  @override
+  void warning(String message, {bool newParagraph = false, LogType? type}) {
+    output.warning(message);
+  }
+}
+
+class MockAnalytics implements Analytics {
+  List<String> trackedEvents = [];
+
+  int numberOfCleanups = 0;
+
+  @override
+  void track({required String event}) {
+    trackedEvents.add(event);
+  }
+
+  @override
+  void cleanUp() {
+    numberOfCleanups++;
+  }
+}
