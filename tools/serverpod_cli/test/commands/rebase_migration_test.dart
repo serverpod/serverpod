761f2d8c3afcdab13ae771b2046a08a9deaee465
Jermaine <j.mcfarlane.mbbs@gmail.com>
2025-12-28 14:27:37 -0500

    test(cli): rebase command with mock implementation

--- tools/serverpod_cli/test/commands/rebase_migration_test.dart
+++ tools/serverpod_cli/test/commands/rebase_migration_test.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:cli_tools/cli_tools.dart';
+import 'package:collection/collection.dart';
 import 'package:pub_semver/pub_semver.dart';
 import 'package:serverpod_cli/src/commands/rebase_migration.dart';
+import 'package:serverpod_cli/src/migrations/rebase_migration_impl.dart';
 import 'package:serverpod_cli/src/runner/serverpod_command_runner.dart';
 import 'package:serverpod_cli/src/util/serverpod_cli_logger.dart';
 import 'package:test/test.dart';
@@ -16,9 +18,7 @@ class _RebaseMigrationArgs {
   _RebaseMigrationArgs({
     this.onto,
     this.ontoBranch,
-    // ignore: unused_element_parameter
     this.check = false,
-    // ignore: unused_element_parameter
     this.force = false,
   });
 
@@ -39,11 +39,51 @@ class _RebaseMigrationArgs {
   }
 }
 
+/// Mock rebase migration implementation
+class MockRebaseMigrationImpl extends RebaseMigrationImpl {
+  bool success;
+
+  int rebaseMigrationCallCount = 0;
+  int checkMigrationCallCount = 0;
+
+  MockRebaseMigrationImpl({
+    this.success = true,
+  });
+
+  @override
+  Future<bool> rebaseMigration() async {
+    rebaseMigrationCallCount++;
+    return success;
+  }
+
+  @override
+  Future<bool> checkMigration() async {
+    checkMigrationCallCount++;
+    return success;
+  }
+}
+
 void main() {
   var version = Version(1, 1, 0);
   late MockLogger testLogger;
   late ServerpodCommandRunner runner;
   late Directory originalDir;
+  late MockRebaseMigrationImpl mockRebaseMigrationImpl;
+
+  /// Creates a valid project for testing
+  Future<void> createValidProject({bool withDatabase = true}) async {
+    final currentDir = createMockServerpodProject(
+      projectName: 'myapp',
+      generatorYamlContent:
+          '''
+type: server
+features:
+  database: $withDatabase
+''',
+    );
+    await currentDir.create();
+    Directory.current = currentDir.io;
+  }
 
   setUp(() async {
     testLogger = MockLogger();
@@ -54,21 +94,14 @@ void main() {
       version,
       onBeforeRunCommand: (_) => Future.value(),
     );
+    mockRebaseMigrationImpl = MockRebaseMigrationImpl();
     runner.addCommand(
-      RebaseMigrationCommand(),
+      RebaseMigrationCommand(
+        rebaseMigrationImpl: mockRebaseMigrationImpl,
+      ),
     );
 
     originalDir = Directory.current;
-    final currentDir = createMockServerpodProject(
-      projectName: 'myapp',
-      generatorYamlContent: '''
-type: server
-features:
-  database: true
-''',
-    );
-    await currentDir.create();
-    Directory.current = currentDir.io;
   });
 
   tearDown(() {
@@ -78,14 +111,13 @@ features:
 
   group('RebaseMigrationCommand', () {
     test(
-      'throws error when both --onto and --onto-branch are specified',
+      'given both --onto and --onto-branch are specified then an error is thrown',
       () async {
         final rebaseArgs = _RebaseMigrationArgs(
           onto: 'm1',
           ontoBranch: 'other',
+          force: true,
         );
-        // Note: --onto-branch has a default value 'main'.
-        // So we need to specify a different onto-branch to trigger the error.
         await expectLater(
           runner.run(rebaseArgs.args),
           throwsA(isA<ExitException>()),
@@ -97,5 +129,67 @@ features:
         );
       },
     );
+
+    test(
+      'given command environment is not valid then an error is thrown',
+      () async {
+        final rebaseArgs = _RebaseMigrationArgs();
+        await expectLater(
+          runner.run(rebaseArgs.args),
+          throwsA(isA<ExitException>()),
+        );
+
+        expect(testLogger.output.messages, isNotEmpty);
+        expect(
+          testLogger.output.messages.first,
+          contains('Failed to load generator config'),
+        );
+      },
+    );
+
+    test(
+      'given database feature is not enabled then an error is thrown',
+      () async {
+        await createValidProject(withDatabase: false);
+        final rebaseArgs = _RebaseMigrationArgs();
+        await expectLater(
+          runner.run(rebaseArgs.args),
+          throwsA(isA<ExitException>()),
+        );
+
+        expect(
+          testLogger.output.messages.firstWhereOrNull(
+            (message) => message.contains(
+              'The database feature is not enabled in this project',
+            ),
+          ),
+          isNotNull,
+        );
+      },
+    );
+
+    group('given valid command environment', () {
+      test(
+        'when check is false then rebase migration is executed',
+        () async {
+          await createValidProject();
+          final rebaseArgs = _RebaseMigrationArgs(check: false);
+          await runner.run(rebaseArgs.args);
+          expect(mockRebaseMigrationImpl.rebaseMigrationCallCount, 1);
+          expect(mockRebaseMigrationImpl.checkMigrationCallCount, 0);
+        },
+      );
+
+      test(
+        'when check is true then check migration is executed',
+        () async {
+          await createValidProject();
+          final rebaseArgs = _RebaseMigrationArgs(check: true);
+          await runner.run(rebaseArgs.args);
+          expect(mockRebaseMigrationImpl.rebaseMigrationCallCount, 0);
+          expect(mockRebaseMigrationImpl.checkMigrationCallCount, 1);
+        },
+      );
+    });
   });
 }
