44a8b51a08e8f184c9b73c044908ba6bc0a92419
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-01-08 15:56:51 -0300

    fix: Improve the variable name

--- modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/src/providers/email/util/default_code_generators.dart
+++ modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/src/providers/email/util/default_code_generators.dart
@@ -6,12 +6,12 @@ final Random _random = Random.secure();
 ///
 /// Excludes the digit 0 to avoid confusion with the letter 'o'.
 String defaultVerificationCodeGenerator() {
-  const alphabet = '123456789';
+  const characters = '123456789';
 
   return String.fromCharCodes(
     Iterable.generate(
       8,
-      (_) => alphabet.codeUnitAt(_random.nextInt(alphabet.length)),
+      (_) => characters.codeUnitAt(_random.nextInt(characters.length)),
     ),
   );
 }
