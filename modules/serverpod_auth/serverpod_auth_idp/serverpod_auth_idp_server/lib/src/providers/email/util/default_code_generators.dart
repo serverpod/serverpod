93cd4b9e8b03189cc2249b57bc2b434f7423c995
Marcelo Mendonça Soares <marcelo.me.soares@gmail.com>
2026-01-08 16:36:37 -0300

    fix: Minor sign-In with email polishings (#4552)

--- modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/src/providers/email/util/default_code_generators.dart
+++ modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/lib/src/providers/email/util/default_code_generators.dart
@@ -4,15 +4,14 @@ final Random _random = Random.secure();
 
 /// Returns a secure random string to be used for short-lived verification codes.
 ///
-/// The code uses a slightly reduced alphabet from [a-z0-9] to exclude easily confused characters.
+/// Excludes the digit 0 to avoid confusion with the letter 'o'.
 String defaultVerificationCodeGenerator() {
-  const alphabet = 'abcdefghikmnpqrstuvwxyz123456789';
+  const characters = '123456789';
 
-  return List<String>.generate(
-    8,
-    (final _) {
-      final i = _random.nextInt(alphabet.length);
-      return alphabet.substring(i, i + 1);
-    },
-  ).join();
+  return String.fromCharCodes(
+    Iterable.generate(
+      8,
+      (_) => characters.codeUnitAt(_random.nextInt(characters.length)),
+    ),
+  );
 }
