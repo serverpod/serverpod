c71db788f03cec0df02e6c6b90c7091fc6bde4af
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-01-08 15:52:06 -0300

    feat: Hide the `serverUrl` configuration on the generated Flutter app

--- templates/serverpod_templates/projectname_flutter/lib/config/app_config.dart
+++ templates/serverpod_templates/projectname_flutter/lib/config/app_config.dart
@@ -2,17 +2,39 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
-class AppConfig {
-  final String? apiUrl;
+/// Gets the Server URL from the current environment.
+Future<String> getServerUrl() async {
+  final config = await AppConfig.loadConfig();
+  return config.apiUrl;
+}
 
+/// AppConfig loads the API server URL from the current environment.
+///
+/// If the SERVER_URL environment variable is set, it will be used as the API
+/// server URL. Otherwise, it will be loaded from the assets/config.json file.
+///
+/// When the app is hosted on the server, the config.json file is fetched from
+/// the server, allowing the server to change the API URL at runtime. This
+/// ensures the app always uses the correct API URL, no matter which environment
+/// it is running in.
+class AppConfig {
+  /// The API server URL to use when instantiating the client.
+  final String apiUrl;
+
+  /// Creates a new [AppConfig] instance.
   AppConfig({
     required this.apiUrl,
   });
 
+  /// Loads the API server URL from the environment or assets/config.json file.
   static Future<AppConfig> loadConfig() async {
-    final config = await _loadJsonConfig();
-    final String? apiUrl = config['apiUrl'];
+    const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
+    if (serverUrlFromEnv.isNotEmpty) {
+      return AppConfig(apiUrl: serverUrlFromEnv);
+    }
 
+    final config = await _loadJsonConfig();
+    final apiUrl = config['apiUrl'] ?? 'http://$localhost:8080/';
     return AppConfig(apiUrl: apiUrl);
   }
 
--- templates/serverpod_templates/projectname_flutter/lib/main.dart
+++ templates/serverpod_templates/projectname_flutter/lib/main.dart
@@ -21,18 +21,13 @@ void main() async {
   // When you are running the app on a physical device, you need to set the
   // server URL to the IP address of your computer. You can find the IP
   // address by running `ipconfig` on Windows or `ifconfig` on Mac/Linux.
+  //
   // You can set the variable when running or building your app like this:
-  // E.g. `flutter run --dart-define=SERVER_URL=https://api.example.com/`
-  const serverUrlFromEnv = String.fromEnvironment('SERVER_URL');
-  // AppConfig loads the API server URL from the assets/config.json file.
-  // When the app runs in a browser, this file is fetched from the server,
-  // allowing the server to change the API URL at runtime.
-  // This ensures the app always uses the correct API URL,
-  // no matter which environment it is running in.
-  final config = await AppConfig.loadConfig();
-  final serverUrl = serverUrlFromEnv.isEmpty
-      ? config.apiUrl ?? 'http://$localhost:8080/'
-      : serverUrlFromEnv;
+  // E.g. `flutter run --dart-define=SERVER_URL=https://api.example.com/`.
+  //
+  // Otherwise, the server URL is fetched from the assets/config.json file or
+  // defaults to http://$localhost:8080/ if not found.
+  final serverUrl = await getServerUrl();
 
   client = Client(serverUrl)
     ..connectivityMonitor = FlutterConnectivityMonitor();
