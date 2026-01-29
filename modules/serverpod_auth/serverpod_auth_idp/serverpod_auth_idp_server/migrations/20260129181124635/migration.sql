a0d7a5875bdb98ad6756bf137788176c539ae8d7
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-01-29 15:12:56 -0300

    chore: Create migrations for the new log index

---
+++ modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/migrations/20260129181124635/migration.sql
@@ -0,0 +1,33 @@
+BEGIN;
+
+--
+-- ACTION ALTER TABLE
+--
+CREATE INDEX "serverpod_session_log_time_idx" ON "serverpod_session_log" USING btree ("time");
+
+--
+-- MIGRATION VERSION FOR serverpod_auth_idp
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_auth_idp', '20260129181124635', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260129181124635', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20260129180959368', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260129180959368', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod_auth_core
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_auth_core', '20260129181112269', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260129181112269', "timestamp" = now();
+
+
+COMMIT;
