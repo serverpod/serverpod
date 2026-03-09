614474eb33cc706f33246d1664877bbfd27f29f9
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-03-09 01:10:13 -0300

    generate: Generate the `serverpod_database` and `serverpod` packages

---
+++ packages/serverpod_database/lib/src/generated/bulk_query_result.dart
@@ -0,0 +1,108 @@
+/* AUTOMATICALLY GENERATED CODE DO NOT MODIFY */
+/*   To generate run: "serverpod generate"    */
+
+// ignore_for_file: implementation_imports
+// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: public_member_api_docs
+// ignore_for_file: type_literal_in_constant_pattern
+// ignore_for_file: use_super_parameters
+// ignore_for_file: invalid_use_of_internal_member
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:serverpod_serialization/serverpod_serialization.dart' as _i1;
+import 'package:serverpod_database/serverpod_database.dart' as _i2;
+
+abstract class BulkQueryResult implements _i1.SerializableModel {
+  BulkQueryResult._({
+    required this.headers,
+    required this.data,
+    required this.numAffectedRows,
+    required this.duration,
+  });
+
+  factory BulkQueryResult({
+    required List<_i2.BulkQueryColumnDescription> headers,
+    required String data,
+    required int numAffectedRows,
+    required Duration duration,
+  }) = _BulkQueryResultImpl;
+
+  factory BulkQueryResult.fromJson(Map<String, dynamic> jsonSerialization) {
+    return BulkQueryResult(
+      headers: _i2.Protocol().deserialize<List<_i2.BulkQueryColumnDescription>>(
+        jsonSerialization['headers'],
+      ),
+      data: jsonSerialization['data'] as String,
+      numAffectedRows: jsonSerialization['numAffectedRows'] as int,
+      duration: _i1.DurationJsonExtension.fromJson(
+        jsonSerialization['duration'],
+      ),
+    );
+  }
+
+  List<_i2.BulkQueryColumnDescription> headers;
+
+  String data;
+
+  int numAffectedRows;
+
+  Duration duration;
+
+  /// Returns a shallow copy of this [BulkQueryResult]
+  /// with some or all fields replaced by the given arguments.
+  @_i1.useResult
+  BulkQueryResult copyWith({
+    List<_i2.BulkQueryColumnDescription>? headers,
+    String? data,
+    int? numAffectedRows,
+    Duration? duration,
+  });
+  @override
+  Map<String, dynamic> toJson() {
+    return {
+      '__className__': 'serverpod.BulkQueryResult',
+      'headers': headers.toJson(valueToJson: (v) => v.toJson()),
+      'data': data,
+      'numAffectedRows': numAffectedRows,
+      'duration': duration.toJson(),
+    };
+  }
+
+  @override
+  String toString() {
+    return _i1.SerializationManager.encode(this);
+  }
+}
+
+class _BulkQueryResultImpl extends BulkQueryResult {
+  _BulkQueryResultImpl({
+    required List<_i2.BulkQueryColumnDescription> headers,
+    required String data,
+    required int numAffectedRows,
+    required Duration duration,
+  }) : super._(
+         headers: headers,
+         data: data,
+         numAffectedRows: numAffectedRows,
+         duration: duration,
+       );
+
+  /// Returns a shallow copy of this [BulkQueryResult]
+  /// with some or all fields replaced by the given arguments.
+  @_i1.useResult
+  @override
+  BulkQueryResult copyWith({
+    List<_i2.BulkQueryColumnDescription>? headers,
+    String? data,
+    int? numAffectedRows,
+    Duration? duration,
+  }) {
+    return BulkQueryResult(
+      headers: headers ?? this.headers.map((e0) => e0.copyWith()).toList(),
+      data: data ?? this.data,
+      numAffectedRows: numAffectedRows ?? this.numAffectedRows,
+      duration: duration ?? this.duration,
+    );
+  }
+}
