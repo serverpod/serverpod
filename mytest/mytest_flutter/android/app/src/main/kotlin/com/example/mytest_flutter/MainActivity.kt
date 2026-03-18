225c1b48c5fc841ee81294adb5f09e7f0ab68657
Crazelu <eberelucky64@gmail.com>
2026-03-18 20:59:18 +0100

    chore: Remove test project

--- mytest/mytest_flutter/android/app/src/main/kotlin/com/example/mytest_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mytest_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mytest/mytest_server/lib/src/auth/jwt_refresh_endpoint.dart
+++
@@ -1,5 +0,0 @@
-import 'package:serverpod_auth_idp_server/core.dart';
-
-/// By extending [RefreshJwtTokensEndpoint], the JWT token refresh endpoint
-/// is made available on the server and enables automatic token refresh on the client.
-class JwtRefreshEndpoint extends RefreshJwtTokensEndpoint {}
