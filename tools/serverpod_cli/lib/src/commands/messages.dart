e834fdddaa9edeec017cb1d0a6138572b085c1a8
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-03-19 15:29:58 -0300

    chore: Add `✓` to standardize success watch messages

--- tools/serverpod_cli/lib/src/commands/messages.dart
+++ tools/serverpod_cli/lib/src/commands/messages.dart
@@ -1,12 +1,12 @@
 // Generate command messages
 const initialCodeGenerationComplete =
-    'Initial code generation complete. Listening for changes.';
+    '✓ Initial code generation complete. Listening for changes.';
 const incrementalCodeGenerationComplete =
-    'Incremental code generation complete.';
+    '✓ Incremental code generation complete.';
 
 // Start command messages
 const serverRunning = 'Server running.';
 
 // Watch command messages
-const serverReloaded = 'Server reloaded.';
-const serverRestarted = 'Server restarted.';
+const serverReloaded = '✓ Server reloaded.';
+const serverRestarted = '✓ Server restarted.';
