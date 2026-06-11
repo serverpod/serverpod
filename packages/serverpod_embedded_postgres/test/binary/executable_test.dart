4ba5a370a7846fdcb705c243e04e55cdaa42600f
Preston <preston.schwartz505@gmail.com>
2026-06-11 14:01:46 -0500

    Adjusted test name and group descriptions.

--- packages/serverpod_embedded_postgres/test/binary/executable_test.dart
+++ packages/serverpod_embedded_postgres/test/binary/executable_test.dart
@@ -5,18 +5,19 @@ import 'package:serverpod_embedded_postgres/src/binary/executable.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Given binExecutable', () {
+  group('Given the host platform', () {
     var installDir = Directory(p.join('install', '16.13.0'));
 
-    test('when resolving a binary then it lands in the bin/ directory.', () {
+    test('when getting the binary executable then it lands in the bin/ '
+        'directory.', () {
       var path = binExecutable(installDir, 'postgres');
 
       expect(p.dirname(path), p.join(installDir.path, 'bin'));
     });
 
     test(
-      'when on Windows then the name carries the .exe extension, '
-      'otherwise the bare name is used.',
+      'when getting the binary executable then the name carries the .exe '
+      'extension on Windows and the bare name otherwise.',
       () {
         var name = p.basename(binExecutable(installDir, 'postgres'));
 
