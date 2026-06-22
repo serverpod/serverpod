df1841b210b991f608a21444357f5f49d75787a9
Charles Archibong <charlesarchibong10@gmail.com>
2026-06-22 09:15:44 +0100

    refactor: move Geography JSON extensions to serialization_extensions.dart

--- packages/serverpod_serialization/lib/src/postgis.dart
+++ packages/serverpod_serialization/lib/src/postgis.dart
@@ -1,9 +1,6 @@
 export 'postgis/geography.dart' show Geography;
 export 'postgis/geography_geometry_collection.dart'
-    show GeographyGeometryCollection, GeographyGeometryCollectionJsonExtension;
-export 'postgis/geography_line_string.dart'
-    show GeographyLineString, GeographyLineStringJsonExtension;
-export 'postgis/geography_point.dart'
-    show GeographyPoint, GeographyPointJsonExtension;
-export 'postgis/geography_polygon.dart'
-    show GeographyPolygon, GeographyPolygonJsonExtension;
+    show GeographyGeometryCollection;
+export 'postgis/geography_line_string.dart' show GeographyLineString;
+export 'postgis/geography_point.dart' show GeographyPoint;
+export 'postgis/geography_polygon.dart' show GeographyPolygon;
