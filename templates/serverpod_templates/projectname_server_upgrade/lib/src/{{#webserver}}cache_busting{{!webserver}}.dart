394446992eebbc2452918a6392dd2345559acf9d
Crazelu <eberelucky64@gmail.com>
2026-07-08 04:29:51 +0100

    chore: add doc string for default cacheBustingConfig in template

--- templates/serverpod_templates/projectname_server_upgrade/lib/src/{{#webserver}}cache_busting{{!webserver}}.dart
+++ templates/serverpod_templates/projectname_server_upgrade/lib/src/{{#webserver}}cache_busting{{!webserver}}.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:serverpod/serverpod.dart';
 
+/// Cache busting configuration.
+/// Used in WidgetRoutes to cache-bust asset paths.
 final cacheBustingConfig = CacheBustingConfig(
   mountPrefix: '/web',
   fileSystemRoot: Directory(Uri(path: 'web/static').toFilePath()),
