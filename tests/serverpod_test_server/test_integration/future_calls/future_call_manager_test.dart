35ab08d4c7aa91d0a3d93e39a92aa2c9dbd86d53
Preston <preston.schwartz505@gmail.com>
2026-07-31 09:35:26 -0500

    Update unregistered future call message expectation.

--- tests/serverpod_test_server/test_integration/future_calls/future_call_manager_test.dart
+++ tests/serverpod_test_server/test_integration/future_calls/future_call_manager_test.dart
@@ -795,8 +795,8 @@ void main() async {
             matches(
               'Attempted to run a FutureCall that was not registered. This is likely due '
               'to changing a FutureCall method after it was scheduled, leading to an '
-              'entry that no longer has a matching method. For legacy future calls, '
-              r'make sure they are registered in the server start. Entry: \{.*\"name\":\s*\"scheduled-but-unregistered-call\".*\}',
+              'entry that no longer has a matching method. '
+              r'Entry: \{.*\"name\":\s*\"scheduled-but-unregistered-call\".*\}',
             ),
           );
         },
