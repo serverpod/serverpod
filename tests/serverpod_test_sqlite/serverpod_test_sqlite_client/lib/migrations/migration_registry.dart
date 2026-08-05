4b6cc821983b5851340e3de0a80e9cf153793356
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-08-05 11:00:24 -0300

    test: Add integration tests to the nulls distinct feature

--- tests/serverpod_test_sqlite/serverpod_test_sqlite_client/lib/migrations/migration_registry.dart
+++ tests/serverpod_test_sqlite/serverpod_test_sqlite_client/lib/migrations/migration_registry.dart
@@ -9,6 +9,7 @@ part '20260620012700766/migration.dart';
 part '20260625141124530/migration.dart';
 part '20260708135909929/migration.dart';
 part '20260711023021788/migration.dart';
+part '20260805140851716/migration.dart';
 
 /// Migration registry for the client-side database.
 class MigrationRegistry {
@@ -20,6 +21,7 @@ class MigrationRegistry {
     _Migration20260625141124530(),
     _Migration20260708135909929(),
     _Migration20260711023021788(),
+    _Migration20260805140851716(),
   ];
 
   /// List of all client-side database migration versions.
