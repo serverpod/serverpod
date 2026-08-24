84327fd94809a0c39740ca435ac7a15f5d4b0fb7
Crazelu <eberelucky64@gmail.com>
2026-08-24 18:24:00 +0100

    refactor: update e2e tests for cloud storage

--- tests/serverpod_test_server/test_e2e/cloud_storage_test.dart
+++ tests/serverpod_test_server/test_e2e/cloud_storage_test.dart
@@ -1,5 +1,5 @@
-import 'dart:typed_data';
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:http/http.dart' as http;
 import 'package:serverpod_test_client/serverpod_test_client.dart';
@@ -18,14 +18,14 @@ extension<T> on Stream<T> {
       chunk.add(e);
       if (chunk.length >= chunkSize) {
         yield chunk;
-        chunk = <T>[]; // new
+        chunk = <T>[];
       }
     }
   }
 }
 
 ByteData createByteData(int len) {
-  var ints = Uint8List(len);
+  final ints = Uint8List(len);
   for (var i = 0; i < len; i++) {
     ints[i] = i % 256;
   }
@@ -33,7 +33,7 @@ ByteData createByteData(int len) {
 }
 
 bool verifyByteData(ByteData byteData) {
-  var ints = byteData.buffer.asUint8List();
+  final ints = byteData.buffer.asUint8List();
   for (var i = 0; i < ints.length; i++) {
     if (ints[i] != i % 256) return false;
   }
@@ -41,210 +41,309 @@ bool verifyByteData(ByteData byteData) {
 }
 
 void main() {
-  var client = Client(serverUrl);
+  final client = Client(serverUrl);
 
-  setUp(() {});
-
-  group('Database cloud storage', () {
-    test('Clear files', () async {
+  group('Given the database cloud storage with no stored files', () {
+    setUp(() async {
       await client.cloudStorage.reset();
     });
 
-    test('Store file 1', () async {
-      await client.cloudStorage.storePublicFile(
-        'testdir/myfile1.bin',
-        createByteData(256),
-      );
+    tearDown(() async {
+      await client.cloudStorage.reset();
     });
 
-    test('Store file 2', () async {
-      await client.cloudStorage.storePublicFile(
-        'testdir/myfile2.bin',
-        createByteData(256),
-      );
-    });
+    test(
+      'when a public file is stored, '
+      'then it can be retrieved with its original contents',
+      () async {
+        const path = 'testdir/myfile.bin';
 
-    test('Replace file 1', () async {
-      await client.cloudStorage.storePublicFile(
-        'testdir/myfile1.bin',
-        createByteData(128),
-      );
-    });
+        await client.cloudStorage.storePublicFile(
+          path,
+          createByteData(256),
+        );
+        final actual = await client.cloudStorage.retrievePublicFile(path);
 
-    test('Retrieve file 1', () async {
-      var byteData = await client.cloudStorage.retrievePublicFile(
-        'testdir/myfile1.bin',
-      );
-      expect(byteData!.lengthInBytes, equals(128));
-      expect(verifyByteData(byteData), equals(true));
-    });
+        expect(actual.lengthInBytes, 256);
+        expect(verifyByteData(actual), isTrue);
+      },
+    );
 
-    test('Retrieve file 2 through URL', () async {
-      var url = Uri.parse(
-        '${serverUrl}serverpod_cloud_storage?method=file&path=testdir/myfile2.bin',
-      );
-      var response = await http.get(url);
-      expect(response.statusCode, equals(200));
-      var bytes = response.bodyBytes;
-      expect(bytes.length, equals(256));
-      verifyByteData(ByteData.view(bytes.buffer));
-    });
-
-    test('Retrieve file 1 URL', () async {
-      var urlStr = await client.cloudStorage.getPublicUrlForFile(
-        'testdir/myfile1.bin',
-      );
-      expect(urlStr, isNotNull);
-    });
-
-    test('Retrieve file 2 through fetched URL', () async {
-      var urlStr = await client.cloudStorage.getPublicUrlForFile(
-        'testdir/myfile2.bin',
-      );
-      var url = Uri.parse(urlStr!);
-      var response = await http.get(url);
-      expect(response.statusCode, equals(200));
-      var bytes = response.bodyBytes;
-      expect(bytes.length, equals(256));
-      verifyByteData(ByteData.view(bytes.buffer));
-    });
-
-    test('Retrieve file 2', () async {
-      var byteData = await client.cloudStorage.retrievePublicFile(
-        'testdir/myfile2.bin',
-      );
-      expect(byteData!.lengthInBytes, equals(256));
-      expect(verifyByteData(byteData), equals(true));
-    });
-
-    test('Retrieve non existing file', () async {
-      var byteData = await client.cloudStorage.retrievePublicFile(
-        'testdir/myfile3.bin',
-      );
-      expect(byteData, isNull);
-    });
-
-    test('Retrieve non existing file through URL', () async {
-      var url = Uri.parse(
-        '${serverUrl}serverpod_cloud_storage?method=file&path=testdir/myfile3.bin',
-      );
-      var response = await http.get(url);
-      expect(response.statusCode, equals(404));
-    });
-
-    test('Attempt retrieve file through URL with invalid params', () async {
-      var url = Uri.parse(
-        '${serverUrl}serverpod_cloud_storage?method=file&foo=testdir/myfile2.bin',
-      );
-      var response = await http.get(url);
-      expect(response.statusCode, equals(400));
-    });
-
-    test('Attempt retrieve file through URL with invalid method', () async {
-      var url = Uri.parse(
-        '${serverUrl}serverpod_cloud_storage?foo=file&path=testdir/myfile2.bin',
-      );
-      var response = await http.get(url);
-      expect(response.statusCode, equals(400));
-    });
-
-    test('Exists file 1', () async {
-      var exists = await client.cloudStorage.existsPublicFile(
-        'testdir/myfile1.bin',
-      );
-      expect(exists, true);
-    });
-
-    test('Exists non existing file', () async {
-      var exists = await client.cloudStorage.existsPublicFile(
-        'testdir/myfile3.bin',
-      );
-      expect(exists, false);
-    });
-
-    test('Delete file 1', () async {
-      await client.cloudStorage.deletePublicFile('testdir/myfile1.bin');
-    });
-
-    test('Exists file 1 after deletion', () async {
-      var exists = await client.cloudStorage.existsPublicFile(
-        'testdir/myfile1.bin',
-      );
-      expect(exists, false);
-    });
-
-    test('Direct file upload (ByteData)', () async {
-      var uploadDescription = await client.cloudStorage.getDirectFilePostUrl(
-        'testdir/directupload.bin',
-      );
-      expect(uploadDescription, isNotNull);
-      var byteData = createByteData(1024);
-
-      var uploader = FileUploader(uploadDescription!);
-      var result = await uploader.uploadByteData(byteData);
-
-      expect(result, equals(true));
-
-      var verified = await client.cloudStorage.verifyDirectFileUpload(
-        'testdir/directupload.bin',
-      );
-      expect(verified, equals(true));
-    });
-
-    test('Retrieve directly uploaded file (ByteData)', () async {
-      var byteData = await client.cloudStorage.retrievePublicFile(
-        'testdir/directupload.bin',
-      );
-      expect(byteData!.lengthInBytes, equals(1024));
-      expect(verifyByteData(byteData), equals(true));
-    });
-
-    test('Direct file upload (Stream<List<int>>)', () async {
-      var uploadDescription = await client.cloudStorage.getDirectFilePostUrl(
-        'testdir/directupload_stream_binary.bin',
-      );
-      expect(uploadDescription, isNotNull);
-
-      var uploader = FileUploader(uploadDescription!);
-      var result = await uploader.upload(
-        streamBytes().take(512).inChunksOf(64),
-      );
-
-      expect(result, equals(true));
-
-      var verified = await client.cloudStorage.verifyDirectFileUpload(
-        'testdir/directupload_stream_binary.bin',
-      );
-      expect(verified, equals(true));
-
-      var retrievedByteData = await client.cloudStorage.retrievePublicFile(
-        'testdir/directupload_stream_binary.bin',
-      );
-      expect(retrievedByteData!.lengthInBytes, equals(512));
-      expect(verifyByteData(retrievedByteData), equals(true));
-    });
-
-    test('Attempt to upload twice with the same FileUploader', () async {
-      var uploadDescription = await client.cloudStorage.getDirectFilePostUrl(
-        'testdir/directupload_duplicate.bin',
-      );
-      expect(uploadDescription, isNotNull);
-      var byteData = createByteData(100);
-
-      var uploader = FileUploader(uploadDescription!);
-      var result = await uploader.uploadByteData(byteData);
-      expect(result, equals(true));
-
-      expect(
-        () async => await uploader.uploadByteData(byteData),
-        throwsA(
-          isA<Exception>().having(
-            (e) => e.toString(),
-            'message',
-            contains('Data has already been uploaded using this FileUploader.'),
+    test(
+      'when a missing file is retrieved, '
+      'then a ServerpodClientException is thrown',
+      () async {
+        await expectLater(
+          () => client.cloudStorage.retrievePublicFile(
+            'testdir/missing.bin',
           ),
-        ),
+          throwsA(isA<ServerpodClientException>()),
+        );
+      },
+    );
+
+    test(
+      'when a missing file is retrieved through the storage route, '
+      'then the response status is 404',
+      () async {
+        final url = Uri.parse(
+          '${serverUrl}serverpod_cloud_storage?method=file&path=testdir/missing.bin',
+        );
+
+        final response = await http.get(url);
+
+        expect(response.statusCode, 404);
+      },
+    );
+
+    test(
+      'when file existence is checked for a missing file, '
+      'then it returns false',
+      () async {
+        final exists = await client.cloudStorage.existsPublicFile(
+          'testdir/missing.bin',
+        );
+
+        expect(exists, isFalse);
+      },
+    );
+  });
+
+  group('Given the database cloud storage with an existing public file', () {
+    const path = 'testdir/myfile.bin';
+
+    setUp(() async {
+      await client.cloudStorage.storePublicFile(
+        path,
+        createByteData(256),
       );
     });
+
+    tearDown(() async {
+      await client.cloudStorage.reset();
+    });
+
+    test(
+      'when the file is replaced, '
+      'then the replacement contents can be retrieved',
+      () async {
+        await client.cloudStorage.storePublicFile(
+          path,
+          createByteData(128),
+        );
+
+        final actual = await client.cloudStorage.retrievePublicFile(path);
+
+        expect(actual.lengthInBytes, 128);
+        expect(verifyByteData(actual), isTrue);
+      },
+    );
+
+    test(
+      'when the file is retrieved through the storage route, '
+      'then the response contains the file contents',
+      () async {
+        final url = Uri.parse(
+          '${serverUrl}serverpod_cloud_storage?method=file&path=$path',
+        );
+
+        final response = await http.get(url);
+
+        expect(response.statusCode, 200);
+        expect(response.bodyBytes.length, 256);
+        expect(
+          verifyByteData(ByteData.view(response.bodyBytes.buffer)),
+          isTrue,
+        );
+      },
+    );
+
+    test(
+      'when its public download URL is requested, '
+      'then a URL is returned',
+      () async {
+        final url = await client.cloudStorage.publicDownloadUrlForFile(path);
+
+        expect(url, isNotEmpty);
+      },
+    );
+
+    test(
+      'when it is fetched through its public download URL, '
+      'then the response contains the file contents',
+      () async {
+        final url = await client.cloudStorage.publicDownloadUrlForFile(path);
+
+        final response = await http.get(Uri.parse(url));
+
+        expect(response.statusCode, 200);
+        expect(response.bodyBytes.length, 256);
+        expect(
+          verifyByteData(ByteData.view(response.bodyBytes.buffer)),
+          isTrue,
+        );
+      },
+    );
+
+    test(
+      'when it is fetched through a temporary download URL, '
+      'then the response contains the file contents',
+      () async {
+        final url = await client.cloudStorage.temporaryDownloadUrlForFile(
+          path,
+        );
+
+        final response = await http.get(Uri.parse(url));
+
+        expect(response.statusCode, 200);
+        expect(response.bodyBytes.length, 256);
+        expect(
+          verifyByteData(ByteData.view(response.bodyBytes.buffer)),
+          isTrue,
+        );
+      },
+    );
+
+    test(
+      'when file existence is checked, '
+      'then it returns true',
+      () async {
+        final exists = await client.cloudStorage.existsPublicFile(path);
+
+        expect(exists, isTrue);
+      },
+    );
+
+    test(
+      'when it is deleted, '
+      'then it no longer exists',
+      () async {
+        await client.cloudStorage.deletePublicFile(path);
+
+        final exists = await client.cloudStorage.existsPublicFile(path);
+
+        expect(exists, isFalse);
+      },
+    );
   });
+
+  group('Given an invalid storage route request', () {
+    test(
+      'when the path parameter is missing, '
+      'then the response status is 400',
+      () async {
+        final url = Uri.parse(
+          '${serverUrl}serverpod_cloud_storage?method=file&foo=testdir/myfile.bin',
+        );
+
+        final response = await http.get(url);
+
+        expect(response.statusCode, 400);
+      },
+    );
+
+    test(
+      'when the method parameter is missing, '
+      'then the response status is 400',
+      () async {
+        final url = Uri.parse(
+          '${serverUrl}serverpod_cloud_storage?foo=file&path=testdir/myfile.bin',
+        );
+
+        final response = await http.get(url);
+
+        expect(response.statusCode, 400);
+      },
+    );
+  });
+
+  group(
+    'Given the database cloud storage with a direct file upload description',
+    () {
+      const path = 'testdir/directupload.bin';
+      late String uploadDescription;
+
+      setUp(() async {
+        uploadDescription = await client.cloudStorage
+            .createUploadDescriptionForFile(path);
+      });
+
+      tearDown(() async {
+        await client.cloudStorage.reset();
+      });
+
+      test(
+        'when ByteData is uploaded, '
+        'then the upload succeeds and can be verified',
+        () async {
+          final uploader = FileUploader(uploadDescription);
+
+          final uploaded = await uploader.uploadByteData(
+            createByteData(1024),
+          );
+          final verified = await client.cloudStorage.verifyUpload(path);
+
+          expect(uploaded, isTrue);
+          expect(verified, isTrue);
+        },
+      );
+
+      test(
+        'when uploaded ByteData is retrieved, '
+        'then it contains the uploaded data',
+        () async {
+          final uploader = FileUploader(uploadDescription);
+          await uploader.uploadByteData(createByteData(1024));
+
+          final byteData = await client.cloudStorage.retrievePublicFile(path);
+
+          expect(byteData.lengthInBytes, 1024);
+          expect(verifyByteData(byteData), isTrue);
+        },
+      );
+
+      test(
+        'when a byte stream is uploaded, '
+        'then it can be verified and retrieved',
+        () async {
+          final uploader = FileUploader(uploadDescription);
+
+          final uploaded = await uploader.upload(
+            streamBytes().take(512).inChunksOf(64),
+          );
+          final verified = await client.cloudStorage.verifyUpload(path);
+          final byteData = await client.cloudStorage.retrievePublicFile(path);
+
+          expect(uploaded, isTrue);
+          expect(verified, isTrue);
+          expect(byteData.lengthInBytes, 512);
+          expect(verifyByteData(byteData), isTrue);
+        },
+      );
+
+      test(
+        'when the same FileUploader uploads data twice, '
+        'then the second upload throws an exception',
+        () async {
+          final byteData = createByteData(100);
+          final uploader = FileUploader(uploadDescription);
+          final firstUpload = await uploader.uploadByteData(byteData);
+
+          expect(firstUpload, isTrue);
+          await expectLater(
+            () => uploader.uploadByteData(byteData),
+            throwsA(
+              isA<Exception>().having(
+                (e) => e.toString(),
+                'message',
+                contains(
+                  'Data has already been uploaded using this FileUploader.',
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    },
+  );
 }
