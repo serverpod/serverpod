e45a4df65c5d44fce03624fd8e0a9742f7c9ffd6
Preston <preston.schwartz505@gmail.com>
2026-08-25 10:15:39 -0500

    Attach the VS Code extension to .spy model files only.

--- tools/serverpod_vscode_extension/src/extension.ts
+++ tools/serverpod_vscode_extension/src/extension.ts
@@ -52,8 +52,6 @@ export function activate(context: ExtensionContext) {
 	const clientOptions: LanguageClientOptions = {
 		revealOutputChannelOn: RevealOutputChannelOn.Info,
 		documentSelector: [
-			{ scheme: 'file', language: 'yaml', pattern: '**/protocol/**/*.yaml' },
-			{ scheme: 'file', language: 'yaml', pattern: '**/models/**/*.yaml' },
 			{ scheme: 'file', pattern: '**/*.spy.yaml' },
 			{ scheme: 'file', pattern: '**/*.spy.yml' },
 			{ scheme: 'file', pattern: '**/*.spy' },
