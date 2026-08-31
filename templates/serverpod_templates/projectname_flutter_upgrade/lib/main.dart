7a9b81e2ed537ba2678056ba59c89a54a6ca8f56
Kasper Overgård Nielsen <kasper@byolimit.com>
2026-08-31 12:51:06 +0200

    refactor: Extract client setup into client.dart in template

--- templates/serverpod_templates/projectname_flutter_upgrade/lib/main.dart
+++ templates/serverpod_templates/projectname_flutter_upgrade/lib/main.dart
@@ -1,48 +1,11 @@
-import 'package:projectname_client/projectname_client.dart';
 import 'package:flutter/material.dart';
-import 'package:serverpod_flutter/serverpod_flutter.dart';
-// {{#auth}}
-import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
-// {{/auth}}
 
+import 'client.dart';
 import 'screens/greetings_screen.dart';
-
-/// Sets up a global client object that can be used to talk to the server from
-/// anywhere in our app. The client is generated from your server code
-/// and is set up to connect to a Serverpod running on a local server on
-/// the default port. You will need to modify this to connect to staging or
-/// production servers.
-/// In a larger app, you may want to use the dependency injection of your choice
-/// instead of using a global client object. This is just a simple example.
-late final Client client;
-
-late String serverUrl;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // When you are running the app on a physical device, you need to set the
-  // server URL to the IP address of your computer. You can find the IP
-  // address by running `ipconfig` on Windows or `ifconfig` on Mac/Linux.
-  //
-  // You can set the variable when running or building your app like this:
-  // E.g. `flutter run --dart-define=SERVER_URL=https://api.example.com/`.
-  //
-  // Otherwise, the server URL is fetched from the assets/config.json file or
-  // defaults to http://$localhost:8080/ if not found.
-  final serverUrl = await getServerUrl();
-
-  client = Client(serverUrl)
-    ..connectivityMonitor = FlutterConnectivityMonitor()
-    // {{#auth}}
-    ..authSessionManager = FlutterAuthSessionManager()
-  // {{/auth}}
-  ;
-
-  // {{#auth}}
-  client.auth.initialize();
-  // {{/auth}}
-
+  await initializeClient();
   runApp(const MyApp());
 }
 
--- templates/serverpod_templates/projectname_flutter_upgrade/lib/screens/{{#auth}}sign_in_screen{{!auth}}.dart
+++ templates/serverpod_templates/projectname_flutter_upgrade/lib/screens/{{#auth}}sign_in_screen{{!auth}}.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:serverpod_auth_idp_flutter/serverpod_auth_idp_flutter.dart';
 
-import '../main.dart';
+import '../client.dart';
 
 class SignInScreen extends StatefulWidget {
   final Widget child;
