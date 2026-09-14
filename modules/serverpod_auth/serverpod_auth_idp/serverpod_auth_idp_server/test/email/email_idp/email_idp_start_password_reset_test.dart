d370dad129b1ac36c32823bc03a6e7fe972e91c9
Marcelo Soares <marcelo.me.soares@gmail.com>
2026-09-14 10:02:31 -0300

    fix: Export the `RateLimit` config class on the email provider barrel file

--- modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/test/email/email_idp/email_idp_start_password_reset_test.dart
+++ modules/serverpod_auth/serverpod_auth_idp/serverpod_auth_idp_server/test/email/email_idp/email_idp_start_password_reset_test.dart
@@ -1,5 +1,4 @@
 import 'package:serverpod/serverpod.dart';
-import 'package:serverpod_auth_idp_server/core.dart';
 import 'package:serverpod_auth_idp_server/providers/email.dart';
 import 'package:test/test.dart';
 
